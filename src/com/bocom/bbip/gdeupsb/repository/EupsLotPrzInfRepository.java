130a55b1591ead672d0c5ac0ae06032d74144516
Li_Guilin <Li_Guilin@2ac714ae-9a16-11e4-ad83-2f06aba1930a>
2015-02-04 02:30:14 +0000

    git-svn-id: svn://182.53.32.18/bbip/project/GDEUPSB/TRUNK/GDEUPSB@6649 2ac714ae-9a16-11e4-ad83-2f06aba1930a

--- src/com/bocom/bbip/gdeupsb/repository/EupsLotPrzInfRepository.java
+++
@@ -1,8 +0,0 @@
-package com.bocom.bbip.gdeupsb.repository;
-
-import com.bocom.bbip.data.domain.NoId;
-import com.bocom.bbip.data.repository.PagingAndSortingRepository;
-import com.bocom.bbip.gdeupsb.entity.EupsLotPrzInf;
-
-public interface EupsLotPrzInfRepository extends PagingAndSortingRepository<EupsLotPrzInf, NoId> {
-}
