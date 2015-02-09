66af8cfb98ba81ef484fff63c5466da7ea5f1b70
Li_Haoxun <Li_Haoxun@2ac714ae-9a16-11e4-ad83-2f06aba1930a>
2015-02-09 12:19:32 +0000

    git-svn-id: svn://182.53.32.18/bbip/project/GDEUPSB/TRUNK/GDEUPSB@8858 2ac714ae-9a16-11e4-ad83-2f06aba1930a

--- src/com/bocom/bbip/gdeupsb/action/hscard/HSCardFeeQueryAction.java
+++ src/com/bocom/bbip/gdeupsb/action/hscard/HSCardFeeQueryAction.java
@@ -1,9 +1,13 @@
 package com.bocom.bbip.gdeupsb.action.hscard;
 
+import java.util.Map;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
-import com.bocom.bbip.comp.account.AccountService;
+import com.bocom.bbip.comp.gems.GIA;
+import com.bocom.bbip.comp.gems.GemsResult;
+import com.bocom.bbip.comp.gems.service.DefaultGemsServiceAccessObject;
 import com.bocom.bbip.eups.action.BaseAction;
 import com.bocom.jump.bp.core.Context;
 import com.bocom.jump.bp.core.CoreException;
@@ -15,7 +19,15 @@ public class HSCardFeeQueryAction extends BaseAction{
 	
 	public void execute(Context ctx) throws CoreException,CoreRuntimeException{
 		log.info("HSCardFeeQueryAction start......");
-		
+		// 查询内部账户余额
+				// 判断账户是否余额不足
+				// TODO: for test,先注释掉
+ DefaultGemsServiceAccessObject gemsServiceAccessObject = get(DefaultGemsServiceAccessObject.class);
+				 GIA gia = gemsServiceAccessObject.build(ctx, "TP9000");
+				 GemsResult gemsResult = gemsServiceAccessObject.callService(gia,
+				 ctx.getDataMap());
+				 Map<String, Object> balQry = gemsResult.getPayload();
+				 String curBal = (String) balQry.get("bal"); // 余额
 
 	}
 
