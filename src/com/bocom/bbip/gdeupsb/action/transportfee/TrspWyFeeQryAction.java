90bbec8848318729291a7c85ea8f33e4e3fdf967
Li_Haoxun <Li_Haoxun@2ac714ae-9a16-11e4-ad83-2f06aba1930a>
2015-02-09 13:05:12 +0000

    git-svn-id: svn://182.53.32.18/bbip/project/GDEUPSB/TRUNK/GDEUPSB@8923 2ac714ae-9a16-11e4-ad83-2f06aba1930a

--- src/com/bocom/bbip/gdeupsb/action/transportfee/TrspWyFeeQryAction.java
+++ src/com/bocom/bbip/gdeupsb/action/transportfee/TrspWyFeeQryAction.java
@@ -46,6 +46,26 @@ public class TrspWyFeeQryAction extends BaseAction{
 //        <Return/>
 //      </If>
         get(BBIPPublicService.class).synExecute(TRANSPORT_FEE_QUERY_PROCESS, ctx);
+        
+//        <Set>MsgTyp=E</Set>
+//        <If condition="IS_EQUAL_STRING($RspCod,000000)">
+//        	  <Set>display_zone=STRCAT(该车辆具体信息如下：&lt;br&gt;车牌号码：[,$plateNo,] &lt;br&gt;车牌类别：[,$plateType,] &lt;br&gt;缴费月数：[,$monthCount,] &lt;br&gt;有效起始日期：[,$BegDat,] &lt;br&gt;)</Set>
+//        	  <Set>display_zone=STRCAT($display_zone,有效
+//           <Set>RspMsg=交易成功</Set>结束日期：[,$EndDat,] &lt;br&gt;车型说明：[,$CarName,] &lt;br&gt;吨数或座数：[,$CarDzs,] &lt;br&gt;标准年收费额：[,$FeeStd,] &lt;br&gt;本金：[,$Corpus,] &lt;br&gt;滞纳金：[,AMTSIMPLEFMT($delayFee),] &lt;br&gt;总金额：[,AMTSIMPLEFMT($oweAmount),] &lt;br&gt;车主姓名：[,$custName,] &lt;br&gt;)</Set>
+//           <Set>MsgTyp=N</Set>
+//           <!--返回包体-->
+//        </If>
+//
+//        <Set>responseType=$MsgTyp</Set>
+//        <Set>responseCode=$RspCod</Set>
+//        <Set>replyMessage=$RspMsg</Set>
+//        <!-- 如果是核心返回错误则返回说明 等于 返回码-->
+//        <If condition="INTCMP(STRLEN($RspMsg),5,10)">
+//          <If condition="SUBCMP($RspMsg,1,10,银行主机错)=0">
+//              <Set>replyMessage=$RspCod</Set>
+//          </If>
+//        </If>
+        
 	}
 
 }
