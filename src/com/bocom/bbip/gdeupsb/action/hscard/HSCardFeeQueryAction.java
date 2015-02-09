eeebca27867ad4ef29c86cc2e1bb65812c21f707
Li_Haoxun <Li_Haoxun@2ac714ae-9a16-11e4-ad83-2f06aba1930a>
2015-02-09 12:06:24 +0000

    git-svn-id: svn://182.53.32.18/bbip/project/GDEUPSB/TRUNK/GDEUPSB@8841 2ac714ae-9a16-11e4-ad83-2f06aba1930a

---
+++ src/com/bocom/bbip/gdeupsb/action/hscard/HSCardFeeQueryAction.java
@@ -0,0 +1,22 @@
+package com.bocom.bbip.gdeupsb.action.hscard;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import com.bocom.bbip.comp.account.AccountService;
+import com.bocom.bbip.eups.action.BaseAction;
+import com.bocom.jump.bp.core.Context;
+import com.bocom.jump.bp.core.CoreException;
+import com.bocom.jump.bp.core.CoreRuntimeException;
+
+public class HSCardFeeQueryAction extends BaseAction{
+	
+	private final static Log log = LogFactory.getLog(HSCardFeeQueryAction.class);
+	
+	public void execute(Context ctx) throws CoreException,CoreRuntimeException{
+		log.info("HSCardFeeQueryAction start......");
+		
+
+	}
+
+}
