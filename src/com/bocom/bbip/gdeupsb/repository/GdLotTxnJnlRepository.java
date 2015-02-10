33214230b1b31834995cd687346b52b355ec4d6d
He_Fengwen <He_Fengwen@2ac714ae-9a16-11e4-ad83-2f06aba1930a>
2015-02-10 12:41:53 +0000

    福彩流水表添加更新方法
    git-svn-id: svn://182.53.32.18/bbip/project/GDEUPSB/TRUNK/GDEUPSB@9481 2ac714ae-9a16-11e4-ad83-2f06aba1930a

--- src/com/bocom/bbip/gdeupsb/repository/GdLotTxnJnlRepository.java
+++ src/com/bocom/bbip/gdeupsb/repository/GdLotTxnJnlRepository.java
@@ -7,6 +7,7 @@ import com.bocom.bbip.data.annotation.Find;
 import com.bocom.bbip.data.annotation.Update;
 import com.bocom.bbip.data.domain.Pageable;
 import com.bocom.bbip.data.repository.PagingAndSortingRepository;
+import com.bocom.bbip.gdeupsb.entity.GdLotDrwTbl;
 import com.bocom.bbip.gdeupsb.entity.GdLotTxnJnl;
 
 public interface GdLotTxnJnlRepository extends PagingAndSortingRepository<GdLotTxnJnl, String> {
@@ -29,5 +30,11 @@ public interface GdLotTxnJnlRepository extends PagingAndSortingRepository<GdLotT
 
     @Find
     public int statLotTxnJnlUnChk(GdLotTxnJnl gdLotTxnJnl);
+    /**
+     * 更新投注流水表中的中奖标志
+     * @param gdLotDrwTbl
+     */
+    @Update
+    public void updateAwdFlg(GdLotDrwTbl gdLotDrwTbl);
 
 }
