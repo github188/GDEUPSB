7a97068fdb1b8470e0f85b2edb75d082e47b2913
Zhang_Rongshen <Zhang_Rongshen@2ac714ae-9a16-11e4-ad83-2f06aba1930a>
2015-02-10 06:07:17 +0000

    git-svn-id: svn://182.53.32.18/bbip/project/GDEUPSB/TRUNK/GDEUPSB@9146 2ac714ae-9a16-11e4-ad83-2f06aba1930a

---
+++ src/com/bocom/bbip/gdeupsb/action/efek/batch/BatchDataResultFileAction.java
@@ -0,0 +1,61 @@
+package com.bocom.bbip.gdeupsb.action.efek.batch;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.bocom.bbip.eups.action.BaseAction;
+import com.bocom.bbip.eups.action.common.OperateFTPAction;
+import com.bocom.bbip.eups.action.common.OperateFileAction;
+import com.bocom.bbip.eups.common.Constants;
+import com.bocom.bbip.eups.common.ParamKeys;
+import com.bocom.bbip.eups.entity.EupsBatchConsoleInfo;
+import com.bocom.bbip.eups.entity.EupsThdFtpConfig;
+import com.bocom.bbip.eups.repository.EupsBatchConsoleInfoRepository;
+import com.bocom.bbip.eups.repository.EupsThdFtpConfigRepository;
+import com.bocom.bbip.gdeupsb.entity.AgtFileBatchDetail;
+import com.bocom.bbip.gdeupsb.entity.GDEupsEleTmp;
+import com.bocom.bbip.gdeupsb.repository.GDEupsEleTmpRepository;
+import com.bocom.bbip.utils.BeanUtils;
+import com.bocom.jump.bp.core.Context;
+import com.bocom.jump.bp.core.CoreException;
+import com.bocom.jump.bp.core.CoreRuntimeException;
+
+public class BatchDataResultFileAction extends BaseAction{
+	@Autowired
+	OperateFileAction operateFile;
+	@Autowired
+	OperateFTPAction operateFTP;
+	@Autowired
+	EupsBatchConsoleInfoRepository eupsBatchConsoleInfoRepository;
+	@Autowired
+	GDEupsEleTmpRepository gdEupsEleTmpRepository;
+		public void execute(Context context)throws CoreException,CoreRuntimeException{
+			//
+			Map<String, Object> resultMap = new HashMap<String, Object>();
+			
+			String batNo=context.getData(ParamKeys.BAT_NO).toString();
+			EupsBatchConsoleInfo eupsBatchConsoleInfo = eupsBatchConsoleInfoRepository.findOne(batNo);
+			//头部
+			Map<String, Object> resultMapHead = BeanUtils.toMap(eupsBatchConsoleInfo);
+			resultMap.put(ParamKeys.EUPS_FILE_HEADER, resultMapHead);
+			//文件名
+			String fileName=eupsBatchConsoleInfo.getFleNme();
+			//文件内容 
+			GDEupsEleTmp gdEupsEleTmps =new GDEupsEleTmp();
+			gdEupsEleTmps.setBakFld(batNo);
+			List<GDEupsEleTmp> detailList=gdEupsEleTmpRepository.find(gdEupsEleTmps);
+			resultMap.put(ParamKeys.EUPS_FILE_DETAIL, detailList);
+			
+			EupsThdFtpConfig eupsThdFtpConfig =get(EupsThdFtpConfigRepository.class).findOne("efekBatchResulf");
+			
+			// 生成文件
+			operateFile.createCheckFile(eupsThdFtpConfig, "efekBatchResulf", fileName, resultMap);
+			// 将生成的文件上传至指定服务器
+			eupsThdFtpConfig.setLocFleNme(fileName);
+			eupsThdFtpConfig.setRmtFleNme(fileName);
+			operateFTP.putCheckFile(eupsThdFtpConfig);
+		}
+}
