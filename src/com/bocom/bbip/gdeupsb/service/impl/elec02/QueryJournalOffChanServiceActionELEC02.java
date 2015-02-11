4f9e7fe602911f380de471f33839d07b8b755592
He_Fengwen <He_Fengwen@2ac714ae-9a16-11e4-ad83-2f06aba1930a>
2015-02-11 07:08:16 +0000

    汕头电力会计流水查询
    git-svn-id: svn://182.53.32.18/bbip/project/GDEUPSB/TRUNK/GDEUPSB@9780 2ac714ae-9a16-11e4-ad83-2f06aba1930a

---
+++ src/com/bocom/bbip/gdeupsb/service/impl/elec02/QueryJournalOffChanServiceActionELEC02.java
@@ -0,0 +1,55 @@
+package com.bocom.bbip.gdeupsb.service.impl.elec02;
+
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.bocom.bbip.eups.action.BaseAction;
+import com.bocom.bbip.eups.common.ErrorCodes;
+import com.bocom.bbip.eups.common.ParamKeys;
+import com.bocom.bbip.eups.entity.EupsTransJournal;
+import com.bocom.bbip.eups.repository.EupsTransJournalRepository;
+import com.bocom.bbip.thd.org.apache.commons.collections.CollectionUtils;
+import com.bocom.bbip.utils.BeanUtils;
+import com.bocom.bbip.utils.DateUtils;
+import com.bocom.jump.bp.core.Context;
+import com.bocom.jump.bp.core.CoreException;
+import com.bocom.jump.bp.core.CoreRuntimeException;
+
+public class QueryJournalOffChanServiceActionELEC02 extends BaseAction {
+	private static Logger logger = LoggerFactory.getLogger(QueryJournalOffChanServiceActionELEC02.class);
+	
+	@Override
+	public void execute(Context context) throws CoreException,	CoreRuntimeException {
+		logger.info("QueryJournalOffChanServiceActionELEC02 start ... ...");
+		String mfmVchNo = context.getData("mfmVchNo");
+		logger.info("mfmVchNo["+mfmVchNo+"]");
+		EupsTransJournal eupsTransJournal = new EupsTransJournal();
+		eupsTransJournal.setMfmVchNo(mfmVchNo);
+		eupsTransJournal.setTxnDte(new Date());
+		eupsTransJournal.setEupsBusTyp((String)context.getData(ParamKeys.EUPS_BUSS_TYPE));
+		List<EupsTransJournal> eupsTransJournals = get(EupsTransJournalRepository.class).find(eupsTransJournal);
+		if(CollectionUtils.isEmpty(eupsTransJournals)){
+			//不存在流水信息
+			logger.error("流水信息不存在");
+			throw new CoreException(ErrorCodes.EUPS_SQN_IS_EMPTY);
+		}
+		eupsTransJournal = eupsTransJournals.get(0);
+		String oldTxnSqn = eupsTransJournal.getSqn();
+		String thdCusNo = eupsTransJournal.getThdCusNo();
+		String thdCusNme = eupsTransJournal.getThdCusNme();
+		String txnSts = eupsTransJournal.getTxnSts();
+		String txnTlr = eupsTransJournal.getTxnTlr();
+		BigDecimal txnAmt = eupsTransJournal.getTxnAmt();
+		String txnTme = DateUtils.format(eupsTransJournal.getTxnTme(),DateUtils.STYLE_TRANS_TIME);
+		String comNo = eupsTransJournal.getComNo();
+		String cusAc = eupsTransJournal.getCusAc();
+		logger.info("oldTxnSqn["+oldTxnSqn+"]thdCusNo["+thdCusNo+"]thdCusNme["+thdCusNme+"]txnSts["+txnSts+"]txnTlr["+txnTlr+"]" +
+				"txnAmt["+txnAmt+"]txnTme["+txnTme+"]comNo["+comNo+"]cusAc["+cusAc+"]");
+		context.setDataMap(BeanUtils.toMap(eupsTransJournal));
+		logger.info("QueryJournalOffChanServiceActionELEC02 end ... ...");
+	}
+}
