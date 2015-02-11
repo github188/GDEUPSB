d7e6a2e986bd93dbe04253814d00d04af6e61f41
Qian_Chen <Qian_Chen@2ac714ae-9a16-11e4-ad83-2f06aba1930a>
2015-02-11 09:18:55 +0000

    git-svn-id: svn://182.53.32.18/bbip/project/GDEUPSB/TRUNK/GDEUPSB@9879 2ac714ae-9a16-11e4-ad83-2f06aba1930a

--- src/com/bocom/bbip/gdeupsb/strategy/trsp/QryDealStrategyAction.java
+++ src/com/bocom/bbip/gdeupsb/strategy/trsp/QryDealStrategyAction.java
@@ -7,12 +7,10 @@ import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 
-import com.bocom.bbip.common.ErrorCodes;
 import com.bocom.bbip.eups.adaptor.ThirdPartyAdaptor;
 import com.bocom.bbip.eups.common.BPState;
 import com.bocom.bbip.eups.common.Constants;
 import com.bocom.bbip.eups.common.ParamKeys;
-import com.bocom.bbip.eups.entity.EupsAmountInfo;
 import com.bocom.bbip.gdeupsb.common.GDConstants;
 import com.bocom.bbip.gdeupsb.common.GDErrorCodes;
 import com.bocom.bbip.gdeupsb.entity.GDEupsbAmountInfo;
@@ -22,39 +20,38 @@ import com.bocom.jump.bp.core.CoreException;
 import com.bocom.jump.bp.core.CoreRuntimeException;
 import com.bocom.jump.bp.core.Executable;
 
-public class QryDealStrategyAction implements Executable{
+public class QryDealStrategyAction implements Executable {
 
 	public final static Log logger = LogFactory.getLog(QryDealStrategyAction.class);
-	
+
 	@Qualifier("callThdTradeManager")
-    ThirdPartyAdaptor callThdTradeManager;
-	
+	ThirdPartyAdaptor callThdTradeManager;
+
 	@Autowired
 	GDEupsbAmountInfoRepository gdEupsAmountInfoRepository;
-	
-	public void execute(Context ctx) throws CoreException,CoreRuntimeException{
+
+	public void execute(Context ctx) throws CoreException, CoreRuntimeException {
 		logger.info("qryDealStrategyAction start......");
 		ctx.setState(BPState.BUSINESS_PROCESSNIG_STATE_FAIL);
 		ctx.setData(ParamKeys.THD_TXN_CDE, GDConstants.QRY_CAR);
-//		TODO:<Arg name="ObjSvr" value="@PARA.ThdSvr"/>
-		Map<String,Object> thdReturnMessage = callThdTradeManager.trade(ctx);
+		// TODO:<Arg name="ObjSvr" value="@PARA.ThdSvr"/>
+		Map<String, Object> thdReturnMessage = callThdTradeManager.trade(ctx);
 		logger.info("call third start....[the state is" + ctx.getState() + "]");
-		String inTRspCde = (String)thdReturnMessage.get(ParamKeys.THIRD_RETURN_CODE);
-		if(!Constants.HOST_RESPONSE_CODE_SUCC.equals(inTRspCde)){
-			logger.info("call third error");
+		String inTRspCde = (String) thdReturnMessage.get(ParamKeys.THIRD_RETURN_CODE);
+		if (!Constants.HOST_RESPONSE_CODE_SUCC.equals(inTRspCde)) {
+			logger.error("call third error");
 			throw new CoreException(GDErrorCodes.EUPS_THD_SYS_ERROR);
-		}else{
+		} else {
 			logger.info("查询汽车费用第三方返回成功");
 			ctx.setData(ParamKeys.RESPONSE_CODE, Constants.RESPONSE_CODE_SUCC);
 		}
-		
+
 		GDEupsbAmountInfo gdEups = new GDEupsbAmountInfo();
-		gdEups.setBr(ctx.getData(ParamKeys.BR).toString());  //网点号
-		gdEups.setBakFld1(ctx.getData(ParamKeys.BAK_FLD1).toString());  //车牌类型
-		gdEups.setBakFld2(ctx.getData(ParamKeys.BAK_FLD2).toString());  //车牌号
-		
+		gdEups.setBr(ctx.getData(ParamKeys.BR).toString()); // 网点号
+		gdEups.setBakFld1(ctx.getData(ParamKeys.BAK_FLD1).toString()); // 车牌类型
+		gdEups.setBakFld2(ctx.getData(ParamKeys.BAK_FLD2).toString()); // 车牌号
+
 		gdEupsAmountInfoRepository.deleteByCarNo(gdEups);
-		
-		
+
 	}
 }
