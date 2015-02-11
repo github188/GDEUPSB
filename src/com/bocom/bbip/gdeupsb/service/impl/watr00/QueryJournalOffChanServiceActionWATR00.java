9dafe5e821e3257a1f1ff7bf187f92fec01dcf52
He_Fengwen <He_Fengwen@2ac714ae-9a16-11e4-ad83-2f06aba1930a>
2015-02-11 07:08:37 +0000

    汕头水费会计流水查询修改
    git-svn-id: svn://182.53.32.18/bbip/project/GDEUPSB/TRUNK/GDEUPSB@9781 2ac714ae-9a16-11e4-ad83-2f06aba1930a

--- src/com/bocom/bbip/gdeupsb/service/impl/watr00/QueryJournalOffChanServiceActionWATR00.java
+++ src/com/bocom/bbip/gdeupsb/service/impl/watr00/QueryJournalOffChanServiceActionWATR00.java
@@ -9,6 +9,7 @@ import org.slf4j.LoggerFactory;
 
 import com.bocom.bbip.eups.action.BaseAction;
 import com.bocom.bbip.eups.common.ErrorCodes;
+import com.bocom.bbip.eups.common.ParamKeys;
 import com.bocom.bbip.eups.entity.EupsTransJournal;
 import com.bocom.bbip.eups.repository.EupsTransJournalRepository;
 import com.bocom.bbip.thd.org.apache.commons.collections.CollectionUtils;
@@ -29,6 +30,7 @@ public class QueryJournalOffChanServiceActionWATR00 extends BaseAction {
 		EupsTransJournal eupsTransJournal = new EupsTransJournal();
 		eupsTransJournal.setMfmVchNo(mfmVchNo);
 		eupsTransJournal.setTxnDte(new Date());
+		eupsTransJournal.setEupsBusTyp((String)context.getData(ParamKeys.EUPS_BUSS_TYPE));
 		List<EupsTransJournal> eupsTransJournals = get(EupsTransJournalRepository.class).find(eupsTransJournal);
 		if(CollectionUtils.isEmpty(eupsTransJournals)){
 			//不存在流水信息
