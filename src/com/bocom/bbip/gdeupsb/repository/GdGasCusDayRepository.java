b8fc120fd637ef40a23455dd089e53945e446f77
Wang_Mingqin <Wang_Mingqin@2ac714ae-9a16-11e4-ad83-2f06aba1930a>
2015-02-11 07:34:40 +0000

    git-svn-id: svn://182.53.32.18/bbip/project/GDEUPSB/TRUNK/GDEUPSB@9801 2ac714ae-9a16-11e4-ad83-2f06aba1930a

--- src/com/bocom/bbip/gdeupsb/repository/GdGasCusDayRepository.java
+++ src/com/bocom/bbip/gdeupsb/repository/GdGasCusDayRepository.java
@@ -1,8 +1,7 @@
 package com.bocom.bbip.gdeupsb.repository;
 
-import com.bocom.bbip.data.domain.NoId;
 import com.bocom.bbip.data.repository.PagingAndSortingRepository;
 import com.bocom.bbip.gdeupsb.entity.GdGasCusDay;
 
-public interface GdGasCusDayRepository extends PagingAndSortingRepository<GdGasCusDay, NoId> {
+public interface GdGasCusDayRepository extends PagingAndSortingRepository<GdGasCusDay, String> {
 }
