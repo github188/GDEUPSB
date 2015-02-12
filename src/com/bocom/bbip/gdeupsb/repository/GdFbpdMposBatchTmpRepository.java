519110ae3e04a6b197e2d085f86f60d057c6deda
Wang_Mingqin <Wang_Mingqin@2ac714ae-9a16-11e4-ad83-2f06aba1930a>
2015-02-12 02:35:28 +0000

    git-svn-id: svn://182.53.32.18/bbip/project/GDEUPSB/TRUNK/GDEUPSB@10227 2ac714ae-9a16-11e4-ad83-2f06aba1930a

---
+++ src/com/bocom/bbip/gdeupsb/repository/GdFbpdMposBatchTmpRepository.java
@@ -0,0 +1,7 @@
+package com.bocom.bbip.gdeupsb.repository;
+
+import com.bocom.bbip.data.repository.PagingAndSortingRepository;
+import com.bocom.bbip.gdeupsb.entity.GdFbpdMposBatchTmp;
+
+public interface GdFbpdMposBatchTmpRepository extends PagingAndSortingRepository<GdFbpdMposBatchTmp, String> {
+}
---
+++ src/com/bocom/bbip/gdeupsb/repository/GdFbpdObusBatchTmpRepository.java
@@ -0,0 +1,7 @@
+package com.bocom.bbip.gdeupsb.repository;
+
+import com.bocom.bbip.data.repository.PagingAndSortingRepository;
+import com.bocom.bbip.gdeupsb.entity.GdFbpdObusBatchTmp;
+
+public interface GdFbpdObusBatchTmpRepository extends PagingAndSortingRepository<GdFbpdObusBatchTmp, String> {
+}
