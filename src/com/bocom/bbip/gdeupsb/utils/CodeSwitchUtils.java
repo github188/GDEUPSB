1f4cd2d22b80af91837ba3fbd56a798e413f0be5
He_Fengwen <He_Fengwen@2ac714ae-9a16-11e4-ad83-2f06aba1930a>
2015-02-12 11:06:28 +0000

    git-svn-id: svn://182.53.32.18/bbip/project/GDEUPSB/TRUNK/GDEUPSB@10525 2ac714ae-9a16-11e4-ad83-2f06aba1930a

--- src/com/bocom/bbip/gdeupsb/utils/CodeSwitchUtils.java
+++ src/com/bocom/bbip/gdeupsb/utils/CodeSwitchUtils.java
@@ -1,12 +1,5 @@
 package com.bocom.bbip.gdeupsb.utils;
 
-import java.util.Map;
-
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-
-import xmlDefinition.TdsBean;
-import xmlDefinition.TdsBeanPro;
 
 //import com.bocom.bbip.gdeupsb.utils.proswitch.bean.TdsBean;
 //import com.bocom.bbip.gdeupsb.utils.proswitch.bean.TdsBeanPro;
@@ -22,20 +15,21 @@ public class CodeSwitchUtils {
 
 	public static String codeGenerator(String tableId, String key) {
 
-		String xml = "classpath:config/switchcode/tds-test.xml";
-
-		ApplicationContext ctx = null;
-		ctx = new ClassPathXmlApplicationContext(new String[] { xml });
-
-		TdsBean tds = ctx.getBean(tableId, TdsBean.class);
-
-		Map<String, TdsBeanPro> p = tds.getKey();
-		TdsBeanPro t = p.get(key);
-		if(null!=t){
-			return t.getValue();
-		}else{
-			return null;
-		}
+//		String xml = "classpath:config/switchcode/tds-test.xml";
+//
+//		ApplicationContext ctx = null;
+//		ctx = new ClassPathXmlApplicationContext(new String[] { xml });
+//
+//		TdsBean tds = ctx.getBean(tableId, TdsBean.class);
+//
+//		Map<String, TdsBeanPro> p = tds.getKey();
+//		TdsBeanPro t = p.get(key);
+//		if(null!=t){
+//			return t.getValue();
+//		}else{
+//			return null;
+//		}
+		return null;
 		
 	}
 
