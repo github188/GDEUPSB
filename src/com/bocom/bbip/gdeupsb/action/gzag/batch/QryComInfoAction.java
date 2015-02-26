4a8e4c3fa2f7312f158636450c4874effd56f6b4
Li_Yawei <Li_Yawei@2ac714ae-9a16-11e4-ad83-2f06aba1930a>
2015-02-26 01:46:32 +0000

    git-svn-id: svn://182.53.32.18/bbip/project/GDEUPSB/TRUNK/GDEUPSB@11821 2ac714ae-9a16-11e4-ad83-2f06aba1930a

--- src/com/bocom/bbip/gdeupsb/action/gzag/batch/QryComInfoAction.java
+++ src/com/bocom/bbip/gdeupsb/action/gzag/batch/QryComInfoAction.java
@@ -5,16 +5,12 @@ import java.util.Map;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.springframework.util.CollectionUtils;
 
 import com.bocom.bbip.eups.action.BaseAction;
 import com.bocom.bbip.eups.common.ParamKeys;
-import com.bocom.bbip.gdeupsb.common.GDConstants;
-import com.bocom.bbip.gdeupsb.common.GDErrorCodes;
-import com.bocom.bbip.gdeupsb.entity.GdLotSysCfg;
-import com.bocom.bbip.gdeupsb.repository.GdLotSysCfgRepository;
 import com.bocom.bbip.service.BGSPServiceAccessObject;
 import com.bocom.bbip.service.Result;
-import com.bocom.bbip.utils.Assert;
 import com.bocom.jump.bp.core.Context;
 import com.bocom.jump.bp.core.CoreException;
 import com.bocom.jump.bp.core.CoreRuntimeException;
@@ -25,12 +21,17 @@ public class QryComInfoAction extends BaseAction{
 	
 	public void execute(Context context) throws CoreException,CoreRuntimeException{
 		logger.info("===============Start   QryComInfoAction");
-
+		 Map<String,Object> rspMap = new HashMap<String, Object>();
+		 String  comNo=context.getData(ParamKeys.COMPANY_NO).toString();
+		 rspMap.put(ParamKeys.COMPANY_NO, comNo);
 		// 查询代收单位协议信息
-		 Result respData = get(BGSPServiceAccessObject.class).callServiceFlatting("queryCorporInfo", context.getDataMap());
-		            Map<String,Object> rspMap = new HashMap<String, Object>();
-		            rspMap = respData.getPayload();
-		            String  comNo=context.getData(ParamKeys.COMPANY_NO).toString();
-//		            Assert.hasLength(comNo, GDErrorCodes.EUPS_SCH_TO_EBNK_SCHCDE_IS_NULL, "该单位未签代扣协议");
+		 Result respData = get(BGSPServiceAccessObject.class).callServiceFlatting("queryCorporInfo", rspMap);
+		
+		 if(CollectionUtils.isEmpty(respData.getPayload())){			
+				throw new CoreRuntimeException("该单位未签约");
+		}
+
+		 context.setDataMap(rspMap);
+		 logger.info("===============End   QryComInfoAction");
 	}
 }
