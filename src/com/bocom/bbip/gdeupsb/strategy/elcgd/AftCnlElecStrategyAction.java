5cfdd3e16081d16a6883833db6e27e9df94a5468
Qian_Chen <Qian_Chen@2ac714ae-9a16-11e4-ad83-2f06aba1930a>
2015-02-27 13:50:04 +0000

    git-svn-id: svn://182.53.32.18/bbip/project/GDEUPSB/TRUNK/GDEUPSB@12456 2ac714ae-9a16-11e4-ad83-2f06aba1930a

--- src/com/bocom/bbip/gdeupsb/strategy/elcgd/AftCnlElecStrategyAction.java
+++ src/com/bocom/bbip/gdeupsb/strategy/elcgd/AftCnlElecStrategyAction.java
@@ -17,17 +17,20 @@ import com.bocom.jump.bp.core.Executable;
  */
 public class AftCnlElecStrategyAction implements Executable {
 
+	private final static Logger log = LoggerFactory.getLogger(AftCnlElecStrategyAction.class);
+
 	@Override
 	public void execute(Context context) throws CoreException, CoreRuntimeException {
-		
-		//获取sqn，经过处理后作为银行方流水号发送给第三方
-		String transJnl=context.getData(ParamKeys.SEQUENCE);  
-		transJnl=transJnl.substring(2,8)+transJnl.substring(transJnl.length()-4,transJnl.length());
-		context.setData("transJournal11", transJnl);
-		
-		
-		context.setData("eleClrDte", context.getData("pwrtxnDate15")); // 供电公司清算日期
 
+		log.info("AftCnlElecStrategyAction start!..");
+		// 获取sqn，经过处理后作为银行方流水号发送给第三方
+		// String transJnl=context.getData(ParamKeys.SEQUENCE);
+		// transJnl=transJnl.substring(2,8)+transJnl.substring(transJnl.length()-4,transJnl.length());
+		//
+		// log.info("after sub deal,transJnl="+transJnl);
+		// context.setData("transJournal11", transJnl);
+
+		context.setData("eleClrDte", context.getData("pwrtxnDate15")); // 供电公司清算日期
 		context.setData("thdSqn", context.getData("eleThdSqn37")); // 第三方流水号
 	}
 
