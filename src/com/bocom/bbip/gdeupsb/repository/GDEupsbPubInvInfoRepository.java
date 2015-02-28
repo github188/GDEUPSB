7452a28c2af4c564e1d29f3fd5cf2455b58289d3
Li_Haoxun <Li_Haoxun@2ac714ae-9a16-11e4-ad83-2f06aba1930a>
2015-02-28 12:18:55 +0000

    git-svn-id: svn://182.53.32.18/bbip/project/GDEUPSB/TRUNK/GDEUPSB@12904 2ac714ae-9a16-11e4-ad83-2f06aba1930a

--- src/com/bocom/bbip/gdeupsb/repository/GDEupsbPubInvInfoRepository.java
+++ src/com/bocom/bbip/gdeupsb/repository/GDEupsbPubInvInfoRepository.java
@@ -1,7 +1,12 @@
 package com.bocom.bbip.gdeupsb.repository;
 
+import com.bocom.bbip.data.domain.Page;
+import com.bocom.bbip.data.domain.Pageable;
 import com.bocom.bbip.data.repository.PagingAndSortingRepository;
 import com.bocom.bbip.gdeupsb.entity.GDEupsbPubInvInfo;
+import com.bocom.bbip.gdeupsb.entity.GDEupsbTrspNpManag;
 
 public interface GDEupsbPubInvInfoRepository extends PagingAndSortingRepository<GDEupsbPubInvInfo, String> {
+	public int findCount(GDEupsbPubInvInfo gdeupsb);
+	public Page<GDEupsbTrspNpManag> find(Pageable pageable, GDEupsbTrspNpManag gdeupsb);
 }
