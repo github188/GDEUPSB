f4deadf8cfc98245c34b911e39e3ad5d5b0410ea
Li_Haoxun <Li_Haoxun@2ac714ae-9a16-11e4-ad83-2f06aba1930a>
2015-03-02 12:56:12 +0000

    git-svn-id: svn://182.53.32.18/bbip/project/GDEUPSB/TRUNK/GDEUPSB@13488 2ac714ae-9a16-11e4-ad83-2f06aba1930a

--- src/com/bocom/bbip/gdeupsb/action/hscard/HSCardFeeQueryAction.java
+++ src/com/bocom/bbip/gdeupsb/action/hscard/HSCardFeeQueryAction.java
@@ -6,6 +6,7 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import com.bocom.bbip.comp.BBIPPublicService;
 import com.bocom.bbip.comp.CommonRequest;
 import com.bocom.bbip.comp.account.AccountService;
 import com.bocom.bbip.comp.account.support.CusActInfResult;
@@ -21,12 +22,20 @@ public class HSCardFeeQueryAction extends BaseAction {
 
 	@Autowired
 	AccountService accountService;
+	
+	 @Autowired
+	 private BBIPPublicService bbipPublicService;
 
 	public void execute(Context ctx) throws CoreException, CoreRuntimeException {
 		log.info("HSCardFeeQueryAction start......");
 		// 判断账户是否余额不足
 		
 		// 查询内部账户余额
+		ctx.setData(ParamKeys.BK, bbipPublicService.getParam("BBIP", "BK"));
+		ctx.setData(ParamKeys.BR, "12");
+		ctx.setData(ParamKeys.TELLER, "123");
+		ctx.setData(ParamKeys.CHANNEL, "1234");
+		ctx.setData(ParamKeys.TRACE_NO, ctx.getData(ParamKeys.SEQUENCE));
 		String cusAc = ctx.getData(ParamKeys.CUS_AC);
 		CommonRequest comRequest=CommonRequest.build(ctx);
 		CusActInfResult acResult = accountService.getAcInf(comRequest, cusAc);
