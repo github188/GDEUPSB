4ff7f14831f91e23cc1197a2bcf64f16eb529235
Li_Yawei <Li_Yawei@2ac714ae-9a16-11e4-ad83-2f06aba1930a>
2015-03-02 12:00:25 +0000

    git-svn-id: svn://182.53.32.18/bbip/project/GDEUPSB/TRUNK/GDEUPSB@13444 2ac714ae-9a16-11e4-ad83-2f06aba1930a

--- src/com/bocom/bbip/gdeupsb/strategy/efek/payFeeOnline/PreCheckDealAction.java
+++ src/com/bocom/bbip/gdeupsb/strategy/efek/payFeeOnline/PreCheckDealAction.java
@@ -1,16 +1,21 @@
 package com.bocom.bbip.gdeupsb.strategy.efek.payFeeOnline;
 
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.CollectionUtils;
 
 import com.bocom.bbip.comp.BBIPPublicService;
 import com.bocom.bbip.eups.common.ParamKeys;
-import com.bocom.bbip.eups.entity.EupsThdTranCtlInfo;
 import com.bocom.bbip.eups.repository.EupsThdTranCtlInfoRepository;
+import com.bocom.bbip.gdeupsb.common.GDConstants;
 import com.bocom.bbip.gdeupsb.common.GDParamKeys;
+import com.bocom.bbip.service.BGSPServiceAccessObject;
+import com.bocom.bbip.service.Result;
 import com.bocom.bbip.utils.DateUtils;
 import com.bocom.jump.bp.core.Context;
 import com.bocom.jump.bp.core.CoreException;
@@ -23,6 +28,8 @@ public class PreCheckDealAction implements Executable{
 	EupsThdTranCtlInfoRepository eupsThdTranCtlInfoRepository;
 	@Autowired
 	BBIPPublicService bbipPublicService;
+	@Autowired
+	BGSPServiceAccessObject bgspServiceAccessObject;
 	/**
 	 * 交易前策略处理
 	 */
@@ -32,33 +39,34 @@ public class PreCheckDealAction implements Executable{
 		logger.info("============Start  PreCheckDealAction");
 		
 			context.setData(ParamKeys.THD_CUS_NO, context.getData(GDParamKeys.PAY_NO));
-			String comNo=context.getData(ParamKeys.COMPANY_NO).toString();
 
 //			comNo=comNo.substring(1,5);         
 			
-			EupsThdTranCtlInfo eupsThdTranCtlInfo=new EupsThdTranCtlInfo();
-			eupsThdTranCtlInfo.setComNo(comNo);
-			eupsThdTranCtlInfo=eupsThdTranCtlInfoRepository.findOne(comNo);
-				if(null == eupsThdTranCtlInfo){
-								context.setData(GDParamKeys.MSGTYP, "E");                  //  Contants常量   
-								context.setData(ParamKeys.RSP_CDE,"EFE999");        //  Contants常量   
-								context.setData(ParamKeys.RSP_MSG, "获取单位编码【"+comNo+"】交易参数错");
-								throw new CoreRuntimeException("获取单位编码【"+comNo+"】交易参数错");
-				}
-				context.setData(ParamKeys.TELLER, eupsThdTranCtlInfo.getTxnTlr());
+			//验证单位协议
+			 Map<String,Object> rspMap = new HashMap<String, Object>();
+			 String comNo=context.getData(ParamKeys.COMPANY_NO).toString();
+			 rspMap.put(ParamKeys.COMPANY_NO, comNo);
+		    Result respData = bgspServiceAccessObject.callServiceFlatting("queryCorporInfo", rspMap);
+			
+			 if(CollectionUtils.isEmpty(respData.getPayload())){			
+				    context.setData(GDParamKeys.MSGTYP, "E");                  //  Contants常量   
+					context.setData(ParamKeys.RSP_CDE,"EFE999");        //  Contants常量   
+					throw new CoreRuntimeException("该单位未签约");
+			}
 				context.setData(GDParamKeys.TOTNUM, "1");
 				//日期时间格式修改
-				context.setData(ParamKeys.CCY, "RMB");
-				context.setData("thdTxnDte", DateUtils.parse(DateUtils.format(new Date(),DateUtils.STYLE_SIMPLE_DATE)));
-				context.setData("thdTxnTme", DateUtils.parse(DateUtils.formatAsTranstime(new Date())));
+				context.setData(ParamKeys.CCY, GDConstants.RENMINBI);
+				context.setData(ParamKeys.THD_TXN_DATE, DateUtils.parse(DateUtils.format(new Date(),DateUtils.STYLE_SIMPLE_DATE)));
+				context.setData(ParamKeys.THD_TXN_TIME, DateUtils.parse(DateUtils.formatAsTranstime(new Date())));
 				
 				context.setData(ParamKeys.TXN_DATE, DateUtils.parse(DateUtils.format(new Date(),DateUtils.STYLE_SIMPLE_DATE)));
 				context.setData(ParamKeys.TXN_TIME,DateUtils.parse(DateUtils.format(new Date(),DateUtils.STYLE_TRANS_TIME)));
 				
-				//TODO
+				//TODO 
 				context.setData(ParamKeys.BUS_TYP,context.getData(GDParamKeys.BUS_TYPE));
 				logger.info("~~~~~~~~~~~交易日期："+context.getData(ParamKeys.TXN_DATE)
 						+"~~~~~~~~~~~交易时间："+context.getData(ParamKeys.TXN_TIME));
-				context.setData(ParamKeys.REQ_JRN_NO, context.getData(ParamKeys.SEQUENCE));
+				//TODO 
+//				context.setData(ParamKeys.REQ_JRN_NO, context.getData(ParamKeys.SEQUENCE));
 	}
 }
