791e06cc5e5475c84017604ea8bdb57f792e255b
Wu_Yanhui <Wu_Yanhui@2ac714ae-9a16-11e4-ad83-2f06aba1930a>
2015-03-03 02:10:59 +0000

    git-svn-id: svn://182.53.32.18/bbip/project/GDEUPSB/TRUNK/GDEUPSB@13667 2ac714ae-9a16-11e4-ad83-2f06aba1930a

---
+++ src/com/bocom/bbip/gdeupsb/repository/ZHActNoInfRepository.java
@@ -0,0 +1,24 @@
+package com.bocom.bbip.gdeupsb.repository;
+
+import java.util.List;
+import java.util.Map;
+
+import com.bocom.bbip.data.annotation.Delete;
+import com.bocom.bbip.data.annotation.FindOne;
+import com.bocom.bbip.data.annotation.Insert;
+import com.bocom.bbip.data.annotation.Update;
+import com.bocom.bbip.data.repository.PagingAndSortingRepository;
+import com.bocom.bbip.gdeupsb.entity.ZHActNoInf;
+
+public interface ZHActNoInfRepository extends PagingAndSortingRepository<ZHActNoInf, String>{
+	
+	//更新一本通帐号到主帐号－实体帐号
+	@FindOne
+	public List<ZHActNoInf> queryNewByOld(final String oldActNo);
+	@Delete
+	public void deleteInfo(Map<String,String> map);
+	@Update
+	public void updateInfo(Map<String,String> map);
+	@Insert
+	public void insertInfo(ZHActNoInf info);
+}
