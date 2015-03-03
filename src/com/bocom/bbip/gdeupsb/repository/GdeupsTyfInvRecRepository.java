1cb1ca2a58b4cb3f33273ea90286d335d5f090c6
Li_Haoxun <Li_Haoxun@2ac714ae-9a16-11e4-ad83-2f06aba1930a>
2015-03-03 12:39:40 +0000

    git-svn-id: svn://182.53.32.18/bbip/project/GDEUPSB/TRUNK/GDEUPSB@14193 2ac714ae-9a16-11e4-ad83-2f06aba1930a

--- src/com/bocom/bbip/gdeupsb/repository/GdeupsTyfInvRecRepository.java
+++ src/com/bocom/bbip/gdeupsb/repository/GdeupsTyfInvRecRepository.java
@@ -1,8 +1,11 @@
 package com.bocom.bbip.gdeupsb.repository;
 
+import java.util.List;
+
 import com.bocom.bbip.data.domain.NoId;
 import com.bocom.bbip.data.repository.PagingAndSortingRepository;
 import com.bocom.bbip.gdeupsb.entity.GdeupsTyfInvRec;
 
 public interface GdeupsTyfInvRecRepository extends PagingAndSortingRepository<GdeupsTyfInvRec, NoId> {
+	public List<Integer> findCount(GdeupsTyfInvRec gdeupsTyfInvRec);
 }
