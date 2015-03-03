5e6757113655cf72f6911b59f6c1a69e2ce48797
Wu_Yanhui <Wu_Yanhui@2ac714ae-9a16-11e4-ad83-2f06aba1930a>
2015-03-03 02:10:05 +0000

    git-svn-id: svn://182.53.32.18/bbip/project/GDEUPSB/TRUNK/GDEUPSB@13661 2ac714ae-9a16-11e4-ad83-2f06aba1930a

---
+++ src/com/bocom/bbip/gdeupsb/entity/ZHAgtActno.java
@@ -0,0 +1,22 @@
+package com.bocom.bbip.gdeupsb.entity;
+
+public class ZHAgtActno {
+	private String OldAct;
+	private String ActNo;
+
+	public String getOldAct() {
+		return OldAct;
+	}
+
+	public void setOldAct(String oldAct) {
+		OldAct = oldAct;
+	}
+
+	public String getActNo() {
+		return ActNo;
+	}
+
+	public void setActNo(String actNo) {
+		ActNo = actNo;
+	}
+}
