f859b625f992f915bf321eb78a76431ac4a29675
Qian_Chen <Qian_Chen@2ac714ae-9a16-11e4-ad83-2f06aba1930a>
2015-03-03 06:38:04 +0000

    git-svn-id: svn://182.53.32.18/bbip/project/GDEUPSB/TRUNK/GDEUPSB@13909 2ac714ae-9a16-11e4-ad83-2f06aba1930a

--- src/com/bocom/bbip/gdeupsb/action/transportfee/PrintAction.java
+++ src/com/bocom/bbip/gdeupsb/action/transportfee/PrintAction.java
@@ -12,6 +12,7 @@ public class PrintAction extends BaseAction{
 
 	public void execute(Context ctx) throws CoreRuntimeException,CoreException{
 		ctx.setData(GDParamKeys.OINV_NO, ctx.getData(GDParamKeys.INV_NO));
+		//TODO:待考虑！！！！
 		if(!ctx.getData(GDParamKeys.OINV_NO).toString().trim().equals(ctx.getData(GDParamKeys.INV_NO).toString().trim())){
 			ctx.setData(ParamKeys.RSP_MSG, "发票号码改变，请补打");
 			throw new CoreRuntimeException(GDErrorCodes.EUPS_INVOIC_NO_ERROR);
