986db1a923647de468c7fae82dbcf71e22e488cc
Qian_Chen <Qian_Chen@2ac714ae-9a16-11e4-ad83-2f06aba1930a>
2015-03-03 06:42:23 +0000

    git-svn-id: svn://182.53.32.18/bbip/project/GDEUPSB/TRUNK/GDEUPSB@13914 2ac714ae-9a16-11e4-ad83-2f06aba1930a

--- src/com/bocom/bbip/gdeupsb/action/hscard/HScardSignInAction.java
+++ src/com/bocom/bbip/gdeupsb/action/hscard/HScardSignInAction.java
@@ -20,14 +20,20 @@ import com.bocom.jump.bp.core.CoreException;
 import com.bocom.jump.bp.core.CoreRuntimeException;
 import com.bocom.jump.bp.util.Hex;
 
-public class HScardSignInAction extends BaseAction{
-	public void execute(Context context) throws CoreException,CoreRuntimeException{
+/**
+ * 华商一卡通签退
+ * 
+ * @author lhx
+ * 
+ */
+public class HScardSignInAction extends BaseAction {
+	public void execute(Context context) throws CoreException, CoreRuntimeException {
 		context.setData(ParamKeys.TXN_TYP, Constants.SIGN_SET_TYPE_SIGNIN);
 		Assert.hasLengthInData(context, ParamKeys.COMPANY_NO, ErrorCodes.EUPS_FIELD_EMPTY, "单位编号");
 		Assert.hasLengthInData(context, ParamKeys.TXN_TYP, ErrorCodes.EUPS_FIELD_EMPTY, "交易状态");
-	
-//		String comNo = "";
-		
+
+		// String comNo = "";
+
 		/*
 		 * 渠道签到函数(根据主密钥生成工作秘钥)
 		 */
@@ -35,54 +41,56 @@ public class HScardSignInAction extends BaseAction{
 		EupsThdTranCtlInfo resultThdTranCtlInfo = get(EupsThdTranCtlInfoRepository.class).findOne(eupsThdTranCtlInfo.getComNo());
 		if (resultThdTranCtlInfo == null) {
 			throw new CoreException(ErrorCodes.THD_CHL_NOT_FOUND);
-		} 
+		}
 		if (resultThdTranCtlInfo.getTxnCtlSts().equals(Constants.TXN_CTL_STS_SIGNIN)) {
 			throw new CoreException(ErrorCodes.THD_CHL_ALDEAY_SIGN_IN);
 		} else if (resultThdTranCtlInfo.getTxnCtlSts().equals(Constants.TXN_CTL_STS_CHECKBILL_ING)) {
 			throw new CoreException(ErrorCodes.THD_CHL_SIGNIN_NOT_ALLOWWED);
 		} else {
 			eupsThdTranCtlInfo.setTxnCtlSts(Constants.TXN_CTL_STS_SIGNIN);
-			//根据默认算法生成一个密钥
+			// 根据默认算法生成一个密钥
 			String readyMD5 = buildReadyStr(context);
-			String mainkey = resultThdTranCtlInfo.getManKey();//"535A5F424F434F4D";
-	        String beforeMacChk = "",macChk = "";
-	        // context.getData("");
-            if (StringUtils.isNotEmpty(mainkey)) {
-                try {
-                    // 得到主密钥
-                    mainkey = mainkey.trim();
-                    MessageDigest md = MessageDigest.getInstance("MD5");
-                    md.update(readyMD5.getBytes());
-                    // 待加密的字符串生成MD5码
-                    String afterMD5 = Hex.encode(md.digest());
-                    beforeMacChk = afterMD5.substring(0, 16);
-                    // 用主密钥Des加密转成MD5码的串
-                    String mac = CryptoUtils.desEncrpty(afterMD5, mainkey);
-                    macChk = CryptoUtils.desEncrpty(beforeMacChk, Hex.encode("00000000".getBytes()));
-                    context.setData(ParamKeys.MACKEY, mac.substring(0, 16));
-                    context.setData(ParamKeys.MACCHK, macChk.substring(0, 8));
-                    eupsThdTranCtlInfo.setWrkKey(mac.substring(0, 16));
-                } catch (NoSuchAlgorithmException e) {
-                    throw new CoreException(ErrorCodes.EUPS_GEN_KEY_ERROR);
-                }
-            }
-	        eupsThdTranCtlInfo.setTxnDte(new Date());
-            eupsThdTranCtlInfo.setTxnTme(new Date());
+			String mainkey = resultThdTranCtlInfo.getManKey();// "535A5F424F434F4D";
+			String beforeMacChk = "", macChk = "";
+			// context.getData("");
+			if (StringUtils.isNotEmpty(mainkey)) {
+				try {
+					// 得到主密钥
+					mainkey = mainkey.trim();
+					MessageDigest md = MessageDigest.getInstance("MD5");
+					md.update(readyMD5.getBytes());
+					// 待加密的字符串生成MD5码
+					String afterMD5 = Hex.encode(md.digest());
+					beforeMacChk = afterMD5.substring(0, 16);
+					// 用主密钥Des加密转成MD5码的串
+					String mac = CryptoUtils.desEncrpty(afterMD5, mainkey);
+					macChk = CryptoUtils.desEncrpty(beforeMacChk, Hex.encode("00000000".getBytes()));
+					context.setData(ParamKeys.MACKEY, mac.substring(0, 16));
+					context.setData(ParamKeys.MACCHK, macChk.substring(0, 8));
+					eupsThdTranCtlInfo.setWrkKey(mac.substring(0, 16));
+				} catch (NoSuchAlgorithmException e) {
+					throw new CoreException(ErrorCodes.EUPS_GEN_KEY_ERROR);
+				}
+			}
+			eupsThdTranCtlInfo.setTxnDte(new Date());
+			eupsThdTranCtlInfo.setTxnTme(new Date());
 			get(EupsThdTranCtlInfoRepository.class).update(eupsThdTranCtlInfo);
 		}
 	}
+
 	/**
 	 * 根据MAC加区域编号，渠道编号，交易时间生成签到待加密的字符串
+	 * 
 	 * @param para
 	 * @return
 	 */
-	private String buildReadyStr(Context context){
+	private String buildReadyStr(Context context) {
 		String strDate = DateUtils.formatAsMMddHHmmss(new Date());
 		String strPreifx = "MAC";
-		StringBuffer beforeMAC = new  StringBuffer();
+		StringBuffer beforeMAC = new StringBuffer();
 		beforeMAC.append(strPreifx).
-					append(context.getData(ParamKeys.THD_REGION_NO)).
-					append(context.getData(ParamKeys.TXN_CHL)).append(strDate);
+				append(context.getData(ParamKeys.THD_REGION_NO)).
+				append(context.getData(ParamKeys.TXN_CHL)).append(strDate);
 		return beforeMAC.toString();
 	}
 }
--- src/com/bocom/bbip/gdeupsb/action/hscard/HScardSignOutAction.java
+++ src/com/bocom/bbip/gdeupsb/action/hscard/HScardSignOutAction.java
@@ -12,25 +12,28 @@ import com.bocom.jump.bp.core.Context;
 import com.bocom.jump.bp.core.CoreException;
 import com.bocom.jump.bp.core.CoreRuntimeException;
 
-public class HScardSignOutAction extends BaseAction{
+/**
+ * 华商一卡通签到
+ * 
+ * @author lhx
+ * 
+ */
+public class HScardSignOutAction extends BaseAction {
 	@Override
-
-	public void execute(Context context) throws CoreException,CoreRuntimeException{
+	public void execute(Context context) throws CoreException, CoreRuntimeException {
 		context.setData(ParamKeys.TXN_TYP, Constants.SIGN_SET_TYPE_SIGNOUT);
 		Assert.hasLengthInData(context, ParamKeys.COMPANY_NO, ErrorCodes.EUPS_FIELD_EMPTY, "单位编号");
 		Assert.hasLengthInData(context, ParamKeys.TXN_TYP, ErrorCodes.EUPS_FIELD_EMPTY, "交易状态");
-	
-		
-		
+
 		EupsThdTranCtlInfo eupsThdTranCtlInfo = BeanUtils.toObject(context.getDataMap(), EupsThdTranCtlInfo.class);
 		EupsThdTranCtlInfo resultThdTranCtlInfo = get(EupsThdTranCtlInfoRepository.class).findOne(eupsThdTranCtlInfo.getComNo());
 		Assert.isNotNull(resultThdTranCtlInfo, ErrorCodes.THD_CHL_NOT_FOUND);
-        Assert.isTrue(!Constants.TXN_CTL_STS_SIGNOUT.equals(resultThdTranCtlInfo.getTxnCtlSts()), ErrorCodes.THD_CHL_ALDEAY_SIGN_OUT);
-		Assert.isTrue(!Constants.TXN_CTL_STS_CHECKBILL_ING.equals(resultThdTranCtlInfo.getTxnCtlSts()),ErrorCodes.THD_CHL_SIGNOUT_NOT_ALLOWWED);
-		if (resultThdTranCtlInfo.getTxnCtlSts().equals(Constants.SIGN_SET_TYPE_SIGNIN)){
-//		    TODO://标准版里没此方法eupsThdTranCtlInfo.signOutKeepKey();
-		    get(EupsThdTranCtlInfoRepository.class).update(eupsThdTranCtlInfo);
+		Assert.isTrue(!Constants.TXN_CTL_STS_SIGNOUT.equals(resultThdTranCtlInfo.getTxnCtlSts()), ErrorCodes.THD_CHL_ALDEAY_SIGN_OUT);
+		Assert.isTrue(!Constants.TXN_CTL_STS_CHECKBILL_ING.equals(resultThdTranCtlInfo.getTxnCtlSts()), ErrorCodes.THD_CHL_SIGNOUT_NOT_ALLOWWED);
+		if (resultThdTranCtlInfo.getTxnCtlSts().equals(Constants.SIGN_SET_TYPE_SIGNIN)) {
+			// TODO://标准版里没此方法eupsThdTranCtlInfo.signOutKeepKey();
+			get(EupsThdTranCtlInfoRepository.class).update(eupsThdTranCtlInfo);
 		}
-	
-}
+
+	}
 }
