0894036da7188f7ff937de6ca32b23929f4a136e
Qian_Chen <Qian_Chen@2ac714ae-9a16-11e4-ad83-2f06aba1930a>
2015-03-03 06:38:28 +0000

    git-svn-id: svn://182.53.32.18/bbip/project/GDEUPSB/TRUNK/GDEUPSB@13910 2ac714ae-9a16-11e4-ad83-2f06aba1930a

--- src/com/bocom/bbip/gdeupsb/action/transportfee/PrintAddInvCancelAction.java
+++ src/com/bocom/bbip/gdeupsb/action/transportfee/PrintAddInvCancelAction.java
@@ -24,33 +24,34 @@ import com.bocom.jump.bp.core.Context;
 import com.bocom.jump.bp.core.CoreException;
 import com.bocom.jump.bp.core.CoreRuntimeException;
 
-public class PrintAddInvCancelAction extends BaseAction{
+public class PrintAddInvCancelAction extends BaseAction {
 	private final static Log log = LogFactory.getLog(PrintAddInvCancelAction.class);
-	
+
 	@Autowired
 	GDEupsbTrspInvChgInfoRepository gdEupsbTrspInvChgInfoRepository;
 	@Autowired
 	BBIPPublicService bbipPublicService;
-	
+
 	@Autowired
-    ThirdPartyAdaptor callThdTradeManager;
-	public void execute(Context ctx) throws CoreException,CoreRuntimeException{
+	ThirdPartyAdaptor callThdTradeManager;
+
+	public void execute(Context ctx) throws CoreException, CoreRuntimeException {
 		log.info("PrintAddInvCancelAction start......");
-//		Date today = bbipPublicService.getAcDate();
+		// Date today = bbipPublicService.getAcDate();
 		Date today = new Date();
-//		SELECT OLogNo,InvNo,OInvNO,TCusId,CarTyp,TLogNo,TxnDat,NodNo,TlrId
-//        FROM   rbfbInvChg444
-//        WHERE  LogNo='%s'
+		// SELECT OLogNo,InvNo,OInvNO,TCusId,CarTyp,TLogNo,TxnDat,NodNo,TlrId
+		// FROM rbfbInvChg444
+		// WHERE LogNo='%s'
 		GDEupsbTrspInvChgInfo gdEupsbTrspInvChgInfo = new GDEupsbTrspInvChgInfo();
 		gdEupsbTrspInvChgInfo.setSqn(ctx.getData(GDParamKeys.SQN).toString());
 		List<GDEupsbTrspInvChgInfo> invChgInfoList = gdEupsbTrspInvChgInfoRepository.find(gdEupsbTrspInvChgInfo);
-//		TODO:OLogNo找不到 ,且应该还再查出actDat
-		System.out.println("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"+today);
-		System.out.println("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"+invChgInfoList.get(0).getTactDt());
-		if(!DateUtils.isSameDate(today, invChgInfoList.get(0).getTactDt())){   //此处ics中是actDat，还未明确
+		// TODO:OLogNo找不到 ,且应该还再查出actDat
+		System.out.println("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@" + today);
+		System.out.println("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@" + invChgInfoList.get(0).getTactDt());
+		if (!DateUtils.isSameDate(today, invChgInfoList.get(0).getTactDt())) { // 此处ics中是actDat，还未明确
 			ctx.setData(ParamKeys.RSP_MSG, "禁止跨日抹帐");
 			throw new CoreRuntimeException(GDErrorCodes.DATE_ERROR);
-		}else{
+		} else {
 			ctx.setData(GDParamKeys.INV_NO, invChgInfoList.get(0).getInvNo());
 			ctx.setData(GDParamKeys.OINV_NO, invChgInfoList.get(0).getOinvNo());
 			ctx.setData(GDParamKeys.CAR_NO, invChgInfoList.get(0).getCarNo());
@@ -59,30 +60,26 @@ public class PrintAddInvCancelAction extends BaseAction{
 			ctx.setData(GDParamKeys.NOD_NO, invChgInfoList.get(0).getNodNo());
 			ctx.setData(GDParamKeys.TLR_ID, invChgInfoList.get(0).getTlrId());
 			ctx.setData(GDParamKeys.ACT_DAT, invChgInfoList.get(0).getActDat());
-			
+
 		}
-		
-		
-		
-		
+
 		Map<String, Object> thdReturnMessage = callThdTradeManager.trade(ctx);
-		if(ctx.getState().equals(BPState.BUSINESS_PROCESSNIG_STATE_OVERTIME)){
+		if (ctx.getState().equals(BPState.BUSINESS_PROCESSNIG_STATE_OVERTIME)) {
 			ctx.setData(ParamKeys.RSP_MSG, "路桥方交易超时");
-			throw new CoreRuntimeException(ErrorCodes.TRANSACTION_ERROR_TIMEOUT);			
-			
-		}else if(ctx.getState().equals(BPState.BUSINESS_PROCESSNIG_STATE_TRANS_FAIL)){
+			throw new CoreRuntimeException(ErrorCodes.TRANSACTION_ERROR_TIMEOUT);
+
+		} else if (ctx.getState().equals(BPState.BUSINESS_PROCESSNIG_STATE_TRANS_FAIL)) {
 			ctx.setData(ParamKeys.RSP_MSG, "路桥方交易失败");
-			throw new CoreRuntimeException( ErrorCodes.TRANSACTION_ERROR_OTHER_ERROR);
-		}else{
-			if(!"000000".equals(thdReturnMessage.get("trspCd"))){
+			throw new CoreRuntimeException(ErrorCodes.TRANSACTION_ERROR_OTHER_ERROR);
+		} else {
+			if (!"000000".equals(thdReturnMessage.get("trspCd"))) {
 				ctx.setData(ParamKeys.RSP_MSG, "路桥方返回：" + ctx.getData(GDParamKeys.TRSP_CD));
-				throw new CoreRuntimeException( ErrorCodes.TRANSACTION_ERROR_OTHER_ERROR);
-				
-			}else{
+				throw new CoreRuntimeException(ErrorCodes.TRANSACTION_ERROR_OTHER_ERROR);
+
+			} else {
 				ctx.setData(ParamKeys.RSP_CDE, Constants.RESPONSE_CODE_SUCC);
 			}
 		}
-
 
 	}
 
