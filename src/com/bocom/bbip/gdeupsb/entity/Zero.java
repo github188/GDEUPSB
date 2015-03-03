ed82a667378c491976e11478c0cd346f61388b47
Wu_Yanhui <Wu_Yanhui@2ac714ae-9a16-11e4-ad83-2f06aba1930a>
2015-03-03 02:09:52 +0000

    git-svn-id: svn://182.53.32.18/bbip/project/GDEUPSB/TRUNK/GDEUPSB@13658 2ac714ae-9a16-11e4-ad83-2f06aba1930a

---
+++ src/com/bocom/bbip/gdeupsb/entity/Zero.java
@@ -0,0 +1,114 @@
+package com.bocom.bbip.gdeupsb.entity;
+
+public class Zero {
+
+	private String ZreAct;
+	private String ZActNam;
+	private String ZActNodNo;
+	private String ZActNodNm;
+	private String CAgtNo;
+	private String CAgtNm;
+	private String CActNo;
+	private String CActNm;
+	private String CActNodNo;
+	private String CActNodNm;
+	private String PayCod;
+	private String PayNam;
+
+	public String getZreAct() {
+		return ZreAct;
+	}
+
+	public void setZreAct(String zreAct) {
+		ZreAct = zreAct;
+	}
+
+	public String getZActNam() {
+		return ZActNam;
+	}
+
+	public void setZActNam(String zActNam) {
+		ZActNam = zActNam;
+	}
+
+	public String getZActNodNo() {
+		return ZActNodNo;
+	}
+
+	public void setZActNodNo(String zActNodNo) {
+		ZActNodNo = zActNodNo;
+	}
+
+	public String getZActNodNm() {
+		return ZActNodNm;
+	}
+
+	public void setZActNodNm(String zActNodNm) {
+		ZActNodNm = zActNodNm;
+	}
+
+	public String getCAgtNo() {
+		return CAgtNo;
+	}
+
+	public void setCAgtNo(String cAgtNo) {
+		CAgtNo = cAgtNo;
+	}
+
+	public String getCAgtNm() {
+		return CAgtNm;
+	}
+
+	public void setCAgtNm(String cAgtNm) {
+		CAgtNm = cAgtNm;
+	}
+
+	public String getCActNo() {
+		return CActNo;
+	}
+
+	public void setCActNo(String cActNo) {
+		CActNo = cActNo;
+	}
+
+	public String getCActNm() {
+		return CActNm;
+	}
+
+	public void setCActNm(String cActNm) {
+		CActNm = cActNm;
+	}
+
+	public String getCActNodNo() {
+		return CActNodNo;
+	}
+
+	public void setCActNodNo(String cActNodNo) {
+		CActNodNo = cActNodNo;
+	}
+
+	public String getCActNodNm() {
+		return CActNodNm;
+	}
+
+	public void setCActNodNm(String cActNodNm) {
+		CActNodNm = cActNodNm;
+	}
+
+	public String getPayCod() {
+		return PayCod;
+	}
+
+	public void setPayCod(String payCod) {
+		PayCod = payCod;
+	}
+
+	public String getPayNam() {
+		return PayNam;
+	}
+
+	public void setPayNam(String payNam) {
+		PayNam = payNam;
+	}
+
+}
