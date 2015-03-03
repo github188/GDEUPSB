d2d535902815a30fc9f2549edd905e5ace0c72f7
Wu_Yanhui <Wu_Yanhui@2ac714ae-9a16-11e4-ad83-2f06aba1930a>
2015-03-03 02:07:14 +0000

    git-svn-id: svn://182.53.32.18/bbip/project/GDEUPSB/TRUNK/GDEUPSB@13643 2ac714ae-9a16-11e4-ad83-2f06aba1930a

---
+++ src/com/bocom/bbip/gdeupsb/action/elec02/updateErrorStatusAction.java
@@ -0,0 +1,59 @@
+package com.bocom.bbip.gdeupsb.action.elec02;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import com.bocom.bbip.eups.action.BaseAction;
+import com.bocom.bbip.eups.common.ErrorCodes;
+import com.bocom.bbip.eups.common.ParamKeys;
+import com.bocom.bbip.eups.entity.EupsCorpAgent;
+import com.bocom.bbip.eups.entity.EupsTransJournal;
+import com.bocom.bbip.eups.repository.EupsCorpAgentRepository;
+import com.bocom.bbip.eups.repository.EupsTransJournalRepository;
+import com.bocom.bbip.gdeupsb.common.GDConstants;
+import com.bocom.bbip.gdeupsb.utils.SwitchCodeUtils;
+import com.bocom.bbip.utils.Assert;
+import com.bocom.bbip.utils.ContextUtils;
+import com.bocom.jump.bp.JumpException;
+import com.bocom.jump.bp.core.Context;
+import com.bocom.jump.bp.core.CoreRuntimeException;
+
+
+/** 460262 可疑交易进行状态修改 */
+public class updateErrorStatusAction extends BaseAction {
+	private static final Log logger = LogFactory.getLog(updateErrorStatusAction.class);
+
+    public void process(Context context) throws CoreRuntimeException, JumpException {
+		logger.info("updateErrorStatus start!!");
+		final String bk = ContextUtils.assertDataHasLengthAndGetNNR(context,ParamKeys.BK, ErrorCodes.EUPS_FIELD_EMPTY);
+		Assert.isFalse(bk.equalsIgnoreCase(GDConstants.MANAGE_ORG_445012),  ErrorCodes.EUPS_OPR_TYP_ERR,"非管理机构不能执行本交易");
+		/**单位类型*/
+		final String AppNm=ContextUtils.assertDataHasLengthAndGetNNR(context, "AppNm", ErrorCodes.EUPS_FIELD_EMPTY);
+		/**修改的交易状态*/
+		final String HTxnS=ContextUtils.assertDataHasLengthAndGetNNR(context, ParamKeys.MFM_TXN_STS, ErrorCodes.EUPS_FIELD_EMPTY);
+		final String sqn=ContextUtils.assertDataHasLengthAndGetNNR(context, ParamKeys.SEQ_NO, ErrorCodes.EUPS_FIELD_EMPTY);
+
+		/**单位类型转换为单位代码cmo*/		
+		final String CAgtNo=switchCode(AppNm);
+		/**检查单位协议是否存在*/
+		EupsCorpAgent corpAgent=get(EupsCorpAgentRepository.class).findOne(CAgtNo);
+		Assert.isFalse(null==corpAgent, ErrorCodes.EUPS_FIELD_EMPTY, "单位协议不存在");
+		EupsTransJournal journal=new EupsTransJournal();
+		journal.setMfmTxnSts(HTxnS);
+		journal.setSqn(sqn);
+		get(EupsTransJournalRepository.class).update(journal);
+		
+		logger.info("updateErrorStatus success!!");
+
+	}
+    private String switchCode(final String appName){
+    	String ret="";
+    	try{
+    		 ret=SwitchCodeUtils.getCAgtNo(appName);
+    	}catch(Exception e){
+    		ret="";
+    	}
+    	return ret;
+	
+    }
+}
