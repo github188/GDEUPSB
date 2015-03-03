09fe2ac40e89f50e7230e30d835bfa3e29deeb8d
Wu_Yanhui <Wu_Yanhui@2ac714ae-9a16-11e4-ad83-2f06aba1930a>
2015-03-03 02:09:59 +0000

    git-svn-id: svn://182.53.32.18/bbip/project/GDEUPSB/TRUNK/GDEUPSB@13660 2ac714ae-9a16-11e4-ad83-2f06aba1930a

---
+++ src/com/bocom/bbip/gdeupsb/entity/ZHActNoInf.java
@@ -0,0 +1,77 @@
+package com.bocom.bbip.gdeupsb.entity;
+
+public class ZHActNoInf {
+	private String OldAct;
+	private String ActNo;
+	private String ActSqn;
+	private String CusId;
+	private String ActTyp;
+	private String ActNam;
+	private String OpnNod;
+	private String TckNo;
+
+	public String getOldAct() {
+		return OldAct;
+	}
+
+	public void setOldAct(String oldAct) {
+		OldAct = oldAct;
+	}
+
+	public String getActNo() {
+		return ActNo;
+	}
+
+	public void setActNo(String actNo) {
+		ActNo = actNo;
+	}
+
+	public String getActSqn() {
+		return ActSqn;
+	}
+
+	public void setActSqn(String actSqn) {
+		ActSqn = actSqn;
+	}
+
+	public String getCusId() {
+		return CusId;
+	}
+
+	public void setCusId(String cusId) {
+		CusId = cusId;
+	}
+
+	public String getActTyp() {
+		return ActTyp;
+	}
+
+	public void setActTyp(String actTyp) {
+		ActTyp = actTyp;
+	}
+
+	public String getActNam() {
+		return ActNam;
+	}
+
+	public void setActNam(String actNam) {
+		ActNam = actNam;
+	}
+
+	public String getOpnNod() {
+		return OpnNod;
+	}
+
+	public void setOpnNod(String opnNod) {
+		OpnNod = opnNod;
+	}
+
+	public String getTckNo() {
+		return TckNo;
+	}
+
+	public void setTckNo(String tckNo) {
+		TckNo = tckNo;
+	}
+
+}
