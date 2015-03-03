3126c6bfe376288190ab8c49860f4a0aca160e4e
Wu_Yanhui <Wu_Yanhui@2ac714ae-9a16-11e4-ad83-2f06aba1930a>
2015-03-03 02:09:23 +0000

    git-svn-id: svn://182.53.32.18/bbip/project/GDEUPSB/TRUNK/GDEUPSB@13656 2ac714ae-9a16-11e4-ad83-2f06aba1930a

---
+++ src/com/bocom/bbip/gdeupsb/entity/AreaInfo.java
@@ -0,0 +1,23 @@
+package com.bocom.bbip.gdeupsb.entity;
+
+public class AreaInfo {
+	private String AreaNam;
+	private String AreaCod;
+
+	public String getAreaNam() {
+		return AreaNam;
+	}
+
+	public void setAreaNam(String areaNam) {
+		AreaNam = areaNam;
+	}
+
+	public String getAreaCod() {
+		return AreaCod;
+	}
+
+	public void setAreaCod(String areaCod) {
+		AreaCod = areaCod;
+	}
+
+}
