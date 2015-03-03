bbb4beaed65f2acac7e593214638fdb50d9b6b2e
Wang_Mingqin <Wang_Mingqin@2ac714ae-9a16-11e4-ad83-2f06aba1930a>
2015-03-03 11:32:59 +0000

    git-svn-id: svn://182.53.32.18/bbip/project/GDEUPSB/TRUNK/GDEUPSB@14128 2ac714ae-9a16-11e4-ad83-2f06aba1930a

--- src/com/bocom/bbip/gdeupsb/repository/GDEupsLotPrzInfoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.bocom.bbip.gdeupsb.repository;
-
-import com.bocom.bbip.data.domain.NoId;
-import com.bocom.bbip.data.repository.PagingAndSortingRepository;
-import com.bocom.bbip.gdeupsb.entity.GDEupsLotPrzInfo;
-
-public interface GDEupsLotPrzInfoRepository extends PagingAndSortingRepository<GDEupsLotPrzInfo, NoId> {
-}
