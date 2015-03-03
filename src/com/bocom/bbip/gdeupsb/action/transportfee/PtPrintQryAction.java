25f5a28923dc9a154737f9e0a1eda7d46b289e18
Li_Haoxun <Li_Haoxun@2ac714ae-9a16-11e4-ad83-2f06aba1930a>
2015-03-03 12:42:12 +0000

    git-svn-id: svn://182.53.32.18/bbip/project/GDEUPSB/TRUNK/GDEUPSB@14198 2ac714ae-9a16-11e4-ad83-2f06aba1930a

--- src/com/bocom/bbip/gdeupsb/action/transportfee/PtPrintQryAction.java
+++ src/com/bocom/bbip/gdeupsb/action/transportfee/PtPrintQryAction.java
@@ -6,53 +6,43 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
-
-
-
-
-
-
-
-
-
-
-import com.asn1c.core.Int16;
 import com.bocom.bbip.data.domain.Page;
 import com.bocom.bbip.data.domain.PageRequest;
 import com.bocom.bbip.data.domain.Pageable;
 import com.bocom.bbip.eups.action.BaseAction;
 import com.bocom.bbip.eups.common.ErrorCodes;
 import com.bocom.bbip.gdeupsb.common.GDParamKeys;
-import com.bocom.bbip.gdeupsb.entity.GDEupsbPubInvInfo;
 import com.bocom.bbip.gdeupsb.entity.GdeupsTyfInvRec;
-import com.bocom.bbip.gdeupsb.repository.GDEupsbPubInvInfoRepository;
 import com.bocom.bbip.gdeupsb.repository.GdeupsTyfInvRecRepository;
 import com.bocom.bbip.utils.BeanUtils;
 import com.bocom.jump.bp.core.Context;
 import com.bocom.jump.bp.core.CoreException;
 import com.bocom.jump.bp.core.CoreRuntimeException;
 
-public class PtPrintQryAction extends BaseAction{
-	
+public class PtPrintQryAction extends BaseAction {
+
 	private final static Log log = LogFactory.getLog(PtPrintQryAction.class);
 	@Autowired
 	GdeupsTyfInvRecRepository gdeupsTyfInvRecRepository;
-	
-	public void execute(Context ctx) throws CoreException,CoreRuntimeException{
+
+	public void execute(Context ctx) throws CoreException, CoreRuntimeException {
 		log.info("PtPrintQryAction start......");
-		
+
 		GdeupsTyfInvRec gdeupsTyfInvRec = new GdeupsTyfInvRec();
 		gdeupsTyfInvRec.setActNo(ctx.getData(GDParamKeys.ACT_NO).toString());
 		gdeupsTyfInvRec.setTmp01(ctx.getData(GDParamKeys.CAR_NO).toString());
 		gdeupsTyfInvRec.setTmp02(ctx.getData(GDParamKeys.CAR_TYP).toString());
-		
-		List<Integer> num = gdeupsTyfInvRecRepository.findCount(gdeupsTyfInvRec);
-		if(num.get(0) == 0){
+
+		List<Integer> num = gdeupsTyfInvRecRepository
+				.findCount(gdeupsTyfInvRec);
+		if (num.get(0) == 0) {
 			throw new CoreException(ErrorCodes.EUPS_CONSOLE_INFO_NOTEXIST);
 		}
-		
-		Pageable pageable = BeanUtils.toObject(ctx.getDataMap(), PageRequest.class);
-		Page<GdeupsTyfInvRec> invInfoPage = get(GdeupsTyfInvRecRepository.class).find(pageable, gdeupsTyfInvRec);
+
+		Pageable pageable = BeanUtils.toObject(ctx.getDataMap(),
+				PageRequest.class);
+		Page<GdeupsTyfInvRec> invInfoPage = get(GdeupsTyfInvRecRepository.class)
+				.find(pageable, gdeupsTyfInvRec);
 		setResponseFromPage(ctx, "ptResultList", invInfoPage);
 	}
 
