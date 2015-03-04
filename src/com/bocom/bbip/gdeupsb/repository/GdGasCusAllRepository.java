1e530f1b61e68163947f05a7c6a848cd1145a019
Wang_Mingqin <Wang_Mingqin@2ac714ae-9a16-11e4-ad83-2f06aba1930a>
2015-03-04 11:16:46 +0000

    git-svn-id: svn://182.53.32.18/bbip/project/GDEUPSB/TRUNK/GDEUPSB@14757 2ac714ae-9a16-11e4-ad83-2f06aba1930a

--- src/com/bocom/bbip/gdeupsb/repository/GdGasCusAllRepository.java
+++ src/com/bocom/bbip/gdeupsb/repository/GdGasCusAllRepository.java
@@ -1,12 +1,19 @@
 package com.bocom.bbip.gdeupsb.repository;
 
 import java.util.List;
+import java.util.Map;
 
 import com.bocom.bbip.data.annotation.Find;
 import com.bocom.bbip.data.repository.PagingAndSortingRepository;
 import com.bocom.bbip.gdeupsb.entity.GdGasCusAll;
 
 public interface GdGasCusAllRepository extends PagingAndSortingRepository<GdGasCusAll, String> {
+	
 	@Find
 	List<GdGasCusAll> findDataBeforeOptDat(GdGasCusAll cusAllAgt);
+	
+	@Find
+	public List<Map<String, Object>> findDataByOptDate(Map<String, Object> map);
+	
+	
 }
