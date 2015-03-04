e83c3d0d027c16aa7f314c724ef8947be9bb0c1d
Wang_Mingqin <Wang_Mingqin@2ac714ae-9a16-11e4-ad83-2f06aba1930a>
2015-03-04 11:17:03 +0000

    git-svn-id: svn://182.53.32.18/bbip/project/GDEUPSB/TRUNK/GDEUPSB@14759 2ac714ae-9a16-11e4-ad83-2f06aba1930a

--- src/com/bocom/bbip/gdeupsb/action/gas/QryHzThdCusInfoAction.java
+++ src/com/bocom/bbip/gdeupsb/action/gas/QryHzThdCusInfoAction.java
@@ -1,5 +1,6 @@
 package com.bocom.bbip.gdeupsb.action.gas;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -15,7 +16,9 @@ import com.bocom.bbip.eups.common.ParamKeys;
 import com.bocom.bbip.gdeupsb.common.GDConstants;
 import com.bocom.bbip.gdeupsb.common.GDParamKeys;
 import com.bocom.bbip.gdeupsb.entity.GdEupsCusAgent;
+import com.bocom.bbip.gdeupsb.entity.GdGasCusAll;
 import com.bocom.bbip.gdeupsb.entity.GdGasCusDay;
+import com.bocom.bbip.gdeupsb.repository.GdGasCusAllRepository;
 import com.bocom.bbip.gdeupsb.repository.GdGasCusDayRepository;
 import com.bocom.bbip.service.BGSPServiceAccessObject;
 import com.bocom.bbip.service.Result;
@@ -35,8 +38,31 @@ public class QryHzThdCusInfoAction extends BaseAction{
 	
 	public void execute(Context context) throws CoreException, CoreRuntimeException{
 		logger.info("Enter in QryHzThdCusInfoAction!.......");
-		
+		logger.info("context=" + context);
 		//拼装外发代收付map
+//		Map<String, Object> map = new HashMap<String, Object>();
+//		if(!(null==context.getData("thdCusNo") || "".equals(context.getData("thdCusNo")) || "?".equals(context.getData("thdCusNo"))  )){
+//			map.put("thdCusNo", context.getData("thdCusNo").toString());
+//		}
+//		if(!(null==context.getData("cusAc") || "".equals(context.getData("cusAc")) || "?".equals(context.getData("cusAc"))  )){
+//			map.put("cusAc", context.getData("cusAc").toString());
+//		}
+//		if(!(null==context.getData("cusNme") || "".equals(context.getData("cusNme")) || "?".equals(context.getData("cusNme"))  )){
+//			map.put("cusNme", context.getData("cusNme").toString());
+//		}
+//		if(!(null==context.getData("idNo") || "".equals(context.getData("idNo")) || "?".equals(context.getData("idNo"))  )){
+//			map.put("idNo", context.getData("idNo").toString());
+//		}
+		
+		//惠州燃气代扣用户信息查询  查询明细
+//		Result accessObject = get(BGSPServiceAccessObject.class).callServiceFlatting("queryDetailAgentCollectAgreement", map);
+		
+//		select * from gascusall491 where (userno='%s' or '%s'='') and (actno='%s' or '%s'='') 
+//        and (actnam='%s' or '%s'='') and (idno='%s' or '%s'='') and OPTDAT &gt;='%s' and OPTDAT &lt;='%s'
+		
+		
+//		GdGasCusAll cusAll = new GdGasCusAll();
+		
 		Map<String, Object> map = new HashMap<String, Object>();
 		if(!(null==context.getData("thdCusNo") || "".equals(context.getData("thdCusNo")) || "?".equals(context.getData("thdCusNo"))  )){
 			map.put("thdCusNo", context.getData("thdCusNo").toString());
@@ -50,57 +76,51 @@ public class QryHzThdCusInfoAction extends BaseAction{
 		if(!(null==context.getData("idNo") || "".equals(context.getData("idNo")) || "?".equals(context.getData("idNo"))  )){
 			map.put("idNo", context.getData("idNo").toString());
 		}
+		map.put("beginDate", context.getData("beginDate").toString());
+		map.put("endDate", context.getData("endDate").toString());
 		
-		//惠州燃气代扣用户信息查询  查询明细
-		Result accessObject = get(BGSPServiceAccessObject.class).callServiceFlatting("queryDetailAgentCollectAgreement", map);
-
-		if(CollectionUtils.isEmpty(accessObject.getPayload())){			//无明细信息
+		
+		List<Map<String, Object>> gasCusAllList = get(GdGasCusAllRepository.class).findDataByOptDate(map);
+		if(CollectionUtils.isEmpty(gasCusAllList)){			//无明细信息
 			context.setData(GDParamKeys.GAS_MSG_TYP, "E");
 			context.setData(GDParamKeys.GAS_AP_CDE, "SC");
 			context.setData(GDParamKeys.GAS_OFMT_COD, "D04");
 			context.setData(GDParamKeys.GAS_IN_POS, "0001");
-			context.setData(GDParamKeys.GDEUPSB_RSP_MSG, "无该用户明细信息");
-			throw new CoreRuntimeException("无该用户明细信息");
+			context.setData(GDParamKeys.GDEUPSB_RSP_MSG, "无明细信息");
+			throw new CoreRuntimeException("无明细信息");
+		}
+		logger.info("gasCusAllList.size()=" + gasCusAllList.size());
+		logger.info("==========context=" + context);
+		
+		List<Map<String, Object>> gasCusAllListDtl = new ArrayList<Map<String,Object>>();
+		for(int i=0; i<gasCusAllList.size(); i++){
+			Map<String, Object> cusAllDtlTemp = new HashMap<String, Object>();
+			logger.info("gasCusAllList["+ i + "]:CUS_NO:" + gasCusAllList.get(i).get("CUS_NO"));
+			cusAllDtlTemp.put("cusNo", gasCusAllList.get(i).get("CUS_NO"));
+			cusAllDtlTemp.put("cusAc", gasCusAllList.get(i).get("CUS_AC"));
+			cusAllDtlTemp.put("cusNme", gasCusAllList.get(i).get("CUS_NME"));
+			cusAllDtlTemp.put("acTyp", gasCusAllList.get(i).get("CUS_TYP"));
+			cusAllDtlTemp.put("creDte", gasCusAllList.get(i).get("OPT_DAT"));
+			cusAllDtlTemp.put("agrTme", gasCusAllList.get(i).get("OPT_NOD"));
+			cusAllDtlTemp.put("idTyp", gasCusAllList.get(i).get("ID_TYP"));
+			cusAllDtlTemp.put("idNo", gasCusAllList.get(i).get("ID_NO"));
+			cusAllDtlTemp.put("thdCusNme", gasCusAllList.get(i).get("THD_CUS_NME"));
+			cusAllDtlTemp.put("cmuTel", gasCusAllList.get(i).get("CMU_TEL"));
+			gasCusAllListDtl.add(cusAllDtlTemp);
+			logger.info("gasCusAllListDtl["+ i + "]:cusNo:" + gasCusAllListDtl.get(i).get("cusNo"));
 		}
 		
-//		context.setData(ParamKeys.CUS_NO, context.getData(ParamKeys.THD_CUS_NO));
-//		logger.info("============context=" + context);
-//		
-//		GdGasCusDay dayCusInfoCusDay = BeanUtils.toObject(context.getDataMap(), GdGasCusDay.class);
-//		logger.info("==========dayCusInfoCusDay=" + dayCusInfoCusDay.toString());
-//		
-//		List<GdGasCusDay> cusInfoList = get(GdGasCusDayRepository.class).find(dayCusInfoCusDay);
-//		if(CollectionUtils.isEmpty(cusInfoList)){
-//			throw new CoreRuntimeException("系统异常1");
-//		}
-		
-//		List<Map<String,Object>> resultList=(List<Map<String, Object>>) BeanUtils.toMaps(cusInfoList);
-		
-		
-		List<Map<String, Object>> resultMapList = (List<Map<String, Object>>) BeanUtils.toMap(accessObject);
-		if(CollectionUtils.isEmpty(resultMapList)){
-			context.setData(GDParamKeys.GAS_MSG_TYP, "E");
-			context.setData(GDParamKeys.GAS_AP_CDE, "SC");
-			context.setData(GDParamKeys.GAS_OFMT_COD, "D04");
-			context.setData(GDParamKeys.GAS_IN_POS, "0001");
-			context.setData(GDParamKeys.GDEUPSB_RSP_MSG, "系统错误");
-			context.setData(GDParamKeys.GAS_RSP_COD, GDConstants.GAS_ERROR_CODE);
-			throw new CoreRuntimeException("系统异常");
-		}
-		else{
-		context.setData("loopDtl", resultMapList);
+		context.setData("loopDtl", gasCusAllListDtl);
 		context.setData(GDParamKeys.GAS_AP_CDE, "32");
 	    context.setData(GDParamKeys.GAS_OFMT_COD, "z01");
 	    context.setData(GDParamKeys.GAS_PAGE_NO, "0001");
 	    context.setData(GDParamKeys.GAS_VAR_SIZE, "3");
 	    context.setData(GDParamKeys.GAS_TXN_TTL, "代扣用户信息浏览");
 	    context.setData(GDParamKeys.GAS_SUB_TTL, "查询内容");
-			
 		context.setData(GDParamKeys.GAS_MSG_TYP, "N");
 		context.setData(GDParamKeys.GDEUPSB_RSP_MSG, "交易成功");
 		context.setData(GDParamKeys.GAS_RSP_COD, GDConstants.GDEUPSB_TXN_SUCC_CODE);
 		logger.info("==========context=" + context);
-		}
 		
 		context.setState(BPState.BUSINESS_PROCESSNIG_STATE_NORMAL);
 	}
