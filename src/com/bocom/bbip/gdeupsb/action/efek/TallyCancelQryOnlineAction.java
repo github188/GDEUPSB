bba9448aa8fe657702d0f3e4217738ad0ab5796a
Li_Yawei <Li_Yawei@2ac714ae-9a16-11e4-ad83-2f06aba1930a>
2015-03-05 09:17:01 +0000

    git-svn-id: svn://182.53.32.18/bbip/project/GDEUPSB/TRUNK/GDEUPSB@15320 2ac714ae-9a16-11e4-ad83-2f06aba1930a

--- src/com/bocom/bbip/gdeupsb/action/efek/TallyCancelQryOnlineAction.java
+++ src/com/bocom/bbip/gdeupsb/action/efek/TallyCancelQryOnlineAction.java
@@ -3,6 +3,8 @@ package com.bocom.bbip.gdeupsb.action.efek;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
+import a.a.a.s;
+
 import com.bocom.bbip.eups.action.BaseAction;
 import com.bocom.bbip.eups.common.ParamKeys;
 import com.bocom.bbip.eups.entity.EupsTransJournal;
@@ -33,16 +35,18 @@ public class TallyCancelQryOnlineAction extends BaseAction{
 						context.setData(ParamKeys.SEQUENCE, eupsTransJournal.getSqn());
 						context.setData(ParamKeys.TXN_TME, eupsTransJournal.getTxnTme());
 						context.setData(ParamKeys.OLD_TXN_SQN, eupsTransJournal.getOldTxnSqn());
+						context.setData(ParamKeys.THD_TXN_DATE, eupsTransJournal.getThdTxnDte());
 						context.setData(ParamKeys.THD_TXN_TIME, eupsTransJournal.getThdTxnTme());
 //TODO						context.setData(ParamKeys.BUS_TYP, );
+						context.setData(GDParamKeys.PAY_NO, eupsTransJournal.getThdCusNo());
 						context.setData(ParamKeys.PAYFEE_TYPE, eupsTransJournal.getPfeTyp());
 						context.setData(ParamKeys.COMPANY_NO, eupsTransJournal.getComNo());
 //TODO						context.setData(ParamKeys.TXN_ORG_CDE, );
 //TODO						context.setData(ParamKeys.BANK_NO, );
 						context.setData(ParamKeys.ORDER_REG_AC, eupsTransJournal.getCusAc());
-						context.setData(ParamKeys.CUS_NME, eupsTransJournal.getCusNme());
+						context.setData("CusNme", eupsTransJournal.getCusNme());
 						context.setData(ParamKeys.FULL_DED_FLAG,eupsTransJournal.getFulDedFlg() );
-						context.setData(GDParamKeys.ACCOUNTS_SERIAL_NO, eupsTransJournal.getThdSqn());
+//						context.setData(GDParamKeys.ACCOUNTS_SERIAL_NO, eupsTransJournal.getThdSqn());
 //TODO						context.setData(GDParamKeys.ELECTRICITY_YEARMONTH, );
 						context.setData(ParamKeys.CCY, eupsTransJournal.getCcy());
 						context.setData(ParamKeys.TXN_AMT, eupsTransJournal.getTxnAmt());
@@ -50,16 +54,5 @@ public class TallyCancelQryOnlineAction extends BaseAction{
 					log.error("~~~~~~~~~~没有记录");
 					throw new CoreException("没有记录");
 				}
-				//TODO
-//				DefaultTransport defaultTransport=(DefaultTransport)get("");
-//				try {
-//					defaultTransport.submit(context.getDataMap(), context);
-//				} catch (CommunicationException e) {
-//					// TODO Auto-generated catch block
-//					e.printStackTrace();
-//				} catch (JumpException e) {
-//					// TODO Auto-generated catch block
-//					e.printStackTrace();
-//				}
 		}
 }
