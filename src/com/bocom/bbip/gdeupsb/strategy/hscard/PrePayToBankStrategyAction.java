4c45a576b02399517fc56a303dfbc2d326308da0
Li_Haoxun <Li_Haoxun@2ac714ae-9a16-11e4-ad83-2f06aba1930a>
2015-03-06 11:59:13 +0000

    git-svn-id: svn://182.53.32.18/bbip/project/GDEUPSB/TRUNK/GDEUPSB@15820 2ac714ae-9a16-11e4-ad83-2f06aba1930a

--- src/com/bocom/bbip/gdeupsb/strategy/hscard/PrePayToBankStrategyAction.java
+++ src/com/bocom/bbip/gdeupsb/strategy/hscard/PrePayToBankStrategyAction.java
@@ -1,14 +1,28 @@
 package com.bocom.bbip.gdeupsb.strategy.hscard;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
 
 
+
+
+
+
+
+import org.springframework.util.CollectionUtils;
+
 import com.bocom.bbip.comp.BBIPPublicService;
 import com.bocom.bbip.eups.common.Constants;
+import com.bocom.bbip.eups.common.ErrorCodes;
 import com.bocom.bbip.eups.common.ParamKeys;
+import com.bocom.bbip.service.BGSPServiceAccessObject;
+import com.bocom.bbip.service.Result;
 import com.bocom.jump.bp.core.Context;
 import com.bocom.jump.bp.core.CoreException;
 import com.bocom.jump.bp.core.CoreRuntimeException;
@@ -18,7 +32,11 @@ public class PrePayToBankStrategyAction implements Executable{
 	private final static  Log logger = LogFactory.getLog(PrePayToBankStrategyAction.class);
 	
 	@Autowired
+	BGSPServiceAccessObject bgspServiceAccessObject;
+	
+	@Autowired
 	BBIPPublicService bbipPublicService;
+	@SuppressWarnings("unchecked")
 	public void execute(Context context) throws CoreException,CoreRuntimeException{
 		
 		logger.info("PrePayToBankStrategyAction start......");
@@ -34,119 +52,56 @@ public class PrePayToBankStrategyAction implements Executable{
 	      context.setData(ParamKeys.CCY,Constants.CCY_CDE_CNY );
 //	      context.setData(ParamKeys.AC_TYP,"4" );   //ICS中是ACT_TYP,paramkeys里还有acc_typ,还不确定用哪个
 	      
-	      context.setData(ParamKeys.ACT_NO, context.getData(ParamKeys.CUS_AC));   
+	      context.setData(ParamKeys.ACT_NO, context.getData(ParamKeys.CUS_AC));  
 //	      TODO:
-//	    	  <Exec func="PUB:CallHostOther" error="IGNORE">
-//	        <Arg name="HTxCd"  value="476520"/>
-//	        <Arg name="ObjSvr" value="SHSTPUB1"/>
-//	      </Exec>
-//	      <Switch expression="~RetCod">
-//	        <Case value="0" desc="成功">
-//	          <Set>ActNam=BANKCOMM</Set><!-- 测试用===================================== -->
-//	          <If condition="IS_NOEQUAL_STRING($TIdNo,DELSPACE($IdNo,all))">
-//	            <Set>MsgTyp=E</Set>
-//	            <Set>RspCod=CPL992</Set>
-//	            <Set>RspMsg=证件号码匹配失败</Set>
-//	            <Return/>
-//	          </If>
-//	          <If condition="IS_NOEQUAL_STRING($CardNm,DELSPACE($ActNam,all))">
-//	            <Set>MsgTyp=E</Set>
-//	            <Set>RspCod=CPL993</Set>
-//	            <Set>RspMsg=姓名匹配失败</Set>
-//	            <Return/>
-//	          </If>
-//	          <Break/>
-//	        </Case>
-//	        <Case value="-1" desc="超时"/>
-//	        <Case value="-2" desc="系统错误"/>
-//	        <Case value="-10" desc="未发送">
-//	          <Set>MsgTyp=E</Set>
-//	          <Set>RspCod=CPL999</Set>
-//	          <Set>RspMsg=银行系统繁忙，请稍后再做</Set>
-//	          <Return/>
-//	        </Case>
-//	        <Case value="3" desc="失败">
-//	          <Set>MsgTyp=E</Set>
-//	          <Set>RspMsg=查询用户信息失败</Set>
-//	          <Set>RspCod=CPL991</Set>
-//	          <Return/>
-//	        </Case>
-//	        <Case value="-9" desc="需要授权">
-//	          <Set>MsgTyp=E</Set>
-//	          <Set>RspCod=CPL999</Set>
-//	          <Set>RspMsg=卡状态异常，请咨询开户银行</Set>
-//	          <Return/>
-//	        </Case>
-//	        <Default>
-//	          <Set>MsgTyp=E</Set>
-//	          <Set>RspCod=CPL999</Set>
-//	          <Set>RspMsg=未知错误</Set>
-//	          <Return/>
-//	        </Default>
-//	      </Switch>
-//	      
-//	      <!--交易卡协议验证-->TODO
-//	      <Set>CrdNo=$CardNo</Set>
-//	      <Exec func="PUB:CallHostOther" error="IGNORE">
-//	        <Arg name="HTxnCd" value="010420"/>
-//	        <Arg name="ObjSvr" value="SHSTPUB1"/>
-//	      </Exec>
-//	      <Switch expression="~RetCod">
-//	        <Case value="0" desc="成功">
-//	          <Break/>
-//	        </Case>
-//	        <Case value="-1" desc="超时"/>
-//	        <Case value="-2" desc="系统错误"/>
-//	        <Case value="-10" desc="未发送">
-//	          <Set>RspCod=CPL999</Set>
-//	          <Set>RspMsg=银行系统繁忙，请稍后再做</Set>
-//	          <Break/>
-//	        </Case>
-//	        <Case value="3" desc="失败">
-//	          <Set>RspCod=CPL999</Set>
-//	          <Set>RspMsg=检验交易卡协议失败</Set>
-//	          <Break/>
-//	        </Case>
-//	        <Case value="-9" desc="需要授权">
-//	          <Set>RspCod=CPL999</Set>
-//	          <Set>RspMsg=卡状态异常，请咨询开户银行</Set>
-//	          <Break/>
-//	        </Case>
-//	        <Default>
-//	          <Set>RspCod=CPL999</Set>
-//	          <Set>RspMsg=未知错误</Set>
-//	          <Break/>
-//	        </Default>
-//	      </Switch>
+//	      <Set>BusTyp=PCL52</Set>
+//	      <Set>CnlTyp=L</Set>
+//	      <Set>Mask=9199</Set>
+//	      <Set>ActFlg=4</Set>
+//	      <Set>ActNo=$CardNo</Set>
+//	      <Set>PayMod=0</Set>
+//	      <Set>TActNo=$CrpAct</Set>
+//	      <Set>CcyTyp=1</Set>
+//	      <Set>VchChk=0</Set>
+//	      <Set>CAgtNo=9999999999</Set>
+//	      <Set>GthFlg=N</Set>
+//	      <Set>TckNo=ZZZZZZZZZZZ</Set>
+//	      <Set>TxnSts=U</Set>
+//	      <Set>VchCod=00000000</Set>
+//	      <Delete>CTRL_A</Delete>
+//	      <Delete>CashNo</Delete>
+//	      <Delete>HLogNo</Delete>
 	     
-	      //登记交易流水(这步标准版做了，此处不需要做。)
-//	      context.setData(ParamKeys.BUSS_KIND, "PCL52");
-//	      context.setData(ParamKeys.CHL_TYP, "L");
-//	      context.setData(ParamKeys.REMARK, "9199");
-//	      context.setData(ParamKeys.ACC_TYPE, "4");  //ICS中是ACTFLG,paramkeys里有ac_type,acc_type,暂不确定用哪个。
-//	      context.setData(ParamKeys.ACT_NO, ParamKeys.CUS_AC);
-//	      context.setData(ParamKeys.PAY_MDE, "0");   //paramkeys中有pay_mde,pay_mode,暂不确定。
-//	     <Set>TActNo=$CrpAct</Set>  tactno找不到。
-//	      <Set>VchChk=0</Set>    监督标志，没找到。
-//	      <Set>CAgtNo=9999999999</Set> 没找到。
-//	       <Set>GthFlg=N</Set>  没找到。
-//	      <Set>TckNo=ZZZZZZZZZZZ</Set>  本字段应该是会计流水号，但不知与前文的银行处理流水号有何区别。
-//	      context.setData(ParamKeys.TXN_STS, "U");
-//	     <Set>VchCod=00000000</Set>  没找到。
-//	      context.setData(ParamKeys.CCY, "1");
-
-
-//	      
-	      
-//	      EupsTransJournal eupsTransJournal = BeanUtils.toObject(context.getDataMap(), EupsTransJournal.class);
-	      
-//	      eupsTransJournalRepository.save(eupsTransJournal);
-//	      if(!eupsTransJournal.getRspCde().equals("0")){
-	      
-//	    	  context.setData(ParamKeys.MESSAGE_TYPE, "E");
-//	    	  context.setData(ParamKeys.RESPONSE_CODE, "CPL999");
-//				context.setData(ParamKeys.RESPONSE_MESSAGE, "处理交易信息异常");
-//				return;
-//	      }
+	      //卡协议验证
+	      context.setData(ParamKeys.TELLER, "ABIR148");
+	      context.setData(ParamKeys.BR, "01441131999");
+	      context.setData(ParamKeys.BK, "01441999999");
+	      context.setData(ParamKeys.CHANNEL, "00");
+	      Map<String, Object> agtMap = new HashMap<String, Object>();
+	      agtMap.put(ParamKeys.CUS_AC, context.getData(ParamKeys.CUS_AC));
+	      Result accessObject = bgspServiceAccessObject.callServiceFlatting("queryListAgentCollectAgreement",agtMap);
+	      Map<String,Object> resultMap=accessObject.getPayload();
+	      if (CollectionUtils.isEmpty(resultMap)) {
+	            logger.info("There are no records for select check trans journal ");
+	            context.setData(ParamKeys.RESPONSE_CODE, "000001");
+        		context.setData(ParamKeys.RSP_MSG, "未查到协议信息");
+	            throw new CoreException(ErrorCodes.EUPS_QUERY_NO_DATA);
+	        }else{
+	        	List<Map<String,Object>> customerInfoList=(List<Map<String,Object>>)resultMap.get("customerInfo");
+	        	Map<String,Object> map=customerInfoList.get(0);
+	        	String cusNme = (String)map.get(ParamKeys.CUS_NME);
+	        	String idNo = (String)map.get(ParamKeys.ID_NO);
+	        	if(!context.getData(ParamKeys.CUS_NME).toString().equals(cusNme)){
+	        		context.setData(ParamKeys.RESPONSE_CODE, "000001");
+	        		context.setData(ParamKeys.RSP_MSG, "姓名匹配失败");
+	        		throw new CoreException(ErrorCodes.EUPS_AGENT_CHK_ERROR);
+	        	}
+	        	if(!context.getData(ParamKeys.ID_NO).toString().equals(idNo)){
+	        		context.setData(ParamKeys.RESPONSE_CODE, "000001");
+	        		context.setData(ParamKeys.RSP_MSG, "证件号码匹配失败");
+	        		throw new CoreException(ErrorCodes.EUPS_AGENT_CHK_ERROR);
+	        	}
+	        	
+	        }
 	}
 }
