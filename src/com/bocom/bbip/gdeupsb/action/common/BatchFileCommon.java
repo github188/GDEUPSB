d9ec8a6d018487fc0636fab364ee008e8a92f202
Wu_Yanhui <Wu_Yanhui@2ac714ae-9a16-11e4-ad83-2f06aba1930a>
2015-03-07 05:18:53 +0000

    git-svn-id: svn://182.53.32.18/bbip/project/GDEUPSB/TRUNK/GDEUPSB@16059 2ac714ae-9a16-11e4-ad83-2f06aba1930a

--- src/com/bocom/bbip/gdeupsb/action/common/BatchFileCommon.java
+++ src/com/bocom/bbip/gdeupsb/action/common/BatchFileCommon.java
@@ -15,7 +15,9 @@ import com.bocom.bbip.eups.action.common.OperateFileAction;
 import com.bocom.bbip.eups.common.Constants;
 import com.bocom.bbip.eups.common.ErrorCodes;
 import com.bocom.bbip.eups.common.ParamKeys;
+import com.bocom.bbip.eups.entity.EupsActSysPara;
 import com.bocom.bbip.eups.entity.EupsThdFtpConfig;
+import com.bocom.bbip.eups.repository.EupsActSysParaRepository;
 import com.bocom.bbip.eups.repository.EupsThdFtpConfigRepository;
 import com.bocom.bbip.gdeupsb.common.GDConstants;
 import com.bocom.bbip.gdeupsb.entity.GDEupsBatchConsoleInfo;
@@ -24,7 +26,9 @@ import com.bocom.bbip.service.Result;
 import com.bocom.bbip.utils.Assert;
 import com.bocom.bbip.utils.BeanUtils;
 import com.bocom.bbip.utils.ContextUtils;
+import com.bocom.bbip.utils.DateUtils;
 import com.bocom.bbip.utils.StringUtils;
+import com.bocom.jump.bp.SystemConfig;
 import com.bocom.jump.bp.core.Context;
 import com.bocom.jump.bp.core.CoreException;
 
@@ -73,16 +77,36 @@ public class BatchFileCommon extends BaseAction {
 	}
 
 	public void sendBatchFileToACP(final Context context) throws CoreException {
-		String fileName = ContextUtils.assertVariableNotEmptyAndGet(context,ParamKeys.FLE_NME, ErrorCodes.EUPS_FILE_CREATE_NAME_ISEMPTY);
-		Map<String, Object> fileMap = (Map<String, Object>) ContextUtils.assertVariableNotNullAndGet(context, "agtFileMap","agtFileMap不能为空");
+		final String comNo=(String)context.getData(ParamKeys.COMPANY_NO);
+		final String tlr=(String)context.getData(ParamKeys.TELLER);
+		final String br=(String)context.getData(ParamKeys.BR);
+        final String AcDate=DateUtils.format(
+        	((BBIPPublicServiceImpl)get(GDConstants.BBIP_PUBLIC_SERVICE)).getAcDate(),DateUtils.STYLE_yyyyMMdd);
+        final String systemCode=((SystemConfig)get(SystemConfig.class)).getSystemCode();;
+        final String busTyp=(String)context.getData("busTyp");
+        final String dir="/home/bbipadm/data/mftp/BBIP/"+systemCode+"/"+br+"/"+tlr+"/"+AcDate+"/";
+        EupsActSysPara eupsActSysPara = new EupsActSysPara();
+        eupsActSysPara.setActSysTyp("0");
+        eupsActSysPara.setComNo(comNo);
+        List resultList = ((EupsActSysParaRepository)get(EupsActSysParaRepository.class)).find(eupsActSysPara);
+        Assert.isNotEmpty(resultList, ErrorCodes.EUPS_QUERY_NO_DATA);
+        final String comNoAcps =((EupsActSysPara)resultList.get(0)).getSplNo();
+        final String fleNme="BATC"+comNoAcps+busTyp+".txt";
+
+        Map<String, Object> fileMap = (Map<String, Object>) ContextUtils.assertVariableNotNullAndGet(context, "agtFileMap","agtFileMap不能为空");
 		EupsThdFtpConfig config = get(EupsThdFtpConfigRepository.class).findOne("BatchFileFtpNo");
-		Assert.isNotNull(config, ErrorCodes.EUPS_FTP_INFO_NOTEXIST,"代收付配置信息不存在");
-		config.setLocFleNme(fileName);
-		config.setRmtFleNme(fileName);
+		Assert.isFalse(null==config, ErrorCodes.EUPS_FTP_INFO_NOTEXIST,"代收付FTP配置信息不存在");
+		config.setLocFleNme(fleNme);
+		config.setRmtFleNme(fleNme);
+		config.setRmtWay(dir);
 		/** 产生代收付格式文件 */
-		((OperateFileAction)get("opeFile")).createCheckFile(config, GDConstants.BATCH_FILE_FORMAT, fileName, fileMap);
+		((OperateFileAction)get("opeFile")).createCheckFile(config, GDConstants.BATCH_FILE_FORMAT, fleNme, fileMap);
 		/** 发送到指定路径 */
 		((OperateFTPAction)get("opeFTP")).putCheckFile(config);
+		context.setData(ParamKeys.FLE_NME, fleNme);
+		context.setData(ParamKeys.TXN_MDE, Constants.TXN_MDE_FILE);
+		context.setData(ParamKeys.TOT_CNT, context.getData(ParamKeys.TOT_CNT));
+		context.setData(ParamKeys.TOT_AMT, context.getData(ParamKeys.TOT_AMT));
 	}
 	public void afterBatchProcess(Context context)throws CoreException{
 		final String batNo=ContextUtils.assertDataNotEmptyAndGet(context, ParamKeys.BAT_NO, ErrorCodes.EUPS_QUERY_NO_DATA);
