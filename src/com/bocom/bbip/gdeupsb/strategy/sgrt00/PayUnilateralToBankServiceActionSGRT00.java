eed7a97950c702ce92acf36c64dcd82f12def478
Li_Guilin <Li_Guilin@2ac714ae-9a16-11e4-ad83-2f06aba1930a>
2015-03-09 03:24:07 +0000

    git-svn-id: svn://182.53.32.18/bbip/project/GDEUPSB/TRUNK/GDEUPSB@16537 2ac714ae-9a16-11e4-ad83-2f06aba1930a

--- src/com/bocom/bbip/gdeupsb/strategy/sgrt00/PayUnilateralToBankServiceActionSGRT00.java
+++ src/com/bocom/bbip/gdeupsb/strategy/sgrt00/PayUnilateralToBankServiceActionSGRT00.java
@@ -1,10 +1,12 @@
 package com.bocom.bbip.gdeupsb.strategy.sgrt00;
 
+import java.util.HashMap;
 import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.CollectionUtils;
 
 import com.bocom.bbip.comp.BBIPPublicServiceImpl;
 import com.bocom.bbip.eups.common.BPState;
@@ -18,6 +20,7 @@ import com.bocom.bbip.gdeupsb.entity.GdTbcCusAgtInfo;
 import com.bocom.bbip.gdeupsb.repository.GdTbcCusAgtInfoRepository;
 import com.bocom.bbip.gdeupsb.utils.CodeSwitchUtils;
 import com.bocom.bbip.service.BGSPServiceAccessObject;
+import com.bocom.bbip.service.Result;
 import com.bocom.bbip.utils.DateUtils;
 import com.bocom.jump.bp.core.Context;
 import com.bocom.jump.bp.core.CoreException;
@@ -37,7 +40,7 @@ public class PayUnilateralToBankServiceActionSGRT00 implements PayUnilateralToBa
     public Map<String, Object> aftPayToBank(CommHeadDomain commheaddomain, PayFeeOnlineDomain payfeeonlinedomain,
             Context context) throws CoreException {
         context.setData(GDParamKeys.RSP_CDE, context.getData("responseCode"));
-        context.setData(GDParamKeys.RSP_MSG, Constants.RESPONSE_MSG);
+        context.setData(GDParamKeys.RSP_MSG, context.getData("thdRspMsg"));
         context.setState(BPState.BUSINESS_PROCESSNIG_STATE_NORMAL);
         context.setData("BANK_SEQ", context.getData("sqn"));
         return null;
@@ -54,8 +57,8 @@ public class PayUnilateralToBankServiceActionSGRT00 implements PayUnilateralToBa
             context.setDataMap(accessObject.getPayload());
         } else {
             throw new CoreException("用户未开户！");
-        }
-*/
+        }*/
+
         context.setData("payMod", "0");
         context.setData(ParamKeys.CHL_TYP, "L");// <!--交易渠道类型：L第三方系统-->
         context.setData("vchChk", "1");// <!--监督标志由业务上确定-->
@@ -85,20 +88,20 @@ public class PayUnilateralToBankServiceActionSGRT00 implements PayUnilateralToBa
         // TODO; 待确定
         context.setData("rsFld2", context.getData("dptId"));
         context.setData(ParamKeys.TXN_DATE, DateUtils.parse(context.getData("TRAN_TIME").toString().substring(0, 8), DateUtils.STYLE_yyyyMMdd));
-        // context.setData(ParamKeys.THD_SEQUENCE,context.getData("TRADE_ID"));
-        // context.setData(ParamKeys.CUS_AC, "6222620780003804748");//TODO
-        // context.setData(ParamKeys.TXN_AMT, context.getData("QTY_TRADE"));
-        // context.setData(ParamKeys.TELLER, context.getData("TELLER"));
-        // context.setData(ParamKeys.THD_CUS_NO, context.getData("CUST_ID"));
+        context.setData(ParamKeys.TXN_AMT, context.getData("QTY_TRADE"));
+       // context.setData(ParamKeys.TELLER, context.getData("TELLER"));
+        context.setData(ParamKeys.THD_CUS_NO, context.getData("CUST_ID"));
         GdTbcCusAgtInfo cusAgtInfo = cusAgtInfoRepository.findOne(context.getData("CUST_ID").toString());
         context.setData(ParamKeys.CUS_AC, cusAgtInfo.getActNo());
         context.setData(ParamKeys.THD_SEQUENCE, publicService.getBBIPSequence());
         // 测试
         context.setData(ParamKeys.BR, "01441131999");
-        context.setData(ParamKeys.BK, "01441999999");
+        // context.setData(ParamKeys.BK, "01441999999");
         context.setData(ParamKeys.TELLER, "ABIR148");
-        context.setData(ParamKeys.TXN_AMT, 1);
-        context.setData(ParamKeys.THD_CUS_NO, "123456789");
+        context.setData(ParamKeys.EUPS_BUSS_TYPE, "SGRT00");
+        //context.setData(ParamKeys.TXN_AMT, 1);
+       // context.setData(ParamKeys.THD_CUS_NO, "123456789");
+        
 
         context.setData(ParamKeys.EUPS_BUSS_TYPE, "SGRT00");
         context.setData(ParamKeys.THD_TXN_CDE, "483805");
