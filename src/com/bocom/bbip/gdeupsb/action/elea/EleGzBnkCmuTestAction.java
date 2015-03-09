11d23b97c794391e1b1259d375452bf6b7e645dc
Qian_Chen <Qian_Chen@2ac714ae-9a16-11e4-ad83-2f06aba1930a>
2015-03-09 11:28:22 +0000

    git-svn-id: svn://182.53.32.18/bbip/project/GDEUPSB/TRUNK/GDEUPSB@16845 2ac714ae-9a16-11e4-ad83-2f06aba1930a

--- src/com/bocom/bbip/gdeupsb/action/elea/EleGzBnkCmuTestAction.java
+++ src/com/bocom/bbip/gdeupsb/action/elea/EleGzBnkCmuTestAction.java
@@ -1,7 +1,6 @@
 package com.bocom.bbip.gdeupsb.action.elea;
 
 import java.util.Date;
-import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -32,37 +31,27 @@ public class EleGzBnkCmuTestAction extends BaseAction {
 
 	@Autowired
 	ThirdPartyAdaptor thirdPartyAdaptor;
-	
 
 	@Override
 	public void execute(Context context) throws CoreException, CoreRuntimeException {
 		log.info("EleGzBnkCmuTestAction start!..");
-		
+
 		String sqn = get(BBIPPublicService.class).getBBIPSequence();
 		String sqn1 = sqn.substring(0, 8);
 		String sqn2 = sqn.substring(sqn.length() - 4, sqn.length());
-		
+
 		context.setData(ParamKeys.MESSAGE_TYPE, "0820");
 		context.setData("txnDateTime7", DateUtils.format(new Date(), DateUtils.STYLE_MMddHHmmss)); // txnDateTime7
 		context.setData("transJournal11", sqn1 + sqn2); // transJournal11
 		context.setData("sndFlg33", "315810"); // 发送机构标识码
 		context.setData("cmuCde70", "301"); // 线路测试
+		context.setData("eupsBusTyp", "ELEC01");
 
-		// TODO: for test 为了测试，此处先设好固定值,实际中需要去掉try catch
-		try{
-			Map<String, Object> resultInfo = thirdPartyAdaptor.trade(context);
-		}catch(Exception e){
-			context.setState(BPState.BUSINESS_PROCESSNIG_STATE_NORMAL);
-			System.out.println("for test!....");
-		}
-		context.setState(BPState.BUSINESS_PROCESSNIG_STATE_NORMAL);
-		System.out.println("for test!....");
-		Map<String, Object> resultInfo = new HashMap<String, Object>();
-		resultInfo.put("thdRspCde", "00"); // 供电局系统:成功
+		Map<String, Object> resultInfo = thirdPartyAdaptor.trade(context);
+		
 		// 获取返回码
 		CommThdRspCdeAction rspCdeAction = new CommThdRspCdeAction();
 		String responseCode = rspCdeAction.getThdRspCde(resultInfo, context.getData(ParamKeys.EUPS_BUSS_TYPE).toString());
-		System.out.println("responseCode="+responseCode);
 		if (BPState.isBPStateOvertime(context)) {
 			throw new CoreException(ErrorCodes.TRANSACTION_ERROR_TIMEOUT);
 		} else if (!Constants.RESPONSE_CODE_SUCC.equals(responseCode)) {
@@ -72,7 +61,8 @@ public class EleGzBnkCmuTestAction extends BaseAction {
 			}
 			throw new CoreException(responseCode);
 		}
-
+		context.setData("responseCode", Constants.RESPONSE_CODE_SUCC);  //设置返回码为成功
+		log.info("action end,dataMap=" + context.getDataMap());
 	}
 
 }
