a96cc9b9e5c7982d46590d344260a8bd8145e3f1
Li_Haoxun <Li_Haoxun@2ac714ae-9a16-11e4-ad83-2f06aba1930a>
2015-03-10 00:53:19 +0000

    git-svn-id: svn://182.53.32.18/bbip/project/GDEUPSB/TRUNK/GDEUPSB@17074 2ac714ae-9a16-11e4-ad83-2f06aba1930a

--- src/com/bocom/bbip/gdeupsb/strategy/hscard/CheckBkFileToThirdStrategyAction.java
+++ src/com/bocom/bbip/gdeupsb/strategy/hscard/CheckBkFileToThirdStrategyAction.java
@@ -1,5 +1,6 @@
 package com.bocom.bbip.gdeupsb.strategy.hscard;
 
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -12,15 +13,15 @@ import org.springframework.util.CollectionUtils;
 import com.bocom.bbip.eups.action.common.OperateFTPAction;
 import com.bocom.bbip.eups.action.common.OperateFileAction;
 import com.bocom.bbip.eups.adaptor.ThirdPartyAdaptor;
-import com.bocom.bbip.eups.common.BPState;
 import com.bocom.bbip.eups.common.ErrorCodes;
 import com.bocom.bbip.eups.common.ParamKeys;
 import com.bocom.bbip.eups.entity.EupsThdFtpConfig;
-import com.bocom.bbip.eups.entity.EupsTransJournal;
+import com.bocom.bbip.gdeupsb.common.GDConstants;
+import com.bocom.bbip.gdeupsb.common.GDParamKeys;
 import com.bocom.bbip.gdeupsb.entity.GDEupsbTransJournal;
+import com.bocom.bbip.gdeupsb.entity.GdEupsTransJournal;
 import com.bocom.bbip.gdeupsb.repository.GdEupsTransJournalRepository;
 import com.bocom.bbip.utils.BeanUtils;
-import com.bocom.bbip.utils.DateUtils;
 import com.bocom.jump.bp.core.Context;
 import com.bocom.jump.bp.core.CoreException;
 import com.bocom.jump.bp.core.CoreRuntimeException;
@@ -29,7 +30,7 @@ import com.bocom.jump.bp.core.Executable;
 public class CheckBkFileToThirdStrategyAction implements Executable{
 	
 	@Autowired
-	GdEupsTransJournalRepository gdEupsbTransJournalRepository;
+	GdEupsTransJournalRepository gdEupsTransJournalRepository;
 	
 	@Autowired
 	OperateFTPAction operateFTPAction;
@@ -43,10 +44,16 @@ public class CheckBkFileToThirdStrategyAction implements Executable{
 	public void execute(Context context) throws CoreException,CoreRuntimeException{
 		logger.info("CheckBkFileToThirdStrategyAction start.......");
 		
-		EupsTransJournal eups = new EupsTransJournal();
-//		TODO:<Exec func="PUB:ReadRecord" error="IGNORE">
-//        <Arg name="SqlCmd" value="StatCplTxnJnl"/>
-//      </Exec>
+		GdEupsTransJournal eups = new GdEupsTransJournal();
+		eups.setThdTxnDte((Date)context.getData(GDParamKeys.TTXN_DT));
+		eups.setTxnSts(GDConstants.GAS_TXN_STS_S);
+		eups.setMfmTxnSts(GDConstants.GAS_TXN_STS_S);
+		List<Map<String, Object>> list= gdEupsTransJournalRepository.findSumTxnAmt(eups);
+		Map<String, Object> totMap = list.get(0);
+		context.setData("totNum", totMap.get("totNum"));
+		context.setData("totAmt", totMap.get("totAmt"));
+		System.out.println("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"+totMap.get("totNum")+totMap.get("totAmt"));
+		
 		String schId = context.getData(ParamKeys.BAK_FLD1);
 		String tTxnDt = context.getData(ParamKeys.THD_RCN_DATE);//此处的对账日期字段需确认。
 		String txt = ".txt";
@@ -60,7 +67,7 @@ public class CheckBkFileToThirdStrategyAction implements Executable{
 		
 		// 获取FTP信息,发送文件到指定路径
 		EupsThdFtpConfig eupsThdFtpConfig = context.getData(ParamKeys.CONSOLE_THD_FTP_CONFIG_LIST);
-//		TODO: String ChkDate = DateUtils.format((Date) context.getData(ParamKeys.CHECK_DATE), DateUtils.STYLE_yyyyMMdd);
+
 		
 		try{
 		//生成对账文件到指定路径
@@ -75,8 +82,6 @@ public class CheckBkFileToThirdStrategyAction implements Executable{
 		
 		operateFTPAction.putCheckFile(eupsThdFtpConfig);
 		 logger.info("对账文件FTP放置成功！");
-		 
-		 context.setState(BPState.BUSINESS_PROCESSNIG_STATE_NORMAL);
 	}
 	
 	/**
@@ -87,14 +92,14 @@ public class CheckBkFileToThirdStrategyAction implements Executable{
      */
 	private Map<String,Object> encodeFileMap(Context context) throws CoreException{
 		Map<String,Object> map = new HashMap<String, Object>();
-		String tTxnDt = context.getData(ParamKeys.THD_TXN_DATE);
+		
 		GDEupsbTransJournal gdeups = new GDEupsbTransJournal();
-		//TODO:时间不能写死
-		gdeups.setThdTxnDte(DateUtils.parse("2015-01-22"));
+		//TODO:时间需要改成当前日期前一天
+		gdeups.setThdTxnDte(new Date());
 		gdeups.setTxnSts("S");
 		gdeups.setMfmTxnSts("S");
 		
-		List<GDEupsbTransJournal> chkGDEupsTransJournal = gdEupsbTransJournalRepository.findCheck(gdeups);
+		List<GDEupsbTransJournal> chkGDEupsTransJournal = gdEupsTransJournalRepository.findCheck(gdeups);
 
 		if( CollectionUtils.isEmpty(chkGDEupsTransJournal)){
 			 logger.info("There are no records for select check trans journal ");
