550518dd24856138e3500b1d543d6d08f7604c78
Li_Haoxun <Li_Haoxun@2ac714ae-9a16-11e4-ad83-2f06aba1930a>
2015-03-10 06:29:21 +0000

    git-svn-id: svn://182.53.32.18/bbip/project/GDEUPSB/TRUNK/GDEUPSB@17291 2ac714ae-9a16-11e4-ad83-2f06aba1930a

--- src/com/bocom/bbip/gdeupsb/strategy/hscard/CheckBkFileToThirdStrategyAction.java
+++ src/com/bocom/bbip/gdeupsb/strategy/hscard/CheckBkFileToThirdStrategyAction.java
@@ -18,7 +18,6 @@ import com.bocom.bbip.eups.common.ParamKeys;
 import com.bocom.bbip.eups.entity.EupsThdFtpConfig;
 import com.bocom.bbip.gdeupsb.common.GDConstants;
 import com.bocom.bbip.gdeupsb.common.GDParamKeys;
-
 import com.bocom.bbip.gdeupsb.entity.GdEupsTransJournal;
 import com.bocom.bbip.gdeupsb.repository.GdEupsTransJournalRepository;
 import com.bocom.bbip.utils.BeanUtils;
@@ -26,7 +25,12 @@ import com.bocom.jump.bp.core.Context;
 import com.bocom.jump.bp.core.CoreException;
 import com.bocom.jump.bp.core.CoreRuntimeException;
 import com.bocom.jump.bp.core.Executable;
-
+/**
+ * 
+ * 
+ * @author lhx
+ *
+ */
 public class CheckBkFileToThirdStrategyAction implements Executable{
 	
 	@Autowired
@@ -48,16 +52,17 @@ public class CheckBkFileToThirdStrategyAction implements Executable{
 		eups.setThdTxnDte((Date)context.getData(GDParamKeys.TTXN_DT));
 		eups.setTxnSts(GDConstants.GAS_TXN_STS_S);
 		eups.setMfmTxnSts(GDConstants.GAS_TXN_STS_S);
+		eups.setEupsBusTyp((String)context.getData(ParamKeys.EUPS_BUSS_TYPE));
 		List<Map<String, Object>> list= gdEupsTransJournalRepository.findSumTxnAmt(eups);
 		Map<String, Object> totMap = list.get(0);
-		context.setData("totNum", totMap.get("totNum"));
-		context.setData("totAmt", totMap.get("totAmt"));
-		System.out.println("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"+totMap.get("totNum")+totMap.get("totAmt"));
+		System.out.println("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"+totMap);
+		context.setData("totNum", totMap.get("TOTNUM"));
+		context.setData("totAmt", totMap.get("TOTAMT"));
 		
-		String schId = context.getData(ParamKeys.BAK_FLD1);
-		String tTxnDt = context.getData(ParamKeys.THD_RCN_DATE);//此处的对账日期字段需确认。
-		String txt = ".txt";
-		context.setData(ParamKeys.LOCAL_FILE_NAME, schId + tTxnDt + txt);//文件名字段需确认。
+//		String schId = context.getData(ParamKeys.BAK_FLD1);
+//		String tTxnDt = context.getData(ParamKeys.THD_RCN_DATE);//此处的对账日期字段需确认。
+//		String txt = ".txt";
+//		context.setData(ParamKeys.LOCAL_FILE_NAME, schId + tTxnDt + txt);//文件名字段需确认。
 //		TODO:String datFil;//此字段不确定
 //		String path = "dat/cpla/";//不确定
 //		datFil = path + context.getData(ParamKeys.LOCAL_FILE_NAME).toString();
@@ -71,7 +76,7 @@ public class CheckBkFileToThirdStrategyAction implements Executable{
 		
 		try{
 		//生成对账文件到指定路径
-		operateFileAction.createCheckFile(eupsThdFtpConfig,"HSCardFile","HSCardFile",map);
+		operateFileAction.createCheckFile(eupsThdFtpConfig,"HSCardFile","HSCard.txt",map);
 		logger.info("对账文件已生成");
 		}catch(Exception e){
 			 logger.error("File create error : " + e.getMessage());
@@ -79,7 +84,7 @@ public class CheckBkFileToThirdStrategyAction implements Executable{
 		}
 		
 		//向指定FTP路径放文件
-		
+		eupsThdFtpConfig.setLocFleNme("HSCard.txt");
 		operateFTPAction.putCheckFile(eupsThdFtpConfig);
 		 logger.info("对账文件FTP放置成功！");
 	}
@@ -94,11 +99,11 @@ public class CheckBkFileToThirdStrategyAction implements Executable{
 		Map<String,Object> map = new HashMap<String, Object>();
 		
 		GdEupsTransJournal gdeups = new GdEupsTransJournal();
-		//TODO:时间需要改成当前日期前一天
-		gdeups.setThdTxnDte(new Date());
+		
+		gdeups.setThdTxnDte((Date)context.getData(GDParamKeys.TTXN_DT));
 		gdeups.setTxnSts("S");
 		gdeups.setMfmTxnSts("S");
-		
+		gdeups.setEupsBusTyp((String)context.getData(ParamKeys.EUPS_BUSS_TYPE));
 		List<GdEupsTransJournal> chkGDEupsTransJournal = gdEupsTransJournalRepository.findCheck(gdeups);
 
 		if( CollectionUtils.isEmpty(chkGDEupsTransJournal)){
