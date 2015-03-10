73f75cbdb7cc035a85507eb3b6b8b33dce75109d
Li_Haoxun <Li_Haoxun@2ac714ae-9a16-11e4-ad83-2f06aba1930a>
2015-03-10 11:20:23 +0000

    git-svn-id: svn://182.53.32.18/bbip/project/GDEUPSB/TRUNK/GDEUPSB@17582 2ac714ae-9a16-11e4-ad83-2f06aba1930a

--- src/com/bocom/bbip/gdeupsb/strategy/hscard/CheckBkFileToThirdStrategyAction.java
+++ src/com/bocom/bbip/gdeupsb/strategy/hscard/CheckBkFileToThirdStrategyAction.java
@@ -21,6 +21,7 @@ import com.bocom.bbip.gdeupsb.common.GDParamKeys;
 import com.bocom.bbip.gdeupsb.entity.GdEupsTransJournal;
 import com.bocom.bbip.gdeupsb.repository.GdEupsTransJournalRepository;
 import com.bocom.bbip.utils.BeanUtils;
+import com.bocom.bbip.utils.DateUtils;
 import com.bocom.jump.bp.core.Context;
 import com.bocom.jump.bp.core.CoreException;
 import com.bocom.jump.bp.core.CoreRuntimeException;
@@ -45,6 +46,7 @@ public class CheckBkFileToThirdStrategyAction implements Executable{
 	@Autowired
 	OperateFileAction operateFileAction;
 	public final static Log logger = LogFactory.getLog(CheckBkFileToThirdStrategyAction.class);
+	@SuppressWarnings("unchecked")
 	public void execute(Context context) throws CoreException,CoreRuntimeException{
 		logger.info("CheckBkFileToThirdStrategyAction start.......");
 		
@@ -55,28 +57,29 @@ public class CheckBkFileToThirdStrategyAction implements Executable{
 		eups.setEupsBusTyp((String)context.getData(ParamKeys.EUPS_BUSS_TYPE));
 		List<Map<String, Object>> list= gdEupsTransJournalRepository.findSumTxnAmt(eups);
 		Map<String, Object> totMap = list.get(0);
-		System.out.println("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"+totMap);
+		
 		context.setData("totNum", totMap.get("TOTNUM"));
 		context.setData("totAmt", totMap.get("TOTAMT"));
-		
-//		String schId = context.getData(ParamKeys.BAK_FLD1);
-//		String tTxnDt = context.getData(ParamKeys.THD_RCN_DATE);//此处的对账日期字段需确认。
-//		String txt = ".txt";
-//		context.setData(ParamKeys.LOCAL_FILE_NAME, schId + tTxnDt + txt);//文件名字段需确认。
-//		TODO:String datFil;//此字段不确定
-//		String path = "dat/cpla/";//不确定
-//		datFil = path + context.getData(ParamKeys.LOCAL_FILE_NAME).toString();
 		
 		//拼装对账文件map
 		Map<String, Object> map= encodeFileMap(context);
 		
 		// 获取FTP信息,发送文件到指定路径
 		EupsThdFtpConfig eupsThdFtpConfig = context.getData(ParamKeys.CONSOLE_THD_FTP_CONFIG_LIST);
-
+		
+		List<Map<String, Object>> detailList = (List<Map<String, Object>>)map.get(ParamKeys.EUPS_FILE_DETAIL);
+		for(int i=0;i<detailList.size();i++){
+			detailList.get(i).put(ParamKeys.THD_TXN_DATE, (String)DateUtils.format((Date)detailList.get(i).get(ParamKeys.THD_TXN_DATE), DateUtils.STYLE_yyyyMMdd));
+		}
+		map.put(ParamKeys.EUPS_FILE_DETAIL, detailList);
+		
+		//设置文件名
+		String date = DateUtils.format((Date)context.getData(GDParamKeys.TTXN_DT), DateUtils.STYLE_yyyyMMdd);
+		String fileName = (String)context.getData(ParamKeys.BAK_FLD1) + date + ".txt";
 		
 		try{
 		//生成对账文件到指定路径
-		operateFileAction.createCheckFile(eupsThdFtpConfig,"HSCardFile","HSCard.txt",map);
+		operateFileAction.createCheckFile(eupsThdFtpConfig,"HSCardFile",fileName,map);
 		logger.info("对账文件已生成");
 		}catch(Exception e){
 			 logger.error("File create error : " + e.getMessage());
@@ -84,7 +87,7 @@ public class CheckBkFileToThirdStrategyAction implements Executable{
 		}
 		
 		//向指定FTP路径放文件
-		eupsThdFtpConfig.setLocFleNme("HSCard.txt");
+		eupsThdFtpConfig.setLocFleNme(fileName);
 		operateFTPAction.putCheckFile(eupsThdFtpConfig);
 		 logger.info("对账文件FTP放置成功！");
 	}
