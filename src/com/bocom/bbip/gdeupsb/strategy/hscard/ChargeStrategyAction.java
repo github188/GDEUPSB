242e0ecd20349edd860296cd2d123334ac387345
Li_Haoxun <Li_Haoxun@2ac714ae-9a16-11e4-ad83-2f06aba1930a>
2015-03-10 13:32:07 +0000

    git-svn-id: svn://182.53.32.18/bbip/project/GDEUPSB/TRUNK/GDEUPSB@17780 2ac714ae-9a16-11e4-ad83-2f06aba1930a

--- src/com/bocom/bbip/gdeupsb/strategy/hscard/ChargeStrategyAction.java
+++ src/com/bocom/bbip/gdeupsb/strategy/hscard/ChargeStrategyAction.java
@@ -45,8 +45,7 @@ public class ChargeStrategyAction implements Executable{
 //		      <Set>TxnTim=GETDATETIME(YYYYMMDDHHMISS)</Set>
 		System.out.println("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"+context);
 		Date tTxnDte=DateUtils.parse(context.getData("tTxnDte").toString(), DateUtils.STYLE_yyyyMMdd);//第三方交易日期
-		String thdCusNo = context.getData(GDParamKeys.TML_NO);
-		context.setData(ParamKeys.THD_CUS_NO, thdCusNo);
+		
 		context.setData(ParamKeys.THD_TXN_DATE, tTxnDte);
 //		TODO:现在先设置一个柜员号，以后需要删除。
 		context.setData(ParamKeys.TELLER, "0007");
