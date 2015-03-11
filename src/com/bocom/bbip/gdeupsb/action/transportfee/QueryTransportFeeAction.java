b489ea4ba75e5d67eb16817440148f65228df2b6
Li_Haoxun <Li_Haoxun@2ac714ae-9a16-11e4-ad83-2f06aba1930a>
2015-03-11 11:28:00 +0000

    git-svn-id: svn://182.53.32.18/bbip/project/GDEUPSB/TRUNK/GDEUPSB@18344 2ac714ae-9a16-11e4-ad83-2f06aba1930a

--- src/com/bocom/bbip/gdeupsb/action/transportfee/QueryTransportFeeAction.java
+++ src/com/bocom/bbip/gdeupsb/action/transportfee/QueryTransportFeeAction.java
@@ -39,21 +39,25 @@ public class QueryTransportFeeAction extends BaseAction{
 		ctx.setData(ParamKeys.THD_TXN_CDE, GDConstants.QRY_CAR);
 		ctx.setData("transCode", "484001");
 		ctx.setData(GDParamKeys.ACT_DAT, new Date());
-//		<Set>TLogNo=$LogNo</Set>
-
+		ctx.setData(GDParamKeys.TACT_DT, new Date());
+		ctx.setData(GDParamKeys.TLOG_NO, ctx.getData(GDParamKeys.SQN));
 		ctx.setData(GDParamKeys.THD_KEY, ctx.getData(GDParamKeys.SQN));
-//		TODO:<Arg name="ObjSvr" value="@PARA.ThdSvr"/>
+
 		Map<String,Object> thdReturnMessage = callThdTradeManager.trade(ctx);
 		logger.info("call third start....[the state is" + ctx.getState() + "]");
 		if(ctx.getState().equals(BPState.BUSINESS_PROCESSNIG_STATE_OVERTIME)){
 			ctx.setData(ParamKeys.RSP_MSG, "查询路桥方交易超时");
+			//TODO: <Set>RspCod=RBF999</Set>
 			throw new CoreRuntimeException(ErrorCodes.TRANSACTION_ERROR_TIMEOUT);
 		}else if(ctx.getState().equals(BPState.BUSINESS_PROCESSNIG_STATE_FAIL)){
 			ctx.setData(ParamKeys.RSP_MSG, "查询路桥方交易失败");
+			//TODO: <Set>RspCod=RBF999</Set>
 			throw new CoreRuntimeException(ErrorCodes.TRANSACTION_ERROR_OTHER_ERROR);
 		}else{
 			if(!"000".equals(thdReturnMessage.get(GDParamKeys.TRSP_CD))){
 				ctx.setData(ParamKeys.RSP_MSG, "查询路桥方返回错误");
+				//TODO:<Set>RspMsg=STRCAT(路桥方返回: [,$TRspCd,],$TRspMsg)</Set>
+				//TODO: <Set>RspCod=RBF999</Set>
 				throw new CoreRuntimeException(ErrorCodes.EUPS_FAIL);
 			}else{
 
@@ -62,14 +66,15 @@ public class QueryTransportFeeAction extends BaseAction{
 				gdEupsbTrspPayInfo.setCarTyp(ctx.getData(GDParamKeys.CAR_TYP).toString());
 				gdEupsbTrspPayInfo.setCarNo(ctx.getData(GDParamKeys.CAR_NO).toString());
 				gdEupsbTrspPayInfoRepository.delete(gdEupsbTrspPayInfo);
-
+				
 				ctx.setDataMap(thdReturnMessage);
-				gdEupsbTrspPayInfo.setPayMon(thdReturnMessage.get(GDParamKeys.PAY_MON).toString());
-				gdEupsbTrspPayInfo.setTcusNm(thdReturnMessage.get(GDParamKeys.TCUS_NM).toString());
+				System.out.println("@@@@@@@@@@@@@@@@@@@@@@@@@@@@"+ctx);
+				gdEupsbTrspPayInfo.setPayMon(ctx.getData(GDParamKeys.PAY_MON).toString());
+				gdEupsbTrspPayInfo.setTcusNm(ctx.getData(GDParamKeys.TCUS_NM).toString());
 				gdEupsbTrspPayInfo.setActDat((Date)ctx.getData(GDParamKeys.ACT_DAT));
 				gdEupsbTrspPayInfo.setThdKey(ctx.getData(GDParamKeys.THD_KEY).toString());
-				gdEupsbTrspPayInfo.setTactDt(DateUtils.parse(thdReturnMessage.get(GDParamKeys.TACT_DT).toString()));
-				gdEupsbTrspPayInfo.setTxnAmt(new BigDecimal(thdReturnMessage.get(ParamKeys.TXN_AMOUNT).toString()));
+				gdEupsbTrspPayInfo.setTactDt(DateUtils.parse((String)ctx.getData(GDParamKeys.TACT_DT), DateUtils.STYLE_yyyyMMdd));
+				gdEupsbTrspPayInfo.setTxnAmt(new BigDecimal(ctx.getData(ParamKeys.TXN_AMOUNT).toString()));
 				gdEupsbTrspPayInfoRepository.insert(gdEupsbTrspPayInfo);
 			}
 		}
