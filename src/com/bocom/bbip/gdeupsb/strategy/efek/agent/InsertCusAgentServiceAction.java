06c1f9b737caab0bb5ee3d629c76aa2ae07d092d
Li_Yawei <Li_Yawei@2ac714ae-9a16-11e4-ad83-2f06aba1930a>
2015-03-14 09:03:04 +0000

    git-svn-id: svn://182.53.32.18/bbip/project/GDEUPSB/TRUNK/GDEUPSB@20069 2ac714ae-9a16-11e4-ad83-2f06aba1930a

--- src/com/bocom/bbip/gdeupsb/strategy/efek/agent/InsertCusAgentServiceAction.java
+++ src/com/bocom/bbip/gdeupsb/strategy/efek/agent/InsertCusAgentServiceAction.java
@@ -1,6 +1,5 @@
 package com.bocom.bbip.gdeupsb.strategy.efek.agent;
 
-import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
@@ -15,7 +14,6 @@ import com.bocom.bbip.eups.common.BPState;
 import com.bocom.bbip.eups.common.Constants;
 import com.bocom.bbip.eups.common.ErrorCodes;
 import com.bocom.bbip.eups.common.ParamKeys;
-import com.bocom.bbip.eups.entity.EupsCusAgentJournal;
 import com.bocom.bbip.eups.repository.EupsCusAgentJournalRepository;
 import com.bocom.bbip.eups.repository.EupsTransJournalRepository;
 import com.bocom.bbip.eups.spi.service.agent.CommInsertCusAgentService;
@@ -23,7 +21,6 @@ import com.bocom.bbip.eups.spi.vo.CusAgentCollectDomain;
 import com.bocom.bbip.eups.spi.vo.CustomerDomain;
 import com.bocom.bbip.gdeupsb.common.GDConstants;
 import com.bocom.bbip.gdeupsb.common.GDParamKeys;
-import com.bocom.bbip.utils.DateUtils;
 import com.bocom.bbip.utils.StringUtils;
 import com.bocom.jump.bp.core.Context;
 import com.bocom.jump.bp.core.CoreException;
@@ -118,7 +115,6 @@ public class InsertCusAgentServiceAction implements CommInsertCusAgentService{
 				List<CusAgentCollectDomain> list, Context context)
 				throws CoreException {
 		logger.info("=============Start   InsertCusAgentServiceAction  preInsertCusAgent");
-		constantOfSoapUI(context);
 		logger.info("=============End    InsertCusAgentServiceAction  preInsertCusAgent");
 		return null;
 		}
@@ -130,29 +126,4 @@ public class InsertCusAgentServiceAction implements CommInsertCusAgentService{
 		logger.info("=============End     InsertCusAgentServiceAction  reverseThd");
     	return null;
     }
-    /**
-	 *报文信息 
-	*/
-		public void constantOfSoapUI(Context context){
-			//报文头常量
-			context.setData(GDParamKeys.TREATY_VERSION, GDConstants.TREATY_VERSION);//协议版本
-			context.setData(GDParamKeys.TRADE_PERSON_IDENTIFY, GDConstants.TRADE_PERSON_IDENTIFY);//交易人标识
-			context.setData(GDParamKeys.BAG_TYPE, GDConstants.BAG_TYPE);//数据包类型
-			context.setData(GDParamKeys.TRADE_START,GDConstants.TRADE_START);//交易发起方
-			
-			context.setData(GDParamKeys.TRADE_SEND_DATE,DateUtils.format(new Date(),DateUtils.STYLE_yyyyMMdd));//交易发送日期
-			context.setData(GDParamKeys.TRADE_SEND_TIME, DateUtils.formatAsHHmmss(new Date()));//交易发送时间
-			context.setData(GDParamKeys.TRADE_PRIORITY, GDConstants.TRADE_PRIORITY);//交易优先
-			context.setData(GDParamKeys.REDUCE_SIGN, GDConstants.REDUCE_SIGN);//压缩标志
-			context.setData(GDParamKeys.TRADE_RETURN_CODE, GDConstants.TRADE_RETURN_CODE);//交易返回代码
-
-			
-			context.setData(GDParamKeys.NET_NAME, GDConstants.NET_NAME);//网点名称
-			context.setData(GDParamKeys.SECRETKEY_INDEX, GDConstants.SECRETKEY_INDEX);//密钥索引
-			context.setData(GDParamKeys.SECRETKEY_INIT, GDConstants.SECRETKEY_INIT);//密钥初始向量
-			context.setData(GDParamKeys.TRADE_RECEIVE, GDConstants.TRADE_RECEIVE);//交易接收方
-			context.setData(GDParamKeys.TRADE_SOURCE_ADD, GDConstants.TRADE_SOURCE_ADD);//交易源地址
-			context.setData(GDParamKeys.TRADE_AIM_ADD, GDConstants.TRADE_AIM_ADD);//交易目标地址
-
-		}
 }
