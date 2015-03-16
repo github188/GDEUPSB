72978db3a71d7658e1579b378bc87861a6ffa06c
Wu_Yanhui <Wu_Yanhui@2ac714ae-9a16-11e4-ad83-2f06aba1930a>
2015-03-16 01:09:43 +0000

    git-svn-id: svn://182.53.32.18/bbip/project/GDEUPSB/TRUNK/GDEUPSB@20256 2ac714ae-9a16-11e4-ad83-2f06aba1930a

---
+++ src/com/bocom/bbip/gdeupsb/action/elec02/CancelUnilateralToBankServiceImplELEC02.java
@@ -0,0 +1,47 @@
+package com.bocom.bbip.gdeupsb.action.elec02;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import com.bocom.bbip.eups.action.BaseAction;
+import com.bocom.bbip.eups.common.ErrorCodes;
+import com.bocom.bbip.eups.common.ParamKeys;
+import com.bocom.bbip.eups.entity.EupsTransJournal;
+import com.bocom.bbip.eups.repository.EupsTransJournalRepository;
+import com.bocom.bbip.eups.spi.service.single.CancelUnilateralToBankService;
+import com.bocom.bbip.eups.spi.vo.CancelDomain;
+import com.bocom.bbip.eups.spi.vo.CommHeadDomain;
+import com.bocom.bbip.utils.Assert;
+import com.bocom.jump.bp.core.Context;
+import com.bocom.jump.bp.core.CoreException;
+
+public class CancelUnilateralToBankServiceImplELEC02 extends BaseAction implements
+		CancelUnilateralToBankService {
+	private static final Log logger = LogFactory.getLog(CancelUnilateralToBankServiceImplELEC02.class);
+
+	@Override
+	public Map<String, Object> aftCclToBank(CommHeadDomain arg0,
+			CancelDomain arg1, Context context) throws CoreException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Map<String, Object> preCclToBank(CommHeadDomain arg0,
+			CancelDomain arg1, Context context) throws CoreException {
+		context.setData(ParamKeys.TXN_DTE, new Date());
+		//context.setData(ParamKeys.OLD_TXN_SEQUENCE, "20150305000000004303");
+		EupsTransJournal journal=new EupsTransJournal();
+		//journal.setThdSqn((String)context.getData(ParamKeys.THD_SEQUENCE));
+		//List<EupsTransJournal>list=get(EupsTransJournalRepository.class).find(journal);
+		//Assert.isNotEmpty(list, ErrorCodes.EUPS_QUERY_NO_DATA);
+		context.setData("txnAmt",8);
+		context.setData(ParamKeys.OLD_TXN_SEQUENCE, "20150309000000010010");
+		return null;
+	}
+
+}
