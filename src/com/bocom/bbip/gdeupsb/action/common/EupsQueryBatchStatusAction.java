a1bc6fa3d680c1390304aa235762d735a60f17e2
Wu_Yanhui <Wu_Yanhui@2ac714ae-9a16-11e4-ad83-2f06aba1930a>
2015-03-16 09:11:06 +0000

    git-svn-id: svn://182.53.32.18/bbip/project/GDEUPSB/TRUNK/GDEUPSB@20668 2ac714ae-9a16-11e4-ad83-2f06aba1930a

--- src/com/bocom/bbip/gdeupsb/action/common/EupsQueryBatchStatusAction.java
+++ src/com/bocom/bbip/gdeupsb/action/common/EupsQueryBatchStatusAction.java
@@ -1,5 +1,7 @@
 package com.bocom.bbip.gdeupsb.action.common;
 
+import java.util.List;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
@@ -10,6 +12,7 @@ import com.bocom.bbip.gdeupsb.entity.GDEupsBatchConsoleInfo;
 import com.bocom.bbip.gdeupsb.repository.GDEupsBatchConsoleInfoRepository;
 import com.bocom.bbip.utils.Assert;
 import com.bocom.bbip.utils.BeanUtils;
+import com.bocom.bbip.utils.ContextUtils;
 import com.bocom.jump.bp.core.Context;
 import com.bocom.jump.bp.core.CoreException;
 
@@ -17,15 +20,16 @@ public class EupsQueryBatchStatusAction extends BaseAction {
 	private static final Log logger=LogFactory.getLog(EupsQueryBatchStatusAction.class);
 	
 	public void execute(Context context)throws CoreException{
-		final String batNo=context.getData(ParamKeys.BAT_NO);
+		logger.info("----批次信息查询开始----");
+		final String batNo=ContextUtils.assertDataNotEmptyAndGet(context, "batNo", ErrorCodes.EUPS_FIELD_EMPTY, "批次号");
 		final String eupsBusTyp=context.getData(ParamKeys.EUPS_BUSS_TYPE);
 		GDEupsBatchConsoleInfo info = new GDEupsBatchConsoleInfo();
 		info.setBatNo(batNo);
 		info.setBusKnd(eupsBusTyp);
-		GDEupsBatchConsoleInfo ret = get(GDEupsBatchConsoleInfoRepository.class).findConsoleInfo(info);
-		Assert.isNotNull(ret, ErrorCodes.EUPS_BAT_CTL_INFO_NOT_EXIST);
-		logger.info("批次信息:"+BeanUtils.toFlatMap(ret));
-		context.getDataMapDirectly().putAll(BeanUtils.toFlatMap(ret));
+		List<GDEupsBatchConsoleInfo> ret = get(GDEupsBatchConsoleInfoRepository.class).find(info);
+		Assert.isNotEmpty(ret, ErrorCodes.EUPS_BAT_CTL_INFO_NOT_EXIST);
+		logger.info("批次信息:"+BeanUtils.toFlatMap(ret.get(0)));
+		context.getDataMapDirectly().putAll(BeanUtils.toFlatMap(ret.get(0)));
 	}
 
 }
