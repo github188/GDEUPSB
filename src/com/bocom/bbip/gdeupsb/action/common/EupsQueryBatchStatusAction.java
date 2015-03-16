dbdd6bd860211afe882a272abbb625e43e33c22a
Wu_Yanhui <Wu_Yanhui@2ac714ae-9a16-11e4-ad83-2f06aba1930a>
2015-03-16 01:05:14 +0000

    git-svn-id: svn://182.53.32.18/bbip/project/GDEUPSB/TRUNK/GDEUPSB@20227 2ac714ae-9a16-11e4-ad83-2f06aba1930a

---
+++ src/com/bocom/bbip/gdeupsb/action/common/EupsQueryBatchStatusAction.java
@@ -0,0 +1,31 @@
+package com.bocom.bbip.gdeupsb.action.common;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import com.bocom.bbip.eups.action.BaseAction;
+import com.bocom.bbip.eups.common.ErrorCodes;
+import com.bocom.bbip.eups.common.ParamKeys;
+import com.bocom.bbip.gdeupsb.entity.GDEupsBatchConsoleInfo;
+import com.bocom.bbip.gdeupsb.repository.GDEupsBatchConsoleInfoRepository;
+import com.bocom.bbip.utils.Assert;
+import com.bocom.bbip.utils.BeanUtils;
+import com.bocom.jump.bp.core.Context;
+import com.bocom.jump.bp.core.CoreException;
+
+public class EupsQueryBatchStatusAction extends BaseAction {
+	private static final Log logger=LogFactory.getLog(EupsQueryBatchStatusAction.class);
+	
+	public void execute(Context context)throws CoreException{
+		final String batNo=context.getData(ParamKeys.BAT_NO);
+		final String eupsBusTyp=context.getData(ParamKeys.EUPS_BUSS_TYPE);
+		GDEupsBatchConsoleInfo info = new GDEupsBatchConsoleInfo();
+		info.setBatNo(batNo);
+		info.setBusKnd(eupsBusTyp);
+		GDEupsBatchConsoleInfo ret = get(GDEupsBatchConsoleInfoRepository.class).findConsoleInfo(info);
+		Assert.isNotNull(ret, ErrorCodes.EUPS_BAT_CTL_INFO_NOT_EXIST);
+		logger.info("批次信息:"+BeanUtils.toFlatMap(ret));
+		context.getDataMapDirectly().putAll(BeanUtils.toFlatMap(ret));
+	}
+
+}
