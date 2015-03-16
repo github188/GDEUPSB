df68342b1ba998e8e02cb63fe9b8cd5b6569d2b0
Wu_Yanhui <Wu_Yanhui@2ac714ae-9a16-11e4-ad83-2f06aba1930a>
2015-03-16 01:12:42 +0000

    git-svn-id: svn://182.53.32.18/bbip/project/GDEUPSB/TRUNK/GDEUPSB@20271 2ac714ae-9a16-11e4-ad83-2f06aba1930a

--- src/com/bocom/bbip/gdeupsb/repository/GdLotPrzDtlRepository.java
+++ src/com/bocom/bbip/gdeupsb/repository/GdLotPrzDtlRepository.java
@@ -1,7 +1,11 @@
 package com.bocom.bbip.gdeupsb.repository;
 
+import com.bocom.bbip.data.annotation.Delete;
 import com.bocom.bbip.data.repository.PagingAndSortingRepository;
 import com.bocom.bbip.gdeupsb.entity.GdLotPrzDtl;
 
 public interface GdLotPrzDtlRepository extends PagingAndSortingRepository<GdLotPrzDtl, String> {
+
+@Delete
+public void deleteByGameId(GdLotPrzDtl dtl);
 }
