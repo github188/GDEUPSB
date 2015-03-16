d4949cad5f79a064ed1809103932577f61a641c5
Wang_Mingqin <Wang_Mingqin@2ac714ae-9a16-11e4-ad83-2f06aba1930a>
2015-03-16 01:14:48 +0000

    git-svn-id: svn://182.53.32.18/bbip/project/GDEUPSB/TRUNK/GDEUPSB@20277 2ac714ae-9a16-11e4-ad83-2f06aba1930a

--- src/com/bocom/bbip/gdeupsb/repository/GDVechIndentInfoRepository.java
+++ src/com/bocom/bbip/gdeupsb/repository/GDVechIndentInfoRepository.java
@@ -1,7 +1,11 @@
 package com.bocom.bbip.gdeupsb.repository;
 
+import com.bocom.bbip.data.annotation.Update;
 import com.bocom.bbip.data.repository.PagingAndSortingRepository;
 import com.bocom.bbip.gdeupsb.entity.GDVechIndentInfo;
 
 public interface GDVechIndentInfoRepository extends PagingAndSortingRepository<GDVechIndentInfo, String> {
+	
+	@Update
+	public void update(GDVechIndentInfo updateInfo);
 }
