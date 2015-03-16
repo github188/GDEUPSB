682e376cb7388109497d29100a0463adacad027b
Wu_Yanhui <Wu_Yanhui@2ac714ae-9a16-11e4-ad83-2f06aba1930a>
2015-03-16 01:11:36 +0000

    git-svn-id: svn://182.53.32.18/bbip/project/GDEUPSB/TRUNK/GDEUPSB@20263 2ac714ae-9a16-11e4-ad83-2f06aba1930a

---
+++ src/com/bocom/bbip/gdeupsb/repository/GDEupsbElecstBatchTmpRepository.java
@@ -0,0 +1,14 @@
+package com.bocom.bbip.gdeupsb.repository;
+
+import java.util.List;
+
+import com.bocom.bbip.data.annotation.Find;
+import com.bocom.bbip.data.repository.PagingAndSortingRepository;
+import com.bocom.bbip.gdeupsb.entity.GDEupsZhAGBatchTemp;
+import com.bocom.bbip.gdeupsb.entity.GDEupsbElecstBatchTmp;
+
+public interface GDEupsbElecstBatchTmpRepository extends PagingAndSortingRepository<GDEupsbElecstBatchTmp, Integer> {
+
+	@Find
+	public List<GDEupsbElecstBatchTmp> findByBatNo(final String BatNo);
+}
