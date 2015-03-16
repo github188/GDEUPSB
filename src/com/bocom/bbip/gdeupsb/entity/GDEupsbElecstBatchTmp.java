d8f42c97ee4358752a37eae1e20e1cc0fe37555a
Wu_Yanhui <Wu_Yanhui@2ac714ae-9a16-11e4-ad83-2f06aba1930a>
2015-03-16 01:10:48 +0000

    git-svn-id: svn://182.53.32.18/bbip/project/GDEUPSB/TRUNK/GDEUPSB@20260 2ac714ae-9a16-11e4-ad83-2f06aba1930a

---
+++ src/com/bocom/bbip/gdeupsb/entity/GDEupsbElecstBatchTmp.java
@@ -0,0 +1,279 @@
+package com.bocom.bbip.gdeupsb.entity;
+
+import com.bocom.bbip.data.annotation.GeneratedValue;
+import com.bocom.bbip.data.annotation.Id;
+import java.util.Date;
+
+public class GDEupsbElecstBatchTmp {
+    @Id
+    @GeneratedValue
+    private Integer sqn;
+
+    private String batNo;
+
+    private String comNo;
+
+    private String txnTlr;
+
+    private Date subDte;
+
+    private String cusAc;
+
+    private String cusNme;
+
+    private String txnAmt;
+
+    private String thdCusNo;
+
+    private String thdCusNme;
+
+    private String obkBr;
+
+    private String rsvFld1;
+
+    private String rsvFld2;
+
+    private String rsvFld3;
+
+    private String rsvFld4;
+
+    private String rsvFld5;
+
+    private String rsvFld6;
+
+    private String rsvFld7;
+
+    private String rsvFld8;
+
+    private String rsvFld9;
+
+    private String rsvFld10;
+
+    private String rsvFld11;
+
+    private String rsvFld12;
+
+    private String rsvFld13;
+
+    private String rsvFld14;
+
+    private String rsvFld15;
+
+    private String rsvFld16;
+
+    public Integer getSqn() {
+        return sqn;
+    }
+
+    public void setSqn(Integer sqn) {
+        this.sqn = sqn;
+    }
+
+    public String getBatNo() {
+        return batNo;
+    }
+
+    public void setBatNo(String batNo) {
+        this.batNo = batNo;
+    }
+
+    public String getComNo() {
+        return comNo=comNo==null?"":comNo;
+    }
+
+    public void setComNo(String comNo) {
+        this.comNo = comNo;
+    }
+
+    public String getTxnTlr() {
+        return txnTlr=txnTlr==null?"":txnTlr;
+    }
+
+    public void setTxnTlr(String txnTlr) {
+        this.txnTlr = txnTlr;
+    }
+
+    public Date getSubDte() {
+        return subDte=subDte==null?new Date():subDte;
+    }
+
+    public void setSubDte(Date subDte) {
+        this.subDte = subDte;
+    }
+
+    public String getCusAc() {
+        return cusAc=cusAc==null?"":cusAc;
+    }
+
+    public void setCusAc(String cusAc) {
+        this.cusAc = cusAc;
+    }
+
+    public String getCusNme() {
+        return cusNme=cusNme==null?"":cusNme;
+    }
+
+    public void setCusNme(String cusNme) {
+        this.cusNme = cusNme;
+    }
+
+    public String getTxnAmt() {
+        return txnAmt=txnAmt==null?"":txnAmt;
+    }
+
+    public void setTxnAmt(String txnAmt) {
+        this.txnAmt = txnAmt;
+    }
+
+    public String getThdCusNo() {
+        return thdCusNo=thdCusNo==null?"":thdCusNo;
+    }
+
+    public void setThdCusNo(String thdCusNo) {
+        this.thdCusNo = thdCusNo;
+    }
+
+    public String getThdCusNme() {
+        return thdCusNme=thdCusNme==null?"":thdCusNme;
+    }
+
+    public void setThdCusNme(String thdCusNme) {
+        this.thdCusNme = thdCusNme;
+    }
+
+    public String getObkBr() {
+        return obkBr=obkBr==null?"":obkBr;
+    }
+
+    public void setObkBr(String obkBr) {
+        this.obkBr = obkBr;
+    }
+
+    public String getRsvFld1() {
+        return rsvFld1=rsvFld1==null?"":rsvFld1;
+    }
+
+    public void setRsvFld1(String rsvFld1) {
+        this.rsvFld1 = rsvFld1;
+    }
+
+    public String getRsvFld2() {
+    	 return rsvFld2=rsvFld2==null?"":rsvFld2;
+    }
+
+    public void setRsvFld2(String rsvFld2) {
+        this.rsvFld2 = rsvFld2;
+    }
+
+    public String getRsvFld3() {
+    	 return rsvFld3=rsvFld3==null?"":rsvFld3;
+    }
+
+    public void setRsvFld3(String rsvFld3) {
+        this.rsvFld3 = rsvFld3;
+    }
+
+    public String getRsvFld4() {
+         return rsvFld4=rsvFld4==null?"":rsvFld4;
+    }
+
+    public void setRsvFld4(String rsvFld4) {
+        this.rsvFld4 = rsvFld4;
+    }
+
+    public String getRsvFld5() {
+    	 return rsvFld5=rsvFld5==null?"":rsvFld5;
+    }
+
+    public void setRsvFld5(String rsvFld5) {
+        this.rsvFld5 = rsvFld5;
+    }
+
+    public String getRsvFld6() {
+    	 return rsvFld6=rsvFld6==null?"":rsvFld6;
+    }
+
+    public void setRsvFld6(String rsvFld6) {
+        this.rsvFld6 = rsvFld6;
+    }
+
+    public String getRsvFld7() {
+    	 return rsvFld7=rsvFld7==null?"":rsvFld7;
+    }
+
+    public void setRsvFld7(String rsvFld7) {
+        this.rsvFld7 = rsvFld7;
+    }
+
+    public String getRsvFld8() {
+    	 return rsvFld8=rsvFld8==null?"":rsvFld8;
+    }
+
+    public void setRsvFld8(String rsvFld8) {
+        this.rsvFld8 = rsvFld8;
+    }
+
+    public String getRsvFld9() {
+    	 return rsvFld9=rsvFld9==null?"":rsvFld9;
+    }
+
+    public void setRsvFld9(String rsvFld9) {
+        this.rsvFld9 = rsvFld9;
+    }
+
+    public String getRsvFld10() {
+    	 return rsvFld10=rsvFld10==null?"":rsvFld10;
+    }
+
+    public void setRsvFld10(String rsvFld10) {
+        this.rsvFld10 = rsvFld10;
+    }
+
+    public String getRsvFld11() {
+    	 return rsvFld11=rsvFld11==null?"":rsvFld11;
+    }
+
+    public void setRsvFld11(String rsvFld11) {
+        this.rsvFld11 = rsvFld11;
+    }
+
+    public String getRsvFld12() {
+          return rsvFld12=rsvFld12==null?"":rsvFld12;
+    }
+
+    public void setRsvFld12(String rsvFld12) {
+        this.rsvFld12 = rsvFld12;
+    }
+
+    public String getRsvFld13() {
+    	 return rsvFld13=rsvFld13==null?"":rsvFld13;
+    }
+
+    public void setRsvFld13(String rsvFld13) {
+        this.rsvFld13 = rsvFld13;
+    }
+
+    public String getRsvFld14() {
+    	 return rsvFld14=rsvFld14==null?"":rsvFld14;
+    }
+
+    public void setRsvFld14(String rsvFld14) {
+        this.rsvFld14 = rsvFld14;
+    }
+
+    public String getRsvFld15() {
+    	 return rsvFld15=rsvFld15==null?"":rsvFld15;
+    }
+
+    public void setRsvFld15(String rsvFld15) {
+        this.rsvFld15 = rsvFld15;
+    }
+
+    public String getRsvFld16() {
+    	 return rsvFld16=rsvFld16==null?"":rsvFld16;
+    }
+
+    public void setRsvFld16(String rsvFld16) {
+        this.rsvFld16 = rsvFld16;
+    }
+}
