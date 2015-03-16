6a2fb56013d1b570612a590236da98cc8e72c547
Wu_Yanhui <Wu_Yanhui@2ac714ae-9a16-11e4-ad83-2f06aba1930a>
2015-03-16 01:37:03 +0000

    git-svn-id: svn://182.53.32.18/bbip/project/GDEUPSB/TRUNK/GDEUPSB@20357 2ac714ae-9a16-11e4-ad83-2f06aba1930a

--- src/com/bocom/bbip/gdeupsb/action/lot/DeductMoneyAction.java
+++ src/com/bocom/bbip/gdeupsb/action/lot/DeductMoneyAction.java
@@ -53,13 +53,13 @@ public class DeductMoneyAction extends BaseAction {
         CommonLotAction commAction = new CommonLotAction();
         if (CollectionUtils.isEmpty(lotChkCtls)) {
             //无信息则下载对账信息 
-            Map <String, String> map = commAction.downloadFile("3", gameId, drawId);
-            if (map.get("downloadStatus").equals("0")) {
+             commAction.downloadFile(context,"3", gameId, drawId);
+            /*if (map.get("downloadStatus").equals("0")) {
                 context.setData("msgTyp", Constants.RESPONSE_TYPE_FAIL);
                 context.setData(ParamKeys.RSP_CDE, "LOT999");
                 context.setData(ParamKeys.RSP_MSG, "下载对账文件失败["+map.get("downloadMsg")+"]");
                 return;
-            }
+            }*/
             List<GdLotChkCtl> gdLotChkCtls =  get(GdLotChkCtlRepository.class).find(lotChkCtl);
             if (CollectionUtils.isEmpty(gdLotChkCtls)) {
                 context.setData("msgTyp", Constants.RESPONSE_TYPE_FAIL);
