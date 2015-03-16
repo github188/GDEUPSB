fce69c7909add00830513d6331f5df9ff3e6c858
Li_Yawei <Li_Yawei@2ac714ae-9a16-11e4-ad83-2f06aba1930a>
2015-03-16 12:57:15 +0000

    git-svn-id: svn://182.53.32.18/bbip/project/GDEUPSB/TRUNK/GDEUPSB@20838 2ac714ae-9a16-11e4-ad83-2f06aba1930a

--- src/com/bocom/bbip/gdeupsb/action/efek/batch/BatchDataFileAction.java
+++ src/com/bocom/bbip/gdeupsb/action/efek/batch/BatchDataFileAction.java
@@ -10,12 +10,17 @@ import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.bocom.bbip.comp.BBIPPublicService;
+import com.bocom.bbip.comp.account.AccountService;
+import com.bocom.bbip.eups.action.BaseAction;
+import com.bocom.bbip.eups.action.common.OperateFTPAction;
 import com.bocom.bbip.eups.action.common.OperateFileAction;
 import com.bocom.bbip.eups.common.ParamKeys;
 import com.bocom.bbip.eups.entity.EupsThdFtpConfig;
 import com.bocom.bbip.eups.repository.EupsThdFtpConfigRepository;
 import com.bocom.bbip.eups.spi.service.batch.BatchAcpService;
 import com.bocom.bbip.eups.spi.vo.PrepareBatchAcpDomain;
+import com.bocom.bbip.gdeupsb.action.common.BatchFileCommon;
+import com.bocom.bbip.gdeupsb.common.GDConstants;
 import com.bocom.bbip.gdeupsb.common.GDParamKeys;
 import com.bocom.bbip.gdeupsb.entity.AgtFileBatchDetail;
 import com.bocom.bbip.gdeupsb.entity.GDEupsBatchConsoleInfo;
@@ -24,11 +29,10 @@ import com.bocom.bbip.gdeupsb.repository.GDEupsBatchConsoleInfoRepository;
 import com.bocom.bbip.gdeupsb.repository.GDEupsEleTmpRepository;
 import com.bocom.bbip.thd.org.apache.commons.collections.CollectionUtils;
 import com.bocom.bbip.utils.BeanUtils;
-import com.bocom.bbip.utils.DateUtils;
 import com.bocom.jump.bp.core.Context;
 import com.bocom.jump.bp.core.CoreException;
 
-public class BatchDataFileAction implements BatchAcpService{
+public class BatchDataFileAction extends BaseAction implements BatchAcpService{
 	private final static Log logger=LogFactory.getLog(BatchDataFileAction.class);
 		/**
 		 * 批量代收付  数据准备
@@ -43,49 +47,48 @@ public class BatchDataFileAction implements BatchAcpService{
 	OperateFileAction operateFileAction;
 	@Autowired
 	BBIPPublicService bbipPublicService;
+	@Autowired
+	OperateFTPAction operateFTPAction;
 	@Override
 	public void prepareBatchDeal(PrepareBatchAcpDomain preparebatchacpdomain,
 			Context context) throws CoreException {
 		
-			logger.info("==========Start  BatchDataFileAction");
-			context.setData(ParamKeys.WS_TRANS_CODE, "20");
-			//判断是否处理过
-			//文件名
-			String fleNme=context.getData(ParamKeys.FLE_NME).toString();
-					
-					EupsThdFtpConfig eupsThdFtpConfig=eupsThdFtpConfigRepository.findOne("elecBatch");
+				logger.info("==========Start  BatchDataFileAction");
+				context.setData(ParamKeys.WS_TRANS_CODE, "20");
+				//文件名
+				String fleNme=context.getData(ParamKeys.FLE_NME).toString();
+				//批量前检查和初始化批量控制表 生成批次号batNo
+				((BatchFileCommon)get(GDConstants.BATCH_FILE_COMMON_UTILS)).BeforeBatchProcess(context);
 					//文件下载
-//					downloadFileToThird(eupsThdFtpConfig);
-					String fileId="batchFile";
+					EupsThdFtpConfig eupsThdFtpConfig=eupsThdFtpConfigRepository.findOne("elecBatch");
+					eupsThdFtpConfig.setRmtFleNme(fleNme);
+					operateFTPAction.getFileFromFtp(eupsThdFtpConfig);
 					//获取文件并解析入库   数据库文件名
-					List<Map<String, Object>> mapList=operateFileAction.pareseFile(eupsThdFtpConfig, fileId);
+					List<Map<String, Object>> mapList=operateFileAction.pareseFile(eupsThdFtpConfig, "batchFile");
 						if(CollectionUtils.isEmpty(mapList)){
-								throw new CoreException("处理状态异常");
+								throw new CoreException("处理状态异常或获取数据异常");
 						}
 						String batNo=context.getData(ParamKeys.BAT_NO).toString();
 						for (int i=0;i<mapList.size();i++) {
-							Map<String, Object> map=new HashMap<String, Object>();
-//							if(i==0){
-//									GDEupsBatchConsoleInfo gdEupsBatchConsoleInfo=BeanUtils.toObject(map, GDEupsBatchConsoleInfo.class);
-//									gdEupsBatchConsoleInfoRepository.insert(gdEupsBatchConsoleInfo);
-//							}else{
-									map.put(ParamKeys.SEQUENCE, bbipPublicService.getBBIPSequence());
+							Map<String, Object> map=mapList.get(i);
+							if(i == 0){
+									GDEupsBatchConsoleInfo gdEupsBatchConsoleInfo=BeanUtils.toObject(map, GDEupsBatchConsoleInfo.class);
+									gdEupsBatchConsoleInfo.setBatNo(batNo);
+									//批量代收信息标识  
+//									String rsvFld9=context.getData(ParamKeys.TXN_DTE).toString()+context.getData(ParamKeys.TXN_TME).toString()+context.getData(ParamKeys.COMPANY_NO).toString()+context.getData(GDParamKeys.BUS_TYPE).toString()+"00001";
+//									gdEupsBatchConsoleInfo.setRsvFld9(rsvFld9);
+									gdEupsBatchConsoleInfoRepository.updateConsoleInfo(gdEupsBatchConsoleInfo);
+							}else{
+									 map.put(ParamKeys.SEQUENCE, bbipPublicService.getBBIPSequence());
 									 GDEupsEleTmp gdEupsEleTmp=BeanUtils.toObject(map, GDEupsEleTmp.class);
 									 gdEupsEleTmp.setRsvFld5(batNo);
-									 logger.info("~~~~~map~~~~"+map);
+									 logger.info("~~~~~gdEupsEleTmp~~~~"+gdEupsEleTmp);
 									 gdEupsEleTmpRepository.insert(gdEupsEleTmp);
-//							}
+							}
 						}
-				//生成文件
-						
 						//	文件名		账户类型(2位,PT普通账户)+FS_银行代码(4位)+单位编码（8位）+ 日期（yyyymmdd）(8位)＋批次号（5位）.txt
-						String cusType=context.getData(ParamKeys.RSV_FLD3).toString();
-						String bankNo=context.getData(ParamKeys.RSV_FLD2).toString();
-						String comNo=context.getData(ParamKeys.COMPANY_NO).toString()	;
-						String date=DateUtils.format(DateUtils.parse(context.getData(GDParamKeys.SUB_DATE).toString()),DateUtils.STYLE_yyyyMMdd);
-						//批次号
-						String createFileName=cusType+bankNo+comNo+date+batNo.substring(0, 5)+".txt";
-						
+						//TODO 新文件文件名
+						String createFileName="01"+"FS_301"+context.getData(ParamKeys.COMPANY_NO)+context.getData(ParamKeys.TXN_DTE)+batNo.substring(0, 5)+".txt";
 						//文件内容
 						Map<String, Object> resultMap=createFileMap(context);
 						context.setVariable(GDParamKeys.COM_BATCH_AGT_FILE_NAME, createFileName);
@@ -97,7 +100,6 @@ public class BatchDataFileAction implements BatchAcpService{
 	 */
 		public Map<String, Object> createFileMap(Context context){
 			logger.info("=================Start  BatchDataFileAction  createFileMap ");
-			Map<String, Object> resultMap=new HashMap<String, Object>();
 			//header
 			Map<String, Object> headMap=new HashMap<String, Object>();
 			headMap.put(ParamKeys.COMPANY_NO, context.getData(ParamKeys.COMPANY_NO).toString());
@@ -116,11 +118,11 @@ public class BatchDataFileAction implements BatchAcpService{
 				String cusAc=gdEupsEleTmp.getCusAc();
 				agtFileBatchDetail.setCUSAC(cusAc);
 				//本行标志
-//				if(get(AccountService.class).isOurBankCard(cusAc)){
-				agtFileBatchDetail.setOUROTHFLG("0");
-//				}else{
-//					agtFileBatchDetail.setOUROTHFLG("1");
-//				}
+				if(get(AccountService.class).isOurBankCard(cusAc)){
+							agtFileBatchDetail.setOUROTHFLG("0");
+				}else{
+							agtFileBatchDetail.setOUROTHFLG("1");
+				}
 				agtFileBatchDetail.setCUSNME(gdEupsEleTmp.getCusNme());
 				agtFileBatchDetail.setOBKBK(gdEupsEleTmp.getBankNo());
 				
@@ -128,9 +130,7 @@ public class BatchDataFileAction implements BatchAcpService{
 				//备注 不用
 				detailList.add(agtFileBatchDetail);
 			}
-			for(int i=0;i<detailList.size();i++){
-			System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~"+detailList.get(i));
-			}
+			Map<String, Object> resultMap=new HashMap<String, Object>();
 			resultMap.put(ParamKeys.EUPS_FILE_HEADER, headMap);
 			resultMap.put(ParamKeys.EUPS_FILE_DETAIL, BeanUtils.toMap(detailList));
 			
