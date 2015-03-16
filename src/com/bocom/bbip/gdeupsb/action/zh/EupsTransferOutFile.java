788c15830aef1e71650bea0dc4364f20fc22467d
Wu_Yanhui <Wu_Yanhui@2ac714ae-9a16-11e4-ad83-2f06aba1930a>
2015-03-16 01:07:16 +0000

    git-svn-id: svn://182.53.32.18/bbip/project/GDEUPSB/TRUNK/GDEUPSB@20241 2ac714ae-9a16-11e4-ad83-2f06aba1930a

--- src/com/bocom/bbip/gdeupsb/action/zh/EupsTransferOutFile.java
+++ src/com/bocom/bbip/gdeupsb/action/zh/EupsTransferOutFile.java
@@ -33,20 +33,20 @@ public class EupsTransferOutFile  extends BaseAction {
     private static final int HEAD=0;
 
 	public void process(Context context)throws CoreException,CoreRuntimeException,IOException{
-		logger.info("Eups481215 start!!");
-		final String inFileName=ContextUtils.assertDataHasLengthAndGetNNR(context, "", ErrorCodes.EUPS_FIELD_EMPTY);
-		final String outFileName=ContextUtils.assertDataHasLengthAndGetNNR(context, "", ErrorCodes.EUPS_FIELD_EMPTY);
-        File srcFile=FileUtils.getFile(inFileName);
-        File destFile=FileUtils.getFile(outFileName);
+		logger.info("返盘文件转换 start!!");
+		final String inFileName=ContextUtils.assertDataHasLengthAndGetNNR(context, "inFileNme", ErrorCodes.EUPS_FIELD_EMPTY);
+		final String outFileName=ContextUtils.assertDataHasLengthAndGetNNR(context, "outFileNme", ErrorCodes.EUPS_FIELD_EMPTY);
+        File srcFile=FileUtils.getFile("D:\\"+inFileName+".txt");
+        File destFile=FileUtils.getFile("D:\\"+outFileName+".txt");
 		Assert.isFalse(null==srcFile||null==destFile, ErrorCodes.EUPS_FILE_NOT_EXIST);
-        List <String> lines=FileUtils.readLines(srcFile, Charsets.UTF_8);
-		Assert.isEmpty(lines, ErrorCodes.EUPS_FIELD_EMPTY);
+        List <String> lines=FileUtils.readLines(srcFile, "GBK");
+		Assert.isNotEmpty(lines, ErrorCodes.EUPS_FILE_PARESE_FAIL);
 		/** 账号转换 */
 		List <String>dest=transform(lines);
-		Assert.isEmpty(dest, ErrorCodes.EUPS_FIELD_EMPTY);
-        FileUtils.writeLines(destFile, BBIPCoreConstants.BBIP_DEFAULT_ENCODING, dest);
+		Assert.isNotEmpty(dest, ErrorCodes.EUPS_FILE_PARESE_FAIL);
+        FileUtils.writeLines(destFile, "GBK", dest);
         /** 发送到BBOS */
-		logger.info("转换成功!!");
+		logger.info("返盘文件转换成功!!");
 	}
 private List<String> transform(List<String>list)throws CoreException{
 	final int idx=1;
@@ -58,16 +58,18 @@ private List<String> transform(List<String>list)throws CoreException{
 		Assert.isFalse(temp.length==0||null==temp, ErrorCodes.EUPS_FILE_PARESE_FAIL);
         String ActNo=temp[ACT_NO].trim();
 		Assert.isFalse(StringUtils.isBlank(ActNo), ErrorCodes.EUPS_FILE_PARESE_FAIL);
-		if(ActNo.substring(0, 4).equalsIgnoreCase(GDConstants.GD_ZH_WAGE_ACTNO_PREFIX_4449)&&
+		/*需进一步细化，哪些帐号需要更新
+                          新21位实体帐号,"4449"开头,"202"结尾,转换为新21位主帐号"4449"开头,"998"结尾,主帐号）*/
+		if(21==ActNo.length()&&ActNo.substring(0, 4).equalsIgnoreCase(GDConstants.GD_ZH_WAGE_ACTNO_PREFIX_4449)&&
 				ActNo.substring(18).equalsIgnoreCase(GDConstants.GD_ZH_WAGE_ACTNO_SUFFIX_202)){
 	        logger.info("帐号:"+ActNo+"转换");
 	        List<ZHAgtActno> lt=get(ZHAgtActnoRespository.class).queryOldByNew(ActNo);
-	        Assert.isFalse(null==lt||lt.size()!=1, ErrorCodes.EUPS_QUERY_NO_DATA);
+	        Assert.isNotEmpty(lt, ErrorCodes.EUPS_QUERY_NO_DATA);
 	        ActNo=lt.get(0).getOldAct();		}
 		
-        final String dest=new StringBuilder().append(ActNo).append(Constants.EUPS_FILE_SPLIT_0)
-                        .append(temp[ActNam].trim()).append(Constants.EUPS_FILE_SPLIT_0)
-                        .append(temp[TxnAmt].trim()).append(Constants.EUPS_FILE_SPLIT_0)
+        final String dest=new StringBuilder().append(ActNo).append("|")
+                        .append(temp[ActNam].trim()).append("|")
+                        .append(temp[TxnAmt].trim()).append("|")
                         .append(temp[SucFlg].trim()).toString();
         ret.add(dest);
         
