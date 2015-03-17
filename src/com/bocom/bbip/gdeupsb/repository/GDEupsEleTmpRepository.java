2710ce1edc2196c63fe1b4f6cd5fe0941b3615d5
Li_Yawei <Li_Yawei@2ac714ae-9a16-11e4-ad83-2f06aba1930a>
2015-03-17 03:45:44 +0000

    git-svn-id: svn://182.53.32.18/bbip/project/GDEUPSB/TRUNK/GDEUPSB@21048 2ac714ae-9a16-11e4-ad83-2f06aba1930a

--- src/com/bocom/bbip/gdeupsb/repository/GDEupsEleTmpRepository.java
+++ src/com/bocom/bbip/gdeupsb/repository/GDEupsEleTmpRepository.java
@@ -4,6 +4,7 @@ import com.bocom.bbip.data.repository.PagingAndSortingRepository;
 import com.bocom.bbip.gdeupsb.entity.GDEupsEleTmp;
 
 public interface GDEupsEleTmpRepository extends PagingAndSortingRepository<GDEupsEleTmp, String> {
+		
 		public void deleteAll(String del);
 		
 		public void updateInfo(String cusAc,String thdCusNo);
