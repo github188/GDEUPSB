4d5cc03ab30cda4b97c824d3a0b60ea9ac81dd2d
Wang_Mingqin <Wang_Mingqin@2ac714ae-9a16-11e4-ad83-2f06aba1930a>
2015-03-17 12:11:14 +0000

    git-svn-id: svn://182.53.32.18/bbip/project/GDEUPSB/TRUNK/GDEUPSB@21334 2ac714ae-9a16-11e4-ad83-2f06aba1930a

--- src/com/bocom/bbip/gdeupsb/repository/GdGashBatchTmpRepository.java
+++ src/com/bocom/bbip/gdeupsb/repository/GdGashBatchTmpRepository.java
@@ -1,7 +1,13 @@
 package com.bocom.bbip.gdeupsb.repository;
 
+import java.util.List;
+
+import com.bocom.bbip.data.annotation.Find;
 import com.bocom.bbip.data.repository.PagingAndSortingRepository;
 import com.bocom.bbip.gdeupsb.entity.GdGashBatchTmp;
 
 public interface GdGashBatchTmpRepository extends PagingAndSortingRepository<GdGashBatchTmp, String> {
+	
+	@Find
+	public List<GdGashBatchTmp> findByBatNo(final String data);
 }
