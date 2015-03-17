8d218ac5d3ffba96df6979e3d2a6ecd6a36d1949
Li_Yawei <Li_Yawei@2ac714ae-9a16-11e4-ad83-2f06aba1930a>
2015-03-17 08:29:02 +0000

    git-svn-id: svn://182.53.32.18/bbip/project/GDEUPSB/TRUNK/GDEUPSB@21187 2ac714ae-9a16-11e4-ad83-2f06aba1930a

--- src/com/bocom/bbip/gdeupsb/action/trsp/CheckTrspFileAction.java
+++ src/com/bocom/bbip/gdeupsb/action/trsp/CheckTrspFileAction.java
@@ -10,6 +10,7 @@ import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.math.BigDecimal;
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
@@ -23,7 +24,6 @@ import org.springframework.beans.factory.annotation.Qualifier;
 
 import com.bocom.bbip.comp.BBIPPublicService;
 import com.bocom.bbip.eups.action.BaseAction;
-import com.bocom.bbip.eups.action.common.CommThdRspCdeAction;
 import com.bocom.bbip.eups.action.common.OperateFTPAction;
 import com.bocom.bbip.eups.action.common.OperateFileAction;
 import com.bocom.bbip.eups.adaptor.ThirdPartyAdaptor;
@@ -71,9 +71,9 @@ public class CheckTrspFileAction extends BaseAction{
 		            context.setData(ParamKeys.RSP_MSG,"交易并发，请稍后在做 !!");
 		            throw new CoreException("交易并发，请稍后在做 ");
 		        }
-				//发票号   对账批次
-		        context.setData(GDParamKeys.INV_NO,context.getData(ParamKeys.SEQUENCE).toString());
-		        context.setData(GDParamKeys.CHECK_NO,context.getData(ParamKeys.SEQUENCE).toString());
+		       if(null == context.getData(GDParamKeys.END_DATE)){
+		    	   		context.setData(GDParamKeys.END_DATE, DateUtils.format(new Date(), DateUtils.STYLE_yyyyMMdd));
+		       }
 		        //外发第三方
 		        callThd(context);
 		        //清除对账表中的信息
@@ -83,11 +83,13 @@ public class CheckTrspFileAction extends BaseAction{
 		        //更改表内所有对账状态
 		        trspCheckTmpRepository.updateAll(tChkNo);
 		        
+		        //错误数量 
 		        int numErr=0;
+		        //错误金额 
 		        BigDecimal AmtErr=new BigDecimal("0.00");
 		        String chkErr=""; //错误信息
 		        //TODO 文件名
-		        String fileName=context.getData("fileName").toString()+".txt";
+		        String fileName=context.getData("fileName").toString();
 		        List<TrspCheckTmp> list=trspCheckTmpRepository.findAll();
 		        int chkFlg=-1;
 		        //轮询对账
@@ -96,7 +98,7 @@ public class CheckTrspFileAction extends BaseAction{
 						 	//根据流水得到每条数据
 			        	GDEupsbTrspFeeInfo  gdEupsbTrspFeeInfo=gdEupsbTrspFeeInfoRepository.findOne(trspCheckTmp.getSqn());
 			        	if(gdEupsbTrspFeeInfo == null){
-			        			context.setData(ParamKeys.RSP_MSG, "企业多账");
+			        			chkErr="企业多账";
 			        			chkFlg=3;
 			        	}else{
 					        		if(!gdEupsbTrspFeeInfo.getCarNo().equals(trspCheckTmp.getCarNo()) 
@@ -114,6 +116,7 @@ public class CheckTrspFileAction extends BaseAction{
 							        		chkErr="核对成功";
 						        			chkFlg=1;
 							        	}
+					        		//临时使用实体类  定义类型和数据 使其生成文件使用
 					        		GDEupsbTrspFeeInfo  gdEupsbTrspFeeInfoNew=new GDEupsbTrspFeeInfo();
 					        		gdEupsbTrspFeeInfoNew.setTcusNm(chkErr);
 					        		gdEupsbTrspFeeInfoNew.setThdKey(trspCheckTmp.getSqn());
@@ -124,37 +127,42 @@ public class CheckTrspFileAction extends BaseAction{
 					        		gdEupsbTrspFeeInfoNew.setStatus(gdEupsbTrspFeeInfo.getStatus());
 					        		gdEupsbTrspFeeInfoNew.setPayDat(gdEupsbTrspFeeInfo.getPayDat());
 					        		gdEupsbTrspFeeInfoNew.setInvNo(gdEupsbTrspFeeInfo.getInvNo());
+					        		
 					        	if(1 !=chkFlg){
 						        		numErr=numErr+1;
 						        		AmtErr=AmtErr.add(trspCheckTmp.getTxnAmt());
 //			                           银行交易日期|银行流水号|发生额|发票号|缴费月数|车辆类型|车牌号|状态|
 					        			detailList.add(gdEupsbTrspFeeInfoNew);
+					        	}else if(3 !=chkFlg){
+					        			//跟新对账标志 
+						        		String checkFlg=chkFlg+"";
+						        		gdEupsbTrspFeeInfo.setChkFlg(checkFlg);
+						        		gdEupsbTrspFeeInfo.setTchkNo(tChkNo);
+						        		gdEupsbTrspFeeInfoRepository.update(gdEupsbTrspFeeInfo);
 					        	}
-					        	String checkFlg=chkFlg+"";
-					        	gdEupsbTrspFeeInfo.setChkFlg(checkFlg);
-					        	gdEupsbTrspFeeInfo.setTchkNo(tChkNo);
-					        	gdEupsbTrspFeeInfoRepository.update(gdEupsbTrspFeeInfo);
+					        	//跟新表中对账状态 
+					        	//更新银行单边账   设置3 = chkFlg
+					        	gdEupsbTrspFeeInfoRepository.updateChkFlg(tChkNo);
 				       }
 		        }
+		        context.setData(ParamKeys.TXN_AMT, AmtErr);
+		        //拼装文件
 		        EupsThdFtpConfig eupsThdFtpConfig=get(EupsThdFtpConfigRepository.class).findOne("trspCheckFile");
 		        Map<String, Object> map =  new HashMap<String, Object>();
     	        map.put(ParamKeys.EUPS_FILE_DETAIL, BeanUtils.toMaps(detailList));
-    	        //换行
     			get(OperateFileAction.class).createCheckFile(eupsThdFtpConfig, "trspCheckFile", fileName, map);
-	        	//跟新表中对账状态 
-		        //更新银行单边账
-		        gdEupsbTrspFeeInfoRepository.updateChkFlg(tChkNo);
 		        
-		        context.setData(ParamKeys.TXN_AMT, AmtErr);
+    			//判断是否全部对账
 		        List<GDEupsbTrspFeeInfo> gdEupsbTrspFeeInfoList=gdEupsbTrspFeeInfoRepository.findNotCheck(tChkNo);
-		        if(CollectionUtils.isEmpty(gdEupsbTrspFeeInfoList)){
+		        if(CollectionUtils.isNotEmpty(gdEupsbTrspFeeInfoList)){
 		        	context.setData(ParamKeys.RSP_CDE, "329999");
 		        	context.setData(ParamKeys.RSP_MSG, "系统错误");
 		        	throw new CoreException("系统错误");
 		        }
-		        if(gdEupsbTrspFeeInfoList.size()>0){
-		        	sendFile(context);
-		        }
+		        //上传文件
+		        sendFile(context,fileName);
+		        
+		        
 		        context.setData(GDParamKeys.MSGTYP, "N");
 		        context.setData(ParamKeys.RSP_CDE, "000000");
 		        context.setData(ParamKeys.RSP_MSG, "交易成功");
@@ -184,23 +192,12 @@ public class CheckTrspFileAction extends BaseAction{
 						context.setData(ParamKeys.RSP_MSG, "调用路桥方时:系统错误或超时或未发送,请检查后重做");
 						throw new CoreException("调用路桥方时:系统错误或超时或未发送,请检查后重做");
 				}
-	             String inTrspCd="|"+context.getData(ParamKeys.RESPONSE_CODE)+"|";
-	             int i="@PARA.TRsp_Suc".split(inTrspCd).length;
-	             //TODO 
-	             i=0;
-	             if(i>1){
-	            	 	context.setData(ParamKeys.RSP_CDE, "329999");
-	            	 	context.setData(ParamKeys.RSP_MSG, "路桥方返回:"+context.getData(ParamKeys.RSP_CDE)+"-"+context.getData(ParamKeys.RSP_MSG));
-	            	 	logger.info("对账不成功");
-	            	 	throw new CoreException("对账不成功");
-	             }
-	             context.setData(ParamKeys.RSP_CDE, Constants.HOST_RESPONSE_CODE_SUCC);
 			} catch (CoreException e) {
 					logger.info("CheckTrspFile   callThd  error="+e);
 			}
 	}
 	/**
-	 * 将文件拆分入对账表
+	 * 将文件拆分入对账表 更改交易表中对账状态
 	 */
 	public void fileInsertTrspCheckTmp(Context context,String tChkNo){
 		logger.info("~~~~~~~~~~~Start  CheckTrspFile   fileInsertTrspCheckTmp");
@@ -213,20 +210,32 @@ public class CheckTrspFileAction extends BaseAction{
 			FileReader fileReader=new FileReader(path+"//"+fileName);
 			BufferedReader bufferedReader=new BufferedReader(fileReader);
 			String string=null;
+			GDEupsbTrspFeeInfo gdEupsbTrspFeeInfo =new GDEupsbTrspFeeInfo();
+			gdEupsbTrspFeeInfo.setChkFlg("0");
 			//读行
 			while((string=bufferedReader.readLine())!=null){
 					String[] str=string.split("\\|");
 //					0银行交易日期|1银行流水号|2发生额|3发票号|4缴费月数|5车辆类型|6车牌号|7状态|				
 					TrspCheckTmp trspCheckTmp=new TrspCheckTmp();
 					trspCheckTmp.setTxnDat(DateUtils.parse(str[0],DateUtils.STYLE_SIMPLE_DATE));
-					trspCheckTmp.setSqn(str[1]);					
-					trspCheckTmp.setTxnAmt(new BigDecimal(str[2]));
+					trspCheckTmp.setSqn(str[1]);		
+					
+					double i=Double.parseDouble(str[2].toString());
+					double d=i/100;
+					DecimalFormat df=new DecimalFormat("#.00");
+					BigDecimal txnAmt=new BigDecimal(df.format(d));
+					trspCheckTmp.setTxnAmt(txnAmt);
+					
 					trspCheckTmp.setInvNo(str[3]);
 					trspCheckTmp.setPayMon(str[4]);
 					trspCheckTmp.setCarTyp(str[5]);
 					trspCheckTmp.setCarNo(str[6]);
 					trspCheckTmp.setStatue(str[7]);
-					trspCheckTmp.setTchkNo(tChkNo);
+					
+					gdEupsbTrspFeeInfo.setThdKey(str[1]);
+					//更改对账状态
+					gdEupsbTrspFeeInfoRepository.update(gdEupsbTrspFeeInfo);
+					//插入对账表
 					trspCheckTmpRepository.insert(trspCheckTmp);
 			}
 			bufferedReader.close();
@@ -238,28 +247,19 @@ public class CheckTrspFileAction extends BaseAction{
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+		logger.info("~~~~~~~~~~~End  CheckTrspFile   fileInsertTrspCheckTmp");
 	}
 	/**
 	 * 发送文件
 	 */
-	public void sendFile(Context context)throws CoreException{
+	public void sendFile(Context context,String fileName)throws CoreException{
 		logger.info("~~~~~~~~~~~Start  CheckTrspFile   printFile");
 		
 		//获取FTP信息,发送文件到指定路径
         EupsThdFtpConfig eupsThdFtpConfig = context.getData(ParamKeys.CONSOLE_THD_FTP_CONFIG_LIST);
-		//TODO 发送文件
-//      <Exec func="PUB:SendFileMessage2">
-//         <Arg name="MsgTyp"  value="4"/><!-- 消息类型，1通知2广播3文件4打印，一般传文件时用3，需要打印时用4-->
-//         <Arg name="ObjNod"  value="$NodNo"/><!-- 目标网点-->
-//         <Arg name="BusTyp"  value="SUBSTR($TxnCod,1,2)"/><!-- 业务类型-->
-//         <Arg name="AplCod"  value="SUBSTR($TxnCod,3,4)"/><!-- 应用码-->
-//         <Arg name="FilNam"  value="$ErrFil"/><!-- 文件名-->
-//         <Arg name="Summary" value="路桥差错账记录清单"/><!-- 消息内容-->
-//         <Arg name="ObjTlr"  value="$TlrId"/><!-- 目标柜员-->
-//         <Arg name="SrcNod"  value="$NodNo"/><!-- 消息来源网点-->
-//         <Arg name="SrcTlr"  value="$TlrId"/><!-- 消息来源柜员-->
-//      </Exec>
+        eupsThdFtpConfig.setLocFleNme(fileName);
+        operateFTPAction.putCheckFile(eupsThdFtpConfig);
+        
 		context.setData(GDParamKeys.MSGTYP, "E");
         context.setData(ParamKeys.RSP_CDE, "329999");
         context.setData(ParamKeys.RSP_MSG, "与路桥对账失败，请查看差错账记录清单");
@@ -297,8 +297,9 @@ public class CheckTrspFileAction extends BaseAction{
 				context.setData(ParamKeys.RSP_MSG, "统计模式错误");
 				throw new CoreException("统计模式错误");
 		}
-		
-			//TODO 报表生成
+		String RptFil="Car"+context.getData("TlrId")+context.getData(GDParamKeys.START_DATE).toString().substring(4);
+		context.setData("RptFil", RptFil);
+		//TODO 报表生成
 		VelocityTemplatedReportRender render = new VelocityTemplatedReportRender();
 		try {
 			render.afterPropertiesSet();
@@ -333,6 +334,7 @@ public class CheckTrspFileAction extends BaseAction{
 		                }
 		            }
 		        }
-		        sendFile(context);
+		        String fileName=context.getData("fileName").toString();
+		        sendFile(context,fileName);
 	}
 }
