ee8ce1e1c0342ad73dd03c188484d48ddfcbe18f
Li_Yawei <Li_Yawei@2ac714ae-9a16-11e4-ad83-2f06aba1930a>
2015-03-17 13:19:15 +0000

    git-svn-id: svn://182.53.32.18/bbip/project/GDEUPSB/TRUNK/GDEUPSB@21393 2ac714ae-9a16-11e4-ad83-2f06aba1930a

--- src/com/bocom/bbip/gdeupsb/action/gzag/batch/GzagBatchDataResultFileAction.java
+++ src/com/bocom/bbip/gdeupsb/action/gzag/batch/GzagBatchDataResultFileAction.java
@@ -1,5 +1,6 @@
 package com.bocom.bbip.gdeupsb.action.gzag.batch;
 
+import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -11,13 +12,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import com.bocom.bbip.eups.action.common.OperateFTPAction;
 import com.bocom.bbip.eups.action.common.OperateFileAction;
 import com.bocom.bbip.eups.common.ParamKeys;
-import com.bocom.bbip.eups.entity.EupsBatchConsoleInfo;
 import com.bocom.bbip.eups.entity.EupsThdFtpConfig;
-import com.bocom.bbip.eups.repository.EupsBatchConsoleInfoRepository;
 import com.bocom.bbip.eups.repository.EupsThdFtpConfigRepository;
 import com.bocom.bbip.eups.spi.service.batch.AfterBatchAcpService;
 import com.bocom.bbip.eups.spi.vo.AfterBatchAcpDomain;
+import com.bocom.bbip.gdeupsb.common.GDParamKeys;
+import com.bocom.bbip.gdeupsb.entity.GDEupsBatchConsoleInfo;
 import com.bocom.bbip.gdeupsb.entity.GDEupsGzagBatchTmp;
+import com.bocom.bbip.gdeupsb.repository.GDEupsBatchConsoleInfoRepository;
 import com.bocom.bbip.gdeupsb.repository.GDEupsGzagBatchTmpRepository;
 import com.bocom.bbip.utils.BeanUtils;
 import com.bocom.jump.bp.core.Context;
@@ -30,7 +32,7 @@ public class GzagBatchDataResultFileAction implements AfterBatchAcpService{
 	@Autowired
 	OperateFTPAction operateFTP;
 	@Autowired
-	EupsBatchConsoleInfoRepository eupsBatchConsoleInfoRepository;
+	GDEupsBatchConsoleInfoRepository gdEupsBatchConsoleInfoRepository;
 	@Autowired
 	GDEupsGzagBatchTmpRepository gdEupsGzagBatchTmpRepository;
 	@Autowired
@@ -41,24 +43,18 @@ public class GzagBatchDataResultFileAction implements AfterBatchAcpService{
 	public void afterBatchDeal(AfterBatchAcpDomain afterbatchacpdomain, Context context) throws CoreException {
 		logger.info("===============Start  GzagBatchDataResultFileAction");
 			
-			Map<String, Object> resultMap = new HashMap<String, Object>();
 			
 			String batNo=context.getData(ParamKeys.BAT_NO).toString();
-			EupsBatchConsoleInfo eupsBatchConsoleInfo = eupsBatchConsoleInfoRepository.findOne(batNo);
-			//header
-			Map<String, Object> resultMapHead = BeanUtils.toMap(eupsBatchConsoleInfo);
-			resultMap.put(ParamKeys.EUPS_FILE_HEADER, resultMapHead);
-			//文件名
-			String fileName=eupsBatchConsoleInfo.getFleNme();
-			//文件内容 
-			GDEupsGzagBatchTmp gdEupsGzagBatchTmps =new  GDEupsGzagBatchTmp();
-			gdEupsGzagBatchTmps.setRsvFld5(batNo);
-			List<GDEupsGzagBatchTmp> detailList=gdEupsGzagBatchTmpRepository.find(gdEupsGzagBatchTmps);
-			resultMap.put(ParamKeys.EUPS_FILE_DETAIL, detailList);
+			GDEupsBatchConsoleInfo gdeupsBatchConsoleInfo = gdEupsBatchConsoleInfoRepository.findOne(batNo);
 			
+			//更改控制表 并得到控制信息
+			GDEupsBatchConsoleInfo info=updateInfo(context,gdeupsBatchConsoleInfo); 
+			//文件名
+			String fileName=info.getFleNme();
+			//拼装Map文件
+			Map<String, Object> resultMap = createFileMap(context,info);
 			String fileIDResult=context.getData("fileID").toString()+"Result";
 			EupsThdFtpConfig eupsThdFtpConfig =eupsThdFtpConfigRepository.findOne(fileIDResult);
-			
 			// 生成文件
 			operateFile.createCheckFile(eupsThdFtpConfig, fileIDResult, fileName, resultMap);
 			// 将生成的文件上传至指定服务器
@@ -68,4 +64,47 @@ public class GzagBatchDataResultFileAction implements AfterBatchAcpService{
 			
 			logger.info("===============End  GzagBatchDataResultFileAction");
 		}
+	/**
+	 * 把信息保存到控制表
+	 */
+	public GDEupsBatchConsoleInfo  updateInfo(Context context,GDEupsBatchConsoleInfo gdeupsBatchConsoleInfo){
+			logger.info("===============Start  BatchDataResultFileAction  updateInfo");	
+			Integer totCnt=Integer.parseInt(gdeupsBatchConsoleInfo.getTotCnt().toString());
+			//成功笔数
+			Integer sucTotCnt=Integer.parseInt(context.getData(GDParamKeys.SUC_TOT_CNT).toString());
+			gdeupsBatchConsoleInfo.setSucTotCnt(sucTotCnt);
+			//失败笔数
+			Integer falTotCnt=totCnt-sucTotCnt;
+			gdeupsBatchConsoleInfo.setFalTotCnt(falTotCnt);
+			
+			BigDecimal totAmt=gdeupsBatchConsoleInfo.getTotAmt();
+			//成功总金额
+			BigDecimal sucTotAmt=new BigDecimal(context.getData("sucTotAmt").toString());
+			gdeupsBatchConsoleInfo.setSucTotAmt(sucTotAmt);
+			//失败总金额
+			BigDecimal falTotAmt=totAmt.subtract(sucTotAmt);
+			gdeupsBatchConsoleInfo.setFalTotAmt(falTotAmt);
+			
+			//更新批次表
+			gdEupsBatchConsoleInfoRepository.updateConsoleInfo(gdeupsBatchConsoleInfo);
+			logger.info("===============End  BatchDataResultFileAction  updateInfo");	
+			return gdeupsBatchConsoleInfo;
+	}
+	/**
+	 * 拼装Map返回文件
+	 */
+	public Map<String, Object> createFileMap(Context context,GDEupsBatchConsoleInfo info){
+			logger.info("===============Start  BatchDataResultFileAction  createFileMap");	
+			Map<String, Object> resultMap=new HashMap<String, Object>();
+			//header
+			Map<String, Object> resultMapHead = BeanUtils.toMap(info);
+			resultMap.put(ParamKeys.EUPS_FILE_HEADER, resultMapHead);
+			//文件内容 
+			GDEupsGzagBatchTmp gdEupsGzagBatchTmps =new  GDEupsGzagBatchTmp();
+			gdEupsGzagBatchTmps.setRsvFld5(info.getBatNo());
+			List<GDEupsGzagBatchTmp> detailList=gdEupsGzagBatchTmpRepository.find(gdEupsGzagBatchTmps);
+			resultMap.put(ParamKeys.EUPS_FILE_DETAIL, detailList);
+			logger.info("===============End   BatchDataResultFileAction  createFileMap");	
+			return resultMap;
+	}
 }
