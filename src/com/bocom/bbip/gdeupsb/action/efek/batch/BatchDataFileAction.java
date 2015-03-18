c29ca642ef119086589814d65d462236c34d5e55
Li_Yawei <Li_Yawei@2ac714ae-9a16-11e4-ad83-2f06aba1930a>
2015-03-18 13:49:56 +0000

    git-svn-id: svn://182.53.32.18/bbip/project/GDEUPSB/TRUNK/GDEUPSB@22014 2ac714ae-9a16-11e4-ad83-2f06aba1930a

--- src/com/bocom/bbip/gdeupsb/action/efek/batch/BatchDataFileAction.java
+++ src/com/bocom/bbip/gdeupsb/action/efek/batch/BatchDataFileAction.java
@@ -1,5 +1,10 @@
 package com.bocom.bbip.gdeupsb.action.efek.batch;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -29,6 +34,7 @@ import com.bocom.bbip.gdeupsb.repository.GDEupsBatchConsoleInfoRepository;
 import com.bocom.bbip.gdeupsb.repository.GDEupsEleTmpRepository;
 import com.bocom.bbip.thd.org.apache.commons.collections.CollectionUtils;
 import com.bocom.bbip.utils.BeanUtils;
+import com.bocom.bbip.utils.DateUtils;
 import com.bocom.jump.bp.core.Context;
 import com.bocom.jump.bp.core.CoreException;
 
@@ -54,6 +60,9 @@ public class BatchDataFileAction extends BaseAction implements BatchAcpService{
 			Context context) throws CoreException {
 		
 				logger.info("==========Start  BatchDataFileAction  prepareBatchDeal");
+				String totAmt=context.getData("totAmt").toString();
+				String totCnt=context.getData("totCnt").toString();
+				String comNo=context.getData("comNo").toString();
 				context.setData(ParamKeys.WS_TRANS_CODE, "20");
 				//文件名
 				String fleNme=context.getData(ParamKeys.FLE_NME).toString();
@@ -62,38 +71,40 @@ public class BatchDataFileAction extends BaseAction implements BatchAcpService{
 					//文件下载
 					EupsThdFtpConfig eupsThdFtpConfig=eupsThdFtpConfigRepository.findOne("elecBatch");
 					eupsThdFtpConfig.setRmtFleNme(fleNme);
+					eupsThdFtpConfig.setLocFleNme(fleNme);
 					operateFTPAction.getFileFromFtp(eupsThdFtpConfig);
+					String batNo=context.getData(ParamKeys.BAT_NO).toString();
+					//该更控制表
+					updateInfo(context, eupsThdFtpConfig, batNo,totAmt ,totCnt);
 					//获取文件并解析入库   数据库文件名
 					List<Map<String, Object>> mapList=operateFileAction.pareseFile(eupsThdFtpConfig, "batchFile");
-						if(CollectionUtils.isEmpty(mapList)){
+					if(CollectionUtils.isEmpty(mapList)){
 								throw new CoreException("处理状态异常或获取数据异常");
-						}
-						String batNo=context.getData(ParamKeys.BAT_NO).toString();
+					}
+						context.setData(ParamKeys.COMPANY_NO, comNo);
 						for (int i=0;i<mapList.size();i++) {
 							Map<String, Object> map=mapList.get(i);
-							if(i == 0){
-									GDEupsBatchConsoleInfo gdEupsBatchConsoleInfo=BeanUtils.toObject(map, GDEupsBatchConsoleInfo.class);
-									gdEupsBatchConsoleInfo.setBatNo(batNo);
-									//批量代收信息标识  
-//									String rsvFld9=context.getData(ParamKeys.TXN_DTE).toString()+context.getData(ParamKeys.TXN_TME).toString()+context.getData(ParamKeys.COMPANY_NO).toString()+context.getData(GDParamKeys.BUS_TYPE).toString()+"00001";
-//									gdEupsBatchConsoleInfo.setRsvFld9(rsvFld9);
-									gdEupsBatchConsoleInfoRepository.updateConsoleInfo(gdEupsBatchConsoleInfo);
-							}else{
 									 map.put(ParamKeys.SEQUENCE, bbipPublicService.getBBIPSequence());
 									 GDEupsEleTmp gdEupsEleTmp=BeanUtils.toObject(map, GDEupsEleTmp.class);
+									 if(!map.get("capital").toString().equals("")){
+										 	map.put("capital", (new BigDecimal(map.get("capital").toString()).scaleByPowerOfTen(-2)));
+									 }
+									 if(!map.get("dedit").toString().equals("")){
+										 	map.put("dedit", (new BigDecimal(map.get("capital").toString()).scaleByPowerOfTen(-2)));
+									 }
+									 BigDecimal txnAmt=gdEupsEleTmp.getPaymentMoney().scaleByPowerOfTen(-2);
+									 gdEupsEleTmp.setTxnAmt(txnAmt);
 									 gdEupsEleTmp.setRsvFld5(batNo);
 									 logger.info("~~~~~gdEupsEleTmp~~~~"+gdEupsEleTmp);
 									 gdEupsEleTmpRepository.insert(gdEupsEleTmp);
-							}
 						}
 						//	文件名		账户类型(2位,PT普通账户)+FS_银行代码(4位)+单位编码（8位）+ 日期（yyyymmdd）(8位)＋批次号（5位）.txt
 						//TODO 新文件文件名
 						String createFileName="01"+"FS_301"+context.getData(ParamKeys.COMPANY_NO)+context.getData(ParamKeys.TXN_DTE)+batNo.substring(0, 5)+".txt";
 						//文件内容
-						Map<String, Object> resultMap=createFileMap(context);
+						Map<String, Object> resultMap=createFileMap(context,comNo,totAmt,totCnt);
 						context.setVariable(GDParamKeys.COM_BATCH_AGT_FILE_NAME, createFileName);
 						context.setVariable(GDParamKeys.COM_BATCH_AGT_FILE_MAP, resultMap);
-						
 						//提交代收付
 						userProcessToSubmit(context);
 						//得到反盘文件 
@@ -104,41 +115,49 @@ public class BatchDataFileAction extends BaseAction implements BatchAcpService{
 	/**
 	 * 文件map拼装
 	 */
-		public Map<String, Object> createFileMap(Context context){
+		public Map<String, Object> createFileMap(Context context,String comNo,String totAmt,String totCnt){
 			logger.info("=================Start  BatchDataFileAction  createFileMap ");
+			BigDecimal totAmts=(new BigDecimal(totAmt)).scaleByPowerOfTen(-2);
 			//header
 			Map<String, Object> headMap=new HashMap<String, Object>();
-			headMap.put(ParamKeys.COMPANY_NO, context.getData(ParamKeys.COMPANY_NO).toString());
-			headMap.put(GDParamKeys.TOT_COUNT, context.getData(GDParamKeys.TOT_CNT));
-			headMap.put(ParamKeys.TOT_AMT, context.getData(ParamKeys.TOT_AMT));
+			headMap.put(ParamKeys.COMPANY_NO, comNo);
+			headMap.put(GDParamKeys.TOT_COUNT, totCnt);
+			headMap.put(ParamKeys.TOT_AMT, totAmts);
+			System.out.println(headMap);
 			//detail
 			GDEupsEleTmp gdEupsEleTmps=new GDEupsEleTmp();
-			gdEupsEleTmps.setComNo(context.getData(ParamKeys.COMPANY_NO).toString());
+			gdEupsEleTmps.setComNo(comNo);
 			List<GDEupsEleTmp> gdEupsEleTmpList=gdEupsEleTmpRepository.findAllOrderBySqn();
 			
 			List<AgtFileBatchDetail> detailList=new ArrayList<AgtFileBatchDetail>();
 			for (GDEupsEleTmp gdEupsEleTmp : gdEupsEleTmpList) {
+				System.out.println();
+				System.out.println(gdEupsEleTmp.getSqn());
+				
 				AgtFileBatchDetail agtFileBatchDetail=new AgtFileBatchDetail();
-				agtFileBatchDetail.setAGTSRVCUSID(gdEupsEleTmp.getThdCusNo());
-				agtFileBatchDetail.setAGTSRVCUSNME(gdEupsEleTmp.getThdCusNme());
 				String cusAc=gdEupsEleTmp.getCusAc();
 				agtFileBatchDetail.setCUSAC(cusAc);
+				agtFileBatchDetail.setTXNAMT(gdEupsEleTmp.getTxnAmt());
+				agtFileBatchDetail.setCUSNME(gdEupsEleTmp.getCusNme());
+				agtFileBatchDetail.setAGTSRVCUSID(gdEupsEleTmp.getThdCusNo());
+				agtFileBatchDetail.setAGTSRVCUSNME(gdEupsEleTmp.getThdCusNme());
 				//本行标志
 				if(get(AccountService.class).isOurBankCard(cusAc)){
-							agtFileBatchDetail.setOUROTHFLG("0");
+					agtFileBatchDetail.setOUROTHFLG("0");
 				}else{
-							agtFileBatchDetail.setOUROTHFLG("1");
+					agtFileBatchDetail.setOUROTHFLG("1");
 				}
-				agtFileBatchDetail.setCUSNME(gdEupsEleTmp.getCusNme());
 				agtFileBatchDetail.setOBKBK(gdEupsEleTmp.getBankNo());
 				
-				agtFileBatchDetail.setTXNAMT(gdEupsEleTmp.getTxnAmt());
+				if(gdEupsEleTmp.getDedit() !=null){
+					agtFileBatchDetail.setTXNAMT(gdEupsEleTmp.getTxnAmt().add(gdEupsEleTmp.getDedit()));
+				}
 				//备注 不用
 				detailList.add(agtFileBatchDetail);
 			}
 			Map<String, Object> resultMap=new HashMap<String, Object>();
 			resultMap.put(ParamKeys.EUPS_FILE_HEADER, headMap);
-			resultMap.put(ParamKeys.EUPS_FILE_DETAIL, BeanUtils.toMap(detailList));
+			resultMap.put(ParamKeys.EUPS_FILE_DETAIL, BeanUtils.toMaps(detailList));
 			
 			logger.info("~~~~~~~~resultMap~~~~~~~~"+resultMap.get(ParamKeys.EUPS_FILE_DETAIL));
 			logger.info("=================End  BatchDataFileAction  createFileMap ");
@@ -149,8 +168,7 @@ public class BatchDataFileAction extends BaseAction implements BatchAcpService{
 	 */
 		public void userProcessToSubmit(Context context)throws CoreException{
 			logger.info("==========Start  BatchDataFileAction  userProcess");
-			String mothed="eups.batchPaySubmitDataProcess";
-			bbipPublicService.synExecute(mothed, context);
+			get(BatchFileCommon.class).sendBatchFileToACP(context);
 			logger.info("==========End  BatchDataFileAction  userProcess");
 		}
 	/**
@@ -161,6 +179,69 @@ public class BatchDataFileAction extends BaseAction implements BatchAcpService{
 			String mothed="eups.commNotifyBatchStatus";
 			bbipPublicService.synExecute(mothed, context);
 			logger.info("==========End  BatchDataFileAction  userProcess");
+		}
+	/**
+	 * 第一行数据保存到控制表中
+	 */
+		public void updateInfo(Context context,EupsThdFtpConfig eupsThdFtpConfig,String batNo,String totAmt,String totCnt) throws CoreException{
+				logger.info("==========Start  BatchDataFileAction  updateInfo");
+				String fleNme=context.getData(ParamKeys.FLE_NME).toString();
+				String filePath=eupsThdFtpConfig.getLocDir();
+				try {
+						FileReader fileReader=new FileReader(filePath+"//"+fleNme);
+						BufferedReader bufferedReader=new BufferedReader(fileReader);
+						String firstLine=null;
+						int i=0;
+						while((firstLine=bufferedReader.readLine())!=null){
+								if(i == 0){
+									GDEupsBatchConsoleInfo gdEupsBatchConsoleInfo=new GDEupsBatchConsoleInfo();
+									String rsvFld9=firstLine.substring(0,27);   //27
+									gdEupsBatchConsoleInfo.setRsvFld9(rsvFld9);
+									String rsvFld8=firstLine.substring(27,31); //4
+									gdEupsBatchConsoleInfo.setRsvFld8(rsvFld8);
+									String comNo=firstLine.substring(31,39); //8
+									gdEupsBatchConsoleInfo.setComNo(comNo);
+									String subDte=firstLine.substring(39,47); //8
+									gdEupsBatchConsoleInfo.setSubDte(DateUtils.parse(subDte));
+									String rsvFld7=firstLine.substring(47,52); //5
+									gdEupsBatchConsoleInfo.setRsvFld7(rsvFld7);
+									String rsvFld6=firstLine.substring(52,55); //3
+									gdEupsBatchConsoleInfo.setRsvFld6(rsvFld6);
+									String rTotCnt=firstLine.substring(55,66); //11
+									if(!rTotCnt.trim().equals(totCnt.trim())){
+											throw new CoreException("总笔数不相同");
+									}
+									gdEupsBatchConsoleInfo.setTotCnt(Integer.parseInt(totCnt));
+									String rTotAmt=firstLine.substring(66,82); //16
+									if(!rTotAmt.trim().equals(totAmt.trim())){
+										throw new CoreException("总金额不相同");
+									}
+									gdEupsBatchConsoleInfo.setTotAmt((new BigDecimal(totAmt)).scaleByPowerOfTen(-2));
+									String rsvFld1=firstLine.substring(82);
+									gdEupsBatchConsoleInfo.setRsvFld1(rsvFld1);
+									gdEupsBatchConsoleInfo.setBatNo(batNo);
+									gdEupsBatchConsoleInfo.setRsvFld9(rsvFld9);
+									gdEupsBatchConsoleInfoRepository.updateConsoleInfo(gdEupsBatchConsoleInfo);
+									logger.info("==========Successful to update GDEupsBatchConsoleInfo");
+								}
+								i++;
+						}
+						bufferedReader.close();
+						fileReader.close();
+				} catch (FileNotFoundException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				context.setData(ParamKeys.TOT_CNT, Integer.parseInt(totCnt));
+				context.setData(ParamKeys.TOT_AMT, new BigDecimal(totAmt));
+				System.out.println();
+				System.out.println();
+				System.out.println(context.getData("totCnt"));
+				System.out.println(context.getData("totAmt"));
+				logger.info("==========End  BatchDataFileAction  updateInfo");
 		}
 }
 
