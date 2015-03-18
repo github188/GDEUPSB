8e609154a3cd8fdda4e659ac947d6fc66fe6c027
Li_Haoxun <Li_Haoxun@2ac714ae-9a16-11e4-ad83-2f06aba1930a>
2015-03-18 01:09:53 +0000

    git-svn-id: svn://182.53.32.18/bbip/project/GDEUPSB/TRUNK/GDEUPSB@21523 2ac714ae-9a16-11e4-ad83-2f06aba1930a

--- src/com/bocom/bbip/gdeupsb/action/transportfee/PrintCallThirdAction.java
+++ src/com/bocom/bbip/gdeupsb/action/transportfee/PrintCallThirdAction.java
@@ -1,11 +1,13 @@
 package com.bocom.bbip.gdeupsb.action.transportfee;
 
 import java.util.Date;
+import java.util.HashMap;
 import java.util.Map;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 
 import com.bocom.bbip.eups.action.BaseAction;
 import com.bocom.bbip.eups.adaptor.ThirdPartyAdaptor;
@@ -18,6 +20,16 @@ import com.bocom.bbip.gdeupsb.entity.GDEupsbTrspFeeInfo;
 import com.bocom.bbip.gdeupsb.entity.GDEupsbTrspTxnJnl;
 import com.bocom.bbip.gdeupsb.repository.GDEupsbTrspFeeInfoRepository;
 import com.bocom.bbip.gdeupsb.repository.GDEupsbTrspTxnJnlRepository;
+import com.bocom.bbip.gdeupsb.utils.ConnectThdUtils;
+import com.bocom.jump.bp.JumpException;
+import com.bocom.jump.bp.channel.CommunicationException;
+import com.bocom.jump.bp.channel.DefaultTransport;
+import com.bocom.jump.bp.channel.Transform;
+import com.bocom.jump.bp.channel.interceptors.DecoderTransform;
+import com.bocom.jump.bp.channel.interceptors.EncoderTransform;
+import com.bocom.jump.bp.channel.interceptors.RequestTransform;
+import com.bocom.jump.bp.channel.interceptors.ResponseTransform;
+import com.bocom.jump.bp.channel.tcp.SocketGateway;
 import com.bocom.jump.bp.core.Context;
 import com.bocom.jump.bp.core.CoreException;
 import com.bocom.jump.bp.core.CoreRuntimeException;
@@ -31,12 +43,41 @@ public class PrintCallThirdAction extends BaseAction{
 	GDEupsbTrspFeeInfoRepository gdEupsbTrspFeeInfoRepository;
 	@Autowired
 	GDEupsbTrspTxnJnlRepository gdEupsbTrspTxnJnlRepository;
+	
+	@Autowired
+	@Qualifier("TRSP00Transport")
+	DefaultTransport trspTransport;
+	
+	@Autowired
+	@Qualifier("trspGateWay")
+	SocketGateway gateway;
+	
+	@SuppressWarnings({ "unused", "unchecked" })
 	public void execute(Context ctx) throws CoreException,CoreRuntimeException{
 		log.info("PrintCallThirdAction start.......");
 		ctx.setState("fail");
 		GDEupsbTrspTxnJnl gdEupsbTrspTxnJnl = new GDEupsbTrspTxnJnl();
 		
-		Map<String,Object> thdReturnMessage = callThdTradeManager.trade(ctx);
+		String enCodePath="packet://WEB-INF/classes/config/stream/TRSP00/f484014.xml";
+		String deCodePath="packet://WEB-INF/classes/config/stream/TRSP00/p484004.xml";
+		trspTransport.setEncodeTransforms(new Transform[] { new EncoderTransform(enCodePath), new RequestTransform() });
+		trspTransport.setDecodeTransforms(new Transform[] { new DecoderTransform(deCodePath), new ResponseTransform() });
+		trspTransport.setGateway(gateway);
+		
+		
+		Map responseMessage=new HashMap();
+		
+		try {
+			 responseMessage = (Map)trspTransport.submit(ctx.getDataMap(), ctx);
+		} catch (CommunicationException e) {
+			e.printStackTrace();
+		} catch (JumpException e) {
+			e.printStackTrace();
+		}
+//		ConnectThdUtils connectThdUtils = new ConnectThdUtils();
+//		Map<String,Object> thdReturnMessage = connectThdUtils.getThdResponse(enCodePath,deCodePath,ctx);
+		
+//		Map<String,Object> thdReturnMessage = callThdTradeManager.trade(ctx);
 		log.info("call third start....[the state is" + ctx.getState() + "]");
 		
 
@@ -79,7 +120,7 @@ public class PrintCallThirdAction extends BaseAction{
 			ctx.setData(ParamKeys.RSP_MSG, "交易失败");
 			throw new CoreRuntimeException( ErrorCodes.TRANSACTION_ERROR_OTHER_ERROR);
 		}else{
-			if(!"000".equals(thdReturnMessage.get(GDParamKeys.TRSP_CD))){				
+			if(!"000".equals(responseMessage.get(GDParamKeys.TRSP_CD))){				
 				ctx.setData(GDParamKeys.TXN_ST, "F");
 				ctx.setData(GDParamKeys.TTXN_ST, "F");
 				gdEupsbTrspTxnJnl.setTtxnSt(ctx.getData(GDParamKeys.TTXN_ST).toString());
@@ -95,7 +136,7 @@ public class PrintCallThirdAction extends BaseAction{
 //	       <Exec func="PUB:DefaultErrorProc"/>
 
 				ctx.setData(ParamKeys.RSP_CDE, ErrorCodes.TRANSACTION_ERROR_OTHER_ERROR);
-				ctx.setData(ParamKeys.RSP_MSG, "路桥方返回：" + thdReturnMessage.get(GDParamKeys.TRSP_CD));
+				ctx.setData(ParamKeys.RSP_MSG, "路桥方返回：" + responseMessage.get(GDParamKeys.TRSP_CD));
 				throw new CoreRuntimeException( ErrorCodes.TRANSACTION_ERROR_OTHER_ERROR);
 				
 
