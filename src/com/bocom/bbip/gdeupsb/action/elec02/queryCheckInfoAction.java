18a13b0be6fb586ab9faf48c6c1918a1f9bb65ff
Qian_Chen <Qian_Chen@2ac714ae-9a16-11e4-ad83-2f06aba1930a>
2015-03-19 13:34:57 +0000

    git-svn-id: svn://182.53.32.18/bbip/project/GDEUPSB/TRUNK/GDEUPSB@22591 2ac714ae-9a16-11e4-ad83-2f06aba1930a

--- src/com/bocom/bbip/gdeupsb/action/elec02/queryCheckInfoAction.java
+++ src/com/bocom/bbip/gdeupsb/action/elec02/queryCheckInfoAction.java
@@ -14,16 +14,18 @@ import com.bocom.bbip.eups.action.BaseAction;
 import com.bocom.bbip.eups.common.ErrorCodes;
 import com.bocom.bbip.eups.common.ParamKeys;
 import com.bocom.bbip.gdeupsb.entity.EupsTransJournalELEC02Check;
-import com.bocom.bbip.gdeupsb.repository.EupsTransJournalELEC02CheckRepository;
+import com.bocom.bbip.gdeupsb.repository.GdEupsTransJournalRepository;
 import com.bocom.bbip.utils.Assert;
 import com.bocom.bbip.utils.ContextUtils;
 import com.bocom.bbip.utils.DateUtils;
 import com.bocom.jump.bp.core.Context;
 import com.bocom.jump.bp.core.CoreException;
 import com.bocom.jump.bp.core.CoreRuntimeException;
-/**460265*/
+
+/** 460265 */
 public class queryCheckInfoAction extends BaseAction {
 	private static final Log logger = LogFactory.getLog(queryCheckInfoAction.class);
+
 	public void process(Context context) throws CoreException,
 			CoreRuntimeException {
 		logger.info("query checkinfo start!!");
@@ -32,29 +34,29 @@ public class queryCheckInfoAction extends BaseAction {
 		context.setData(ParamKeys.PAGE_NUM, 1);
 		context.setData(ParamKeys.PAGE_SIZE, 10);
 		final String NodNo = ContextUtils.assertDataHasLengthAndGetNNR(context,
-				ParamKeys.BK,  ErrorCodes.EUPS_FIELD_EMPTY);
-		//Assert.isFalse(NodNo.equalsIgnoreCase(GDConstants.MANAGE_ORG_445012),  ErrorCodes.EUPS_FIELD_EMPTY,
-		//		"非管理机构不能执行本交易");
+				ParamKeys.BK, ErrorCodes.EUPS_FIELD_EMPTY);
+		// Assert.isFalse(NodNo.equalsIgnoreCase(GDConstants.MANAGE_ORG_445012),
+		// ErrorCodes.EUPS_FIELD_EMPTY,
+		// "非管理机构不能执行本交易");
 		final String brNo = ContextUtils.assertDataHasLengthAndGetNNR(context,
-				ParamKeys.BR,  ErrorCodes.EUPS_FIELD_EMPTY);
+				ParamKeys.BR, ErrorCodes.EUPS_FIELD_EMPTY);
 		final String BgnDat = ContextUtils.assertDataHasLengthAndGetNNR(
-				context, "BgnDat",  ErrorCodes.EUPS_FIELD_EMPTY);
+				context, "BgnDat", ErrorCodes.EUPS_FIELD_EMPTY);
 		final String EndDat = ContextUtils.assertDataHasLengthAndGetNNR(
-				context, "EndDat",  ErrorCodes.EUPS_FIELD_EMPTY);
-		Pageable pageable=ContextUtils.getDataAsObject(context, PageRequest.class);
-		Assert.isFalse(null==pageable, ErrorCodes.EUPS_FIELD_EMPTY);
-		Map map=new HashMap();
-		Date start=DateUtils.parse(BgnDat);
-		Date end=DateUtils.parse(EndDat);
+				context, "EndDat", ErrorCodes.EUPS_FIELD_EMPTY);
+		Pageable pageable = ContextUtils.getDataAsObject(context, PageRequest.class);
+		Assert.isFalse(null == pageable, ErrorCodes.EUPS_FIELD_EMPTY);
+		Map map = new HashMap();
+		Date start = DateUtils.parse(BgnDat);
+		Date end = DateUtils.parse(EndDat);
 		map.put("EupsBusType", "ELEC02");
 		map.put("startDate", start);
 		map.put("endDate", end);
-		Page<EupsTransJournalELEC02Check> page=get(EupsTransJournalELEC02CheckRepository.class).getCheckInfo(pageable, map);
-		
+
+		Page<EupsTransJournalELEC02Check> page = get(GdEupsTransJournalRepository.class).getCheckInfo(pageable, map);
+
 		logger.info("query checkinfo success!!");
-		
-	
-		
+
 	}
 
 }
