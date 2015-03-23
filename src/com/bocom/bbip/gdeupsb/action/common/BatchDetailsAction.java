a689ab2031a37e84e76b4e07b9fed3fc32d0b884
Li_Yawei <Li_Yawei@2ac714ae-9a16-11e4-ad83-2f06aba1930a>
2015-03-23 13:21:46 +0000

    git-svn-id: svn://182.53.32.18/bbip/project/GDEUPSB/TRUNK/GDEUPSB@24249 2ac714ae-9a16-11e4-ad83-2f06aba1930a

---
+++ src/com/bocom/bbip/gdeupsb/action/common/BatchDetailsAction.java
@@ -0,0 +1,36 @@
+package com.bocom.bbip.gdeupsb.action.common;
+
+import java.util.List;
+
+import com.bocom.bbip.eups.action.BaseAction;
+import com.bocom.bbip.eups.common.ParamKeys;
+import com.bocom.bbip.eups.entity.EupsBatchConsoleInfo;
+import com.bocom.bbip.eups.entity.EupsBatchInfoDetail;
+import com.bocom.bbip.eups.repository.EupsBatchConsoleInfoRepository;
+import com.bocom.bbip.eups.repository.EupsBatchInfoDetailRepository;
+import com.bocom.bbip.gdeupsb.entity.GDEupsBatchConsoleInfo;
+import com.bocom.bbip.gdeupsb.repository.GDEupsBatchConsoleInfoRepository;
+import com.bocom.bbip.utils.BeanUtils;
+import com.bocom.jump.bp.core.Context;
+import com.bocom.jump.bp.core.CoreException;
+import com.bocom.jump.bp.core.CoreRuntimeException;
+
+public class BatchDetailsAction extends BaseAction{
+		@Override
+		public void execute(Context context) throws CoreException,
+				CoreRuntimeException {
+				String batNo=context.getData(ParamKeys.BAT_NO).toString();
+				//GDEupsBatchConsoleInfo 得到代收付文件名字
+				GDEupsBatchConsoleInfo gdEupsBatchConsoleInfo=get(GDEupsBatchConsoleInfoRepository.class).findOne(batNo);
+				String fleNme=gdEupsBatchConsoleInfo.getRsvFld8();
+				//由代收付文件得到批次号
+				EupsBatchConsoleInfo eupsBatchConsoleInfos=new EupsBatchConsoleInfo();
+				eupsBatchConsoleInfos.setFleNme(fleNme);
+				String eupsBatNo=get(EupsBatchConsoleInfoRepository.class).find(eupsBatchConsoleInfos).get(0).getBatNo();
+				//根据批次号获得信息
+				EupsBatchInfoDetail eupsBatchInfoDetails=new EupsBatchInfoDetail();
+				eupsBatchInfoDetails.setBatNo(eupsBatNo);
+				List<EupsBatchInfoDetail> list=get(EupsBatchInfoDetailRepository.class).find(eupsBatchInfoDetails);
+				context.setData("detailList", BeanUtils.toMaps(list));
+		}
+}
