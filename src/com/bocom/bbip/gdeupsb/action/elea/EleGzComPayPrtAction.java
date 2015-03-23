cb576630ad244229c2a7d12d92fbb5ebbe739de5
Qian_Chen <Qian_Chen@2ac714ae-9a16-11e4-ad83-2f06aba1930a>
2015-03-23 01:45:13 +0000

    git-svn-id: svn://182.53.32.18/bbip/project/GDEUPSB/TRUNK/GDEUPSB@23778 2ac714ae-9a16-11e4-ad83-2f06aba1930a

--- src/com/bocom/bbip/gdeupsb/action/elea/EleGzComPayPrtAction.java
+++ src/com/bocom/bbip/gdeupsb/action/elea/EleGzComPayPrtAction.java
@@ -1,5 +1,6 @@
 package com.bocom.bbip.gdeupsb.action.elea;
 
+import java.math.BigDecimal;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
@@ -8,10 +9,8 @@ import java.util.Map;
 import com.bocom.bbip.eups.action.BaseAction;
 import com.bocom.bbip.eups.action.eupsreport.ReportHelper;
 import com.bocom.bbip.eups.common.ParamKeys;
-import com.bocom.bbip.eups.entity.EupsTransJournal;
 import com.bocom.bbip.eups.entity.MFTPConfigInfo;
 import com.bocom.bbip.eups.repository.EupsThdFtpConfigRepository;
-import com.bocom.bbip.eups.repository.EupsTransJournalRepository;
 import com.bocom.bbip.file.reporting.impl.VelocityTemplatedReportRender;
 import com.bocom.bbip.gdeupsb.common.GDParamKeys;
 import com.bocom.bbip.gdeupsb.repository.GdEupsTransJournalRepository;
@@ -36,13 +35,15 @@ public class EleGzComPayPrtAction extends BaseAction {
 		log.info("EleGzComPayPrtAction start!..");
 		// 转换为转入账号
 		String dpTyp = context.getData(GDParamKeys.GZ_ELE_DPT_TYP);
-		String comNo = CodeSwitchUtils.codeGenerator("DL_IActNo", dpTyp);
+		String inAct = CodeSwitchUtils.codeGenerator("DL_IActNo", dpTyp);
+		context.setData("inAct", inAct);
 
-		log.info("单位编号经过转换后，由[" + dpTyp + "]，变为[" + comNo + "]");
+		log.info("转入帐号经过转换后，由[" + dpTyp + "]，变为[" + inAct + "]");
 
 		// 转换为转入账号名称
-		String comNm = CodeSwitchUtils.codeGenerator("DL_IActNm", dpTyp);
-		log.info("单位名称经过转换后，由[" + dpTyp + "]，变为[" + comNm + "]");
+		String inActNm = CodeSwitchUtils.codeGenerator("DL_IActNm", dpTyp);
+		log.info("转入帐号名称经过转换后，由[" + dpTyp + "]，变为[" + inActNm + "]");
+		context.setData("inActNm", inActNm);
 
 		// 内部账户转换-电力发起划账
 		String cAgtNo = CodeSwitchUtils.codeGenerator("DptTypToCAgtNo_DL", dpTyp);
@@ -64,62 +65,86 @@ public class EleGzComPayPrtAction extends BaseAction {
 		String bgnDte = context.getData("bgnDat"); // 开始日期
 		String endDte = context.getData("endDat"); // 结束日期
 
+		context.setData(ParamKeys.TXN_DAT, DateUtils.format(new Date(), DateUtils.STYLE_yyyyMMdd));
+
 		// 会计流水号
 		String vchNo = context.getData(ParamKeys.MFM_VCH_NO);
 		if (StringUtils.isEmpty(vchNo)) {
 			vchNo = "";
 		}
 		// 客户账号
-		String cusAc = context.getData(ParamKeys.MFM_VCH_NO);
+		String cusAc = context.getData(ParamKeys.CUS_AC);
 		if (StringUtils.isEmpty(cusAc)) {
 			cusAc = "";
 		}
 
-		String rptName = "电力对公缴费记账回执" + bgnDte + "-" + endDte + ".rpt"; // 生成的报表名称
+		StringBuffer rptName = new StringBuffer();
+		rptName.append("电力对公缴费记账回执");
+		rptName.append(bgnDte);
+		rptName.append("-");
+		rptName.append(endDte);
+		rptName.append(".rpt");
 
 		// 生成对公回执报表
 		Map<String, Object> inparaMap = new HashMap<String, Object>();
 		inparaMap.put("cAgtNoDl", cAgtNoDl);
 		inparaMap.put("cAgtNoYh", cAgtNoYh);
 		inparaMap.put("cAgtNoPl", cAgtNoPl);
-		inparaMap.put("endDte", DateUtils.parse(endDte,DateUtils.STYLE_SIMPLE_DATE));
-		inparaMap.put("bgnDte", DateUtils.parse(bgnDte,DateUtils.STYLE_SIMPLE_DATE));
+		inparaMap.put("endDte", DateUtils.parse(endDte, DateUtils.STYLE_yyyyMMdd));
+		inparaMap.put("bgnDte", DateUtils.parse(bgnDte, DateUtils.STYLE_yyyyMMdd));
 		inparaMap.put("tckNo", vchNo); // 会计流水号
 		inparaMap.put("cusAc", cusAc);
 		log.info("经过组装，最终的查询条件inparaMap=" + inparaMap);
 
 		List<Map<String, Object>> conPayList = get(GdEupsTransJournalRepository.class).findComPayInfo(inparaMap);
-		log.info("conPayList查询结果为:" + conPayList);
 
-		// EupsThdFtpConfigRepository eupsThdFtpConfigRepository =
-		// get(EupsThdFtpConfigRepository.class);
-		//
-		// ReportHelper reportHelper = get(ReportHelper.class);
-		// MFTPConfigInfo mftpConfigInfo =
-		// reportHelper.getMFTPConfigInfo(eupsThdFtpConfigRepository);
-		// log.info((new
-		// StringBuilder("mftpConfigInfo:>>>>").append(BeanUtils.toMap(mftpConfigInfo))).toString());
-		//
-		// VelocityTemplatedReportRender render = new
-		// VelocityTemplatedReportRender();
-		// try {
-		// render.afterPropertiesSet();
-		// } catch (Exception e) {
-		// e.printStackTrace();
-		// }
-		// Map<String,String> map = new HashMap<String,String>();
-		// map.put("sample", "config/report/watr00_printTransJournal.vm");
-		// render.setReportNameTemplateLocationMapping(map);
-		// context.setData("eles", eupsTransJournals);
-		// String result = render.renderAsString("sample", context);
-		// logger.info(result);
-		// String date = DateUtils.format(new Date(), DateUtils.STYLE_HHmmss);
-		// StringBuffer fileName = new StringBuffer((new
-		// StringBuilder("WATR00"+br+txnDat).append(date).toString()));
-		// reportHelper.createFileAndSendMFTP(context, result, fileName,
-		// mftpConfigInfo);
-		// context.setData("filName", fileName);
-		// logger.info("QueryAndPrintTransJournalServiceActionWATR00 execute end ... ...");
+		Map<String, Object> conPayMap = conPayList.get(0);
+		BigDecimal txnAmtB = (BigDecimal) conPayMap.get("TXN_AMT");
+		context.setData("txnAmt", txnAmtB.toString());
+
+		Date acDteD = (Date) conPayMap.get("AC_DTE"); // 会计日期
+		context.setData("acDte", DateUtils.format(acDteD, DateUtils.STYLE_yyyyMMdd));
+
+		String txnTlr = (String) conPayMap.get("TXN_TLR"); // 交易柜员
+		context.setData("txnTlr", txnTlr);
+
+		String mfmVchNo = (String) conPayMap.get("MFM_VCH_NO"); // 会计流水号
+		context.setData("mfmVchNo", mfmVchNo);
+
+		String cusNm = (String) conPayMap.get("CUS_NME"); // 会计流水号
+		context.setData("cusNm", cusNm);
+		
+		String jnlCusAc = (String) conPayMap.get("CUS_AC"); // 会计流水号
+		context.setData("jnlCusAc", jnlCusAc);
+
+		log.info("交易金额为=" + txnAmtB.toString());
+
+		log.info("conPayList查询结果为:" + conPayList);
+		context.setData("eles", conPayList);
+
+		EupsThdFtpConfigRepository eupsThdFtpConfigRepository = get(EupsThdFtpConfigRepository.class);
+		ReportHelper reportHelper = get(ReportHelper.class);
+		MFTPConfigInfo mftpConfigInfo =
+				reportHelper.getMFTPConfigInfo(eupsThdFtpConfigRepository);
+		log.info((new StringBuilder("mftpConfigInfo:>>>>").append(BeanUtils.toMap(mftpConfigInfo))).toString());
+
+		VelocityTemplatedReportRender render = new
+				VelocityTemplatedReportRender();
+		try {
+			render.afterPropertiesSet();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		Map<String, String> map = new HashMap<String, String>();
+		map.put("sample", "config/report/elec01/eleGzComPay.vm");
+		render.setReportNameTemplateLocationMapping(map);
+		String result = render.renderAsString("sample", context);
+		log.info(result);
+		String date = DateUtils.format(new Date(), DateUtils.STYLE_HHmmss);
+		reportHelper.createFileAndSendMFTP(context, result, rptName,
+				mftpConfigInfo);
+		context.setData("filName", rptName);
+		log.info("QueryAndPrintTransJournalServiceActionWATR00 execute end ... ...");
 	}
 
 }
