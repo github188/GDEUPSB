017debd5fffef4cd4f2be34283fb9105a86e7f53
Li_Yawei <Li_Yawei@2ac714ae-9a16-11e4-ad83-2f06aba1930a>
2015-03-24 11:09:57 +0000

    git-svn-id: svn://182.53.32.18/bbip/project/GDEUPSB/TRUNK/GDEUPSB@24872 2ac714ae-9a16-11e4-ad83-2f06aba1930a

---
+++ src/com/bocom/bbip/gdeupsb/action/efek/QryComNoBycomNmeAction.java
@@ -0,0 +1,26 @@
+package com.bocom.bbip.gdeupsb.action.efek;
+
+import com.bocom.bbip.eups.action.BaseAction;
+import com.bocom.bbip.eups.common.ParamKeys;
+import com.bocom.jump.bp.core.Context;
+import com.bocom.jump.bp.core.CoreException;
+import com.bocom.jump.bp.core.CoreRuntimeException;
+
+public class QryComNoBycomNmeAction extends BaseAction{
+		@Override
+		public void execute(Context context) throws CoreException,
+				CoreRuntimeException {
+				log.info("===================Start   QryComNoBycomNmeAction");
+				String comNme=context.getData(ParamKeys.COMPANY_NAME).toString();
+				String comNo="";
+				if(comNme.equals("佛山") || comNme.equals("1")){
+						comNo="foshan";
+				}else if(comNme.equals("清远") || comNme.equals("2")){
+						comNo="qingyuan";
+				}else if(comNme.equals("中山") || comNme.equals("3")){
+						comNo="GDELEC00";
+				}
+				context.setData(ParamKeys.COMPANY_NO, comNo);
+				
+		}
+}
