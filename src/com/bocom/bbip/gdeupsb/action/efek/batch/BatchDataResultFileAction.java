d8b1a70504725af486e03f22a6575df016e8c368
Li_Yawei <Li_Yawei@2ac714ae-9a16-11e4-ad83-2f06aba1930a>
2015-03-24 13:09:25 +0000

    git-svn-id: svn://182.53.32.18/bbip/project/GDEUPSB/TRUNK/GDEUPSB@25053 2ac714ae-9a16-11e4-ad83-2f06aba1930a

--- src/com/bocom/bbip/gdeupsb/action/efek/batch/BatchDataResultFileAction.java
+++ src/com/bocom/bbip/gdeupsb/action/efek/batch/BatchDataResultFileAction.java
@@ -10,9 +10,15 @@ import java.util.Map;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 
+import com.bocom.bbip.eups.action.common.CommThdRspCdeAction;
 import com.bocom.bbip.eups.action.common.OperateFTPAction;
 import com.bocom.bbip.eups.action.common.OperateFileAction;
+import com.bocom.bbip.eups.adaptor.ThirdPartyAdaptor;
+import com.bocom.bbip.eups.common.BPState;
+import com.bocom.bbip.eups.common.Constants;
+import com.bocom.bbip.eups.common.ErrorCodes;
 import com.bocom.bbip.eups.common.ParamKeys;
 import com.bocom.bbip.eups.entity.EupsBatchInfoDetail;
 import com.bocom.bbip.eups.entity.EupsThdFtpConfig;
@@ -20,6 +26,7 @@ import com.bocom.bbip.eups.repository.EupsBatchInfoDetailRepository;
 import com.bocom.bbip.eups.repository.EupsThdFtpConfigRepository;
 import com.bocom.bbip.eups.spi.service.batch.AfterBatchAcpService;
 import com.bocom.bbip.eups.spi.vo.AfterBatchAcpDomain;
+import com.bocom.bbip.gdeupsb.common.GDConstants;
 import com.bocom.bbip.gdeupsb.common.GDParamKeys;
 import com.bocom.bbip.gdeupsb.entity.GDEupsBatchConsoleInfo;
 import com.bocom.bbip.gdeupsb.entity.GDEupsEleTmp;
@@ -27,6 +34,7 @@ import com.bocom.bbip.gdeupsb.repository.GDEupsBatchConsoleInfoRepository;
 import com.bocom.bbip.gdeupsb.repository.GDEupsEleTmpRepository;
 import com.bocom.bbip.utils.BeanUtils;
 import com.bocom.bbip.utils.DateUtils;
+import com.bocom.bbip.utils.StringUtils;
 import com.bocom.jump.bp.core.Context;
 import com.bocom.jump.bp.core.CoreException;
 
@@ -44,6 +52,9 @@ public class BatchDataResultFileAction implements AfterBatchAcpService{
 	EupsThdFtpConfigRepository eupsThdFtpConfigRepository;
 	@Autowired
 	EupsBatchInfoDetailRepository eupsBatchInfoDetailRepository;
+	@Autowired
+	@Qualifier("callThdTradeManager")
+	ThirdPartyAdaptor callThdTradeManager;
 	/**
 	 * 南方电网 结果文件处理
 	 */
@@ -77,7 +88,7 @@ public class BatchDataResultFileAction implements AfterBatchAcpService{
 			operateFTP.putCheckFile(eupsThdFtpConfig);
 			
 			//TODO 通知第三方
-			
+//			callThd(context);
 			logger.info("===============End  BatchDataResultFileAction  afterBatchDeal");	
 		}
 	/**
@@ -151,5 +162,100 @@ public class BatchDataResultFileAction implements AfterBatchAcpService{
 			resultMap.put(ParamKeys.EUPS_FILE_DETAIL, BeanUtils.toMaps(list));
 			logger.info("===============End  BatchDataResultFileAction  createFileMap");	
 			return resultMap;
+	}
+/**
+ * 外发第三方
+ */
+	public void callThd(Context context){
+		logger.info("========Start BatchDataResultFileAction callThd");	
+		
+		context.setData(GDParamKeys.TREATY_VERSION, GDConstants.TREATY_VERSION);//协议版本
+		context.setData(GDParamKeys.TRADE_PERSON_IDENTIFY, GDConstants.TRADE_PERSON_IDENTIFY);//交易人标识
+		context.setData(GDParamKeys.BAG_TYPE, GDConstants.BAG_TYPE);//数据包类型
+		context.setData(GDParamKeys.TRADE_START,GDConstants.TRADE_START);//交易发起方
+			
+			context.setData(GDParamKeys.TRADE_SEND_DATE,DateUtils.format(new Date(),DateUtils.STYLE_yyyyMMdd));//交易发送日期
+			context.setData(GDParamKeys.TRADE_SEND_TIME, DateUtils.formatAsHHmmss(new Date()));//交易发送时间
+			context.setData(GDParamKeys.TRADE_PRIORITY, GDConstants.TRADE_PRIORITY);//交易优先
+			context.setData(GDParamKeys.REDUCE_SIGN, GDConstants.REDUCE_SIGN);//压缩标志
+			context.setData(GDParamKeys.TRADE_RETURN_CODE, GDConstants.TRADE_RETURN_CODE);//交易返回代码
+
+			
+			context.setData(GDParamKeys.NET_NAME, GDConstants.NET_NAME);//网点名称
+			context.setData(GDParamKeys.SECRETKEY_INDEX, GDConstants.SECRETKEY_INDEX);//密钥索引
+			context.setData(GDParamKeys.SECRETKEY_INIT, GDConstants.SECRETKEY_INIT);//密钥初始向量
+			context.setData(GDParamKeys.TRADE_RECEIVE, GDConstants.TRADE_RECEIVE);//交易接收方
+			context.setData(GDParamKeys.TRADE_SOURCE_ADD, GDConstants.TRADE_SOURCE_ADD);//交易源地址
+			context.setData(GDParamKeys.TRADE_AIM_ADD, GDConstants.TRADE_AIM_ADD);//交易目标地址
+			context.setData("PKGCNT", "000000");	
+					
+					try{
+						Map<String, Object> rspMap = callThdTradeManager.trade(context);
+						
+							if(BPState.isBPStateNormal(context)){
+									if(null !=rspMap){
+										 	context.setDataMap(rspMap);
+							                context.setData(ParamKeys.THIRD_RETURN_MESSAGE, rspMap);
+							               context.setData("qryCusInformation", rspMap.get("qryCusInformation"));
+							                //第三方返回码
+							                CommThdRspCdeAction rspCdeAction = new CommThdRspCdeAction();
+							                String responseCode = rspCdeAction.getThdRspCde(rspMap, context.getData(ParamKeys.EUPS_BUSS_TYPE).toString());
+							                logger.info("third response code="+responseCode);
+							                if(StringUtils.isEmpty(responseCode)){
+							                	responseCode=ErrorCodes.EUPS_THD_SYS_ERROR;
+							                }
+							                context.setData(ParamKeys.RESPONSE_CODE, responseCode);
+							                
+							             // 第三方交易成功
+								                if (GDConstants.SUCCESS_CODE.equals(responseCode)) {
+								                    logger.info("The third process response successful.");
+								                    context.setData(ParamKeys.TXN_STS, Constants.TXNSTS_SUCCESS);
+								                    context.setData(ParamKeys.THD_TXN_STS, Constants.THD_TXNSTS_SUCCESS);
+								                    context.setData(ParamKeys.RSP_CDE, GDConstants.SUCCESS_CODE);
+								                    context.setData(ParamKeys.RSP_MSG, "交易成功");
+								                }else if(BPState.isBPStateReversalFail(context)){
+								                	context.setData(ParamKeys.THD_TXN_STS,Constants.THD_TXNSTS_FAIL);
+								                	context.setData(GDParamKeys.MSGTYP, "E");
+								                	context.setData(ParamKeys.RSP_CDE, "EFE999");
+								                	context.setData(ParamKeys.RSP_MSG, "交易失败");
+								                }else if(BPState.isBPStateOvertime(context)){
+								                	context.setData(ParamKeys.THD_TXN_STS,Constants.THD_TXNSTS_FAIL);
+								                	context.setData(GDParamKeys.MSGTYP, "E");
+								                	context.setData(ParamKeys.RSP_CDE, "EFE999");
+								                	context.setData(ParamKeys.RSP_MSG, "交易超时");
+								                }else if(BPState.isBPStateSystemError(context)){
+								                	context.setData(ParamKeys.THD_TXN_STS,Constants.THD_TXNSTS_FAIL);
+								                	context.setData(GDParamKeys.MSGTYP, "E");
+								                	context.setData(ParamKeys.RSP_CDE, "EFE999");
+								                	context.setData(ParamKeys.RSP_MSG, "系统错误");
+								                }else if(BPState.isBPStateTransFail(context)){
+								                	context.setData(ParamKeys.THD_TXN_STS,Constants.THD_TXNSTS_FAIL);
+								                	context.setData(GDParamKeys.MSGTYP, "E");
+								                	context.setData(ParamKeys.RSP_CDE, "EFE999");
+								                	context.setData(ParamKeys.RSP_MSG, "发送失败");
+								                }else{
+								                	context.setData(ParamKeys.THD_TXN_STS,Constants.THD_TXNSTS_FAIL);
+								                	context.setData(GDParamKeys.MSGTYP, "E");
+								                	context.setData(ParamKeys.RSP_CDE, "EFE999");
+								                	context.setData(ParamKeys.RSP_MSG, "交易失败，其他未知情况");
+								                }
+									}
+							}else{
+									logger.info("~~~~~~~~~~~发送失败");
+								    context.setState(BPState.BUSINESS_PROCESSNIG_STATE_FAIL);
+					                context.setData(ParamKeys.TXN_STS, Constants.TXNSTS_REVERSE);
+					                context.setData(ParamKeys.THD_TXN_STS, Constants.TXNSTS_FAIL);
+					                context.setData(ParamKeys.RESPONSE_TYPE, Constants.RESPONSE_TYPE_FAIL);
+					                context.setData(ParamKeys.RESPONSE_CODE, ErrorCodes.EUPS_THD_SYS_ERROR);
+					                context.setData(ParamKeys.RESPONSE_MESSAGE, Constants.RESPONSE_MSG_FAIL);
+					                context.setData(ParamKeys.THD_RSP_MSG,Constants.RESPONSE_MSG_FAIL);
+					                throw new CoreException("发送失败");
+							}
+					}catch(CoreException e){
+						logger.info("Bypass call THIRD response failed or unknow error.");
+						context.setData(ParamKeys.TXN_STS, Constants.TXNSTS_REVERSE);
+						context.setData(ParamKeys.THD_TXN_STS, Constants.TXNSTS_FAIL);
+						context.setState(BPState.BUSINESS_PROCESSNIG_STATE_FAIL);
+					}	
 	}
 }
