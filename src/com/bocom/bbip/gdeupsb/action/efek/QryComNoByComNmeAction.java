6d3518d3f11ad2da15ffe1e6e02784d456578518
Li_Yawei <Li_Yawei@2ac714ae-9a16-11e4-ad83-2f06aba1930a>
2015-03-24 11:14:35 +0000

    git-svn-id: svn://182.53.32.18/bbip/project/GDEUPSB/TRUNK/GDEUPSB@24876 2ac714ae-9a16-11e4-ad83-2f06aba1930a

--- src/com/bocom/bbip/gdeupsb/action/efek/QryComNoByComNmeAction.java
+++ src/com/bocom/bbip/gdeupsb/action/efek/QryComNoByComNmeAction.java
@@ -5,8 +5,10 @@ import com.bocom.bbip.eups.common.ParamKeys;
 import com.bocom.jump.bp.core.Context;
 import com.bocom.jump.bp.core.CoreException;
 import com.bocom.jump.bp.core.CoreRuntimeException;
-
-public class QryComNoBycomNmeAction extends BaseAction{
+/**
+ *得到单位编号 
+ */
+public class QryComNoByComNmeAction extends BaseAction{
 		@Override
 		public void execute(Context context) throws CoreException,
 				CoreRuntimeException {
@@ -21,6 +23,5 @@ public class QryComNoBycomNmeAction extends BaseAction{
 						comNo="GDELEC00";
 				}
 				context.setData(ParamKeys.COMPANY_NO, comNo);
-				
 		}
 }
