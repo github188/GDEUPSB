f9b95816e97c06571f2f729d2a64fa31e5ed9c19
Li_Yawei <Li_Yawei@2ac714ae-9a16-11e4-ad83-2f06aba1930a>
2015-03-25 12:16:37 +0000

    git-svn-id: svn://182.53.32.18/bbip/project/GDEUPSB/TRUNK/GDEUPSB@25754 2ac714ae-9a16-11e4-ad83-2f06aba1930a

--- src/com/bocom/bbip/gdeupsb/strategy/efek/queryFeeOnline/QueryFeeAction.java
+++ src/com/bocom/bbip/gdeupsb/strategy/efek/queryFeeOnline/QueryFeeAction.java
@@ -1,10 +1,13 @@
 package com.bocom.bbip.gdeupsb.strategy.efek.queryFeeOnline;
 
+import java.util.Date;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
 import com.bocom.bbip.eups.common.ParamKeys;
 import com.bocom.bbip.gdeupsb.common.GDParamKeys;
+import com.bocom.bbip.utils.DateUtils;
 import com.bocom.jump.bp.core.Context;
 import com.bocom.jump.bp.core.CoreException;
 import com.bocom.jump.bp.core.CoreRuntimeException;
@@ -22,10 +25,12 @@ public class QueryFeeAction implements Executable{
 			logger.info("=========Start  CheckFeeAction ");
 			context.setData(GDParamKeys.TOTNUM, "1");
 			context.setData(GDParamKeys.SVRCOD, "10");
+			if(context.getData(GDParamKeys.CHECK_TYPE).toString().equals("1")){
+					context.setData(GDParamKeys.ELECTRICITY_YEARMONTH, DateUtils.format(new Date(), DateUtils.STYLE_yyyy+DateUtils.STYLE_MM));
+			}
 			context.setData(ParamKeys.RSV_FLD1, context.getData(GDParamKeys.ELECTRICITY_YEARMONTH));
 			context.setData(ParamKeys.THD_CUS_NO, context.getData(GDParamKeys.PAY_NO));
 			context.setData("PKGCNT", "000001");
 			context.setData(GDParamKeys.BUS_TYPE,"110");
-//			context.setData(ParamKeys.BAK_FLD1, context.getData(GDParamKeys.PAY_NO));
 	}
 }
