b10efff5c190d5b261eb23c76a6be577ce0ce8e8
Li_Yawei <Li_Yawei@2ac714ae-9a16-11e4-ad83-2f06aba1930a>
2015-03-26 14:37:03 +0000

    git-svn-id: svn://182.53.32.18/bbip/project/GDEUPSB/TRUNK/GDEUPSB@26723 2ac714ae-9a16-11e4-ad83-2f06aba1930a

--- src/com/bocom/bbip/gdeupsb/repository/TrspCheckTmpRepository.java
+++ src/com/bocom/bbip/gdeupsb/repository/TrspCheckTmpRepository.java
@@ -8,7 +8,7 @@ import com.bocom.bbip.gdeupsb.entity.TrspCheckTmp;
 
 public interface TrspCheckTmpRepository extends PagingAndSortingRepository<TrspCheckTmp, String> {
 		@Delete
-		public void deleteAll(String string);
+		public void deleteAll(String tchkNo);
 		
 		public void updateAll(String tChkNo);
 		
