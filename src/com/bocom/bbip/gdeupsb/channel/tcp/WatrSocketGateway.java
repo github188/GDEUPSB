5c94b39edd841b272e0bcdc62584820b9ab13a7e
Qian_Chen <Qian_Chen@2ac714ae-9a16-11e4-ad83-2f06aba1930a>
2015-03-26 14:35:26 +0000

    git-svn-id: svn://182.53.32.18/bbip/project/GDEUPSB/TRUNK/GDEUPSB@26721 2ac714ae-9a16-11e4-ad83-2f06aba1930a

--- src/com/bocom/bbip/gdeupsb/channel/tcp/WatrSocketGateway.java
+++ src/com/bocom/bbip/gdeupsb/channel/tcp/WatrSocketGateway.java
@@ -166,15 +166,19 @@ public class WatrSocketGateway implements Gateway<Object, Object> {
 		String sndStrPre;
 		try {
 			sndStrPre = new String(arrayOfByte1, "GBK");
+			log.info("sndStrPre="+sndStrPre);
 			String detailDate = sndStrPre.substring(131, sndStrPre.length()); // 子报文
-
+			
+			log.info("detailDate="+detailDate);
 			String md5Date = CryptoUtils.md5(detailDate.toString().getBytes("GBK")); // 校验位
 			// String md5Date=sndStrPre.substring(99,131); //md5加密数据
 			log.info("md5Date=" + md5Date);
 
 			StringBuffer sb = new StringBuffer();
+			log.info("初始报文头(不含校验位)=" + sndStrPre.substring(0, 99));
 			sb.append(sndStrPre.substring(0, 99)); // 初始报文头(不含校验位)
 			sb.append(md5Date);
+			log.info("后面拼接报文=" +detailDate);
 			sb.append(detailDate);
 
 			arrayOfByte1 = sb.toString().getBytes();
