26a331df2f98976beb9ea1f42ff8863bb6b04eae
Li_Guilin <Li_Guilin@2ac714ae-9a16-11e4-ad83-2f06aba1930a>
2015-03-29 09:46:46 +0000

    git-svn-id: svn://182.53.32.18/bbip/project/GDEUPSB/TRUNK/GDEUPSB@27620 2ac714ae-9a16-11e4-ad83-2f06aba1930a

--- src/com/bocom/bbip/gdeupsb/action/transportfee/QueryTransportFeeAction.java
+++ src/com/bocom/bbip/gdeupsb/action/transportfee/QueryTransportFeeAction.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 
 import com.bocom.bbip.eups.action.BaseAction;
+import com.bocom.bbip.eups.adaptor.ThirdPartyAdaptor;
 import com.bocom.bbip.eups.common.BPState;
 import com.bocom.bbip.eups.common.ErrorCodes;
 import com.bocom.bbip.eups.common.ParamKeys;
@@ -19,6 +20,7 @@ import com.bocom.bbip.gdeupsb.common.GDParamKeys;
 import com.bocom.bbip.gdeupsb.entity.GDEupsbTrspPayInfo;
 import com.bocom.bbip.gdeupsb.repository.GDEupsbTrspPayInfoRepository;
 import com.bocom.bbip.utils.DateUtils;
+import com.bocom.bbip.utils.StringUtils;
 import com.bocom.jump.bp.JumpException;
 import com.bocom.jump.bp.channel.CommunicationException;
 import com.bocom.jump.bp.channel.DefaultTransport;
@@ -43,47 +45,43 @@ public class QueryTransportFeeAction extends BaseAction{
 	GDEupsbTrspPayInfoRepository gdEupsbTrspPayInfoRepository;
 	
 	@Autowired
-	@Qualifier("TRSP00Transport")
-	DefaultTransport trspTransport;
-	
-	@Autowired
-	@Qualifier("trspGateWay")
-	SocketGateway gateway;
+	ThirdPartyAdaptor callThdTradeManager;
 	
 	@SuppressWarnings({ "unchecked", "unused" })
 	public void execute(Context ctx) throws CoreException,CoreRuntimeException{
 		logger.info("QueryTransportFeeAction start......");
 		
 		ctx.setData(ParamKeys.THD_TXN_CDE, GDConstants.QRY_CAR);
+		String sqn = ctx.getData(GDParamKeys.SQN).toString();
+		Date date = new Date();
+		ctx.setData(GDParamKeys.ACT_DAT, date);
+		ctx.setData(GDParamKeys.TACT_DT, DateUtils.format(date, DateUtils.STYLE_yyyyMMdd));
+		ctx.setData(GDParamKeys.TLOG_NO, StringUtils.substring(sqn, 0, 8)+StringUtils.substring(sqn, 16, 20));
+		ctx.setData(GDParamKeys.THD_KEY, sqn);
 		
-		ctx.setData(GDParamKeys.ACT_DAT, new Date());
-		ctx.setData(GDParamKeys.TACT_DT, new Date());
-		ctx.setData(GDParamKeys.TLOG_NO, ctx.getData(GDParamKeys.SQN));
-		ctx.setData(GDParamKeys.THD_KEY, ctx.getData(GDParamKeys.SQN));
 		
 		
-		
-		String enCodePath="packet://WEB-INF/classes/config/stream/TRSP00/f484011.xml";
-		String deCodePath="packet://WEB-INF/classes/config/stream/TRSP00/p484011.xml";
-		trspTransport.setEncodeTransforms(new Transform[] { new EncoderTransform(enCodePath), new RequestTransform() });
-		trspTransport.setDecodeTransforms(new Transform[] { new DecoderTransform(deCodePath), new ResponseTransform() });
-		trspTransport.setGateway(gateway);
-		
-		Map responseMessage=new HashMap();
-		
-		try {
-			 responseMessage = (Map)trspTransport.submit(ctx.getDataMap(), ctx);
-		} catch (CommunicationException e) {
-			e.printStackTrace();
-		} catch (JumpException e) {
-			e.printStackTrace();
-		}
+//		String enCodePath="packet://WEB-INF/classes/config/stream/TRSP00/f484011.xml";
+//		String deCodePath="packet://WEB-INF/classes/config/stream/TRSP00/p484011.xml";
+//		trspTransport.setEncodeTransforms(new Transform[] { new EncoderTransform(enCodePath), new RequestTransform() });
+//		trspTransport.setDecodeTransforms(new Transform[] { new DecoderTransform(deCodePath), new ResponseTransform() });
+//		trspTransport.setGateway(gateway);
+//		
+//		Map responseMessage=new HashMap();
+//		
+//		try {
+//			 responseMessage = (Map)trspTransport.submit(ctx.getDataMap(), ctx);
+//		} catch (CommunicationException e) {
+//			e.printStackTrace();
+//		} catch (JumpException e) {
+//			e.printStackTrace();
+//		}
 
 //		ConnectThdUtils connectThdUtils = new ConnectThdUtils();
 //		Map<String,Object> responseMessage = connectThdUtils.getThdReosponse(enCodePath,deCodePath,ctx);
 		
 
-//		Map<String,Object> thdReturnMessage = callThdTradeManager.trade(ctx);
+		Map<String,Object> thdReturnMessage = callThdTradeManager.trade(ctx);
 		logger.info("call third start....[the state is" + ctx.getState() + "]");
 		if(ctx.getState().equals(BPState.BUSINESS_PROCESSNIG_STATE_OVERTIME)){
 			ctx.setData(ParamKeys.RSP_MSG, "查询路桥方交易超时");
@@ -94,7 +92,7 @@ public class QueryTransportFeeAction extends BaseAction{
 			//TODO: <Set>RspCod=RBF999</Set>
 			throw new CoreRuntimeException(ErrorCodes.TRANSACTION_ERROR_OTHER_ERROR);
 		}else{
-			if(!"000".equals(responseMessage.get(GDParamKeys.TRSP_CD))){
+			if(!"000".equals(thdReturnMessage.get(GDParamKeys.TRSP_CD))){
 				ctx.setData(ParamKeys.RSP_MSG, "查询路桥方返回错误");
 				//TODO:<Set>RspMsg=STRCAT(路桥方返回: [,$TRspCd,],$TRspMsg)</Set>
 				//TODO: <Set>RspCod=RBF999</Set>
@@ -107,14 +105,14 @@ public class QueryTransportFeeAction extends BaseAction{
 				gdEupsbTrspPayInfo.setCarNo(ctx.getData(GDParamKeys.CAR_NO).toString());
 				gdEupsbTrspPayInfoRepository.delete1(gdEupsbTrspPayInfo);
 				
-				ctx.setDataMap(responseMessage);
+				ctx.setDataMap(thdReturnMessage);
 				ctx.setData(GDParamKeys.BEG_DAT, DateUtils.parse((String)ctx.getData(GDParamKeys.BEG_DAT)));
 				ctx.setData(GDParamKeys.END_DAT, DateUtils.parse((String)ctx.getData(GDParamKeys.END_DAT)));
 				ctx.setData("feeStd", new BigDecimal((String)ctx.getData("feeStd")));
 				ctx.setData("corpus", new BigDecimal((String)ctx.getData("corpus")));
 				ctx.setData("lateFee", new BigDecimal((String)ctx.getData("lateFee")));
 				ctx.setData("txnAmt", new BigDecimal((String)ctx.getData("txnAmt")));
-				System.out.println("@@@@@@@@@@@@@@@@@@@@@@@@@@@@"+ctx);
+				log.info("@@@@@@@@@@@@@@@@@@@@@@@@@@@@"+ctx);
 				gdEupsbTrspPayInfo.setPayMon(ctx.getData(GDParamKeys.PAY_MON).toString());
 				gdEupsbTrspPayInfo.setTcusNm(ctx.getData(GDParamKeys.TCUS_NM).toString());
 				gdEupsbTrspPayInfo.setActDat((Date)ctx.getData(GDParamKeys.ACT_DAT));
