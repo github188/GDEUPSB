7e0846eb4dd7efe2f9e55aa33a4b8cb97af47e98
Wang_Mingqin <Wang_Mingqin@2ac714ae-9a16-11e4-ad83-2f06aba1930a>
2015-03-29 09:20:58 +0000

    git-svn-id: svn://182.53.32.18/bbip/project/GDEUPSB/TRUNK/GDEUPSB@27570 2ac714ae-9a16-11e4-ad83-2f06aba1930a

--- src/com/bocom/bbip/gdeupsb/action/common/PrintEupsbRptsAction.java
+++ src/com/bocom/bbip/gdeupsb/action/common/PrintEupsbRptsAction.java
@@ -6,7 +6,7 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
-import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
@@ -14,19 +14,18 @@ import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.util.CollectionUtils;
 
 import com.bocom.bbip.eups.action.BaseAction;
 import com.bocom.bbip.eups.action.eupsreport.ReportHelper;
 import com.bocom.bbip.eups.common.ErrorCodes;
 import com.bocom.bbip.eups.common.ParamKeys;
 import com.bocom.bbip.eups.entity.EupsThdBaseInfo;
-import com.bocom.bbip.eups.entity.EupsTransJournal;
 import com.bocom.bbip.eups.entity.MFTPConfigInfo;
 import com.bocom.bbip.eups.repository.EupsThdBaseInfoRepository;
 import com.bocom.bbip.eups.repository.EupsThdFtpConfigRepository;
-import com.bocom.bbip.eups.repository.EupsTransJournalRepository;
 import com.bocom.bbip.file.reporting.impl.VelocityTemplatedReportRender;
+import com.bocom.bbip.gdeupsb.entity.GdEupsTransJournal;
+import com.bocom.bbip.gdeupsb.repository.GdEupsTransJournalRepository;
 import com.bocom.bbip.utils.BeanUtils;
 import com.bocom.bbip.utils.DateUtils;
 import com.bocom.bbip.utils.StringUtils;
@@ -47,7 +46,7 @@ public class PrintEupsbRptsAction extends BaseAction {
 		// 业务类型eupsBusTyp 必输
 		// 单位编码comNo 非必输
 		// 需打印交易日期（交易发起前一天？）prtDte 必输yyyy-MM-dd
-		// 报表类型 (全部0，成功1，失败2) prtFlg 必输
+		// 报表类型 (全部0，成功1，失败2,可疑3) prtFlg 必输
 		// 输出：
 		// 成功（打印报表）/失败
 
@@ -72,7 +71,7 @@ public class PrintEupsbRptsAction extends BaseAction {
 		context.setData(ParamKeys.COMPANY_NAME, comNme);
 
 		logger.info("=============context:" + context);
-		EupsTransJournal eupsJnl = new EupsTransJournal();
+		GdEupsTransJournal eupsJnl = new GdEupsTransJournal();
 		eupsJnl.setEupsBusTyp((String) context
 				.getData(ParamKeys.EUPS_BUSS_TYPE));
 		eupsJnl.setBr((String) context.getData(ParamKeys.BR));
@@ -82,68 +81,68 @@ public class PrintEupsbRptsAction extends BaseAction {
 		}
 		eupsJnl.setTxnDte(DateUtils.parse((String) context.getData("prtDte"),
 				DateUtils.STYLE_SIMPLE_DATE));
-
+		
+		List<Map<String, Object>> prtList = new ArrayList<Map<String,Object>>(); 
+		
+		logger.info("======================context:" + context);
+		
 		String prtTtl = null;
 		if ("0".equals(context.getData("prtTyp"))) {
+			prtList = get(GdEupsTransJournalRepository.class).findAllTxnList(eupsJnl);
+			
+			context.setData("TOTCNT", prtList.get(0).get("TOTCNT"));
+			context.setData("TOTAMT", prtList.get(0).get("TOTAMT"));
+			context.setData("SUCCCNT", prtList.get(0).get("SUCCCNT"));
+			context.setData("TOTSUCCAMT", prtList.get(0).get("TOTSUCCAMT"));
+			context.setData("FAILCNT", prtList.get(0).get("FAILCNT"));
+			context.setData("TOTFAILAMT", prtList.get(0).get("TOTFAILAMT"));
+			context.setData("DOUBTCNT", prtList.get(0).get("DOUBTCNT"));
+			context.setData("TOTDOUBTAMT", prtList.get(0).get("TOTDOUBTAMT"));
+			context.setData("OTHERCNT", prtList.get(0).get("OTHERCNT"));
+			context.setData("TOTOTHERAMT", prtList.get(0).get("TOTOTHERAMT"));
+			
 			prtTtl = (String) context.getData(ParamKeys.COMPANY_NAME)
 					.toString().trim()
-					+ "全部交易报表";
+					+ "全部交易清单报表";
 		}
 		if ("1".equals(context.getData("prtTyp"))) {
-			eupsJnl.setTxnSts("S");
+			prtList = get(GdEupsTransJournalRepository.class).findSuccTxnList(eupsJnl);
+			context.setData("SUCCCNT", prtList.get(0).get("SUCCCNT"));
+			context.setData("TOTSUCCAMT", prtList.get(0).get("TOTSUCCAMT"));
 			prtTtl = (String) context.getData(ParamKeys.COMPANY_NAME)
 					.toString().trim()
-					+ "成功交易报表";
+					+ "成功交易清单报表";
 		}
 		if ("2".equals(context.getData("prtTyp"))) {
-			eupsJnl.setTxnSts("F");
+			prtList = get(GdEupsTransJournalRepository.class).findFailTxnList(eupsJnl);
+			context.setData("FAILCNT", prtList.get(0).get("FAILCNT"));
+			context.setData("TOTFAILAMT", prtList.get(0).get("TOTFAILAMT"));
 			prtTtl = (String) context.getData(ParamKeys.COMPANY_NAME)
 					.toString().trim()
-					+ "失败交易报表";
+					+ "失败交易清单报表";
 		}
+		if ("3".equals(context.getData("prtTyp"))) {
+			prtList =get(GdEupsTransJournalRepository.class).findDoubtTxnList(eupsJnl);
+			context.setData("DOUBTCNT", prtList.get(0).get("DOUBTCNT"));
+			context.setData("TOTDOUBTAMT", prtList.get(0).get("TOTDOUBTAMT"));
+			prtTtl = (String) context.getData(ParamKeys.COMPANY_NAME)
+					.toString().trim()
+					+ "可疑交易清单报表";
+		}
+		if ("4".equals(context.getData("prtTyp"))) {
+			prtList = get(GdEupsTransJournalRepository.class).findOthTxnList(eupsJnl);
+			context.setData("OTHERCNT", prtList.get(0).get("OTHERCNT"));
+			context.setData("TOTOTHERAMT", prtList.get(0).get("TOTOTHERAMT"));
+			prtTtl = (String) context.getData(ParamKeys.COMPANY_NAME)
+					.toString().trim()
+					+ "其他情况清单报表";
+		}
+		
 		context.setData("prtTtl", prtTtl);
 		fileName = new StringBuffer((new StringBuilder(prtTtl + "_" + br + "_"
 				+ prtDte + ".txt").toString()));
 
 		logger.info("==============fileName:" + fileName);
-
-		List<EupsTransJournal> prtList = get(EupsTransJournalRepository.class)
-				.find(eupsJnl);
-		if (null == prtList || CollectionUtils.isEmpty(prtList)) {
-			logger.info("There are no records for select check trans journal ");
-			throw new CoreException(ErrorCodes.EUPS_QUERY_NO_DATA);
-		}
-		logger.info("================prtList.size:" + prtList.size());
-
-		context.setData("sumCnt", prtList.size());
-
-		int succCnt = 0;
-		int failCnt = 0;
-
-		BigDecimal sumAmt = new BigDecimal(0.00);
-		BigDecimal sussAmt = new BigDecimal(0.00);
-		BigDecimal failAmt = new BigDecimal(0.00);
-		
-		for (EupsTransJournal perJnl : prtList) {
-			if (!("C".equals(perJnl.getTxnTyp()))) { // 剔除已冲正交易
-				if ("S".equals(perJnl.getTxnSts())) {
-					succCnt++;
-					sussAmt = sussAmt.add(perJnl.getTxnAmt());
-				}
-				if ("F".equals(perJnl.getTxnSts())) {
-					failCnt++;
-					failAmt = failAmt.add(perJnl.getTxnAmt());
-				}
-				sumAmt = sumAmt.add(perJnl.getTxnAmt());
-			}
-		}
-
-		context.setData("succCnt", succCnt);
-		context.setData("failCnt", failCnt);
-
-		context.setData("sumAmt", sumAmt);
-		context.setData("sussAmt", sussAmt);
-		context.setData("failAmt", failAmt);
 
 		EupsThdFtpConfigRepository eupsThdFtpConfigRepository = get(EupsThdFtpConfigRepository.class);
 		ReportHelper reportHelper = get(ReportHelper.class);
@@ -163,32 +162,45 @@ public class PrintEupsbRptsAction extends BaseAction {
 		Map<String, String> map = new HashMap<String, String>();
 
 		if ("0".equals(context.getData("prtTyp"))) {
-			map.put("commonPrtRpt",
-					"config/report/common/commonPrintReport_all.vm");
+			map.put("commonPrtRpt", "config/report/common/commonPrintReport_all.vm");
 			render.setReportNameTemplateLocationMapping(map);
 			context.setData("eles", prtList);
 			result = render.renderAsString("commonPrtRpt", context);
 			logger.info(result);
-		} else {
-			map.put("commonRpt", "config/report/common/commonPrintReport.vm");
+		} else if ("1".equals(context.getData("prtTyp"))) {
+			map.put("commonPrtRpt", "config/report/common/commonPrintReport_succ.vm");
 			render.setReportNameTemplateLocationMapping(map);
 			context.setData("eles", prtList);
-			result = render.renderAsString("commonRpt", context);
+			result = render.renderAsString("commonPrtRpt", context);
+			logger.info(result);
+		} else if ("2".equals(context.getData("prtTyp"))) {
+			map.put("commonPrtRpt", "config/report/common/commonPrintReport_fail.vm");
+			render.setReportNameTemplateLocationMapping(map);
+			context.setData("eles", prtList);
+			result = render.renderAsString("commonPrtRpt", context);
+			logger.info(result);
+		} else if ("3".equals(context.getData("prtTyp"))) {
+			map.put("commonPrtRpt", "config/report/common/commonPrintReport_doubt.vm");
+			render.setReportNameTemplateLocationMapping(map);
+			context.setData("eles", prtList);
+			result = render.renderAsString("commonPrtRpt", context);
 			logger.info(result);
 		}
-		// if("2".equals(context.getData(ParamKeys.PRT_FLG))){
-		// map.put("commonFailRpt",
-		// "config/report/common/commonPrintReport_fail.vm");
-		// render.setReportNameTemplateLocationMapping(map);
-		// context.setData("eles", prtList);
-		// result = render.renderAsString("commonFailRpt", context);
-		// logger.info(result);
-		// }
+		else if ("4".equals(context.getData("prtTyp"))) {
+			map.put("commonPrtRpt", "config/report/common/commonPrintReport_oth.vm");
+			render.setReportNameTemplateLocationMapping(map);
+			context.setData("eles", prtList);
+			result = render.renderAsString("commonPrtRpt", context);
+			logger.info(result);
+		}
+
+		logger.info("=============ready to print report list=============");
+
 		PrintWriter printWriter = null;
 
 		// TODO 拼装本地路径
 		StringBuffer sbLocDir = new StringBuffer();
-		sbLocDir.append("D:/testGash/checkFilTest/");
+		sbLocDir.append("D:/testGash/checkFilTest/").append(DateUtils.format(new Date(), DateUtils.STYLE_yyyyMMdd)).append("/");
 		try {
 			File file = new File(sbLocDir.toString());
 			if (!file.exists()) {
