349317aec10c89b8447a32dd430ba4042308e72f
Li_Yawei <Li_Yawei@2ac714ae-9a16-11e4-ad83-2f06aba1930a>
2015-03-29 07:27:53 +0000

    git-svn-id: svn://182.53.32.18/bbip/project/GDEUPSB/TRUNK/GDEUPSB@27483 2ac714ae-9a16-11e4-ad83-2f06aba1930a

--- src/com/bocom/bbip/gdeupsb/action/efek/QryCusMsgAction.java
+++ src/com/bocom/bbip/gdeupsb/action/efek/QryCusMsgAction.java
@@ -44,6 +44,16 @@ public class QryCusMsgAction extends BaseAction{
 		context.setData(ParamKeys.TXN_TME, txnTme);
 		
 		callThd(context);
+		String comNo=context.getData("comNo").toString();
+		String bakFld1="";
+		if(comNo.equals("foshan")){
+				bakFld1="佛山";
+		}else if(comNo.equals("qingyuan")){
+				bakFld1="清远";
+		}else if(comNo.equals("zhongshan")){
+				bakFld1="中山";
+		}
+		context.setData(ParamKeys.BAK_FLD1, bakFld1);
 	}
 	/**
 	 *报文信息  外发第三方
