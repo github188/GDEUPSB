fa355a204f7b31136d42ff45969212fdc7dd590f
Qian_Chen <Qian_Chen@2ac714ae-9a16-11e4-ad83-2f06aba1930a>
2015-04-01 17:37:24 +0000

    git-svn-id: svn://182.53.32.18/bbip/project/GDEUPSB/TRUNK/GDEUPSB@29671 2ac714ae-9a16-11e4-ad83-2f06aba1930a

---
+++ src/com/bocom/bbip/gdeupsb/repository/GDEupsWatAgtInfRepository.java
@@ -0,0 +1,8 @@
+package com.bocom.bbip.gdeupsb.repository;
+
+import com.bocom.bbip.data.repository.PagingAndSortingRepository;
+import com.bocom.bbip.gdeupsb.entity.GDEupsWatAgtInf;
+
+public interface GDEupsWatAgtInfRepository extends PagingAndSortingRepository<GDEupsWatAgtInf, String> {
+	
+}
