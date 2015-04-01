da4b3ada6d6ba5e158c4248a4239cf45ebf7ab4d
Li_Yawei <Li_Yawei@2ac714ae-9a16-11e4-ad83-2f06aba1930a>
2015-04-01 09:27:00 +0000

    git-svn-id: svn://182.53.32.18/bbip/project/GDEUPSB/TRUNK/GDEUPSB@29296 2ac714ae-9a16-11e4-ad83-2f06aba1930a

--- src/com/bocom/bbip/gdeupsb/strategy/efek/cancelFeeOnline/AftCancelFeeAction.java
+++ src/com/bocom/bbip/gdeupsb/strategy/efek/cancelFeeOnline/AftCancelFeeAction.java
@@ -34,29 +34,29 @@ public class AftCancelFeeAction implements Executable{
 				context.setData(ParamKeys.THD_TXN_TIME, thdTxnTme);
 				context.setData(ParamKeys.TXN_DTE, DateUtils.formatAsSimpleDate((Date)context.getData(ParamKeys.TXN_DTE)));
 		
-				EupsTransJournal eupsTransJournal=eupsTransJournalRepository.findOne(context.getData(ParamKeys.OLD_TXN_SQN).toString());
-				if(null != eupsTransJournal){
-						String txnSts=eupsTransJournal.getTxnSts();
-						if("c".equals(txnSts) || "C".equals(txnSts)){
-								context.setData(GDParamKeys.MSGTYP, "N");
-								context.setData(ParamKeys.RSP_CDE, GDConstants.SUCCESS_CODE);
-								context.setData(ParamKeys.RSP_MSG, "原记录【"+context.getData(ParamKeys.OLD_TXN_SQN).toString()+"】已经抹账");
-						}else if("b".equals(txnSts) || "B".equals(txnSts)){
-						}else if("s".equals(txnSts) || "S".equals(txnSts)){
-								context.setData(ParamKeys.RESPONSE_MESSAGE, "交易成功");
-								context.setData(ParamKeys.RESPONSE_CODE, GDConstants.SUCCESS_CODE);
-								context.setData(ParamKeys.RSP_MSG, "原记录【"+context.getData(ParamKeys.OLD_TXN_SQN).toString()+"】准备抹账");
-						}else{
-							context.setData(GDParamKeys.MSGTYP, "E");
-							context.setData(ParamKeys.RSP_CDE, "EFE999");
-							context.setData(ParamKeys.RSP_MSG, "原记录【"+context.getData(ParamKeys.OLD_TXN_SQN).toString()+"】状态【"+txnSts+"】未明，不进行抹账");
-							throw new CoreRuntimeException("原记录【"+context.getData(ParamKeys.OLD_TXN_SQN).toString()+"】状态【"+txnSts+"】未明，不进行抹账");
-						}
-				}else{
-						context.setData(GDParamKeys.MSGTYP, "N");
-						context.setData(ParamKeys.RSP_CDE, GDConstants.SUCCESS_CODE);
-						context.setData(ParamKeys.RSP_MSG, "原记录【"+context.getData(ParamKeys.OLD_TXN_SQN).toString()+"】不存在");
-						throw new CoreRuntimeException("原记录【"+context.getData(ParamKeys.OLD_TXN_SQN).toString()+"】不存在");
-					}
+//				EupsTransJournal eupsTransJournal=eupsTransJournalRepository.findOne(context.getData(ParamKeys.OLD_TXN_SQN).toString());
+//				if(null != eupsTransJournal){
+//						String txnSts=eupsTransJournal.getTxnSts();
+//						if("c".equals(txnSts) || "C".equals(txnSts)){
+//								context.setData(GDParamKeys.MSGTYP, "N");
+//								context.setData(ParamKeys.RSP_CDE, GDConstants.SUCCESS_CODE);
+//								context.setData(ParamKeys.RSP_MSG, "原记录【"+context.getData(ParamKeys.OLD_TXN_SQN).toString()+"】已经抹账");
+//						}else if("b".equals(txnSts) || "B".equals(txnSts)){
+//						}else if("s".equals(txnSts) || "S".equals(txnSts)){
+//								context.setData(ParamKeys.RESPONSE_MESSAGE, "交易成功");
+//								context.setData(ParamKeys.RESPONSE_CODE, GDConstants.SUCCESS_CODE);
+//								context.setData(ParamKeys.RSP_MSG, "原记录【"+context.getData(ParamKeys.OLD_TXN_SQN).toString()+"】准备抹账");
+//						}else{
+//							context.setData(GDParamKeys.MSGTYP, "E");
+//							context.setData(ParamKeys.RSP_CDE, "EFE999");
+//							context.setData(ParamKeys.RSP_MSG, "原记录【"+context.getData(ParamKeys.OLD_TXN_SQN).toString()+"】状态【"+txnSts+"】未明，不进行抹账");
+//							throw new CoreRuntimeException("原记录【"+context.getData(ParamKeys.OLD_TXN_SQN).toString()+"】状态【"+txnSts+"】未明，不进行抹账");
+//						}
+//				}else{
+//						context.setData(GDParamKeys.MSGTYP, "N");
+//						context.setData(ParamKeys.RSP_CDE, GDConstants.SUCCESS_CODE);
+//						context.setData(ParamKeys.RSP_MSG, "原记录【"+context.getData(ParamKeys.OLD_TXN_SQN).toString()+"】不存在");
+//						throw new CoreRuntimeException("原记录【"+context.getData(ParamKeys.OLD_TXN_SQN).toString()+"】不存在");
+//					}
 	}
 }
