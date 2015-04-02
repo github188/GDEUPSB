6d415de3391af76bfa5d8d47bb7c6909d575d575
Li_Yawei <Li_Yawei@2ac714ae-9a16-11e4-ad83-2f06aba1930a>
2015-04-02 08:41:36 +0000

    git-svn-id: svn://182.53.32.18/bbip/project/GDEUPSB/TRUNK/GDEUPSB@30057 2ac714ae-9a16-11e4-ad83-2f06aba1930a

--- src/com/bocom/bbip/gdeupsb/action/efek/QryCusMsgAction.java
+++ src/com/bocom/bbip/gdeupsb/action/efek/QryCusMsgAction.java
@@ -45,14 +45,14 @@ public class QryCusMsgAction extends BaseAction{
 		
 		callThd(context);
 		String comNo=context.getData("comNo").toString();
-		String bakFld1="";
-		if(comNo.equals("foshan")){
-				bakFld1="佛山";
-		}else if(comNo.equals("qingyuan")){
-				bakFld1="清远";
-		}else if(comNo.equals("zhongshan")){
-				bakFld1="中山";
-		}
+		String bakFld1=" ";
+//		if(comNo.equals("foshan")){
+//				bakFld1="佛山";
+//		}else if(comNo.equals("qingyuan")){
+//				bakFld1="清远";
+//		}else if(comNo.equals("zhongshan")){
+//				bakFld1="中山";
+//		}
 		context.setData(ParamKeys.BAK_FLD1, bakFld1);
 	}
 	/**
