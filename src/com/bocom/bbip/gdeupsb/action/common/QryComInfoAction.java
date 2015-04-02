eae9806fac2a9b9f87eebd4719ec3b1feab8f8ab
Li_Yawei <Li_Yawei@2ac714ae-9a16-11e4-ad83-2f06aba1930a>
2015-04-02 08:40:03 +0000

    git-svn-id: svn://182.53.32.18/bbip/project/GDEUPSB/TRUNK/GDEUPSB@30053 2ac714ae-9a16-11e4-ad83-2f06aba1930a

---
+++ src/com/bocom/bbip/gdeupsb/action/common/QryComInfoAction.java
@@ -0,0 +1,42 @@
+package com.bocom.bbip.gdeupsb.action.common;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.bocom.bbip.comp.BBIPPublicService;
+import com.bocom.bbip.eups.action.BaseAction;
+import com.bocom.bbip.eups.common.ParamKeys;
+import com.bocom.bbip.eups.entity.EupsThdBaseInfo;
+import com.bocom.bbip.eups.repository.EupsThdBaseInfoRepository;
+import com.bocom.jump.bp.core.Context;
+import com.bocom.jump.bp.core.CoreException;
+import com.bocom.jump.bp.core.CoreRuntimeException;
+
+public class QryComInfoAction extends BaseAction{
+	@Autowired
+	BBIPPublicService bbipPublicService;
+	@Autowired
+	EupsThdBaseInfoRepository eupsThdBaseInfoRepository;
+	private final static Log logger = LogFactory.getLog(QryComInfoAction.class);
+	
+	public void execute(Context context) throws CoreException,CoreRuntimeException{
+		logger.info("===============Start   QryComInfoAction");
+		 Map<String,Object> rspMap = new HashMap<String, Object>();
+		 String  comNo=context.getData(ParamKeys.COMPANY_NO).toString();
+//		 context.setData("traceNo", bbipPublicService.getTraceNo());
+		 rspMap.put(ParamKeys.COMPANY_NO, comNo);
+		 rspMap.put("inqBusLstFlg", "N");
+		// 查询代收单位协议信息
+//		 Result respData = get(BGSPServiceAccessObject.class).callServiceFlatting("queryCorporInfo", rspMap);
+//		 if(CollectionUtils.isEmpty(respData.getPayload())){			
+//				throw new CoreRuntimeException("该单位未签约");
+//		}
+		 EupsThdBaseInfo eupsThdBaseInfo=eupsThdBaseInfoRepository.findOne(comNo);
+		 context.setData("comNme", eupsThdBaseInfo.getComNme());
+		 logger.info("===============End   QryComInfoAction");
+	}
+}
