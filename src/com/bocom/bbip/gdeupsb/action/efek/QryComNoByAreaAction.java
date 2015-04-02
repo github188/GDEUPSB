0079adef9f634d91b7d6e0e4eb798e1658875d30
Li_Yawei <Li_Yawei@2ac714ae-9a16-11e4-ad83-2f06aba1930a>
2015-04-02 08:03:02 +0000

    git-svn-id: svn://182.53.32.18/bbip/project/GDEUPSB/TRUNK/GDEUPSB@30015 2ac714ae-9a16-11e4-ad83-2f06aba1930a

--- src/com/bocom/bbip/gdeupsb/action/efek/QryComNoByAreaAction.java
+++ src/com/bocom/bbip/gdeupsb/action/efek/QryComNoByAreaAction.java
@@ -15,13 +15,13 @@ public class QryComNoByAreaAction extends BaseAction{
 				log.info("===================Start   QryComNoByAreaAction");
 				String area=context.getData("area").toString();
 				String comNo="";
-				if(area.equals("佛山") || area.equals("1")){
-						comNo="foshan";
-				}else if(area.equals("清远") || area.equals("2")){
-						comNo="qingyuan";
-				}else if(area.equals("中山") || area.equals("3")){
-						comNo="GDELEC00";
-				}
+//				if(area.equals("佛山") || area.equals("1")){
+//						comNo="foshan";
+//				}else if(area.equals("清远") || area.equals("2")){
+//						comNo="qingyuan";
+//				}else if(area.equals("中山") || area.equals("3")){
+//						comNo="GDELEC00";
+//				}
 				context.setData(ParamKeys.COMPANY_NO, comNo);
 				log.info("===================End   QryComNoByAreaAction");
 		}
