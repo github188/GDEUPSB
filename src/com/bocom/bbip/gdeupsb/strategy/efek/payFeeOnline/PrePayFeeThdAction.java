eca50c5d6c5d908f091c60195782d082cb485473
Li_Yawei <Li_Yawei@2ac714ae-9a16-11e4-ad83-2f06aba1930a>
2015-04-02 11:48:39 +0000

    git-svn-id: svn://182.53.32.18/bbip/project/GDEUPSB/TRUNK/GDEUPSB@30190 2ac714ae-9a16-11e4-ad83-2f06aba1930a

--- src/com/bocom/bbip/gdeupsb/strategy/efek/payFeeOnline/PrePayFeeThdAction.java
+++ src/com/bocom/bbip/gdeupsb/strategy/efek/payFeeOnline/PrePayFeeThdAction.java
@@ -78,6 +78,7 @@ public class PrePayFeeThdAction extends BaseAction implements Executable{
 				context.setData(GDParamKeys.TRADE_SOURCE_ADD, GDConstants.TRADE_SOURCE_ADD);//交易源地址
 				context.setData(GDParamKeys.TRADE_AIM_ADD, GDConstants.TRADE_AIM_ADD);//交易目标地址
 				context.setData("PKGCNT", "000001");
+				context.setData("sqns", context.getData("sqn"));
 				
 				Date txnDte=context.getData(ParamKeys.TXN_DATE);
 				Date txnTme=context.getData(ParamKeys.TXN_TIME);
