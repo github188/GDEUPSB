7b32f92e79bdfd8c2fdbd2dec0774e8e425206ca
Wang_Mingqin <Wang_Mingqin@2ac714ae-9a16-11e4-ad83-2f06aba1930a>
2015-04-03 02:36:04 +0000

    git-svn-id: svn://182.53.32.18/bbip/project/GDEUPSB/TRUNK/GDEUPSB@30574 2ac714ae-9a16-11e4-ad83-2f06aba1930a

---
+++ src/com/bocom/bbip/gdeupsb/utils/GdZeroBanlanceUtils.java
@@ -0,0 +1,17 @@
+package com.bocom.bbip.gdeupsb.utils;
+
+public class GdZeroBanlanceUtils {
+	/**
+	 * 记账要素(必输) json 格式
+	 * 
+	 * @return
+	 */
+	public static String getPrvDat() {
+		String str = "{\"GIA-SYS-NO\":\"1001\",\"optId\":\"doBE2010\",\"AMT\":\"100\",\"GIA-TR-BR\":\"01310800999\",\"OPP-AC\":\"310066629018170206519\",\"GIA-TRACE-SEQ\":\"20141028CN2110000201\",\"GIA-TR-TLR\":\"EPA0BB7\",\"GIA-OP-BK\":\"01310999999"
+				+ "\",\"GIA-SRV-CODE\":\"342010\",\"CCY\":\"CNY\",\"GIA-OP-TLR\":\"EPA0BB7\",\"EXG-NO\":\"000000404508\",\"GIA-REQ-TYPE\":\"T\",\"RMK-100\":null,\"OPP-AC-NME\":\"XX??\",\"GIA-OP-BR\":\"01310800999\",\"EXG-BUS-KND\":\"11\",\"GIA-TR-BK\":\"01310999999\",\"GIA-TRACE-SYS-ID\":\"11110000"
+				+ "\",\"GIA-VERSION\":\"1.00\",\"AC-DTE\":\"20141025\",\"CUS-AC\":\"310066629018170206519\",\"OPP-EXG-NO\":\"000000021541\",\"BV-NO\":\"0\",\"ISE-BK\":\"01310999999\",\"GIA-CHNL-TYPE\":\"00\",\"CTL-WRD\":\"0\",\"CUS-NME\":\"?1ō?\",\"EXG-ARA-NO\":\"443001\",\"GIA-FILLER\":\"Y\",\"BV-CDE"
+				+ "\":\"123\",\"TRADE_CODE\":\"GEMS\",\"GIA-TX-SYS-ID\":\"11110000\",\"EXI-SCE\":\"001\"}";
+
+		return str;
+	}
+}
