967c0be6afa2c6014e20053fa6e1d3b663879cc3
Li_Yawei <Li_Yawei@2ac714ae-9a16-11e4-ad83-2f06aba1930a>
2015-04-04 09:15:02 +0000

    git-svn-id: svn://182.53.32.18/bbip/project/GDEUPSB/TRUNK/GDEUPSB@31290 2ac714ae-9a16-11e4-ad83-2f06aba1930a

--- src/com/bocom/bbip/gdeupsb/action/common/QryComInfoAction.java
+++ src/com/bocom/bbip/gdeupsb/action/common/QryComInfoAction.java
@@ -4,27 +4,29 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import com.bocom.bbip.comp.BBIPPublicService;
 import com.bocom.bbip.eups.action.BaseAction;
 import com.bocom.bbip.eups.common.ParamKeys;
-import com.bocom.bbip.eups.entity.EupsThdBaseInfo;
-import com.bocom.bbip.eups.repository.EupsThdBaseInfoRepository;
+import com.bocom.bbip.service.BGSPServiceAccessObject;
+import com.bocom.bbip.service.Result;
 import com.bocom.jump.bp.core.Context;
 import com.bocom.jump.bp.core.CoreException;
 import com.bocom.jump.bp.core.CoreRuntimeException;
 
 public class QryComInfoAction extends BaseAction{
 	@Autowired
-	BBIPPublicService bbipPublicService;
-	@Autowired
-	EupsThdBaseInfoRepository eupsThdBaseInfoRepository;
+	BGSPServiceAccessObject bgspServiceAccessObject;
 	private final static Log logger = LogFactory.getLog(QryComInfoAction.class);
 	
 	public void execute(Context context) throws CoreException,CoreRuntimeException{
 		logger.info("===============Start   QryComInfoAction");
-		 String  comNo=context.getData(ParamKeys.COMPANY_NO).toString();
-		 EupsThdBaseInfo eupsThdBaseInfo=eupsThdBaseInfoRepository.findOne(comNo);
-		 context.setData("comNme", eupsThdBaseInfo.getComNme());
+		 context.setData("inqBusLstFlg","N");
+		 Result comResult= bgspServiceAccessObject.callServiceFlatting("queryCorporInfo",context.getDataMap());
+		 log.info("==========="+comResult);
+		 if(!comResult.isSuccess()){
+			 		throw new CoreException("单位未签约");
+		 }
+		 String comNme=comResult.getPayload().get("comNum").toString();
+		 context.setData(ParamKeys.COMPANY_NAME, comNme);
 		 logger.info("===============End   QryComInfoAction");
 	}
 }
