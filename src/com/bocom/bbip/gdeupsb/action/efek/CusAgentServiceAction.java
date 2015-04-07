d92f4b1c75c4ef66676d6326e74aeca20d372a8d
Li_Yawei <Li_Yawei@2ac714ae-9a16-11e4-ad83-2f06aba1930a>
2015-04-07 03:27:10 +0000

    git-svn-id: svn://182.53.32.18/bbip/project/GDEUPSB/TRUNK/GDEUPSB@31799 2ac714ae-9a16-11e4-ad83-2f06aba1930a

--- src/com/bocom/bbip/gdeupsb/action/efek/CusAgentServiceAction.java
+++ src/com/bocom/bbip/gdeupsb/action/efek/CusAgentServiceAction.java
@@ -141,7 +141,7 @@ public class CusAgentServiceAction extends BaseAction{
 				context.setData(GDParamKeys.TRADE_RECEIVE, GDConstants.TRADE_RECEIVE);//交易接收方
 				context.setData(GDParamKeys.TRADE_SOURCE_ADD, GDConstants.TRADE_SOURCE_ADD);//交易源地址
 				context.setData(GDParamKeys.TRADE_AIM_ADD, GDConstants.TRADE_AIM_ADD);//交易目标地址
-				context.setData("PKGCNT", "000001");
+				context.setData("PKGCNT", "000000");
 				context.setData(GDParamKeys.BUS_IDENTIFY, "YDLW07");		
 				context.setData("callThd", "callThd");
 			}
