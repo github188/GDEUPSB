1063d3de230f1e5c0afe57e38252feecff1aea82
Li_Guilin <Li_Guilin@2ac714ae-9a16-11e4-ad83-2f06aba1930a>
2015-04-07 15:45:05 +0000

    git-svn-id: svn://182.53.32.18/bbip/project/GDEUPSB/TRUNK/GDEUPSB@32256 2ac714ae-9a16-11e4-ad83-2f06aba1930a

--- src/com/bocom/bbip/gdeupsb/service/impl/watr00/PrintPublicMsgServiceActionWATR00.java
+++ src/com/bocom/bbip/gdeupsb/service/impl/watr00/PrintPublicMsgServiceActionWATR00.java
@@ -1,5 +1,9 @@
 package com.bocom.bbip.gdeupsb.service.impl.watr00;
 
+import java.io.BufferedOutputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
@@ -10,10 +14,16 @@ import org.slf4j.LoggerFactory;
 
 import com.bocom.bbip.eups.action.BaseAction;
 import com.bocom.bbip.eups.action.eupsreport.ReportHelper;
+import com.bocom.bbip.eups.common.ErrorCodes;
+import com.bocom.bbip.eups.common.ParamKeys;
 import com.bocom.bbip.eups.entity.EupsTransJournal;
 import com.bocom.bbip.eups.entity.MFTPConfigInfo;
 import com.bocom.bbip.eups.repository.EupsThdFtpConfigRepository;
 import com.bocom.bbip.file.reporting.impl.VelocityTemplatedReportRender;
+import com.bocom.bbip.gdeupsb.common.GDConstants;
+import com.bocom.bbip.gdeupsb.common.GDParamKeys;
+import com.bocom.bbip.gdeupsb.entity.GDEupsbTrspFeeInfo;
+import com.bocom.bbip.utils.Assert;
 import com.bocom.bbip.utils.BeanUtils;
 import com.bocom.bbip.utils.DateUtils;
 import com.bocom.bbip.utils.StringUtils;
@@ -21,6 +31,7 @@ import com.bocom.jump.bp.core.Context;
 import com.bocom.jump.bp.core.CoreException;
 import com.bocom.jump.bp.core.CoreRuntimeException;
 import com.bocom.jump.bp.service.sqlmap.SqlMap;
+import com.bocom.jump.bp.support.CollectionUtils;
 /**
  * 水费对公记账回执打印
  * @author hefengwen
@@ -47,12 +58,12 @@ public class PrintPublicMsgServiceActionWATR00 extends BaseAction {
 		}
 		param.put("br", context.getData("br"));
 		param.put("eupsBusTyp", context.getData("eupsBusTyp"));
-		
-		EupsThdFtpConfigRepository eupsThdFtpConfigRepository = get(EupsThdFtpConfigRepository.class);
-		ReportHelper reportHelper = get(ReportHelper.class);
-		MFTPConfigInfo mftpConfigInfo = reportHelper.getMFTPConfigInfo(eupsThdFtpConfigRepository);
-		logger.info((new StringBuilder("mftpConfigInfo:>>>>").append(BeanUtils.toMap(mftpConfigInfo))).toString());
-		
+//		
+//		EupsThdFtpConfigRepository eupsThdFtpConfigRepository = get(EupsThdFtpConfigRepository.class);
+//		ReportHelper reportHelper = get(ReportHelper.class);
+//		MFTPConfigInfo mftpConfigInfo = reportHelper.getMFTPConfigInfo(eupsThdFtpConfigRepository);
+//		logger.info((new StringBuilder("mftpConfigInfo:>>>>").append(BeanUtils.toMap(mftpConfigInfo))).toString());
+//		
 		List<Map<String,Object>> eupsTransJournals = ((SqlMap)get("sqlMap")).queryForList("watr00.queryPublicMsg", param);
 		for(Map<String,Object> m:eupsTransJournals){
 			m.put("cusAc", m.get("CUS_AC"));
@@ -68,27 +79,65 @@ public class PrintPublicMsgServiceActionWATR00 extends BaseAction {
 		context.setData("actNo", "445007012620194007699");
 		context.setData("actNm", "汕头自来水公司");
 		
+//		VelocityTemplatedReportRender render = new VelocityTemplatedReportRender();
+//		try {
+//			render.afterPropertiesSet();
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
+//		Map<String,String> map = new HashMap<String,String>();
+//		map.put("sample", "config/report/watr00/watr00_printPublicMsg.vm");
+//		render.setReportNameTemplateLocationMapping(map);
+//		context.setData("eles", eupsTransJournals);
+//		String result = render.renderAsString("sample", context);
+//		logger.info(result);
+//		String date = DateUtils.format(new Date(), DateUtils.STYLE_yyyyMMdd);
+//		StringBuffer fileName = new StringBuffer((new StringBuilder("").append(date).toString()));
+//		reportHelper.createFileAndSendMFTP(context, result, fileName, mftpConfigInfo);
+		logger.info("PrintPublicMsgServiceActionWATR00 end ... ...");
+		
+		
+		
+		
+		Map<String, String> mapping = CollectionUtils.createMap();
 		VelocityTemplatedReportRender render = new VelocityTemplatedReportRender();
+		String sampleFile="config/report/zhTransport/transportInv.vm";
+//		GDEupsbTrspFeeInfo gdEupsbTrspFeeInfo = new GDEupsbTrspFeeInfo();
+//		gdEupsbTrspFeeInfo.setPayLog(context.getData(ParamKeys.OLD_TXN_SEQUENCE).toString());
+//		List<GDEupsbTrspFeeInfo> feeInfoList = gdEupsbTrspFeeInfoRepository.find(gdEupsbTrspFeeInfo);
+//		Assert.isNotEmpty(feeInfoList, ErrorCodes.EUPS_QUERY_NO_DATA);
+
+		mapping.put("sample", sampleFile);
 		try {
 			render.afterPropertiesSet();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		Map<String,String> map = new HashMap<String,String>();
-		map.put("sample", "config/report/watr00/watr00_printPublicMsg.vm");
-		render.setReportNameTemplateLocationMapping(map);
-		context.setData("eles", eupsTransJournals);
+//
+//		context.setDataMap(BeanUtils.toMap(feeInfoList.get(0)));
+//		System.out.println("@@@@@@@@@@@@@@@@@@@@"+context);
+		render.setReportNameTemplateLocationMapping(mapping);
 		String result = render.renderAsString("sample", context);
-		logger.info(result);
-		String date = DateUtils.format(new Date(), DateUtils.STYLE_yyyyMMdd);
-		StringBuffer fileName = new StringBuffer((new StringBuilder("").append(date).toString()));
-		reportHelper.createFileAndSendMFTP(context, result, fileName, mftpConfigInfo);
-		logger.info("PrintPublicMsgServiceActionWATR00 end ... ...");
-	}
-	
-//	public static void main(String[] args) {
-//		String amt = "123";
-//		System.out.println(StringUtils.amountInWords(amt));
-//	}
+		try {
+			log.info("generate report content:****"+new String(result.getBytes(GDConstants.CHARSET_ENCODING_GBK)));
+		} catch (UnsupportedEncodingException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		String fileName = context.getData(GDParamKeys.INV_NO).toString()+context.getData(ParamKeys.TELLER)+"00";
+		BufferedOutputStream outStream = null;
+		try {
 
+			outStream = new BufferedOutputStream(new FileOutputStream(
+					"D:\\test"+fileName));
+			outStream.write(result.getBytes(GDConstants.CHARSET_ENCODING_GBK));
+			outStream.close();
+		} catch (IOException e) {
+			throw new CoreException("BBIP0004EU0128");
+		}
+
+	
+
+
+	}
 }
