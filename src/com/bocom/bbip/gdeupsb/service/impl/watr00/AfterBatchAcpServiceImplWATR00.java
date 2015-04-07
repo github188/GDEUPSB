7c46e5163d4d9dc785cd4dd238957be9b594df70
Li_Guilin <Li_Guilin@2ac714ae-9a16-11e4-ad83-2f06aba1930a>
2015-04-07 03:11:52 +0000

    git-svn-id: svn://182.53.32.18/bbip/project/GDEUPSB/TRUNK/GDEUPSB@31785 2ac714ae-9a16-11e4-ad83-2f06aba1930a

--- src/com/bocom/bbip/gdeupsb/service/impl/watr00/AfterBatchAcpServiceImplWATR00.java
+++ src/com/bocom/bbip/gdeupsb/service/impl/watr00/AfterBatchAcpServiceImplWATR00.java
@@ -1,6 +1,6 @@
 package com.bocom.bbip.gdeupsb.service.impl.watr00;
 
-import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
@@ -10,20 +10,22 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import com.bocom.bbip.comp.BBIPPublicService;
 import com.bocom.bbip.eups.action.common.OperateFTPAction;
 import com.bocom.bbip.eups.action.common.OperateFileAction;
 import com.bocom.bbip.eups.common.ParamKeys;
 import com.bocom.bbip.eups.entity.EupsBatchConsoleInfo;
+import com.bocom.bbip.eups.entity.EupsBatchInfoDetail;
 import com.bocom.bbip.eups.entity.EupsThdFtpConfig;
 import com.bocom.bbip.eups.repository.EupsBatchConsoleInfoRepository;
+import com.bocom.bbip.eups.repository.EupsBatchInfoDetailRepository;
 import com.bocom.bbip.eups.repository.EupsThdFtpConfigRepository;
 import com.bocom.bbip.eups.spi.service.batch.AfterBatchAcpService;
 import com.bocom.bbip.eups.spi.vo.AfterBatchAcpDomain;
-import com.bocom.bbip.gdeupsb.entity.GDEupsEleTmp;
 import com.bocom.bbip.gdeupsb.repository.GDEupsEleTmpRepository;
 import com.bocom.bbip.utils.BeanUtils;
 import com.bocom.bbip.utils.DateUtils;
-import com.bocom.bbip.utils.NumberUtils;
+import com.bocom.bbip.utils.StringUtils;
 import com.bocom.jump.bp.core.Context;
 import com.bocom.jump.bp.core.CoreException;
 import com.bocom.jump.bp.service.id.seq.StepSequenceFactory;
@@ -40,16 +42,22 @@ public class AfterBatchAcpServiceImplWATR00 implements AfterBatchAcpService{
 	@Autowired
 	OperateFTPAction operateFTP;
 	@Autowired
+	BBIPPublicService service;
+	@Autowired
 	EupsBatchConsoleInfoRepository eupsBatchConsoleInfoRepository;
 	@Autowired
 	GDEupsEleTmpRepository gdEupsEleTmpRepository;
 	@Autowired
 	EupsThdFtpConfigRepository eupsThdFtpConfigRepository;
+	@Autowired
+	EupsBatchInfoDetailRepository eupsBatchInfoDetailRepository;
 
 	@Override
 	public void afterBatchDeal(AfterBatchAcpDomain domain, Context context)	throws CoreException {
 		logger.info("AfterBatchAcpServiceImplWATR00 start ... ...");
 		Map<String, Object> resultMap = new HashMap<String, Object>();
+		String sqn = service.getBBIPSequence();
+		String date = DateUtils.format(new Date(), DateUtils.STYLE_yyyyMMdd);
 		
 		String batNo=context.getData(ParamKeys.BAT_NO).toString();
 		EupsBatchConsoleInfo eupsBatchConsoleInfo = eupsBatchConsoleInfoRepository.findOne(batNo);
@@ -57,17 +65,33 @@ public class AfterBatchAcpServiceImplWATR00 implements AfterBatchAcpService{
 		Map<String, Object> resultMapHead = BeanUtils.toMap(eupsBatchConsoleInfo);
 		resultMap.put(ParamKeys.EUPS_FILE_HEADER, resultMapHead);
 		//文件名
-		String fileName=eupsBatchConsoleInfo.getFleNme();
+		String fileName="BJH"+StringUtils.substring(sqn, 0, 8)+StringUtils.substring(sqn, 15, 20);
 		//文件内容 
-		GDEupsEleTmp gdEupsEleTmps =new GDEupsEleTmp();
-		gdEupsEleTmps.setRsvFld5(batNo);
-		List<GDEupsEleTmp> detailList=gdEupsEleTmpRepository.find(gdEupsEleTmps);
-		resultMap.put(ParamKeys.EUPS_FILE_DETAIL, detailList);
+//		GDEupsEleTmp gdEupsEleTmps =new GDEupsEleTmp();
+//		gdEupsEleTmps.setRsvFld5(batNo);
+//		List<GDEupsEleTmp> detailList=gdEupsEleTmpRepository.find(gdEupsEleTmps);
+//		resultMap.put(ParamKeys.EUPS_FILE_DETAIL, detailList);
+		EupsBatchInfoDetail eupsBatchInfoDetail = new EupsBatchInfoDetail();
+		eupsBatchInfoDetail.setBatNo(batNo);
+		List<EupsBatchInfoDetail> detailList = eupsBatchInfoDetailRepository.find(eupsBatchInfoDetail);
+		
+		
+		
+		List<Object> list1 = new ArrayList<Object>();
+		Map<String, Object> map = new HashMap<String, Object>();
+		int i=0;
+	
+		for(i=0;i< detailList.size();i++){
+			map = BeanUtils.toMap(detailList.get(i));
+			map.put("sj", date);
+			list1.add(map);
+		}
+		resultMap.put(ParamKeys.EUPS_FILE_DETAIL, list1);
 		
 		EupsThdFtpConfig eupsThdFtpConfig =eupsThdFtpConfigRepository.findOne("watr00BatchResulf");
 		
 		// 生成文件
-		operateFile.createCheckFile(eupsThdFtpConfig, "watr00BatchResulf", fileName, resultMap);
+		operateFile.createCheckFile(eupsThdFtpConfig, "watBatchResult", fileName, resultMap);
 		// 将生成的文件上传至指定服务器
 		eupsThdFtpConfig.setLocFleNme(fileName);
 		eupsThdFtpConfig.setRmtFleNme(fileName);
@@ -76,13 +100,13 @@ public class AfterBatchAcpServiceImplWATR00 implements AfterBatchAcpService{
 		
 		
 		context.setData("type", "Y004");
-		context.setData("accountdate", DateUtils.format((Date)context.getData(ParamKeys.AC_DATE), DateUtils.STYLE_yyyyMMdd));
+		context.setData("accountdate", date);
 		
 		StepSequenceFactory s = context.getService("logNoService");
 		String logNo = s.create().toString();
 		context.setData("waterno", "JH"+logNo);//流水号生成
 		
-		context.setData("bankcode", "JT");
+		context.setData("bankcode", "交行");
 		context.setData("salesdepart",((String)context.getData(ParamKeys.BR)).substring(2, 8));
 		context.setData("salesperson", ((String)context.getData(ParamKeys.TELLER)).substring(4, 7));
 		context.setData("busitime", DateUtils.format(new Date(),DateUtils.STYLE_yyyyMMddHHmmss));
@@ -99,5 +123,7 @@ public class AfterBatchAcpServiceImplWATR00 implements AfterBatchAcpService{
 		context.setData("filesize", context.getData("filesize"));
 		logger.info("AfterBatchAcpServiceImplWATR00 end ... ...");
 	}
+
+	
 
 }
