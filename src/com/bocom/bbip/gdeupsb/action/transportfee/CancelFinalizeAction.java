4fae1c6c8138ba6015d277b34483fb1afcecfa8c
Li_Guilin <Li_Guilin@2ac714ae-9a16-11e4-ad83-2f06aba1930a>
2015-04-08 09:12:17 +0000

    git-svn-id: svn://182.53.32.18/bbip/project/GDEUPSB/TRUNK/GDEUPSB@32683 2ac714ae-9a16-11e4-ad83-2f06aba1930a

--- src/com/bocom/bbip/gdeupsb/action/transportfee/CancelFinalizeAction.java
+++ src/com/bocom/bbip/gdeupsb/action/transportfee/CancelFinalizeAction.java
@@ -1,13 +1,18 @@
 package com.bocom.bbip.gdeupsb.action.transportfee;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
 import com.bocom.bbip.eups.action.BaseAction;
 import com.bocom.jump.bp.core.Context;
 import com.bocom.jump.bp.core.CoreException;
 import com.bocom.jump.bp.core.CoreRuntimeException;
 
 public class CancelFinalizeAction extends BaseAction{
+	private static final Log log = LogFactory.getLog(CancelFinalizeAction.class);
+	
 	public void execute(Context ctx) throws CoreException,CoreRuntimeException{
-		
+		log.info("CancelFinalizeAction start......");
 	}
 //	TODO: <Set>FilNam=STRCAT(INVO,$TlrId,00)</Set>
 //     <Exec func="PUB:GenerateReport" error="IGNORE">
