ca32c1a3d0e701174f46b11bbbc6b7b30dd18fa8
Qian_Chen <Qian_Chen@2ac714ae-9a16-11e4-ad83-2f06aba1930a>
2015-04-08 02:40:40 +0000

    git-svn-id: svn://182.53.32.18/bbip/project/GDEUPSB/TRUNK/GDEUPSB@32355 2ac714ae-9a16-11e4-ad83-2f06aba1930a

--- src/com/bocom/bbip/gdeupsb/action/elea/SynExcCheckEleGzFileAction.java
+++ src/com/bocom/bbip/gdeupsb/action/elea/SynExcCheckEleGzFileAction.java
@@ -1,7 +1,10 @@
 package com.bocom.bbip.gdeupsb.action.elea;
 
+import java.util.Map;
+
 import com.bocom.bbip.comp.BBIPPublicService;
 import com.bocom.bbip.eups.action.BaseAction;
+import com.bocom.bbip.eups.common.Constants;
 import com.bocom.bbip.eups.common.ParamKeys;
 import com.bocom.jump.bp.core.Context;
 import com.bocom.jump.bp.core.CoreException;
@@ -23,6 +26,14 @@ public class SynExcCheckEleGzFileAction extends BaseAction {
 	@Override
 	public void execute(Context context) throws CoreException, CoreRuntimeException {
 		log.info("SynTransferJlzfDeal start!..");
+		
+		 Map<String,Object> inmap=context.getData("jopSchedulingData");
+	        if(null!=inmap){
+	        	String dptTyp= (String)inmap.get("dptTyp");
+	            if (null != dptTyp) {
+	                context.setData("dptTyp",dptTyp);
+	            } 
+	        }
 		context.setData(ParamKeys.EUPS_BUSS_TYPE, this.ELEC_EUPS_BUS_TYP);
 		context.setData(ParamKeys.FTP_ID, "eleGzCheckFileToThird"); // ftpID
 		get(BBIPPublicService.class).synExecute(this.ELEC_CHECK_PROCESS, context);
