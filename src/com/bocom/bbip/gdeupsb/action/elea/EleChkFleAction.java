88931d545ef5441a23dc11956c9f950b289d6520
Qian_Chen <Qian_Chen@2ac714ae-9a16-11e4-ad83-2f06aba1930a>
2015-04-10 06:49:30 +0000

    git-svn-id: svn://182.53.32.18/bbip/project/GDEUPSB/TRUNK/GDEUPSB@33954 2ac714ae-9a16-11e4-ad83-2f06aba1930a

--- src/com/bocom/bbip/gdeupsb/action/elea/EleChkFleAction.java
+++ src/com/bocom/bbip/gdeupsb/action/elea/EleChkFleAction.java
@@ -1,23 +1,12 @@
 package com.bocom.bbip.gdeupsb.action.elea;
 
-import java.math.BigDecimal;
-import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import com.bocom.bbip.eups.action.BaseAction;
-import com.bocom.bbip.eups.action.eupsreport.ReportHelper;
-import com.bocom.bbip.eups.common.ParamKeys;
-import com.bocom.bbip.eups.entity.MFTPConfigInfo;
-import com.bocom.bbip.eups.repository.EupsThdFtpConfigRepository;
-import com.bocom.bbip.file.reporting.impl.VelocityTemplatedReportRender;
-import com.bocom.bbip.gdeupsb.common.GDParamKeys;
 import com.bocom.bbip.gdeupsb.repository.GdEupsTransJournalRepository;
 import com.bocom.bbip.gdeupsb.utils.CodeSwitchUtils;
-import com.bocom.bbip.utils.BeanUtils;
-import com.bocom.bbip.utils.DateUtils;
-import com.bocom.bbip.utils.StringUtils;
 import com.bocom.jump.bp.core.Context;
 import com.bocom.jump.bp.core.CoreException;
 import com.bocom.jump.bp.core.CoreRuntimeException;
@@ -32,12 +21,33 @@ public class EleChkFleAction extends BaseAction {
 
 	@Override
 	public void execute(Context context) throws CoreException, CoreRuntimeException {
-		log.info("EleGzComPayPrtAction start!..");
-		
-		
-		
-		
-		
+		log.info("财务对帐文件生成 start!..");
+
+		String dptTyp = context.getData("dptTyp");
+		String clrDat=context.getData("clrDat");
+
+		log.info("当前的dptTyp=" + dptTyp);
+		// 获取CAgtNo
+		String cAgtNoDl = CodeSwitchUtils.codeGenerator("DptTypToCAgtNo_DL", dptTyp);
+		context.setData("cAgtNo_dl", cAgtNoDl);
+
+		String cAgtNoYh = CodeSwitchUtils.codeGenerator("DptTypToCAgtNo_YH", dptTyp);
+		context.setData("cAgtNo_yh", cAgtNoYh);
+
+		String cAgtNoPl = CodeSwitchUtils.codeGenerator("DptTypToCAgtNo_PL", dptTyp);
+		context.setData("cAgtNo_pl", cAgtNoPl);
+
+		String lclFil = "gzdl_" + clrDat;
+
+		Map<String, Object> para = new HashMap<String, Object>();
+		para.put("cAgtNoDl", cAgtNoDl);
+		para.put("cAgtNoYh", cAgtNoYh);
+		para.put("cAgtNoPl", cAgtNoPl);
+		para.put("acDte", clrDat);
+		log.info("输入参数为:" + para);
+		// 产生上送供电方的文件
+		List<Map<String, Object>> reustlList = get(GdEupsTransJournalRepository.class).findGzEleHstSucJnl(para);
+		log.info("返回的list=" + reustlList);
 	}
 
 }
