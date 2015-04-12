8c79838cf1acb29c19ceebce81bd5427235bed20
Li_Guilin <Li_Guilin@2ac714ae-9a16-11e4-ad83-2f06aba1930a>
2015-04-12 02:36:34 +0000

    git-svn-id: svn://182.53.32.18/bbip/project/GDEUPSB/TRUNK/GDEUPSB@34943 2ac714ae-9a16-11e4-ad83-2f06aba1930a

--- src/com/bocom/bbip/gdeupsb/repository/GDEupsbTrspNpManagRepository.java
+++ src/com/bocom/bbip/gdeupsb/repository/GDEupsbTrspNpManagRepository.java
@@ -1,6 +1,7 @@
 package com.bocom.bbip.gdeupsb.repository;
 
 import java.util.List;
+import java.util.Map;
 
 import com.bocom.bbip.data.domain.Page;
 import com.bocom.bbip.data.domain.Pageable;
@@ -12,8 +13,17 @@ public interface GDEupsbTrspNpManagRepository extends PagingAndSortingRepository
 	public List<Integer> findCountByStatus(GDEupsbTrspNpManag gdeupsb);
 	public List<Integer> findCount(GDEupsbTrspNpManag gdeupsb);
 	public List<Integer> findCountSum(GDEupsbTrspNpManag gdeupsb);
-	 public Page<GDEupsbTrspNpManag> findNpInfo(Pageable pageable, GDEupsbTrspNpManag gdeupsb);
-	    public List<GDEupsbTrspNpManag> findNpInfo(GDEupsbTrspNpManag gdeupsb);
-	    public void updateSt(GDEupsbTrspNpManag gdeupsb);
+	
+	
+	 public Page<Map<String,Object>> findNpInfo(Pageable pageable, Object para);
+	public List<Map<String,Object>> findNpInfo(Object para);
+	   
+
+	 /** 协议信息查询-汇总*/
+   public Page<Map<String,Object>> findAgentTotInf(Pageable pageable, Object para);
+   public List<Map<String,Object>> findAgentTotInf(Object gdsAgtInf);
+	
+	
+	public void updateSt(GDEupsbTrspNpManag gdeupsb);
 	    public void update(GDEupsbTrspNpManag gdeupsb);
 }
