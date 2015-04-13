f7aa340db656cbafe6c2b8e99e96473a2da7ed63
Li_Yawei <Li_Yawei@2ac714ae-9a16-11e4-ad83-2f06aba1930a>
2015-04-13 07:05:38 +0000

    git-svn-id: svn://182.53.32.18/bbip/project/GDEUPSB/TRUNK/GDEUPSB@35431 2ac714ae-9a16-11e4-ad83-2f06aba1930a

--- src/com/bocom/bbip/gdeupsb/action/efek/CusAgentServiceAction.java
+++ src/com/bocom/bbip/gdeupsb/action/efek/CusAgentServiceAction.java
@@ -10,7 +10,6 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import com.bea.wlw.runtime.core.config.WlwManifestDocument.WlwManifest.Project.TopLevelComponent.ExternalCallbacks.Control;
 import com.bocom.bbip.comp.BBIPPublicService;
 import com.bocom.bbip.eups.action.BaseAction;
 import com.bocom.bbip.eups.common.ParamKeys;
