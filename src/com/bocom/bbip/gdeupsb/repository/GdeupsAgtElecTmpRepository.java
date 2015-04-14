3d913b097e80e8997a627ef37b5b92f43bafdcac
Li_Guilin <Li_Guilin@2ac714ae-9a16-11e4-ad83-2f06aba1930a>
2015-04-14 01:19:23 +0000

    本地汕头电力维护
    git-svn-id: svn://182.53.32.18/bbip/project/GDEUPSB/TRUNK/GDEUPSB@35657 2ac714ae-9a16-11e4-ad83-2f06aba1930a

---
+++ src/com/bocom/bbip/gdeupsb/repository/GdeupsAgtElecTmpRepository.java
@@ -0,0 +1,12 @@
+package com.bocom.bbip.gdeupsb.repository;
+
+import com.bocom.bbip.data.repository.PagingAndSortingRepository;
+import com.bocom.bbip.gdeupsb.entity.GdeupsAgtElecTmp;
+
+
+
+public interface GdeupsAgtElecTmpRepository 
+	extends 
+PagingAndSortingRepository<GdeupsAgtElecTmp, String>
+ {
+}
