94ad6752311e4255a1ca84a874ac8cbccd069dc8
Li_Guilin <Li_Guilin@2ac714ae-9a16-11e4-ad83-2f06aba1930a>
2015-04-14 01:18:28 +0000

    新增汕头电力协议信息本地临时表
    git-svn-id: svn://182.53.32.18/bbip/project/GDEUPSB/TRUNK/GDEUPSB@35655 2ac714ae-9a16-11e4-ad83-2f06aba1930a

---
+++ src/com/bocom/bbip/gdeupsb/entity/GdeupsAgtElecTmp.java
@@ -0,0 +1,293 @@
+package com.bocom.bbip.gdeupsb.entity;
+
+public class GdeupsAgtElecTmp {
+    private String changeType;
+
+    private String comCode;
+
+    private String feeCode;
+
+    private String feeNum;
+
+    private String userName;
+
+    private String oldBankNum;
+
+    private String oldCardNo;
+
+    private String newBankNum;
+
+    private String actNo;
+
+    private String acountName;
+
+    private String actType;
+
+    private String perComFlag;
+
+    private String idType;
+
+    private String idNo;
+
+    private String checkSendType;
+
+    private String invoiceSnedType;
+
+    private String pointNum;
+
+    private String invoiceSnedMan;
+
+    private String invoiceSendZip;
+
+    private String invoiceSendAddr;
+
+    private String checkSendMan;
+
+    private String checkSendZip;
+
+    private String checkSendAddr;
+
+    private String notifyType;
+
+    private String email;
+
+    private String phoneNum;
+
+    private String telNum;
+
+    private String remark;
+
+    private String prcessPassword;
+
+    public String getChangeType() {
+        return changeType;
+    }
+
+    public void setChangeType(String changeType) {
+        this.changeType = changeType;
+    }
+
+    public String getComCode() {
+        return comCode;
+    }
+
+    public void setComCode(String comCode) {
+        this.comCode = comCode;
+    }
+
+    public String getFeeCode() {
+        return feeCode;
+    }
+
+    public void setFeeCode(String feeCode) {
+        this.feeCode = feeCode;
+    }
+
+    public String getFeeNum() {
+        return feeNum;
+    }
+
+    public void setFeeNum(String feeNum) {
+        this.feeNum = feeNum;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getOldBankNum() {
+        return oldBankNum;
+    }
+
+    public void setOldBankNum(String oldBankNum) {
+        this.oldBankNum = oldBankNum;
+    }
+
+    public String getOldCardNo() {
+        return oldCardNo;
+    }
+
+    public void setOldCardNo(String oldCardNo) {
+        this.oldCardNo = oldCardNo;
+    }
+
+    public String getNewBankNum() {
+        return newBankNum;
+    }
+
+    public void setNewBankNum(String newBankNum) {
+        this.newBankNum = newBankNum;
+    }
+
+    public String getActNo() {
+        return actNo;
+    }
+
+    public void setActNo(String actNo) {
+        this.actNo = actNo;
+    }
+
+    public String getAcountName() {
+        return acountName;
+    }
+
+    public void setAcountName(String acountName) {
+        this.acountName = acountName;
+    }
+
+    public String getActType() {
+        return actType;
+    }
+
+    public void setActType(String actType) {
+        this.actType = actType;
+    }
+
+    public String getPerComFlag() {
+        return perComFlag;
+    }
+
+    public void setPerComFlag(String perComFlag) {
+        this.perComFlag = perComFlag;
+    }
+
+    public String getIdType() {
+        return idType;
+    }
+
+    public void setIdType(String idType) {
+        this.idType = idType;
+    }
+
+    public String getIdNo() {
+        return idNo;
+    }
+
+    public void setIdNo(String idNo) {
+        this.idNo = idNo;
+    }
+
+    public String getCheckSendType() {
+        return checkSendType;
+    }
+
+    public void setCheckSendType(String checkSendType) {
+        this.checkSendType = checkSendType;
+    }
+
+    public String getInvoiceSnedType() {
+        return invoiceSnedType;
+    }
+
+    public void setInvoiceSnedType(String invoiceSnedType) {
+        this.invoiceSnedType = invoiceSnedType;
+    }
+
+    public String getPointNum() {
+        return pointNum;
+    }
+
+    public void setPointNum(String pointNum) {
+        this.pointNum = pointNum;
+    }
+
+    public String getInvoiceSnedMan() {
+        return invoiceSnedMan;
+    }
+
+    public void setInvoiceSnedMan(String invoiceSnedMan) {
+        this.invoiceSnedMan = invoiceSnedMan;
+    }
+
+    public String getInvoiceSendZip() {
+        return invoiceSendZip;
+    }
+
+    public void setInvoiceSendZip(String invoiceSendZip) {
+        this.invoiceSendZip = invoiceSendZip;
+    }
+
+    public String getInvoiceSendAddr() {
+        return invoiceSendAddr;
+    }
+
+    public void setInvoiceSendAddr(String invoiceSendAddr) {
+        this.invoiceSendAddr = invoiceSendAddr;
+    }
+
+    public String getCheckSendMan() {
+        return checkSendMan;
+    }
+
+    public void setCheckSendMan(String checkSendMan) {
+        this.checkSendMan = checkSendMan;
+    }
+
+    public String getCheckSendZip() {
+        return checkSendZip;
+    }
+
+    public void setCheckSendZip(String checkSendZip) {
+        this.checkSendZip = checkSendZip;
+    }
+
+    public String getCheckSendAddr() {
+        return checkSendAddr;
+    }
+
+    public void setCheckSendAddr(String checkSendAddr) {
+        this.checkSendAddr = checkSendAddr;
+    }
+
+    public String getNotifyType() {
+        return notifyType;
+    }
+
+    public void setNotifyType(String notifyType) {
+        this.notifyType = notifyType;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNum() {
+        return phoneNum;
+    }
+
+    public void setPhoneNum(String phoneNum) {
+        this.phoneNum = phoneNum;
+    }
+
+    public String getTelNum() {
+        return telNum;
+    }
+
+    public void setTelNum(String telNum) {
+        this.telNum = telNum;
+    }
+
+    public String getRemark() {
+        return remark;
+    }
+
+    public void setRemark(String remark) {
+        this.remark = remark;
+    }
+
+    public String getPrcessPassword() {
+        return prcessPassword;
+    }
+
+    public void setPrcessPassword(String prcessPassword) {
+        this.prcessPassword = prcessPassword;
+    }
+}
