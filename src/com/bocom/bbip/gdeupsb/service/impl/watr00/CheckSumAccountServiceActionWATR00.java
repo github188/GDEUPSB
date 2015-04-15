10288c21a2793728873d44a7a6e136690dcf1d27
Li_Guilin <Li_Guilin@2ac714ae-9a16-11e4-ad83-2f06aba1930a>
2015-04-15 06:56:14 +0000

    1
    git-svn-id: svn://182.53.32.18/bbip/project/GDEUPSB/TRUNK/GDEUPSB@36332 2ac714ae-9a16-11e4-ad83-2f06aba1930a

--- src/com/bocom/bbip/gdeupsb/service/impl/watr00/CheckSumAccountServiceActionWATR00.java
+++ src/com/bocom/bbip/gdeupsb/service/impl/watr00/CheckSumAccountServiceActionWATR00.java
@@ -1,13 +1,19 @@
 package com.bocom.bbip.gdeupsb.service.impl.watr00;
 
 import java.util.Date;
+import java.util.List;
 import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.bocom.bbip.comp.BBIPPublicServiceImpl;
 import com.bocom.bbip.eups.action.BaseAction;
 import com.bocom.bbip.eups.common.ParamKeys;
+import com.bocom.bbip.eups.entity.EupsBatchConsoleInfo;
+import com.bocom.bbip.eups.repository.EupsBatchConsoleInfoRepository;
+import com.bocom.bbip.gdeupsb.common.GDConstants;
+import com.bocom.bbip.utils.BeanUtils;
 import com.bocom.bbip.utils.DateUtils;
 import com.bocom.bbip.utils.NumberUtils;
 import com.bocom.jump.bp.core.Context;
@@ -28,11 +34,13 @@ public class CheckSumAccountServiceActionWATR00 extends BaseAction {
 	@Override
 	public void execute(Context context) throws CoreException,	CoreRuntimeException {
 		logger.info("CheckSumAccountServiceActionWATR00 execute start ... ...");
-		String beginDate = context.getData("beginDate");
-		String endDate = context.getData("endDate");
-		context.setData("beginDate", DateUtils.format(DateUtils.parse(beginDate, DateUtils.STYLE_yyyyMMdd),DateUtils.STYLE_SIMPLE_DATE));
-		context.setData("endDate", DateUtils.format(DateUtils.parse(endDate, DateUtils.STYLE_yyyyMMdd),DateUtils.STYLE_SIMPLE_DATE));
-		logger.info("beginDate:["+beginDate+"]endDate:["+endDate+"]");
+		EupsBatchConsoleInfo console=new EupsBatchConsoleInfo();
+		console.setBatSts("S");
+		console.setSubDte(new Date());
+		console.setComNo(context.getData(ParamKeys.COMPANY_NO).toString());
+        List<EupsBatchConsoleInfo>ret=get(EupsBatchConsoleInfoRepository.class).find(console);
+        context.getDataMapDirectly().putAll(BeanUtils.toMap(ret.get(0)));
+        ((BBIPPublicServiceImpl)get(GDConstants.BBIP_PUBLIC_SERVICE)).synExecute("eups.commNotifyBatchStatusExt", context);
 		context.setData("eupeBusTyp", "WATR00");
 		context.setData("txnSts", "S");
 		context.setData("txnTyp", "N");
@@ -54,15 +62,20 @@ public class CheckSumAccountServiceActionWATR00 extends BaseAction {
 //		context.setData("operano", "");
 //		context.setData("password", "        ");
 //		context.setData("md5digest", " ");
+		Date startDat=DateUtils.parse(context.getData("beginDate").toString());
+		Date endDat=DateUtils.parse(context.getData("endDate").toString());
+		context.setData("startDat", startDat);
+		context.setData("endDat", endDat);
+		Map<String,Object> map =((SqlMap)get("sqlMap")).queryForObject("watr00.findCheckSum", context.getDataMap());//查询总金额、总笔数
 		
-		Map<String,Object> map =((SqlMap)get("sqlMap")).queryForObject("watr00.findCountAmt", context.getDataMap());//查询总金额、总笔数
-		
-		
-		String je = String.valueOf(map.get("JE"));
+		String je = String.valueOf(map.get("TOTAMT"));
 		if(je==null||"null".equals(je)){
 			je = "0";
 		}
-		String count = String.valueOf( map.get("COUNT"));
+		String count = String.valueOf( map.get("TOTCNT"));
+		if(count==null||"null".equals(count)){
+			count = "0";
+		}
 		context.setData("je", NumberUtils.yuanToCentString(je));
 		context.setData("count", count);
 		logger.info("je:["+je+"]count:["+count+"]");
