640497f8fd2c713bfc81f86c0a419859adbff226
Li_Yawei <Li_Yawei@2ac714ae-9a16-11e4-ad83-2f06aba1930a>
2015-04-15 14:50:24 +0000

    git-svn-id: svn://182.53.32.18/bbip/project/GDEUPSB/TRUNK/GDEUPSB@36776 2ac714ae-9a16-11e4-ad83-2f06aba1930a

--- src/com/bocom/bbip/gdeupsb/strategy/efek/checkThdAcct/CheckThdSumAcctAction.java
+++ src/com/bocom/bbip/gdeupsb/strategy/efek/checkThdAcct/CheckThdSumAcctAction.java
@@ -56,7 +56,7 @@ public class CheckThdSumAcctAction extends BaseAction implements  CheckThdSumAcc
 		
 		//流水日期时间
 		Date txnDte=DateUtils.parse(DateUtils.formatAsSimpleDate(new Date()));
-		txnDte=DateUtils.parse("2015-04-10");
+//		txnDte=DateUtils.parse("2015-04-10");
 		Date txnTme=DateUtils.parse(DateUtils.formatAsTranstime(new Date()));
 		context.setData(ParamKeys.TXN_DTE, txnDte);
 		context.setData(ParamKeys.TXN_TME, txnTme);
