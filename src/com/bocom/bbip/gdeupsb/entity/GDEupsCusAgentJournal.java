173d495aa496a76186bb4895a98f4d28318f6d48
Li_Yawei <Li_Yawei@2ac714ae-9a16-11e4-ad83-2f06aba1930a>
2015-04-15 09:01:51 +0000

    git-svn-id: svn://182.53.32.18/bbip/project/GDEUPSB/TRUNK/GDEUPSB@36459 2ac714ae-9a16-11e4-ad83-2f06aba1930a

---
+++ src/com/bocom/bbip/gdeupsb/entity/GDEupsCusAgentJournal.java
@@ -0,0 +1,329 @@
+package com.bocom.bbip.gdeupsb.entity;
+
+import com.bocom.bbip.data.annotation.GeneratedValue;
+import com.bocom.bbip.data.annotation.Id;
+import java.util.Date;
+
+public class GDEupsCusAgentJournal {
+    @Id
+    @GeneratedValue
+    private String sqn;
+
+    private String eupsBusTyp;
+
+    private String agdAgrNo;
+
+    private String agrOprTyp;
+
+    private String comNo;
+
+    private String comNme;
+
+    private String cusAc;
+
+    private String cusNme;
+
+    private String acoAc;
+
+    private String batNo;
+
+    private String batSeq;
+
+    private String busTyp;
+
+    private String busKnd;
+
+    private String tpsSts;
+
+    private String thdCusNo;
+
+    private String thdCusNme;
+
+    private String thdSts;
+
+    private String idTyp;
+
+    private String idNo;
+
+    private String tel;
+
+    private String agrBr;
+
+    private String agrTlr;
+
+    private String athTlr;
+
+    private Date creDte;
+
+    private Date agrVldDte;
+
+    private Date agrExpDte;
+
+    private String agrSts;
+
+    private Date txnDte;
+
+    private Date txnTme;
+
+    private String rsvFld1;
+
+    private String rsvFld2;
+
+    private String rsvFld3;
+
+    public String getSqn() {
+        return sqn;
+    }
+
+    public void setSqn(String sqn) {
+        this.sqn = sqn;
+    }
+
+    public String getEupsBusTyp() {
+        return eupsBusTyp;
+    }
+
+    public void setEupsBusTyp(String eupsBusTyp) {
+        this.eupsBusTyp = eupsBusTyp;
+    }
+
+    public String getAgdAgrNo() {
+        return agdAgrNo;
+    }
+
+    public void setAgdAgrNo(String agdAgrNo) {
+        this.agdAgrNo = agdAgrNo;
+    }
+
+    public String getAgrOprTyp() {
+        return agrOprTyp;
+    }
+
+    public void setAgrOprTyp(String agrOprTyp) {
+        this.agrOprTyp = agrOprTyp;
+    }
+
+    public String getComNo() {
+        return comNo;
+    }
+
+    public void setComNo(String comNo) {
+        this.comNo = comNo;
+    }
+
+    public String getComNme() {
+        return comNme;
+    }
+
+    public void setComNme(String comNme) {
+        this.comNme = comNme;
+    }
+
+    public String getCusAc() {
+        return cusAc;
+    }
+
+    public void setCusAc(String cusAc) {
+        this.cusAc = cusAc;
+    }
+
+    public String getCusNme() {
+        return cusNme;
+    }
+
+    public void setCusNme(String cusNme) {
+        this.cusNme = cusNme;
+    }
+
+    public String getAcoAc() {
+        return acoAc;
+    }
+
+    public void setAcoAc(String acoAc) {
+        this.acoAc = acoAc;
+    }
+
+    public String getBatNo() {
+        return batNo;
+    }
+
+    public void setBatNo(String batNo) {
+        this.batNo = batNo;
+    }
+
+    public String getBatSeq() {
+        return batSeq;
+    }
+
+    public void setBatSeq(String batSeq) {
+        this.batSeq = batSeq;
+    }
+
+    public String getBusTyp() {
+        return busTyp;
+    }
+
+    public void setBusTyp(String busTyp) {
+        this.busTyp = busTyp;
+    }
+
+    public String getBusKnd() {
+        return busKnd;
+    }
+
+    public void setBusKnd(String busKnd) {
+        this.busKnd = busKnd;
+    }
+
+    public String getTpsSts() {
+        return tpsSts;
+    }
+
+    public void setTpsSts(String tpsSts) {
+        this.tpsSts = tpsSts;
+    }
+
+    public String getThdCusNo() {
+        return thdCusNo;
+    }
+
+    public void setThdCusNo(String thdCusNo) {
+        this.thdCusNo = thdCusNo;
+    }
+
+    public String getThdCusNme() {
+        return thdCusNme;
+    }
+
+    public void setThdCusNme(String thdCusNme) {
+        this.thdCusNme = thdCusNme;
+    }
+
+    public String getThdSts() {
+        return thdSts;
+    }
+
+    public void setThdSts(String thdSts) {
+        this.thdSts = thdSts;
+    }
+
+    public String getIdTyp() {
+        return idTyp;
+    }
+
+    public void setIdTyp(String idTyp) {
+        this.idTyp = idTyp;
+    }
+
+    public String getIdNo() {
+        return idNo;
+    }
+
+    public void setIdNo(String idNo) {
+        this.idNo = idNo;
+    }
+
+    public String getTel() {
+        return tel;
+    }
+
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
+
+    public String getAgrBr() {
+        return agrBr;
+    }
+
+    public void setAgrBr(String agrBr) {
+        this.agrBr = agrBr;
+    }
+
+    public String getAgrTlr() {
+        return agrTlr;
+    }
+
+    public void setAgrTlr(String agrTlr) {
+        this.agrTlr = agrTlr;
+    }
+
+    public String getAthTlr() {
+        return athTlr;
+    }
+
+    public void setAthTlr(String athTlr) {
+        this.athTlr = athTlr;
+    }
+
+    public Date getCreDte() {
+        return creDte;
+    }
+
+    public void setCreDte(Date creDte) {
+        this.creDte = creDte;
+    }
+
+    public Date getAgrVldDte() {
+        return agrVldDte;
+    }
+
+    public void setAgrVldDte(Date agrVldDte) {
+        this.agrVldDte = agrVldDte;
+    }
+
+    public Date getAgrExpDte() {
+        return agrExpDte;
+    }
+
+    public void setAgrExpDte(Date agrExpDte) {
+        this.agrExpDte = agrExpDte;
+    }
+
+    public String getAgrSts() {
+        return agrSts;
+    }
+
+    public void setAgrSts(String agrSts) {
+        this.agrSts = agrSts;
+    }
+
+    public Date getTxnDte() {
+        return txnDte;
+    }
+
+    public void setTxnDte(Date txnDte) {
+        this.txnDte = txnDte;
+    }
+
+    public Date getTxnTme() {
+        return txnTme;
+    }
+
+    public void setTxnTme(Date txnTme) {
+        this.txnTme = txnTme;
+    }
+
+    public String getRsvFld1() {
+        return rsvFld1;
+    }
+
+    public void setRsvFld1(String rsvFld1) {
+        this.rsvFld1 = rsvFld1;
+    }
+
+    public String getRsvFld2() {
+        return rsvFld2;
+    }
+
+    public void setRsvFld2(String rsvFld2) {
+        this.rsvFld2 = rsvFld2;
+    }
+
+    public String getRsvFld3() {
+        return rsvFld3;
+    }
+
+    public void setRsvFld3(String rsvFld3) {
+        this.rsvFld3 = rsvFld3;
+    }
+}
