0e0a12fd50a6e4d63dcb814610d424b1712a14b9
Li_Guilin <Li_Guilin@2ac714ae-9a16-11e4-ad83-2f06aba1930a>
2015-04-17 12:50:30 +0000

    111
    git-svn-id: svn://182.53.32.18/bbip/project/GDEUPSB/TRUNK/GDEUPSB@38079 2ac714ae-9a16-11e4-ad83-2f06aba1930a

--- src/com/bocom/bbip/gdeupsb/repository/GdeupsAgtElecTmpRepository.java
+++ src/com/bocom/bbip/gdeupsb/repository/GdeupsAgtElecTmpRepository.java
@@ -1,5 +1,7 @@
 package com.bocom.bbip.gdeupsb.repository;
 
+
+import com.bocom.bbip.data.annotation.Find;
 import com.bocom.bbip.data.repository.PagingAndSortingRepository;
 import com.bocom.bbip.gdeupsb.entity.GdeupsAgtElecTmp;
 
@@ -9,4 +11,6 @@ public interface GdeupsAgtElecTmpRepository
 	extends 
 PagingAndSortingRepository<GdeupsAgtElecTmp, String>
  {
+	@Find
+	public GdeupsAgtElecTmp findBase(GdeupsAgtElecTmp agtElec);
 }
