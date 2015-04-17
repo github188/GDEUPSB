123880b66aa0bd79bcfde60503cea9370d24b5e9
Li_Guilin <Li_Guilin@2ac714ae-9a16-11e4-ad83-2f06aba1930a>
2015-04-17 06:30:30 +0000

    111
    git-svn-id: svn://182.53.32.18/bbip/project/GDEUPSB/TRUNK/GDEUPSB@37791 2ac714ae-9a16-11e4-ad83-2f06aba1930a

--- src/com/bocom/bbip/gdeupsb/action/elec02/SignInOutAction.java
+++ src/com/bocom/bbip/gdeupsb/action/elec02/SignInOutAction.java
@@ -16,39 +16,48 @@ public class SignInOutAction extends BaseAction{
 	 @Override
 	    public void execute(Context context) throws CoreException, CoreRuntimeException {
 		 System.out.println("@@@@@@@@@@@@@"+context);
+		 context.setData("thdRspCde", "00");
+	    context.setData("rspMsg", "交易成功");
+	    context.setData("DlTxt", "交易成功");
 		 
-		if("gdeupsb.signInOutThird".equals(context.getData("TransCode"))){
-			context.setData(ParamKeys.TXN_TYP, Constants.SIGN_SET_TYPE_SIGNIN);
-		}else{
-			context.setData(ParamKeys.TXN_TYP, Constants.SIGN_SET_TYPE_SIGNOUT);
-		}
-		System.out.println("@@@@@@@@@@@@@"+context.getData(ParamKeys.TXN_TYP));
-	        String txnTyp = context.<String> getData(ParamKeys.TXN_TYP);
-	        // 根据报文中操作选项（txnTyp）判断签到或者签退：0-签到；1-签退
-	        if (Constants.SIGN_SET_TYPE_SIGNIN.equals(txnTyp)) {
-	            chlSignIn(context);
-	        } else if (Constants.SIGN_SET_TYPE_SIGNOUT.equals(txnTyp)) {
-	            chlSignOut(context);
-	        } else {
-	            throw new CoreException(ErrorCodes.EUPS_OPR_TYP_ERR);
-	        }
+//		if("gdeupsb.signInOutThird".equals(context.getData("TransCode"))){
+//			context.setData(ParamKeys.TXN_TYP, Constants.SIGN_SET_TYPE_SIGNIN);
+//		}else{
+//			context.setData(ParamKeys.TXN_TYP, Constants.SIGN_SET_TYPE_SIGNOUT);
+//		}
+//		System.out.println("@@@@@@@@@@@@@"+context.getData(ParamKeys.TXN_TYP));
+//	        String txnTyp = context.<String> getData(ParamKeys.TXN_TYP);
+//	        // 根据报文中操作选项（txnTyp）判断签到或者签退：0-签到；1-签退
+//	        if (Constants.SIGN_SET_TYPE_SIGNIN.equals(txnTyp)) {
+//	            chlSignIn(context);
+//	        } else if (Constants.SIGN_SET_TYPE_SIGNOUT.equals(txnTyp)) {
+//	            chlSignOut(context);
+//	        } else {
+//	            throw new CoreException(ErrorCodes.EUPS_OPR_TYP_ERR);
+//	        }
 	    }
 
 	    /*
 	     * 签到.
 	     */
 	    private void chlSignIn(Context context) throws CoreException, CoreRuntimeException {
-	        EupsThdTranCtlInfo eupsThdTranCtlInfo = ContextUtils.getDataAsObject(context, EupsThdTranCtlInfo.class);
-	        EupsThdTranCtlInfo resultThdTranCtlInfo = get(EupsThdTranCtlInfoRepository.class).findOne(eupsThdTranCtlInfo.getComNo());
-	        context.setData(ParamKeys.CONSOLE_THD_TRANS_CONTROL_INFO_LIST, resultThdTranCtlInfo);
-	        
-	        if (resultThdTranCtlInfo == null) {
-	            throw new CoreException(ErrorCodes.EUPS_THD_TRANS_CTLINFO_NOTEXIST);
-	        } else if (resultThdTranCtlInfo.isTxnCtlStsSignin()) {
-	            throw new CoreException(ErrorCodes.THD_CHL_ALDEAY_SIGN_IN);
-	        } else if (resultThdTranCtlInfo.isTxnCtlStsCheckBillIng()) {
-	            throw new CoreException(ErrorCodes.THD_CHL_SIGNIN_NOT_ALLOWWED);
-	        } 
+	    	
+	    	
+	    	context.setData("thdRspCde", "00");
+	    	context.setData("rspMsg", "交易成功");
+	    	context.setData("DlTxt", "交易成功");
+
+//	        EupsThdTranCtlInfo eupsThdTranCtlInfo = ContextUtils.getDataAsObject(context, EupsThdTranCtlInfo.class);
+//	        EupsThdTranCtlInfo resultThdTranCtlInfo = get(EupsThdTranCtlInfoRepository.class).findOne(eupsThdTranCtlInfo.getComNo());
+//	        context.setData(ParamKeys.CONSOLE_THD_TRANS_CONTROL_INFO_LIST, resultThdTranCtlInfo);
+//	        
+//	        if (resultThdTranCtlInfo == null) {
+//	            throw new CoreException(ErrorCodes.EUPS_THD_TRANS_CTLINFO_NOTEXIST);
+//	        } else if (resultThdTranCtlInfo.isTxnCtlStsSignin()) {
+//	            throw new CoreException(ErrorCodes.THD_CHL_ALDEAY_SIGN_IN);
+//	        } else if (resultThdTranCtlInfo.isTxnCtlStsCheckBillIng()) {
+//	            throw new CoreException(ErrorCodes.THD_CHL_SIGNIN_NOT_ALLOWWED);
+//	        } 
 	    }
 
 	    /*签退
