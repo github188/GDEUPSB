fdb2df2a197e2684a200e5612574e78071ca2b18
Li_Guilin <Li_Guilin@2ac714ae-9a16-11e4-ad83-2f06aba1930a>
2015-04-17 02:02:59 +0000

    git-svn-id: svn://182.53.32.18/bbip/project/GDEUPSB/TRUNK/GDEUPSB@37652 2ac714ae-9a16-11e4-ad83-2f06aba1930a

--- src/com/bocom/bbip/gdeupsb/action/tbc/CheckComTxnCtlAction.java
+++ src/com/bocom/bbip/gdeupsb/action/tbc/CheckComTxnCtlAction.java
@@ -24,6 +24,11 @@ public class CheckComTxnCtlAction  extends BaseAction {
      */
     public void checkThdTxnCtlNormal(Context context) throws Exception {
         context.setState(BPState.BUSINESS_PROCESSNIG_STATE_FAIL);
+        
+        //金额转换分-元 add by zds
+        String txnAmt = context.getData("QTY_TRADE"); 
+        context.setData("txnAmt", changeF2Y(txnAmt));
+        
         GdTbcBasInf info = qryComTxnCtlInf(context);
         if (!info.getSigSts().equals("0")) {
             throw new CoreException(ErrorCodes.THD_CHL_TRADE_NOT_ALLOWWED);
@@ -83,4 +88,39 @@ public class CheckComTxnCtlAction  extends BaseAction {
         Assert.isNotNull(resultTbcBasInfo, ErrorCodes.EUPS_THD_TRANS_CTLINFO_NOTEXIST);
         return resultTbcBasInfo;
     }
+    
+	/**
+	 * 将分为单位的转换为元并返回金额格式的字符串 （除100）  
+	 * @param num
+	 * @return
+	 */
+    public static String changeF2Y(String amount) throws Exception{    
+              
+        int flag = 0;    
+        String amString = amount.toString();    
+        if(amString.charAt(0)=='-'){    
+            flag = 1;    
+            amString = amString.substring(1);    
+        }    
+        StringBuffer result = new StringBuffer();    
+        if(amString.length()==1){    
+            result.append("0.0").append(amString);    
+        }else if(amString.length() == 2){    
+            result.append("0.").append(amString);    
+        }else{    
+            String intString = amString.substring(0,amString.length()-2);    
+            for(int i=1; i<=intString.length();i++){    
+                if( (i-1)%3 == 0 && i !=1){    
+                    result.append(",");    
+                }    
+                result.append(intString.substring(intString.length()-i,intString.length()-i+1));    
+            }    
+            result.reverse().append(".").append(amString.substring(amString.length()-2));    
+        }    
+        if(flag == 1){    
+            return "-"+result.toString();    
+        }else{    
+            return result.toString();    
+        }    
+    } 
 }
