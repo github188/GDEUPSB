26f92b726e0a3d2b8222c3e147aa81d5970d2e6c
Li_Guilin <Li_Guilin@2ac714ae-9a16-11e4-ad83-2f06aba1930a>
2015-04-17 13:34:23 +0000

    22
    git-svn-id: svn://182.53.32.18/bbip/project/GDEUPSB/TRUNK/GDEUPSB@38120 2ac714ae-9a16-11e4-ad83-2f06aba1930a

--- src/com/bocom/bbip/gdeupsb/action/elec02/EupsManageCounterAgt.java
+++ src/com/bocom/bbip/gdeupsb/action/elec02/EupsManageCounterAgt.java
@@ -1,41 +1,15 @@
 package com.bocom.bbip.gdeupsb.action.elec02;
 
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-
 import com.bocom.bbip.comp.BBIPPublicService;
-import com.bocom.bbip.comp.BBIPPublicServiceImpl;
-import com.bocom.bbip.comp.CommonRequest;
-import com.bocom.bbip.comp.account.AccountService;
-import com.bocom.bbip.comp.account.support.CardInfo;
-import com.bocom.bbip.comp.account.support.CusActInfResult;
 import com.bocom.bbip.eups.action.BaseAction;
-import com.bocom.bbip.eups.action.common.CommThdRspCdeAction;
 import com.bocom.bbip.eups.adaptor.ThirdPartyAdaptor;
-import com.bocom.bbip.eups.adaptor.support.CallThdService;
-import com.bocom.bbip.eups.common.BPState;
-import com.bocom.bbip.eups.common.Constants;
-import com.bocom.bbip.eups.common.ErrorCodes;
-import com.bocom.bbip.eups.common.ParamKeys;
-import com.bocom.bbip.eups.entity.EupsThdBaseInfo;
-import com.bocom.bbip.gdeupsb.common.GDConstants;
-import com.bocom.bbip.gdeupsb.common.GDParamKeys;
 import com.bocom.bbip.gdeupsb.entity.GdeupsAgtElecTmp;
 import com.bocom.bbip.gdeupsb.repository.GdeupsAgtElecTmpRepository;
-import com.bocom.bbip.service.BGSPServiceAccessObject;
-import com.bocom.bbip.service.Result;
-import com.bocom.bbip.utils.BeanUtils;
-import com.bocom.bbip.utils.DateUtils;
-import com.bocom.bbip.utils.StringUtils;
-import com.bocom.euif.component.util.StringUtil;
 import com.bocom.jump.bp.core.Context;
 import com.bocom.jump.bp.core.CoreException;
 
@@ -55,7 +29,6 @@ public class EupsManageCounterAgt extends BaseAction {
 	public void process(Context context) throws CoreException {
      logger.info("协议维护");
      
-  
      final int oprType=Integer.parseInt((String)context.getData("CHT"));
 		switch (oprType) {
 		case ADD:
@@ -79,7 +52,9 @@ public class EupsManageCounterAgt extends BaseAction {
 	}
 
 	private void updateAgentDeal(Context context) throws CoreException {
-			
+		GdeupsAgtElecTmp agtElecTmp = toGdeupsAgtElecTmp(context);
+		get(GdeupsAgtElecTmpRepository.class).updateByAc(agtElecTmp);
+		
 	}
 
 	private void queryAgentDeal(Context context) throws CoreException {
@@ -107,16 +82,6 @@ public class EupsManageCounterAgt extends BaseAction {
 	}
 	
 
-	//修改本地协议
-	private void modifyGdeupsAgtElecTmp(Context context ){
-		
-		GdeupsAgtElecTmp agtElecTmp = toGdeupsAgtElecTmp(context);
-		get(GdeupsAgtElecTmpRepository.class).save(agtElecTmp);
-	}
-	
-	
-	
-	
 	//封装GdeupsAgtElecTmp对象
 	private GdeupsAgtElecTmp toGdeupsAgtElecTmp( Context context){
 		
@@ -137,9 +102,7 @@ public class EupsManageCounterAgt extends BaseAction {
 	
 	//为查询返回报文复制
 	private void setResponseResultFromAgts(Context context,GdeupsAgtElecTmp agtElecTmp){
-		
-	
-		
+
 		context.setData("JFH",agtElecTmp.getFeeNum());  //协议编号
 		context.setData("UsrNam", agtElecTmp.getUserName());		//单位编号
 		context.setData("ActNo", agtElecTmp.getActNo());		//第三方客户名称
@@ -148,10 +111,8 @@ public class EupsManageCounterAgt extends BaseAction {
 		context.setData("GPF",agtElecTmp.getPerComFlag());		//名称
 		context.setData("MOB", agtElecTmp.getPhoneNum());		//证件类型
 		context.setData("TEL",agtElecTmp.getTelNum());		//证件号码
-		
 
 	}
-	
 	
 	
 }
