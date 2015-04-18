de8c370bc34b6e64580c8ac4670b0c8052cb42f5
Li_Yawei <Li_Yawei@2ac714ae-9a16-11e4-ad83-2f06aba1930a>
2015-04-18 13:20:59 +0000

    git-svn-id: svn://182.53.32.18/bbip/project/GDEUPSB/TRUNK/GDEUPSB@38738 2ac714ae-9a16-11e4-ad83-2f06aba1930a

--- src/com/bocom/bbip/gdeupsb/repository/GDEupsbTrspInvChgInfoRepository.java
+++ src/com/bocom/bbip/gdeupsb/repository/GDEupsbTrspInvChgInfoRepository.java
@@ -1,7 +1,12 @@
 package com.bocom.bbip.gdeupsb.repository;
 
+import java.util.List;
+import java.util.Map;
+
 import com.bocom.bbip.data.repository.PagingAndSortingRepository;
 import com.bocom.bbip.gdeupsb.entity.GDEupsbTrspInvChgInfo;
 
 public interface GDEupsbTrspInvChgInfoRepository extends PagingAndSortingRepository<GDEupsbTrspInvChgInfo, String> {
+
+	public List<GDEupsbTrspInvChgInfo> findInvGroup(Map<String, Object> map);
 }
