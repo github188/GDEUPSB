7a17d9f2de5ab3b133ebaf4c5675f4d5e7e077dc
Li_Guilin <Li_Guilin@2ac714ae-9a16-11e4-ad83-2f06aba1930a>
2015-04-18 11:13:41 +0000

    111
    git-svn-id: svn://182.53.32.18/bbip/project/GDEUPSB/TRUNK/GDEUPSB@38691 2ac714ae-9a16-11e4-ad83-2f06aba1930a

--- src/com/bocom/bbip/gdeupsb/action/zh/BatchAcpServiceImplELEC02.java
+++ src/com/bocom/bbip/gdeupsb/action/zh/BatchAcpServiceImplELEC02.java
@@ -1,18 +1,24 @@
 package com.bocom.bbip.gdeupsb.action.zh;
 
+import java.math.BigDecimal;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.core.io.Resource;
 
+import com.bocom.bbip.comp.BBIPPublicService;
 import com.bocom.bbip.eups.action.BaseAction;
 import com.bocom.bbip.eups.common.ErrorCodes;
 import com.bocom.bbip.eups.common.ParamKeys;
+import com.bocom.bbip.eups.entity.EupsBatchConsoleInfo;
 import com.bocom.bbip.eups.entity.EupsThdFtpConfig;
+import com.bocom.bbip.eups.repository.EupsBatchConsoleInfoRepository;
 import com.bocom.bbip.eups.repository.EupsThdFtpConfigRepository;
 import com.bocom.bbip.eups.spi.service.batch.BatchAcpService;
 import com.bocom.bbip.eups.spi.vo.PrepareBatchAcpDomain;
@@ -27,14 +33,18 @@ import com.bocom.bbip.gdeupsb.repository.GDEupsbElecstBatchTmpRepository;
 import com.bocom.bbip.utils.Assert;
 import com.bocom.bbip.utils.BeanUtils;
 import com.bocom.bbip.utils.ContextUtils;
+import com.bocom.bbip.utils.DateUtils;
 import com.bocom.jump.bp.JumpException;
 import com.bocom.jump.bp.core.Context;
 import com.bocom.jump.bp.core.CoreException;
 import com.bocom.jump.bp.core.CoreRuntimeException;
-import com.bocom.jump.bp.service.sqlmap.SqlMap;
 import com.bocom.jump.bp.support.CollectionUtils;
 import com.bocom.bbip.eups.action.common.OperateFTPAction;
 public class BatchAcpServiceImplELEC02 extends BaseAction implements BatchAcpService {
+	
+	@Autowired
+	BBIPPublicService bbipPublicService;
+	
 	private static final Log logger=LogFactory.getLog(BatchAcpServiceImplELEC02.class);
 	@Override
 	public void prepareBatchDeal(PrepareBatchAcpDomain domain, Context context)throws CoreException {
@@ -53,6 +63,7 @@ public class BatchAcpServiceImplELEC02 extends BaseAction implements BatchAcpSer
 		Assert.isFalse(null==config, ErrorCodes.EUPS_FTP_INFO_NOTEXIST,"代收付FTP配置信息不存在");
 		config.setRmtWay("/home/bbipadm/home");
 		config.setRmtFleNme("batch.txt");
+	
 		((OperateFTPAction)get("opeFTP")).getFileFromFtp(config);
 		Map<String,Object> result=pareseFileByPath("D:\\batch.txt", "", "ELEC02Batch");
 		Assert.isFalse(null==result||0==result.size(), ErrorCodes.EUPS_FILE_PARESE_FAIL);
@@ -66,8 +77,12 @@ public class BatchAcpServiceImplELEC02 extends BaseAction implements BatchAcpSer
         	tmp.setBatNo((String)context.getData(ParamKeys.BAT_NO));
         }
 		/**插入临时表中*/
-		((SqlMap)get("sqlMap")).insert("com.bocom.bbip.gdeupsb.entity.GDEupsbElecstBatchTmp.batchInsert", list);;
+//		((SqlMap)get("sqlMap")).insert("com.bocom.bbip.gdeupsb.entity.GDEupsbElecstBatchTmp.batchInsert", list);;
 		
+        for (GDEupsbElecstBatchTmp tmp:list){
+        	 get(GDEupsbElecstBatchTmpRepository.class).insert(tmp);
+        }
+        
 		List <GDEupsbElecstBatchTmp>lt=get(GDEupsbElecstBatchTmpRepository.class).findByBatNo((String)context.getData(ParamKeys.BAT_NO));
 		
 		List<Map<String,Object>> detail=(List<Map<String, Object>>) BeanUtils.toMaps(lt);
@@ -85,7 +100,28 @@ public class BatchAcpServiceImplELEC02 extends BaseAction implements BatchAcpSer
 		((BatchFileCommon)get(GDConstants.BATCH_FILE_COMMON_UTILS)).sendBatchFileToACP(context);
 		
 		((BatchFileCommon)get(GDConstants.BATCH_FILE_COMMON_UTILS)).unLock(comNo);
+		
 		logger.info("批量文件数据准备结束-------------");
+
+		// 同步提交
+		
+		Date date=new Date();
+		context.setData("reqTme",DateUtils.formatAsSimpleDate(date)+"T"+DateUtils.format(date, "HH:mm:ss"));
+		
+	
+		//判空todo
+		String totCnt = context.getData("totCnt").toString();
+		String totAmt =  context.getData("totAmt").toString();
+		context.setData("totCnt", Integer.valueOf(totCnt));
+		context.setData("totAmt", new BigDecimal(totAmt));
+		
+		//提交
+		String mothed="eups.batchPaySubmitDataProcess";
+		bbipPublicService.synExecute(mothed, context);
+		
+		
+		
+		
 	}
 	  private  Map<String, Object> pareseFileByPath(String filePath, String fileName, String fileId)
 	    throws CoreException, CoreRuntimeException
@@ -102,4 +138,43 @@ public class BatchAcpServiceImplELEC02 extends BaseAction implements BatchAcpSer
 	    }
 	    return map;
 	  }
+	  
+	  /**
+		 *  同步调用process  批量代扣数据提交
+		 */
+			public void userProcessToSubmit(Context context)throws CoreException{
+				logger.info("==========Start  BatchDataFileAction  userProcessToSubmit");
+
+				Date date=new Date();
+				context.setData("reqTme",DateUtils.formatAsSimpleDate(date)+"T"+DateUtils.format(date, "HH:mm:ss"));
+				//提交
+				String mothed="eups.batchPaySubmitDataProcess";
+//				context.setData(ParamKeys.RSV_FLD3, context.getData(ParamKeys.THD_SQN));
+//				context.setData(ParamKeys.RSV_FLD2, context.getData(ParamKeys.SEQUENCE));
+				bbipPublicService.synExecute(mothed, context);
+				String	rsvFld3=context.getData(ParamKeys.THD_SQN).toString();
+				EupsBatchConsoleInfo eupsBatchConsoleInfo =new EupsBatchConsoleInfo();
+				eupsBatchConsoleInfo.setRsvFld3(rsvFld3);
+				String batNo=get(EupsBatchConsoleInfoRepository.class).find(eupsBatchConsoleInfo).get(0).getBatNo();
+				context.setData(ParamKeys.BAT_NO, batNo);
+				context.setData("PKGCNT", "000000");
+				logger.info("==========End  BatchDataFileAction  userProcessToSubmit");
+
+			}
+			
+	
+		
+			
+			public static void main(String args[]){
+				
+				String a = "2";
+				Object b = "2";
+				Object c = 2;
+				
+//				Integer aa = (Integer) a;
+				Integer d = (Integer) b;
+				
+
+				
+			}
 }
