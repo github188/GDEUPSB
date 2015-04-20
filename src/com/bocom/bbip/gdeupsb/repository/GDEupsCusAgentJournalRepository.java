29966f1243f9b71de55b1558ee52a2f2f117e479
Li_Yawei <Li_Yawei@2ac714ae-9a16-11e4-ad83-2f06aba1930a>
2015-04-20 14:55:12 +0000

    git-svn-id: svn://182.53.32.18/bbip/project/GDEUPSB/TRUNK/GDEUPSB@39765 2ac714ae-9a16-11e4-ad83-2f06aba1930a

--- src/com/bocom/bbip/gdeupsb/repository/GDEupsCusAgentJournalRepository.java
+++ src/com/bocom/bbip/gdeupsb/repository/GDEupsCusAgentJournalRepository.java
@@ -4,9 +4,9 @@ import java.util.List;
 import java.util.Map;
 
 import com.bocom.bbip.data.repository.PagingAndSortingRepository;
-import com.bocom.bbip.eups.entity.EupsCusAgentJournal;
+import com.bocom.bbip.gdeupsb.entity.GDEupsCusAgentJournal;
 
-public interface GDEupsCusAgentJournalRepository extends PagingAndSortingRepository<EupsCusAgentJournal, String> {
+public interface GDEupsCusAgentJournalRepository extends PagingAndSortingRepository<GDEupsCusAgentJournal, String> {
     
     public List<Map<String, Object>> findAllGroupByComNo(Map<String, Object> map);
 }
