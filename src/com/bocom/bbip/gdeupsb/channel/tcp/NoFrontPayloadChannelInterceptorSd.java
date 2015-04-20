c63fdd2d946fe63353f665b45d89bef3b09bf299
Li_Yawei <Li_Yawei@2ac714ae-9a16-11e4-ad83-2f06aba1930a>
2015-04-20 12:25:29 +0000

    git-svn-id: svn://182.53.32.18/bbip/project/GDEUPSB/TRUNK/GDEUPSB@39581 2ac714ae-9a16-11e4-ad83-2f06aba1930a

---
+++ src/com/bocom/bbip/gdeupsb/channel/tcp/NoFrontPayloadChannelInterceptorSd.java
@@ -0,0 +1,48 @@
+package com.bocom.bbip.gdeupsb.channel.tcp;
+
+import java.io.IOException;
+import java.net.Socket;
+
+import com.bocom.jump.bp.JumpException;
+import com.bocom.jump.bp.channel.ChannelContext;
+import com.bocom.jump.bp.channel.tcp.interceptors.SocketChannelInterceptor;
+import com.bocom.jump.bp.core.ContextEx;
+
+public class NoFrontPayloadChannelInterceptorSd extends NoFrontLengthStreamResolverEb
+		implements SocketChannelInterceptor
+{
+
+	public NoFrontPayloadChannelInterceptorSd()
+	{
+	}
+
+	public void onRequest(ChannelContext channelContext, ContextEx context)
+			throws JumpException
+	{
+		Socket socket = (Socket) channelContext.getRequest();
+		try
+		{
+			
+			byte[] ints=resolve(socket.getInputStream());
+			byte[] deal=new byte[ints.length-10];
+//			syste
+			System.arraycopy(ints, 10, deal, 0, deal.length);
+			channelContext.setRequestPayload(deal);
+		} catch (IOException io)
+		{
+			throw new JumpException("JUMPTP8000", "socket_read_error", io);
+		}
+	}
+
+	public void onResponse(ChannelContext channelContext, ContextEx context, Throwable throwable)
+	{
+		byte arrayOfByte[] = (byte[]) channelContext.getResponsePalyload();
+		try
+		{
+			((Socket) channelContext.getResponse()).getOutputStream().write(arrayOfByte);
+		} catch (IOException localIOException)
+		{
+			throw new RuntimeException(localIOException);
+		}
+	}
+}
