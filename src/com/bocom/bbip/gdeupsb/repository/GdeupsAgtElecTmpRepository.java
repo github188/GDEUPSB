cc4817f43626bd85565e18d15f9fde26968c5104
Qian_Chen <Qian_Chen@2ac714ae-9a16-11e4-ad83-2f06aba1930a>
2015-04-21 01:41:08 +0000

    git-svn-id: svn://182.53.32.18/bbip/project/GDEUPSB/TRUNK/GDEUPSB@39831 2ac714ae-9a16-11e4-ad83-2f06aba1930a

--- src/com/bocom/bbip/gdeupsb/repository/GdeupsAgtElecTmpRepository.java
+++ src/com/bocom/bbip/gdeupsb/repository/GdeupsAgtElecTmpRepository.java
@@ -1,28 +1,23 @@
 package com.bocom.bbip.gdeupsb.repository;
 
-
 import java.util.List;
 
 import com.bocom.bbip.data.annotation.Find;
 import com.bocom.bbip.data.repository.PagingAndSortingRepository;
 import com.bocom.bbip.gdeupsb.entity.GdeupsAgtElecTmp;
 
-
-
-public interface GdeupsAgtElecTmpRepository 
-	extends 
-PagingAndSortingRepository<GdeupsAgtElecTmp, String>
- {
+public interface GdeupsAgtElecTmpRepository
+		extends
+		PagingAndSortingRepository<GdeupsAgtElecTmp, String>
+{
 	@Find
 	public List<GdeupsAgtElecTmp> findBase(GdeupsAgtElecTmp agtElec);
-	
-	@Find
+
 	public void deleteByAc(GdeupsAgtElecTmp agtElec);
-	
-	@Find
+
 	public void updateByAc(GdeupsAgtElecTmp agtElec);
-	
-	
-	
-	
+
+//	@Find
+//	public void checkAgtExist(String actNo);
+
 }
