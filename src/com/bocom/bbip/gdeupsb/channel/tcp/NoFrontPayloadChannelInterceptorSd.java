c653b215d040e9b93c3e162490ca9cf3f0feaf0e
Qian_Chen <Qian_Chen@2ac714ae-9a16-11e4-ad83-2f06aba1930a>
2015-04-21 11:25:26 +0000

    git-svn-id: svn://182.53.32.18/bbip/project/GDEUPSB/TRUNK/GDEUPSB@40281 2ac714ae-9a16-11e4-ad83-2f06aba1930a

--- src/com/bocom/bbip/gdeupsb/channel/tcp/NoFrontPayloadChannelInterceptorSd.java
+++ src/com/bocom/bbip/gdeupsb/channel/tcp/NoFrontPayloadChannelInterceptorSd.java
@@ -7,6 +7,7 @@ import com.bocom.jump.bp.JumpException;
 import com.bocom.jump.bp.channel.ChannelContext;
 import com.bocom.jump.bp.channel.tcp.interceptors.SocketChannelInterceptor;
 import com.bocom.jump.bp.core.ContextEx;
+import com.bocom.jump.bp.util.Hex;
 
 public class NoFrontPayloadChannelInterceptorSd extends NoFrontLengthStreamResolverEb
 		implements SocketChannelInterceptor
@@ -24,9 +25,13 @@ public class NoFrontPayloadChannelInterceptorSd extends NoFrontLengthStreamResol
 		{
 			
 			byte[] ints=resolve(socket.getInputStream());
+			
+			System.out.println("接收到的初始报文为:"+Hex.toDumpString(ints));
 			byte[] deal=new byte[ints.length-10];
 //			syste
 			System.arraycopy(ints, 10, deal, 0, deal.length);
+			
+			System.out.println("经过处理后的报文为:"+Hex.toDumpString(deal));
 			channelContext.setRequestPayload(deal);
 		} catch (IOException io)
 		{
