a976099cb59fc86548d3930f7670c29d1a34ebb7
Li_Yawei <Li_Yawei@2ac714ae-9a16-11e4-ad83-2f06aba1930a>
2015-04-21 23:36:51 +0000

    git-svn-id: svn://182.53.32.18/bbip/project/GDEUPSB/TRUNK/GDEUPSB@40544 2ac714ae-9a16-11e4-ad83-2f06aba1930a

--- src/com/bocom/bbip/gdeupsb/strategy/efek/queryFeeOnline/QueryFeeResultAction.java
+++ src/com/bocom/bbip/gdeupsb/strategy/efek/queryFeeOnline/QueryFeeResultAction.java
@@ -21,7 +21,6 @@ import com.bocom.bbip.eups.common.ErrorCodes;
 import com.bocom.bbip.eups.common.ParamKeys;
 import com.bocom.bbip.eups.repository.EupsAmountInfoRepository;
 import com.bocom.bbip.gdeupsb.common.GDConstants;
-import com.bocom.bbip.gdeupsb.common.GDErrorCodes;
 import com.bocom.bbip.gdeupsb.common.GDParamKeys;
 import com.bocom.bbip.utils.DateUtils;
 import com.bocom.bbip.utils.StringUtils;
@@ -87,16 +86,6 @@ public class QueryFeeResultAction implements Executable{
 										if(null !=rspMap){
 											 	context.setDataMap(rspMap);
 								                context.setData(ParamKeys.THIRD_RETURN_MESSAGE, rspMap);
-//								                if(rspMap.get("thdRspCde").toString().trim().equals("18")){
-//								                		throw new CoreException("正在进行批量代扣业务，需等本业务完成后才能办理本手续");
-//								                		throw new CoreException(GDErrorCodes.EUPS_ELEC02_06_ERROR);
-//								                }
-								                List<Map<String, Object>> list=(List<Map<String, Object>>)rspMap.get("Information");
-								               //页数
-								                pageGet(context, list);
-								                //返回list
-								                creatList(list,context);
-
 								                //第三方返回码
 								                CommThdRspCdeAction rspCdeAction = new CommThdRspCdeAction();
 								                String responseCode = rspCdeAction.getThdRspCde(rspMap, context.getData(ParamKeys.EUPS_BUSS_TYPE).toString());
@@ -105,7 +94,6 @@ public class QueryFeeResultAction implements Executable{
 								                	responseCode=ErrorCodes.EUPS_THD_SYS_ERROR;
 								                }
 								                context.setData(ParamKeys.RESPONSE_CODE, responseCode);
-								                
 								             // 第三方交易成功
 									                if (GDConstants.SUCCESS_CODE.equals(responseCode)) {
 									                    logger.info("The third process response successful.");
@@ -134,11 +122,17 @@ public class QueryFeeResultAction implements Executable{
 									                	context.setData(ParamKeys.RSP_CDE, "EFE999");
 									                	context.setData(ParamKeys.RSP_MSG, "发送失败");
 									                }else{
-									                	context.setData(ParamKeys.THD_TXN_STS,Constants.THD_TXNSTS_FAIL);
-									                	context.setData(GDParamKeys.MSGTYP, "E");
-									                	context.setData(ParamKeys.RSP_CDE, "EFE999");
-									                	context.setData(ParamKeys.RSP_MSG, "交易失败，其他未知情况");
+//									                	context.setData(ParamKeys.THD_TXN_STS,Constants.THD_TXNSTS_FAIL);
+//									                	context.setData(GDParamKeys.MSGTYP, "E");
+//									                	context.setData(ParamKeys.RSP_CDE, "EFE999");
+//									                	context.setData(ParamKeys.RSP_MSG, "交易失败，其他未知情况");
+									                	throw new CoreException(responseCode);
 									                }
+									                List<Map<String, Object>> list=(List<Map<String, Object>>)rspMap.get("Information");
+									               //页数
+									                pageGet(context, list);
+									                //返回list
+									                creatList(list,context);
 										}
 								}else{
 										logger.info("~~~~~~~~~~~发送失败");
