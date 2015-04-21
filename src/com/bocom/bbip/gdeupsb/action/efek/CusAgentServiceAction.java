47902bf95bc89209687125e66c50fbb6b3702046
Li_Yawei <Li_Yawei@2ac714ae-9a16-11e4-ad83-2f06aba1930a>
2015-04-21 15:09:25 +0000

    git-svn-id: svn://182.53.32.18/bbip/project/GDEUPSB/TRUNK/GDEUPSB@40531 2ac714ae-9a16-11e4-ad83-2f06aba1930a

--- src/com/bocom/bbip/gdeupsb/action/efek/CusAgentServiceAction.java
+++ src/com/bocom/bbip/gdeupsb/action/efek/CusAgentServiceAction.java
@@ -282,7 +282,7 @@ public class CusAgentServiceAction extends BaseAction{
 				context.setData("br", "01441131999");
 			}
 			map.put("cusAc", context.getData("cusAc"));
-//			map.put("cusAc", "6222627610000064611");
+//			map.put("cusAc", "6222600710013684149");
 			logger.info("~~~~~~~~~~requestHeader~~~~map~~~~~ "+map);
 			logger.info("~~~~~~~~~~列表查询开始 ");
 			//上代收付取协议编号
