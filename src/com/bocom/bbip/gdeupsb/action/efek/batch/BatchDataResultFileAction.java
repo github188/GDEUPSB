a4dc528a46fef9933b081128845b97d9117e65a1
Li_Yawei <Li_Yawei@2ac714ae-9a16-11e4-ad83-2f06aba1930a>
2015-04-22 10:42:51 +0000

    git-svn-id: svn://182.53.32.18/bbip/project/GDEUPSB/TRUNK/GDEUPSB@41076 2ac714ae-9a16-11e4-ad83-2f06aba1930a

--- src/com/bocom/bbip/gdeupsb/action/efek/batch/BatchDataResultFileAction.java
+++ src/com/bocom/bbip/gdeupsb/action/efek/batch/BatchDataResultFileAction.java
@@ -321,6 +321,7 @@ public class BatchDataResultFileAction extends BaseAction implements AfterBatchA
         String cmd="ssh icsadm@182.53.15.200 /app/ics/app/efek/bin/EfeFilSend.sh 182.53.201.46 bcm exchange dat/efek/send "+srcFile+" "+DateUtils.formatAsHHmmss(new Date());
         logger.info("cmd=" + cmd);
         Process proc = Runtime.getRuntime().exec(cmd);
+        proc.waitFor();
         logger.info("en-file success!");
         logger.info("================End BatchDataFileActiion  RecvEnCryptFile");
         
