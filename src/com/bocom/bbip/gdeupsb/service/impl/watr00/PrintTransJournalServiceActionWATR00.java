fca8a7b2f7c3144e3445fcf0183ac9c8d02c0411
Li_Guilin <Li_Guilin@2ac714ae-9a16-11e4-ad83-2f06aba1930a>
2015-04-23 07:04:04 +0000

    git-svn-id: svn://182.53.32.18/bbip/project/GDEUPSB/TRUNK/GDEUPSB@41768 2ac714ae-9a16-11e4-ad83-2f06aba1930a

--- src/com/bocom/bbip/gdeupsb/service/impl/watr00/PrintTransJournalServiceActionWATR00.java
+++ src/com/bocom/bbip/gdeupsb/service/impl/watr00/PrintTransJournalServiceActionWATR00.java
@@ -1,5 +1,6 @@
 package com.bocom.bbip.gdeupsb.service.impl.watr00;
 
+import java.math.BigDecimal;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
@@ -7,20 +8,29 @@ import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import com.bocom.bbip.comp.CommonRequest;
 import com.bocom.bbip.comp.account.AccountService;
 import com.bocom.bbip.eups.action.BaseAction;
+import com.bocom.bbip.eups.action.common.OperateFileAction;
 import com.bocom.bbip.eups.action.eupsreport.ReportHelper;
+import com.bocom.bbip.eups.common.ErrorCodes;
+import com.bocom.bbip.eups.entity.EupsBatchConsoleInfo;
 import com.bocom.bbip.eups.entity.EupsBatchInfoDetail;
+import com.bocom.bbip.eups.entity.EupsThdFtpConfig;
 import com.bocom.bbip.eups.entity.MFTPConfigInfo;
+import com.bocom.bbip.eups.repository.EupsBatchConsoleInfoRepository;
+import com.bocom.bbip.eups.repository.EupsBatchInfoDetailRepository;
 import com.bocom.bbip.eups.repository.EupsThdFtpConfigRepository;
 import com.bocom.bbip.file.reporting.impl.VelocityTemplatedReportRender;
 import com.bocom.bbip.gdeupsb.entity.GdeupsWatBatInfTmp;
 import com.bocom.bbip.gdeupsb.repository.GdeupsWatBatInfTmpRepository;
 import com.bocom.bbip.utils.BeanUtils;
+import com.bocom.bbip.utils.CollectionUtils;
 import com.bocom.bbip.utils.DateUtils;
 import com.bocom.bbip.utils.NumberUtils;
+import com.bocom.bbip.utils.StringUtils;
 import com.bocom.jump.bp.core.Context;
 import com.bocom.jump.bp.core.CoreException;
 import com.bocom.jump.bp.core.CoreRuntimeException;
@@ -33,51 +43,86 @@ public class PrintTransJournalServiceActionWATR00 extends BaseAction {
 	
 	private static Logger logger = LoggerFactory.getLogger(PrintTransJournalServiceActionWATR00.class);
 	
+	
+	@Autowired
+	OperateFileAction operateFile;
+	@Autowired
+	EupsThdFtpConfigRepository eupsThdFtpConfigRepository;
+	
 	@Override
 	public void execute(Context context) throws CoreException,	CoreRuntimeException {
 		logger.info("QueryAndPrintTransJournalServiceActionWATR00 execute start ... ...");
 		String txnDat = context.getData("txnDat").toString();
 		String br = context.getData("br");
         logger.info("txnDat["+txnDat+"]br["+br+"]");
-
+        EupsBatchConsoleInfo eupsBatchConsoleInfo = new EupsBatchConsoleInfo();
+        eupsBatchConsoleInfo.setExeDte(DateUtils.parse(txnDat));
+        eupsBatchConsoleInfo.setComNo("4450000685");
+        List<EupsBatchConsoleInfo> infoList = get(EupsBatchConsoleInfoRepository.class).find(eupsBatchConsoleInfo);
+        String batNo;
+        if(CollectionUtils.isEmpty(infoList)){
+        	throw new CoreException(ErrorCodes.EUPS_ACT_NO_ISEXIST);
+        }else{
+        	EupsBatchConsoleInfo eupsBatchConsoleInfoA = infoList.get(0);
+        	 batNo=eupsBatchConsoleInfoA.getBatNo();
+        }
 //        GdeupsWatBatInfTmp tmp = new GdeupsWatBatInfTmp();
         EupsBatchInfoDetail tmp = new EupsBatchInfoDetail();
-        tmp.setTxnDte(DateUtils.parse(txnDat));
+        tmp.setBatNo(batNo);
         
-        List<GdeupsWatBatInfTmp> ret=get(GdeupsWatBatInfTmpRepository.class).find(tmp);
+        List<EupsBatchInfoDetail> ret=get(EupsBatchInfoDetailRepository.class).find(tmp);
         List<Map<String,Object>> retMap=(List<Map<String, Object>>) BeanUtils.toMaps(ret);
-        double totAmt=0.0;
+        BigDecimal totAmt = new BigDecimal("0.0");
+        int sumCnt=0;
         for(Map map:retMap){
         	
-        	map.put("thdCusNo", map.get("agtSrvCusId"));
+//        	map.put("thdCusNo", map.get("agtSrvCusId"));
 //        	map.put("txnAmt", NumberUtils.centToYuanAsString(map.get("je").toString()));
 //        	map.put("bakFld1", "S");
-        	totAmt+=Double.parseDouble(map.get("txnAmt").toString());
+        	map.put("txnDte", txnDat);
+//        	totAmt+=Double.valueOf(map.get("txnAmt").toString());
+        	if("S".equals(map.get("sts"))){
+        		totAmt=totAmt.add(new BigDecimal(map.get("txnAmt").toString()));
+        		sumCnt+=1;
+        	}
+        	
         	
         }
-        context.setData("sumCnt", ret.size());
-        context.setData("sumAmt", totAmt);
-        EupsThdFtpConfigRepository eupsThdFtpConfigRepository = get(EupsThdFtpConfigRepository.class);
-		ReportHelper reportHelper = get(ReportHelper.class);
-		//MFTPConfigInfo mftpConfigInfo = reportHelper.getMFTPConfigInfo(eupsThdFtpConfigRepository);
-		//logger.info((new StringBuilder("mftpConfigInfo:>>>>").append(BeanUtils.toMap(mftpConfigInfo))).toString());
-		
-		VelocityTemplatedReportRender render = new VelocityTemplatedReportRender();
-		try {
-			render.afterPropertiesSet();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		Map<String,String> map = new HashMap<String,String>();
-		map.put("sample", "config/report/watr00/watr00_printTransJournal.vm");
-		render.setReportNameTemplateLocationMapping(map);
-		context.setData("eles", retMap);
-		String result = render.renderAsString("sample", context);
-		logger.info(result);
-		String date = DateUtils.format(new Date(), DateUtils.STYLE_HHmmss);
-		StringBuffer fileName = new StringBuffer((new StringBuilder("WATR00"+br+txnDat).append(date).toString()));
-	//	reportHelper.createFileAndSendMFTP(context, result, fileName, mftpConfigInfo);
-       context.setData("filName", fileName);
+        Map<String, Object> floorMap = new HashMap<String, Object>();
+        Map<String, Object> resultMap = new HashMap<String, Object>();
+        
+        logger.info("@@@@@@@@="+totAmt.toString());
+        floorMap.put("sumCnt", ret.size());
+        floorMap.put("sumAmt", totAmt.toString());
+        resultMap.put("floor", floorMap);
+        resultMap.put("detail", retMap);
+        
+        
+        EupsThdFtpConfig eupsThdFtpConfig = eupsThdFtpConfigRepository.findOne("watTransJnlFile");
+        String fileName = "wat"+txnDat;
+        // 生成文件
+     	operateFile.createCheckFile(eupsThdFtpConfig, "watTransJnl", fileName, resultMap);
+//        EupsThdFtpConfigRepository eupsThdFtpConfigRepository = get(EupsThdFtpConfigRepository.class);
+//		ReportHelper reportHelper = get(ReportHelper.class);
+//		//MFTPConfigInfo mftpConfigInfo = reportHelper.getMFTPConfigInfo(eupsThdFtpConfigRepository);
+//		//logger.info((new StringBuilder("mftpConfigInfo:>>>>").append(BeanUtils.toMap(mftpConfigInfo))).toString());
+//		
+//		VelocityTemplatedReportRender render = new VelocityTemplatedReportRender();
+//		try {
+//			render.afterPropertiesSet();
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
+//		Map<String,String> map = new HashMap<String,String>();
+//		map.put("sample", "config/report/watr00/watr00_printTransJournal.vm");
+//		render.setReportNameTemplateLocationMapping(map);
+//		context.setData("eles", retMap);
+//		String result = render.renderAsString("sample", context);
+//		logger.info(result);
+//		String date = DateUtils.format(new Date(), DateUtils.STYLE_HHmmss);
+//		StringBuffer fileName = new StringBuffer((new StringBuilder("WATR00"+br+txnDat).append(date).toString()));
+////		reportHelper.createFileAndSendMFTP(context, result, fileName, mftpConfigInfo);
+//       context.setData("filName", fileName);
 		logger.info("QueryAndPrintTransJournalServiceActionWATR00 execute end ... ...");
 	}
 	
