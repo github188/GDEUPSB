97f8fed29ce21442e8095c229067c6581a4a8d95
Li_Yawei <Li_Yawei@2ac714ae-9a16-11e4-ad83-2f06aba1930a>
2015-04-23 13:09:55 +0000

    git-svn-id: svn://182.53.32.18/bbip/project/GDEUPSB/TRUNK/GDEUPSB@42118 2ac714ae-9a16-11e4-ad83-2f06aba1930a

--- src/com/bocom/bbip/gdeupsb/strategy/efek/checkThdAcct/CheckThdDetlAcctAction.java
+++ src/com/bocom/bbip/gdeupsb/strategy/efek/checkThdAcct/CheckThdDetlAcctAction.java
@@ -299,7 +299,8 @@ public class CheckThdDetlAcctAction implements Executable {
 				context.setData(GDParamKeys.TRADE_RECEIVE, GDConstants.TRADE_RECEIVE);//交易接收方
 				context.setData(GDParamKeys.TRADE_SOURCE_ADD, GDConstants.TRADE_SOURCE_ADD);//交易源地址
 				context.setData(GDParamKeys.TRADE_AIM_ADD, GDConstants.TRADE_AIM_ADD);//交易目标地址
-				context.setData("PKGCNT", "000001");
+				//TODO 
+				context.setData("PKGCNT", context.getData("number"));
 				context.setData(GDParamKeys.BUS_IDENTIFY, "YDLW18");
 				context.setData("sqns",context.getData(ParamKeys.SEQUENCE));
 				context.setData("WJS", "1");
