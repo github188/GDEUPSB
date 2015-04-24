e5942069e70f00f675ee9fcb174afafb0755deb6
Li_Guilin <Li_Guilin@2ac714ae-9a16-11e4-ad83-2f06aba1930a>
2015-04-24 01:34:46 +0000

    git-svn-id: svn://182.53.32.18/bbip/project/GDEUPSB/TRUNK/GDEUPSB@42373 2ac714ae-9a16-11e4-ad83-2f06aba1930a

--- src/com/bocom/bbip/gdeupsb/action/wat/CommQryWatCusAgentAction.java
+++ src/com/bocom/bbip/gdeupsb/action/wat/CommQryWatCusAgentAction.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import com.bocom.bbip.eups.action.BaseAction;
 import com.bocom.bbip.eups.common.ErrorCodes;
+import com.bocom.bbip.eups.common.ParamKeys;
 import com.bocom.bbip.gdeupsb.entity.GdEupsWatAgtInf;
 import com.bocom.bbip.gdeupsb.repository.GdEupsWatAgtInfRepository;
 import com.bocom.bbip.utils.BeanUtils;
@@ -18,9 +19,11 @@ public class CommQryWatCusAgentAction extends BaseAction {
 	public void execute(Context context) throws CoreException, CoreRuntimeException {
 		log.info("水费协议信息查询开始！..");
 
-		String cusAc = context.getData("cusAC"); // 记账卡号
-		context.setData("cusAc", cusAc);
-		GdEupsWatAgtInf para = BeanUtils.toObject(context.getDataMap(), GdEupsWatAgtInf.class);
+//		String cusAc = context.getData("cusAC"); // 记账卡号
+//		context.setData("cusAc", cusAc);
+//		GdEupsWatAgtInf para = BeanUtils.toObject(context.getDataMap(), GdEupsWatAgtInf.class);
+		GdEupsWatAgtInf para = new GdEupsWatAgtInf();
+		para.setThdCusNo((String)context.getData(ParamKeys.THD_CUS_NO));
 		List<GdEupsWatAgtInf> resultList = get(GdEupsWatAgtInfRepository.class).find(para);
 		log.info("查询的返回信息为:" + resultList);
 		if (CollectionUtils.isEmpty(resultList)) {
