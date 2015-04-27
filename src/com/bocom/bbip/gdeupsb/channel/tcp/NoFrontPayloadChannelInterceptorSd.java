efa6dbaa6e79ebfc55af8517ddc6a9a4ac4f114a
Qian_Chen <Qian_Chen@2ac714ae-9a16-11e4-ad83-2f06aba1930a>
2015-04-27 01:09:06 +0000

    git-svn-id: svn://182.53.32.18/bbip/project/GDEUPSB/TRUNK/GDEUPSB@43602 2ac714ae-9a16-11e4-ad83-2f06aba1930a

--- src/com/bocom/bbip/gdeupsb/channel/tcp/NoFrontPayloadChannelInterceptorSd.java
+++ src/com/bocom/bbip/gdeupsb/channel/tcp/NoFrontPayloadChannelInterceptorSd.java
@@ -23,15 +23,15 @@ public class NoFrontPayloadChannelInterceptorSd extends NoFrontLengthStreamResol
 		Socket socket = (Socket) channelContext.getRequest();
 		try
 		{
-			
-			byte[] ints=resolve(socket.getInputStream());
-			
-			System.out.println("接收到的初始报文为:"+Hex.toDumpString(ints));
-			byte[] deal=new byte[ints.length-10];
-//			syste
+
+			byte[] ints = resolve(socket.getInputStream());
+
+			System.out.println("接收到的初始报文为:" + Hex.toDumpString(ints));
+			byte[] deal = new byte[ints.length - 10];
+			// syste
 			System.arraycopy(ints, 10, deal, 0, deal.length);
-			
-			System.out.println("经过处理后的报文为:"+Hex.toDumpString(deal));
+
+			System.out.println("经过处理后的报文为:" + Hex.toDumpString(deal));
 			channelContext.setRequestPayload(deal);
 		} catch (IOException io)
 		{
@@ -42,9 +42,21 @@ public class NoFrontPayloadChannelInterceptorSd extends NoFrontLengthStreamResol
 	public void onResponse(ChannelContext channelContext, ContextEx context, Throwable throwable)
 	{
 		byte arrayOfByte[] = (byte[]) channelContext.getResponsePalyload();
+		System.out.println("要发送的报文为:" + Hex.toDumpString(arrayOfByte));
+		int len = arrayOfByte.length;
+
+		byte[] deal = new byte[arrayOfByte.length + 10]; // 最终返回的报文
+
+		byte[] lenB = new byte[] { 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20 };
+
+		System.arraycopy(String.valueOf(len).getBytes(), 0, lenB, 0, String.valueOf(len).getBytes().length);
+
+		System.arraycopy(lenB, 0, deal, 0, 10);
+		System.arraycopy(arrayOfByte, 0, deal, 10, arrayOfByte.length);
+
 		try
 		{
-			((Socket) channelContext.getResponse()).getOutputStream().write(arrayOfByte);
+			((Socket) channelContext.getResponse()).getOutputStream().write(deal);
 		} catch (IOException localIOException)
 		{
 			throw new RuntimeException(localIOException);
