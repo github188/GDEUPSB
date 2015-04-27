817d1ba7e5cfeb345c2a3ebb62275718c0235a14
Wang_Mingqin <Wang_Mingqin@2ac714ae-9a16-11e4-ad83-2f06aba1930a>
2015-04-27 06:07:18 +0000

    git-svn-id: svn://182.53.32.18/bbip/project/GDEUPSB/TRUNK/GDEUPSB@43783 2ac714ae-9a16-11e4-ad83-2f06aba1930a

---
+++ src/com/bocom/bbip/gdeupsb/channel/tcp/NoFrontPayloadChannelInterceptorRq.java
@@ -0,0 +1,47 @@
+package com.bocom.bbip.gdeupsb.channel.tcp;
+
+import java.io.IOException;
+import java.net.Socket;
+
+import com.bocom.bbip.utils.StringUtils;
+import com.bocom.jump.bp.JumpException;
+import com.bocom.jump.bp.channel.ChannelContext;
+import com.bocom.jump.bp.channel.tcp.interceptors.SocketChannelInterceptor;
+import com.bocom.jump.bp.core.ContextEx;
+import com.bocom.jump.bp.util.Hex;
+
+public class NoFrontPayloadChannelInterceptorRq extends NoFrontLengthStreamResolverEb
+		implements SocketChannelInterceptor
+{
+
+	public NoFrontPayloadChannelInterceptorRq()
+	{
+	}
+
+	public void onRequest(ChannelContext channelContext, ContextEx context)
+			throws JumpException
+	{
+		Socket socket = (Socket) channelContext.getRequest();
+		try
+		{
+			channelContext.setRequestPayload(resolve(socket.getInputStream()));
+		} catch (IOException io)
+		{
+			throw new JumpException("JUMPTP8000", "socket_read_error", io);
+		}
+	}
+
+	public void onResponse(ChannelContext channelContext, ContextEx context, Throwable throwable)
+	{
+		byte arrayOfByte[] = (byte[]) channelContext.getResponsePalyload();
+		
+        System.out.println("处理后，byte=\n"+Hex.toDumpString(arrayOfByte)+",发送的报文转化为明文为:"+new String(arrayOfByte));
+		try
+		{
+			((Socket) channelContext.getResponse()).getOutputStream().write(arrayOfByte);
+		} catch (IOException localIOException)
+		{
+			throw new RuntimeException(localIOException);
+		}
+	}
+}
