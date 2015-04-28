bd4f5afa71fa0bf3ffddd163caa3e0dcbd6d52b3
Wang_Mingqin <Wang_Mingqin@2ac714ae-9a16-11e4-ad83-2f06aba1930a>
2015-04-28 13:56:12 +0000

    git-svn-id: svn://182.53.32.18/bbip/project/GDEUPSB/TRUNK/GDEUPSB@45018 2ac714ae-9a16-11e4-ad83-2f06aba1930a

--- src/com/bocom/bbip/gdeupsb/action/gas/QryHzThdCusInfoAction.java
+++ src/com/bocom/bbip/gdeupsb/action/gas/QryHzThdCusInfoAction.java
@@ -1,7 +1,5 @@
 package com.bocom.bbip.gdeupsb.action.gas;
 
-import java.math.BigInteger;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -10,12 +8,15 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.util.CollectionUtils;
 
+import com.bocom.bbip.data.domain.Page;
+import com.bocom.bbip.data.domain.PageRequest;
+import com.bocom.bbip.data.domain.Pageable;
 import com.bocom.bbip.eups.action.BaseAction;
 import com.bocom.bbip.eups.common.BPState;
-import com.bocom.bbip.gdeupsb.common.GDConstants;
-import com.bocom.bbip.gdeupsb.common.GDParamKeys;
+import com.bocom.bbip.eups.common.ErrorCodes;
+import com.bocom.bbip.gdeupsb.entity.GdGasCusAll;
 import com.bocom.bbip.gdeupsb.repository.GdGasCusAllRepository;
-import com.bocom.bbip.utils.DateUtils;
+import com.bocom.bbip.utils.BeanUtils;
 import com.bocom.jump.bp.core.Context;
 import com.bocom.jump.bp.core.CoreException;
 import com.bocom.jump.bp.core.CoreRuntimeException;
@@ -61,88 +62,66 @@ public class QryHzThdCusInfoAction extends BaseAction {
 		map.put("beginDate", context.getData("beginDate").toString());
 		map.put("endDate", context.getData("endDate").toString());
 
-		List<Map<String, Object>> gasCusAllList = get(
+		List<GdGasCusAll> gasCusAllList = get(
 				GdGasCusAllRepository.class).findDataByOptDate(map);
-		if (CollectionUtils.isEmpty(gasCusAllList)) { // 无明细信息
-			context.setData(GDParamKeys.GAS_MSG_TYP, "E");
-			context.setData(GDParamKeys.GAS_AP_CDE, "SC");
-			context.setData(GDParamKeys.GAS_OFMT_COD, "D04");
-			context.setData(GDParamKeys.GAS_IN_POS, "0001");
-			context.setData(GDParamKeys.GDEUPSB_RSP_MSG, "无明细信息");
-			throw new CoreRuntimeException("无明细信息");
+		if (null == gasCusAllList || CollectionUtils.isEmpty(gasCusAllList)) {
+			logger.info("There are no records for select check trans journal ");
+			throw new CoreException(ErrorCodes.EUPS_QUERY_NO_DATA);
 		}
-		logger.info("gasCusAllList.size()=" + gasCusAllList.size());
-		logger.info("==========context=" + context);
+		
+		Pageable pageable = BeanUtils.toObject(context.getDataMap(), PageRequest.class);
+		Page<GdGasCusAll> page = get(GdGasCusAllRepository.class).findDataByOptDate(pageable, map);
+		setResponseFromPage(context, "loopDtl", page);
+		logger.info("========== context after set loopDtl =" + context);
+		
+//		logger.info("gasCusAllList.size()=" + gasCusAllList.size());
+//		logger.info("==========context=" + context);
+//
+//		List<Map<String, Object>> gasCusAllListDtl = new ArrayList<Map<String, Object>>();
+//		for (Map<String, Object> maps : gasCusAllList) {
+//			Map<String, Object> cusAllDtlTemp = new HashMap<String, Object>();
+//			cusAllDtlTemp.put("cusNo", maps.get("CUS_NO"));
+//			cusAllDtlTemp.put("cusAc", maps.get("CUS_AC"));
+//			cusAllDtlTemp.put("cusNme", maps.get("CUS_NME"));
+//			cusAllDtlTemp.put("acTyp", maps.get("CUS_TYP"));
+//			cusAllDtlTemp.put("creDte", (String) maps.get("OPT_DAT"));
+//			cusAllDtlTemp.put("agrTme", maps.get("OPT_NOD"));
+//			cusAllDtlTemp.put("idTyp", maps.get("ID_TYP"));
+//			cusAllDtlTemp.put("idNo", maps.get("ID_NO"));
+//			cusAllDtlTemp.put("thdCusNme", maps.get("THD_CUS_NME"));
+//			cusAllDtlTemp.put("cmuTel", maps.get("CMU_TEL"));
+//			gasCusAllListDtl.add(cusAllDtlTemp);
+//		}
+//
+//		context.setData("loopDtl", gasCusAllListDtl);
 
-		List<Map<String, Object>> gasCusAllListDtl = new ArrayList<Map<String, Object>>();
-		for (Map<String, Object> maps : gasCusAllList) {
-			Map<String, Object> cusAllDtlTemp = new HashMap<String, Object>();
-			cusAllDtlTemp.put("cusNo", maps.get("CUS_NO"));
-			cusAllDtlTemp.put("cusAc", maps.get("CUS_AC"));
-			cusAllDtlTemp.put("cusNme", maps.get("CUS_NME"));
-			cusAllDtlTemp.put("acTyp", maps.get("CUS_TYP"));
-			// cusAllDtlTemp.put("creDte",
-			// DateUtils.parse((String)maps.get("OPT_DAT"),
-			// "yyyy-MM-dd'T'HH:mm:ss"));
-			cusAllDtlTemp.put("creDte", (String) maps.get("OPT_DAT"));
-			cusAllDtlTemp.put("agrTme", maps.get("OPT_NOD"));
-			cusAllDtlTemp.put("idTyp", maps.get("ID_TYP"));
-			cusAllDtlTemp.put("idNo", maps.get("ID_NO"));
-			cusAllDtlTemp.put("thdCusNme", maps.get("THD_CUS_NME"));
-			cusAllDtlTemp.put("cmuTel", maps.get("CMU_TEL"));
-			gasCusAllListDtl.add(cusAllDtlTemp);
-		}
-
-		context.setData("loopDtl", gasCusAllListDtl);
-
-		// 请求-分页信息 <pageableRequest> LIST
-		// 页数 pageNum INT
-		// 每页显示数据数 pageSize INT
-		// 排序信息 sortingInfo REF
-		// 请求-分页信息 </pageableRequest> LIST
-
-		// 返回-分页信息 <pageableResponse> LIST
-		// 总记录数 totalElements BIGINT Y
-		// 总页数 totalPages BIGINT Y
-		// 返回-分页信息 </pageableResponse> LIST
 
 //		List<Map<String, Object>> pageableRequest = context
 //				.getData("pageableRequest");
 //		int pageNum = (Integer) pageableRequest.get(0).get("pageNum");
 //		int pageSize = (Integer) pageableRequest.get(0).get("pageSize");
 		
-		int pageNum = (Integer) context.getData("pageNum");
-		int pageSize = (Integer) context.getData("pageSize");
-
-		int totalElements = gasCusAllListDtl.size();
-		int totalPages = 0;
-		if (totalElements % pageSize == 0) {
-			totalPages = totalElements / pageSize;
-		} else {
-			totalPages = totalElements / pageSize + 1;
-		}
-
-		List<Map<String, Object>> pageableResponse = new ArrayList<Map<String, Object>>();
-		Map<String, Object> pageMap = new HashMap<String, Object>();
-		pageMap.put("totalElements", totalElements);
-		pageMap.put("totalPages", totalPages);
-		pageableResponse.add(pageMap);
-		context.setData("pageableResponse", pageableResponse);
+//		int pageNum = (Integer) context.getData("pageNum");
+//		int pageSize = (Integer) context.getData("pageSize");
+//
+//		int totalElements = gasCusAllListDtl.size();
+//		int totalPages = 0;
+//		if (totalElements % pageSize == 0) {
+//			totalPages = totalElements / pageSize;
+//		} else {
+//			totalPages = totalElements / pageSize + 1;
+//		}
+//
+//		List<Map<String, Object>> pageableResponse = new ArrayList<Map<String, Object>>();
+//		Map<String, Object> pageMap = new HashMap<String, Object>();
+//		pageMap.put("totalElements", totalElements);
+//		pageMap.put("totalPages", totalPages);
+//		pageableResponse.add(pageMap);
+//		context.setData("pageableResponse", pageableResponse);
+//		
+//		context.setData("totalElements", totalElements);
+//		context.setData("totalPages", totalPages);
 		
-		context.setData("totalElements", totalElements);
-		context.setData("totalPages", totalPages);
-
-		// context.setData(GDParamKeys.GAS_AP_CDE, "32");
-		// context.setData(GDParamKeys.GAS_OFMT_COD, "z01");
-		// context.setData(GDParamKeys.GAS_PAGE_NO, "0001");
-		// context.setData(GDParamKeys.GAS_VAR_SIZE, "3");
-		// context.setData(GDParamKeys.GAS_TXN_TTL, "代扣用户信息浏览");
-		// context.setData(GDParamKeys.GAS_SUB_TTL, "查询内容");
-		// context.setData(GDParamKeys.GAS_MSG_TYP, "N");
-		// context.setData(GDParamKeys.GDEUPSB_RSP_MSG, "交易成功");
-		// context.setData(GDParamKeys.GAS_RSP_COD,
-		// GDConstants.GDEUPSB_TXN_SUCC_CODE);
-		logger.info("========== Ending context=" + context);
 
 		context.setState(BPState.BUSINESS_PROCESSNIG_STATE_NORMAL);
 	}
--- src/com/bocom/bbip/gdeupsb/repository/GdGasCusAllRepository.java
+++ src/com/bocom/bbip/gdeupsb/repository/GdGasCusAllRepository.java
@@ -5,6 +5,8 @@ import java.util.Map;
 
 import com.bocom.bbip.data.annotation.Find;
 import com.bocom.bbip.data.annotation.Update;
+import com.bocom.bbip.data.domain.Page;
+import com.bocom.bbip.data.domain.Pageable;
 import com.bocom.bbip.data.repository.PagingAndSortingRepository;
 import com.bocom.bbip.gdeupsb.entity.GdGasCusAll;
 
@@ -14,13 +16,16 @@ public interface GdGasCusAllRepository extends PagingAndSortingRepository<GdGasC
 	public List<GdGasCusAll> findDataBeforeOptDat(GdGasCusAll cusAllAgt);
 	
 	@Find
-	public List<Map<String, Object>> findDataByOptDate(Map<String, Object> map);
+	public List<GdGasCusAll> findDataByOptDate(Map<String, Object> map);
 	
 	@Update
 	public void update(GdGasCusAll addGasCusAll);
 	
 	@Find
 	public List<Map<String, Object>> findCusInfo(GdGasCusAll qryCusAll);
+
+	public Page<GdGasCusAll> findDataByOptDate(Pageable pageable,
+			Map<String, Object> map);
 	
 	
 }
