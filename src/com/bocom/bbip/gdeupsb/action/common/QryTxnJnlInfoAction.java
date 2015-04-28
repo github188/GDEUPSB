47db3c3b25e72609eb8eec67e76f98a7f7b41623
Wang_Mingqin <Wang_Mingqin@2ac714ae-9a16-11e4-ad83-2f06aba1930a>
2015-04-28 09:48:09 +0000

    git-svn-id: svn://182.53.32.18/bbip/project/GDEUPSB/TRUNK/GDEUPSB@44861 2ac714ae-9a16-11e4-ad83-2f06aba1930a

--- src/com/bocom/bbip/gdeupsb/action/common/QryTxnJnlInfoAction.java
+++ src/com/bocom/bbip/gdeupsb/action/common/QryTxnJnlInfoAction.java
@@ -1,6 +1,5 @@
 package com.bocom.bbip.gdeupsb.action.common;
 
-import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
@@ -38,17 +37,6 @@ public class QryTxnJnlInfoAction extends BaseAction{
 		context.setState(BPState.BUSINESS_PROCESSNIG_STATE_FAIL);
 		logger.info("=========context=====" + context);
 		
-//		select userno,payyea,actno,actnam,optamt,opttim,chkdat,tckno,logno,clogno from gastxnjnl491
-//        where (userno='%s' or '%s'='') and (actno='%s' or '%s'='') 
-//        and (actnam='%s' or '%s'='') and (chkflg='%s' or '%s'='') 
-//        and OPTDAT &gt;='%s' and OPTDAT &lt;='%s' and HTxnSt='S' and HRspCd='SC0000' and Status='1' and ThdSts='B0'
-//														主机交易状态		主机返回码		账务状态1正常扣款	返回第三方状态B0扣费成功
-
-//eups流水表无status， thdSts, payyea使用备1， 备2, 预留6
-//不能使用	BeanUtils.toObject();
-		
-		
-//
 //		交易类型	eupsBusTyp	CHAR	10	Y
 //		会计流水号	mfmVchNo	CHAR	20	
 //		用户编号	thdCusNo	CHAR	40	
@@ -56,9 +44,7 @@ public class QryTxnJnlInfoAction extends BaseAction{
 //		开始日期	beginDate	CHAR	10	Y
 //		结束日期	endDate	CHAR	10	Y
 
-			
-	
-			GdEupsTransJournal txnJnl = new GdEupsTransJournal();
+		GdEupsTransJournal txnJnl = new GdEupsTransJournal();
 			txnJnl.setEupsBusTyp((String) context.getData(ParamKeys.EUPS_BUSS_TYPE));
 			if(!((null==context.getData(ParamKeys.THD_CUS_NO)) || "".equals(context.getData(ParamKeys.THD_CUS_NO)) ||  "?".equals(context.getData(ParamKeys.THD_CUS_NO)) )){
 				txnJnl.setThdCusNo(context.getData(ParamKeys.THD_CUS_NO).toString());
@@ -97,15 +83,9 @@ public class QryTxnJnlInfoAction extends BaseAction{
 				tmp.put("reqTxnAmt", tmpMap.get("REQ_TXN_AMT")+"");
 				tmp.put("hfe", ""+tmpMap.get("HFE"));
 				tmp.put("txnAmt", ""+tmpMap.get("TXN_AMT"));
-				/*tmp.put("reqTxnAmt", null);
-				tmp.put("hfe",  null);
-				tmp.put("txnAmt",  null);*/
 				tmp.put("txnDte", DateUtils.format((Date)tmpMap.get("TXN_DTE"), DateUtils.STYLE_SIMPLE_DATE));
 				tmp.put("txnTme", DateUtils.format((Date)tmpMap.get("TXN_TME"), DateUtils.STYLE_FULL));
 				tmp.put("acDte", DateUtils.format((Date)tmpMap.get("AC_DTE"), DateUtils.STYLE_SIMPLE_DATE));
-				/*tmp.put("txnDte", null);
-				tmp.put("txnTme", null);
-				tmp.put("acDte", null);*/
 				
 				tmp.put("txnSts", tmpMap.get("TXN_STS"));
 				tempList.add(tmp);
@@ -140,49 +120,6 @@ public class QryTxnJnlInfoAction extends BaseAction{
 			context.setData("totalPages", totalPages);
 			
 			
-//			List<GdEupsTransJournal> txnJnlList = get(GdEupsTransJournalRepository.class).findGasJnlInfo(txnJnl);
-//			logger.info("==============txnJnlList====" + txnJnlList);
-//			
-//			List<Map<String,Object>> resultList=(List<Map<String, Object>>) BeanUtils.toMaps(txnJnlList);
-//			logger.info("==============resultList====" + resultList);
-//			
-//			if(CollectionUtils.isEmpty(resultList)){
-//				context.setData(GDParamKeys.GAS_MSG_TYP, "E");
-//				context.setData(GDParamKeys.GAS_AP_CDE, "SC");
-//				context.setData(GDParamKeys.GAS_OFMT_COD, "D04");
-//				context.setData(GDParamKeys.GAS_IN_POS, "0001");
-//				context.setData(GDParamKeys.GDEUPSB_RSP_MSG, "系统错误");
-//				context.setData(GDParamKeys.GAS_RSP_COD, GDConstants.GAS_ERROR_CODE);
-//				throw new CoreRuntimeException("系统异常");
-//			}else{
-//				List<Map<String,Object>> resultListTmp = new ArrayList<Map<String,Object>>();
-//				for(Map<String,Object> maps : resultList){
-//			        Map<String, Object> tempMap = new HashMap<String, Object>();
-//			        tempMap.put("thdCusNo", maps.get("thdCusNo"));
-//			        tempMap.put("payYea", maps.get("rsvFld6"));
-//			        tempMap.put("cusAc", maps.get("cusAc"));
-//			        tempMap.put("cusNme", maps.get("cusNme"));
-//			        tempMap.put("txnAmt", maps.get("txnAmt"));
-//			        tempMap.put("txnTme", maps.get("txnTme"));
-//			        tempMap.put("acDte", maps.get("acDte"));
-//			        tempMap.put("mfmVchNo", maps.get("mfmVchNo"));
-//			        tempMap.put("sqn", maps.get("sqn"));
-//			        tempMap.put("thdSqn", maps.get("thdSqn"));
-//					resultListTmp.add(tempMap);
-//				}
-//		        context.setData("loop", resultListTmp);
-//		        logger.info("=======resultListTmp in context ======" + context);
-//		        context.setData(GDParamKeys.GAS_AP_CDE, "32");
-//		        context.setData(GDParamKeys.GAS_OFMT_COD, "z01");
-//		        context.setData(GDParamKeys.GAS_PAGE_NO, "0001");
-//		        context.setData(GDParamKeys.GAS_VAR_SIZE, "3");
-//		        context.setData(GDParamKeys.GAS_TXN_TTL, "惠州燃气代扣流水信息查询");
-//		        context.setData(GDParamKeys.GAS_SUB_TTL, "查询内容");
-//				
-//				context.setData(GDParamKeys.GAS_MSG_TYP, "N");
-//				context.setData(GDParamKeys.GDEUPSB_RSP_MSG, "交易成功");
-//				context.setData(GDParamKeys.GAS_RSP_COD, GDConstants.GDEUPSB_TXN_SUCC_CODE);
-//			}
 		context.setState(BPState.BUSINESS_PROCESSNIG_STATE_NORMAL);
 	}
 }
