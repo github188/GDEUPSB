fb483c8af764fbf7abb53233a928ef304ad270e7
Li_Yawei <Li_Yawei@2ac714ae-9a16-11e4-ad83-2f06aba1930a>
2015-04-29 09:09:43 +0000

    git-svn-id: svn://182.53.32.18/bbip/project/GDEUPSB/TRUNK/GDEUPSB@45488 2ac714ae-9a16-11e4-ad83-2f06aba1930a

---
+++ src/com/bocom/bbip/gdeupsb/action/efek/EfekEndAction.java
@@ -0,0 +1,33 @@
+package com.bocom.bbip.gdeupsb.action.efek;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.bocom.bbip.eups.action.BaseAction;
+import com.bocom.bbip.eups.entity.EupsThdTranCtlInfo;
+import com.bocom.bbip.eups.repository.EupsThdTranCtlInfoRepository;
+import com.bocom.jump.bp.core.Context;
+import com.bocom.jump.bp.core.CoreException;
+import com.bocom.jump.bp.core.CoreRuntimeException;
+
+public class EfekEndAction extends BaseAction{
+	private final static Log logger = LogFactory.getLog(AdvanceTradeAction.class);
+	@Autowired
+	EupsThdTranCtlInfoRepository eupsThdTranCtlInfoRepository;
+	public void execute(Context context)throws CoreException, CoreRuntimeException{
+		logger.info("===========Start  EfekEndAction");
+	        String txnTyp = (String)context.getData("txnTyp");
+	        String comNo=context.getData("comNo").toString().trim();
+	        
+	        EupsThdTranCtlInfo euspThdTranCtlInfo=eupsThdTranCtlInfoRepository.findOne(comNo);
+	        String txnCtlSts=euspThdTranCtlInfo.getTxnCtlSts();
+	        if(txnCtlSts.equals(txnTyp)){
+	        		throw new CoreException("供电已签退");
+	        }else{
+	        		euspThdTranCtlInfo.setTxnCtlSts("0");
+	        		eupsThdTranCtlInfoRepository.update(euspThdTranCtlInfo);
+	        }
+	        logger.info("===========End  EfekEndAction");
+	  }
+}
