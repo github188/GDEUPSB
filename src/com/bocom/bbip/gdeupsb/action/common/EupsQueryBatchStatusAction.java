c03152e3e6996567dcba65058c0632683e5d3cdd
Li_Yawei <Li_Yawei@2ac714ae-9a16-11e4-ad83-2f06aba1930a>
2015-05-04 11:45:17 +0000

    git-svn-id: svn://182.53.32.18/bbip/project/GDEUPSB/TRUNK/GDEUPSB@46969 2ac714ae-9a16-11e4-ad83-2f06aba1930a

--- src/com/bocom/bbip/gdeupsb/action/common/EupsQueryBatchStatusAction.java
+++ src/com/bocom/bbip/gdeupsb/action/common/EupsQueryBatchStatusAction.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import com.bocom.bbip.eups.action.BaseAction;
 import com.bocom.bbip.eups.common.ErrorCodes;
@@ -12,6 +13,8 @@ import com.bocom.bbip.eups.entity.EupsBatchConsoleInfo;
 import com.bocom.bbip.eups.repository.EupsBatchConsoleInfoRepository;
 import com.bocom.bbip.gdeupsb.entity.GDEupsBatchConsoleInfo;
 import com.bocom.bbip.gdeupsb.repository.GDEupsBatchConsoleInfoRepository;
+import com.bocom.bbip.service.BGSPServiceAccessObject;
+import com.bocom.bbip.service.Result;
 import com.bocom.bbip.utils.Assert;
 import com.bocom.bbip.utils.BeanUtils;
 import com.bocom.bbip.utils.ContextUtils;
@@ -20,6 +23,8 @@ import com.bocom.jump.bp.core.CoreException;
 
 public class EupsQueryBatchStatusAction extends BaseAction {
 	private static final Log logger=LogFactory.getLog(EupsQueryBatchStatusAction.class);
+	@Autowired
+	BGSPServiceAccessObject bgspServiceAccessObject;
 /**
  * 查询批次信息	
  */
@@ -41,14 +46,23 @@ public class EupsQueryBatchStatusAction extends BaseAction {
 		logger.info("批次信息:"+BeanUtils.toFlatMap(gdEupsBatchConsoleInfo));
 		context.setData("batNo",gdEupsBatchConsoleInfo.getBatNo());
 		context.setData("comNo",gdEupsBatchConsoleInfo.getComNo());
-		context.setData("comNme",gdEupsBatchConsoleInfo.getComNme());
+		
+		logger.info("===============Start   QryComInfoAction");
+		 context.setData("inqBusLstFlg","N");
+		 //代收付搜索单位信息
+		 Result comResult= bgspServiceAccessObject.callServiceFlatting("queryCorporInfo",context.getDataMap());
+		 log.info("==========="+comResult);
+		 String comNme=comResult.getPayload().get("comNum").toString();
+		 logger.info("===============End   QryComInfoAction");
+		context.setData("comNme",comNme);
 		context.setData("eupsBusTyp",gdEupsBatchConsoleInfo.getEupsBusTyp());
 		context.setData("totCnt",eupsBatchConsoleInfo.getTotCnt());
 		context.setData("totAmt",eupsBatchConsoleInfo.getTotAmt());
 		context.setData("batSts",gdEupsBatchConsoleInfo.getBatSts());
 		context.setData("fleNme",gdEupsBatchConsoleInfo.getRsvFld8());
 		context.setData("thdBatNo",gdEupsBatchConsoleInfo.getRsvFld9());
-
+		
+		 logger.info("===============End   EupsQueryBatchStatusAction");
 	}
 
 }
