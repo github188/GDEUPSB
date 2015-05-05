6d0cba9be1958b5b6ac665d4a3dd5e65e9dbc8a9
Wang_Mingqin <Wang_Mingqin@2ac714ae-9a16-11e4-ad83-2f06aba1930a>
2015-05-05 07:40:38 +0000

    git-svn-id: svn://182.53.32.18/bbip/project/GDEUPSB/TRUNK/GDEUPSB@47218 2ac714ae-9a16-11e4-ad83-2f06aba1930a

--- src/com/bocom/bbip/gdeupsb/action/common/QryReportInfoAction.java
+++ src/com/bocom/bbip/gdeupsb/action/common/QryReportInfoAction.java
@@ -1,8 +1,8 @@
 package com.bocom.bbip.gdeupsb.action.common;
 
 import java.math.BigDecimal;
-import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -17,6 +17,7 @@ import com.bocom.bbip.eups.entity.EupsThdBaseInfo;
 import com.bocom.bbip.eups.repository.EupsBatchConsoleInfoRepository;
 import com.bocom.bbip.eups.repository.EupsThdBaseInfoRepository;
 import com.bocom.bbip.gdeupsb.entity.GdEupsTransJournal;
+import com.bocom.bbip.gdeupsb.repository.GDEupsBatchConsoleInfoRepository;
 import com.bocom.bbip.gdeupsb.repository.GdEupsTransJournalRepository;
 import com.bocom.bbip.utils.CollectionUtils;
 import com.bocom.bbip.utils.DateUtils;
@@ -61,7 +62,7 @@ public class QryReportInfoAction extends BaseAction {
 		String prtTyp = context.getData("prtTyp");
 		
 		if("0".equals(prtTyp) || "1".equals(prtTyp) || "2".equals(prtTyp) || "3".equals(prtTyp) || "4".equals(prtTyp) ){
-		// TODO 统计单笔交易信息
+		// 统计单笔交易信息
 		List<Map<String, Object>> prtList = get(
 				GdEupsTransJournalRepository.class).findRptJnlInfo(eupsJnl);
 		if (null == prtList || CollectionUtils.isEmpty(prtList)) {
@@ -84,41 +85,62 @@ public class QryReportInfoAction extends BaseAction {
 		}
 		else{
 		//统计批量交易信息 批量交易不是一天只有一个批次的！！！
-		EupsBatchConsoleInfo eupsBatchConsoleInfo = new EupsBatchConsoleInfo();
-		// eupsBatchConsoleInfo.setSubDte(printDate);//TODO
-		eupsBatchConsoleInfo.setExeDte(printDate);
-		eupsBatchConsoleInfo.setComNo(comNo);
-		List<EupsBatchConsoleInfo> batInfoList = get(
-				EupsBatchConsoleInfoRepository.class)
-				.find(eupsBatchConsoleInfo);
-		if (null == batInfoList || CollectionUtils.isEmpty(batInfoList)) {
-			logger.info("There are no records for select check trans journal ");
-			throw new CoreException(ErrorCodes.EUPS_QUERY_NO_DATA);
+		if("ELEC02".equals((String) context
+				.getData(ParamKeys.EUPS_BUSS_TYPE))){
+			Map<String, Object> baseMap = new HashMap<String, Object>();
+			baseMap.put(ParamKeys.EUPS_BUSS_TYPE, (String) context
+					.getData(ParamKeys.EUPS_BUSS_TYPE));
+			baseMap.put(ParamKeys.TXN_DTE, printDate);
+			List<Map<String, Object>> elec02BatInfoList = get(GDEupsBatchConsoleInfoRepository.class).findElec02BatchRptInfo(baseMap);
+			if (null == elec02BatInfoList || CollectionUtils.isEmpty(elec02BatInfoList)) {
+				logger.info("There are no records for select elec02BatInfoList ");
+				throw new CoreException(ErrorCodes.EUPS_QUERY_NO_DATA);
+			}
+			
+			context.setData("batTotCnt", elec02BatInfoList.get(0).get("TOTCNT") + "");
+			context.setData("batTotAmt", elec02BatInfoList.get(0).get("TOTAMT") + "");
+			context.setData("batSucCnt", elec02BatInfoList.get(0).get("SUCTOTCNT") + "");
+			context.setData("batSucAmt", elec02BatInfoList.get(0).get("SUCTOTAMT") + "");
+			context.setData("batFalCnt", elec02BatInfoList.get(0).get("FALTOTCNT") + "");
+			context.setData("batFalAmt", elec02BatInfoList.get(0).get("FALTOTAMT") + "");
+			
+		}else{
+			EupsBatchConsoleInfo eupsBatchConsoleInfo = new EupsBatchConsoleInfo();
+			eupsBatchConsoleInfo.setExeDte(printDate);
+			eupsBatchConsoleInfo.setComNo(comNo);
+			List<EupsBatchConsoleInfo> batInfoList = get(
+					EupsBatchConsoleInfoRepository.class)
+					.find(eupsBatchConsoleInfo);
+			if (null == batInfoList || CollectionUtils.isEmpty(batInfoList)) {
+				logger.info("There are no records for select check trans journal ");
+				throw new CoreException(ErrorCodes.EUPS_QUERY_NO_DATA);
+			}
+
+			int batTotCnt = 0;
+			int batSucCnt = 0;
+			int batFalCnt = 0;
+			BigDecimal batTotAmt = new BigDecimal(0.00);
+			BigDecimal batSucAmt = new BigDecimal(0.00);
+			BigDecimal batFalAmt = new BigDecimal(0.00);
+
+			for (EupsBatchConsoleInfo perInfo : batInfoList) {
+				batTotCnt = batTotCnt + perInfo.getTotCnt();
+				batSucCnt = batSucCnt + perInfo.getSucTotCnt();
+				batFalCnt = batFalCnt + perInfo.getFalTotCnt();
+				batTotAmt = batTotAmt.add(perInfo.getTotAmt());
+				batSucAmt = batSucAmt.add(perInfo.getSucTotAmt());
+				batFalAmt = batFalAmt.add(perInfo.getFalTotAmt());
+			}
+
+			context.setData("batTotCnt", batTotCnt + "");
+			context.setData("batTotAmt", batTotAmt + "");
+			context.setData("batSucCnt", batSucCnt + "");
+			context.setData("batSucAmt", batSucAmt + "");
+			context.setData("batFalCnt", batFalCnt + "");
+			context.setData("batFalAmt", batFalAmt + "");
+			logger.info("======== bat rpt info =====context:" + context);
 		}
-
-		int batTotCnt = 0;
-		int batSucCnt = 0;
-		int batFalCnt = 0;
-		BigDecimal batTotAmt = new BigDecimal(0.00);
-		BigDecimal batSucAmt = new BigDecimal(0.00);
-		BigDecimal batFalAmt = new BigDecimal(0.00);
-
-		for (EupsBatchConsoleInfo perInfo : batInfoList) {
-			batTotCnt = batTotCnt + perInfo.getTotCnt();
-			batSucCnt = batSucCnt + perInfo.getSucTotCnt();
-			batFalCnt = batFalCnt + perInfo.getFalTotCnt();
-			batTotAmt = batTotAmt.add(perInfo.getTotAmt());
-			batSucAmt = batSucAmt.add(perInfo.getSucTotAmt());
-			batFalAmt = batFalAmt.add(perInfo.getFalTotAmt());
-		}
-
-		context.setData("batTotCnt", batTotCnt + "");
-		context.setData("batTotAmt", batTotAmt + "");
-		context.setData("batSucCnt", batSucCnt + "");
-		context.setData("batSucAmt", batSucAmt + "");
-		context.setData("batFalCnt", batFalCnt + "");
-		context.setData("batFalAmt", batFalAmt + "");
-		logger.info("======== bat rpt info =====context:" + context);
+		
 		}
 
 	}
