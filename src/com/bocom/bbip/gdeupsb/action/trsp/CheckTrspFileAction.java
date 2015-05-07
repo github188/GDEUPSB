598526d5e09f831b346478307d1d8f311971c1ea
Li_Yawei <Li_Yawei@2ac714ae-9a16-11e4-ad83-2f06aba1930a>
2015-05-07 14:00:35 +0000

    git-svn-id: svn://182.53.32.18/bbip/project/GDEUPSB/TRUNK/GDEUPSB@48344 2ac714ae-9a16-11e4-ad83-2f06aba1930a

--- src/com/bocom/bbip/gdeupsb/action/trsp/CheckTrspFileAction.java
+++ src/com/bocom/bbip/gdeupsb/action/trsp/CheckTrspFileAction.java
@@ -366,7 +366,6 @@ public class CheckTrspFileAction extends BaseAction {
 		EupsThdFtpConfig eupsThdFtpConfig = eupsThdFtpConfigRepository.findOne("trspCheckFile");
 		eupsThdFtpConfig.setFtpDir("0");
 		eupsThdFtpConfig.setLocFleNme(fileName);
-		eupsThdFtpConfig.setRmtWay("/home/bbipadm/data/GDEUPSB/report/");
 		eupsThdFtpConfig.setRmtFleNme("TRSP00" + fileName);
 		operateFTPAction.putCheckFile(eupsThdFtpConfig);
 	}
