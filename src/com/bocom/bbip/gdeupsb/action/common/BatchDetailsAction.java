aa1ad1eeb81801437791a8b2d99a321dd82410cf
Li_Yawei <Li_Yawei@2ac714ae-9a16-11e4-ad83-2f06aba1930a>
2015-05-08 12:44:16 +0000

    git-svn-id: svn://182.53.32.18/bbip/project/GDEUPSB/TRUNK/GDEUPSB@48880 2ac714ae-9a16-11e4-ad83-2f06aba1930a

--- src/com/bocom/bbip/gdeupsb/action/common/BatchDetailsAction.java
+++ src/com/bocom/bbip/gdeupsb/action/common/BatchDetailsAction.java
@@ -1,6 +1,9 @@
 package com.bocom.bbip.gdeupsb.action.common;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import com.bocom.bbip.eups.action.BaseAction;
 import com.bocom.bbip.eups.common.ParamKeys;
@@ -20,27 +23,42 @@ public class BatchDetailsAction extends BaseAction{
 		public void execute(Context context) throws CoreException,
 				CoreRuntimeException {
 				String batNo=context.getData(ParamKeys.BAT_NO).toString();
-				//GDEupsBatchConsoleInfo 得到代收付文件名字
-				GDEupsBatchConsoleInfo gdEupsBatchConsoleInfo=get(GDEupsBatchConsoleInfoRepository.class).findOne(batNo);
-				String fleNme=gdEupsBatchConsoleInfo.getRsvFld8();
 				//由代收付文件得到批次号
 				EupsBatchConsoleInfo eupsBatchConsoleInfos=new EupsBatchConsoleInfo();
-				eupsBatchConsoleInfos.setFleNme(fleNme);
-				String eupsBatNo=get(EupsBatchConsoleInfoRepository.class).find(eupsBatchConsoleInfos).get(0).getBatNo();
+				eupsBatchConsoleInfos.setRsvFld1(batNo);
+				EupsBatchConsoleInfo eupsBatchConsoleInfo=get(EupsBatchConsoleInfoRepository.class).find(eupsBatchConsoleInfos).get(0);
+				if(eupsBatchConsoleInfo==null){
+						throw new CoreException("批次"+batNo+"没有返回信息");
+				}
+				String eupsBatNo=eupsBatchConsoleInfo.getBatNo();
 				//根据批次号获得信息
 				EupsBatchInfoDetail eupsBatchInfoDetails=new EupsBatchInfoDetail();
 				eupsBatchInfoDetails.setBatNo(eupsBatNo);
 				List<EupsBatchInfoDetail> list=get(EupsBatchInfoDetailRepository.class).find(eupsBatchInfoDetails);
-				String eupsBusTyp=context.getData(ParamKeys.EUPS_BUSS_TYPE).toString();
-				if(eupsBusTyp!="ZHAG00"){
-						for (EupsBatchInfoDetail eupsBatchInfoDetail : list) {
-								eupsBatchInfoDetail.setRmk2(eupsBatchInfoDetail.getAgtSrvCusId());
-						}
-				}else{
-						for (EupsBatchInfoDetail eupsBatchInfoDetail : list) {
-								eupsBatchInfoDetail.setRmk2(eupsBatchInfoDetail.getAgtSrvCusId());
-						}
+				for (EupsBatchInfoDetail eupsBatchInfoDetail : list) {
+						eupsBatchInfoDetail.setRmk2(eupsBatchInfoDetail.getAgtSrvCusId());
 				}
+				int pageNum = (Integer) context.getData("pageNum");
+				int pageSize = (Integer) context.getData("pageSize");
+
+				int totalElements = list.size();
+				int totalPages = 0;
+				if (totalElements % pageSize == 0) {
+					totalPages = totalElements / pageSize;
+				} else {
+					totalPages = totalElements / pageSize + 1;
+				}
+
+				List<Map<String, Object>> pageableResponse = new ArrayList<Map<String, Object>>();
+				Map<String, Object> pageMap = new HashMap<String, Object>();
+				pageMap.put("totalElements", totalElements);
+				pageMap.put("totalPages", totalPages);
+				pageableResponse.add(pageMap);
+				context.setData("pageableResponse", pageableResponse);
+				
+				context.setData("totalElements", totalElements);
+				context.setData("totalPages", totalPages);
+				
 				context.setData("detailList", BeanUtils.toMaps(list));
 		}
 }
