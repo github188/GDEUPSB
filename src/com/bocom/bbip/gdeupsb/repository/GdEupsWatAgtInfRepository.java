ad59892a17ce1860b6e8f4d1e34f05557d21da58
Li_Guilin <Li_Guilin@2ac714ae-9a16-11e4-ad83-2f06aba1930a>
2015-05-10 03:29:05 +0000

    git-svn-id: svn://182.53.32.18/bbip/project/GDEUPSB/TRUNK/GDEUPSB@49444 2ac714ae-9a16-11e4-ad83-2f06aba1930a

--- src/com/bocom/bbip/gdeupsb/repository/GdEupsWatAgtInfRepository.java
+++ src/com/bocom/bbip/gdeupsb/repository/GdEupsWatAgtInfRepository.java
@@ -1,8 +1,11 @@
 package com.bocom.bbip.gdeupsb.repository;
 
+import java.util.List;
+
 import com.bocom.bbip.data.repository.PagingAndSortingRepository;
 import com.bocom.bbip.gdeupsb.entity.GdEupsWatAgtInf;
 
 public interface GdEupsWatAgtInfRepository extends PagingAndSortingRepository<GdEupsWatAgtInf, String> {
 	public void update(GdEupsWatAgtInf gdeups);
+	public List<GdEupsWatAgtInf> findA(GdEupsWatAgtInf gdeups);
 }
