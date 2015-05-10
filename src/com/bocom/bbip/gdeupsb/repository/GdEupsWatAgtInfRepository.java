86fb5e4635edea51154e0e83d8142cdbfba28960
Li_Guilin <Li_Guilin@2ac714ae-9a16-11e4-ad83-2f06aba1930a>
2015-05-10 06:33:53 +0000

    git-svn-id: svn://182.53.32.18/bbip/project/GDEUPSB/TRUNK/GDEUPSB@49504 2ac714ae-9a16-11e4-ad83-2f06aba1930a

--- src/com/bocom/bbip/gdeupsb/repository/GdEupsWatAgtInfRepository.java
+++ src/com/bocom/bbip/gdeupsb/repository/GdEupsWatAgtInfRepository.java
@@ -1,6 +1,7 @@
 package com.bocom.bbip.gdeupsb.repository;
 
 import java.util.List;
+import java.util.Map;
 
 import com.bocom.bbip.data.repository.PagingAndSortingRepository;
 import com.bocom.bbip.gdeupsb.entity.GdEupsWatAgtInf;
@@ -8,4 +9,5 @@ import com.bocom.bbip.gdeupsb.entity.GdEupsWatAgtInf;
 public interface GdEupsWatAgtInfRepository extends PagingAndSortingRepository<GdEupsWatAgtInf, String> {
 	public void update(GdEupsWatAgtInf gdeups);
 	public List<GdEupsWatAgtInf> findA(GdEupsWatAgtInf gdeups);
+	public List<GdEupsWatAgtInf> findDelFail(Map<String, Object> map);
 }
