9d79bf10714fe7a44c2998ac12fe7ef464d1b05b
Li_Guilin <Li_Guilin@2ac714ae-9a16-11e4-ad83-2f06aba1930a>
2015-05-10 03:28:54 +0000

    git-svn-id: svn://182.53.32.18/bbip/project/GDEUPSB/TRUNK/GDEUPSB@49443 2ac714ae-9a16-11e4-ad83-2f06aba1930a

---
+++ src/com/bocom/bbip/gdeupsb/action/wat/DelFailFtp.java
@@ -0,0 +1,20 @@
+package com.bocom.bbip.gdeupsb.action.wat;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import com.bocom.bbip.eups.action.BaseAction;
+import com.bocom.jump.bp.core.Context;
+import com.bocom.jump.bp.core.CoreException;
+import com.bocom.jump.bp.core.CoreRuntimeException;
+
+public class DelFailFtp extends BaseAction{
+	
+	private final static Log log = LogFactory.getLog(DelFailFtp.class);
+	public void execute(Context ctx) throws CoreRuntimeException,CoreException{
+		log.info("DelFailFtp start......");
+		
+		
+	}
+
+}
