3c1db0b8e48db275a7b1497c7a79ffdb573ea8c5
Li_Yawei <Li_Yawei@2ac714ae-9a16-11e4-ad83-2f06aba1930a>
2015-05-11 12:55:43 +0000

    git-svn-id: svn://182.53.32.18/bbip/project/GDEUPSB/TRUNK/GDEUPSB@49941 2ac714ae-9a16-11e4-ad83-2f06aba1930a

--- src/com/bocom/bbip/gdeupsb/entity/GDEupsZhAGBatchTemp.java
+++ src/com/bocom/bbip/gdeupsb/entity/GDEupsZhAGBatchTemp.java
@@ -1,174 +1,179 @@
 package com.bocom.bbip.gdeupsb.entity;
 
+import com.bocom.bbip.data.annotation.GeneratedValue;
+import com.bocom.bbip.data.annotation.Id;
 import java.util.Date;
 
 public class GDEupsZhAGBatchTemp {
-	private String sqn;
+    @Id
+    @GeneratedValue
+    private String sqn;
 
-	private String batNo;
+    private String batNo;
 
-	private String comNo;
+    private String comNo;
 
-	private String txnTlr;
+    private String txnTlr;
 
-	private Date subDte;
+    private Date subDte;
 
-	private String cusAc;
+    private String cusAc;
 
-	private String cusNme;
+    private String cusNme;
 
-	private String txnAmt;
+    private String txnAmt;
 
-	private String thdCusNo;
+    private String thdCusNo;
 
-	private String thdCusNme;
+    private String thdCusNme;
 
-	private String rsvFld1;
+    private String rsvFld1;
 
-	private String rsvFld2;
+    private String rsvFld2;
 
-	private String rsvFld3;
-	private String rsvFld4;
-	private String rsvFld5;
+    private String rsvFld3;
 
-	private String rsvFld6;
-	private String rsvFld7;
+    private String rsvFld4;
 
-	public String getSqn() {
-		return sqn;
-	}
+    private String rsvFld5;
 
-	public void setSqn(String sqn) {
-		this.sqn = sqn;
-	}
+    private String rsvFld6;
 
-	public String getBatNo() {
-		return batNo;
-	}
+    private String rsvFld7;
 
-	public void setBatNo(String batNo) {
-		this.batNo = batNo == null ? null : batNo.trim();
-	}
+    public String getSqn() {
+        return sqn;
+    }
 
-	public String getComNo() {
-		return comNo= comNo == null ? " " : comNo;
-	}
+    public void setSqn(String sqn) {
+        this.sqn = sqn;
+    }
 
-	public void setComNo(String comNo) {
-		this.comNo = comNo == null ? null : comNo;
-	}
+    public String getBatNo() {
+        return batNo;
+    }
 
-	public String getTxnTlr() {
-		return txnTlr= txnTlr == null ? " " : txnTlr;
-	}
+    public void setBatNo(String batNo) {
+        this.batNo = batNo;
+    }
 
-	public void setTxnTlr(String txnTlr) {
-		this.txnTlr = txnTlr == null ? " " : txnTlr;
-	}
+    public String getComNo() {
+        return comNo;
+    }
 
-	public Date getSubDte() {
-		return subDte=subDte==null? new Date():subDte;
-	}
+    public void setComNo(String comNo) {
+        this.comNo = comNo;
+    }
 
-	public void setSubDte(Date subDte) {
-		this.subDte = subDte;
-	}
+    public String getTxnTlr() {
+        return txnTlr;
+    }
 
-	public String getCusAc() {
-		return cusAc= cusAc == null ? " " : cusAc;
-	}
+    public void setTxnTlr(String txnTlr) {
+        this.txnTlr = txnTlr;
+    }
 
-	public void setCusAc(String cusAc) {
-		this.cusAc = cusAc ;
-	}
+    public Date getSubDte() {
+        return subDte;
+    }
 
-	public String getCusNme() {
-		return cusNme = cusNme == null ? " " :cusNme;
-	}
+    public void setSubDte(Date subDte) {
+        this.subDte = subDte;
+    }
 
-	public void setCusNme(String cusNme) {
-		this.cusNme = cusNme ;
-	}
+    public String getCusAc() {
+        return cusAc;
+    }
 
-	public String getTxnAmt() {
-		return txnAmt=txnAmt==null?" ":txnAmt;
-	}
+    public void setCusAc(String cusAc) {
+        this.cusAc = cusAc;
+    }
 
-	public void setTxnAmt(String txnAmt) {
-		this.txnAmt=txnAmt;
-		/*final String temp=NumberUtils.yuanToCentString(txnAmt.trim());
-		this.txnAmt=StringUtils.leftPad(temp, 18, "0");*/
-	}
+    public String getCusNme() {
+        return cusNme;
+    }
 
-	public String getThdCusNo() {
-		return thdCusNo= thdCusNo == null ? " " : thdCusNo;
-	}
+    public void setCusNme(String cusNme) {
+        this.cusNme = cusNme;
+    }
 
-	public void setThdCusNo(String thdCusNo) {
-		this.thdCusNo = thdCusNo ;
-	}
+    public String getTxnAmt() {
+        return txnAmt;
+    }
 
-	public String getThdCusNme() {
-		return thdCusNme = thdCusNme == null ? " " :thdCusNme;
-	}
+    public void setTxnAmt(String txnAmt) {
+        this.txnAmt = txnAmt;
+    }
 
-	public void setThdCusNme(String thdCusNme) {
-		this.thdCusNme = thdCusNme ;
-	}
+    public String getThdCusNo() {
+        return thdCusNo;
+    }
 
-	public String getRsvFld1() {
-		return rsvFld1= rsvFld1 == null ? " " : rsvFld1;
-	}
+    public void setThdCusNo(String thdCusNo) {
+        this.thdCusNo = thdCusNo;
+    }
 
-	public void setRsvFld1(String rsvFld1) {
-		this.rsvFld1 = rsvFld1 ;
-	}
+    public String getThdCusNme() {
+        return thdCusNme;
+    }
 
-	public String getRsvFld2() {
-		return rsvFld2= rsvFld2 == null ? " " : rsvFld2;
-	}
+    public void setThdCusNme(String thdCusNme) {
+        this.thdCusNme = thdCusNme;
+    }
 
-	public void setRsvFld2(String rsvFld2) {
-		this.rsvFld2 = rsvFld2 ;
-	}
+    public String getRsvFld1() {
+        return rsvFld1;
+    }
 
-	public String getRsvFld3() {
-		return rsvFld3= rsvFld3 == null ? " " : rsvFld3;
-	}
+    public void setRsvFld1(String rsvFld1) {
+        this.rsvFld1 = rsvFld1;
+    }
 
-	public void setRsvFld3(String rsvFld3) {
-		this.rsvFld3 = rsvFld3 ;
-	}
+    public String getRsvFld2() {
+        return rsvFld2;
+    }
 
-	public String getRsvFld4() {
-		return rsvFld4= rsvFld4 == null ? " " : rsvFld4;
-	}
+    public void setRsvFld2(String rsvFld2) {
+        this.rsvFld2 = rsvFld2;
+    }
 
-	public void setRsvFld4(String rsvFld4) {
-		this.rsvFld4 = rsvFld4 ;
-	}
+    public String getRsvFld3() {
+        return rsvFld3;
+    }
 
-	public String getRsvFld5() {
-		return rsvFld5;
-	}
+    public void setRsvFld3(String rsvFld3) {
+        this.rsvFld3 = rsvFld3;
+    }
 
-	public void setRsvFld5(String rsvFld5) {
-		this.rsvFld5 = rsvFld5;
-	}
+    public String getRsvFld4() {
+        return rsvFld4;
+    }
 
-	public String getRsvFld6() {
-		return rsvFld6;
-	}
+    public void setRsvFld4(String rsvFld4) {
+        this.rsvFld4 = rsvFld4;
+    }
 
-	public void setRsvFld6(String rsvFld6) {
-		this.rsvFld6 = rsvFld6;
-	}
+    public String getRsvFld5() {
+        return rsvFld5;
+    }
 
-	public String getRsvFld7() {
-		return rsvFld7;
-	}
+    public void setRsvFld5(String rsvFld5) {
+        this.rsvFld5 = rsvFld5;
+    }
 
-	public void setRsvFld7(String rsvFld7) {
-		this.rsvFld7 = rsvFld7;
-	}
+    public String getRsvFld6() {
+        return rsvFld6;
+    }
+
+    public void setRsvFld6(String rsvFld6) {
+        this.rsvFld6 = rsvFld6;
+    }
+
+    public String getRsvFld7() {
+        return rsvFld7;
+    }
+
+    public void setRsvFld7(String rsvFld7) {
+        this.rsvFld7 = rsvFld7;
+    }
 }
