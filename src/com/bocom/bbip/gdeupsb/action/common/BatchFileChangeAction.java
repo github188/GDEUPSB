c071e929c74e0796d0ab3aa1cd20fb77fbfe8e48
Li_Yawei <Li_Yawei@2ac714ae-9a16-11e4-ad83-2f06aba1930a>
2015-05-13 16:32:43 +0000

    git-svn-id: svn://182.53.32.18/bbip/project/GDEUPSB/TRUNK/GDEUPSB@50734 2ac714ae-9a16-11e4-ad83-2f06aba1930a

--- src/com/bocom/bbip/gdeupsb/action/common/BatchFileChangeAction.java
+++ src/com/bocom/bbip/gdeupsb/action/common/BatchFileChangeAction.java
@@ -26,18 +26,25 @@ public class BatchFileChangeAction extends BaseAction{
 		if(eupsBusTyp.equals("FSAG00")){
 			log.info("====================Start   BatchFileChangeAction  FSAG00");
 			bbipPublicService.synExecute("eups.fileBatchPayCreateDataProcess",context);
-		}else if(eupsBusTyp.equals("ZHAG00") || eupsBusTyp.equals("ZHAG01") || eupsBusTyp.equals("ZHAG02")){
-			log.info("====================Start   BatchFileChangeAction  "+eupsBusTyp);
+		}else if(eupsBusTyp.equals("ZHAG00")){
+			log.info("====================Start   BatchFileChangeAction  ZHAG00");
 			bbipPublicService.asynExecute("eups.fileBatchPayCreateDataProcess",context);
-			String comNo=context.getData("comNo").toString();
-			EupsActSysPara eupsActSysParas=new EupsActSysPara();
-			eupsActSysParas.setComNo(comNo);
-			String comNoAcps=eupsActSysParaRepository.find(eupsActSysParas).get(0).getSplNo();
-			String fileName="BATC"+comNoAcps+"0.txt";
-			context.setData(ParamKeys.FLE_NME, fileName);
+		}else if(eupsBusTyp.equals("ZHAG01")){
+			log.info("====================Start   BatchFileChangeAction  ZHAG01");
+			bbipPublicService.asynExecute("eups.fileBatchPayCreateDataProcess",context);
+		}else if(eupsBusTyp.equals("ZHAG02")){
+			log.info("====================Start   BatchFileChangeAction  ZHAG02");
+			bbipPublicService.asynExecute("eups.fileBatchPayCreateDataProcess",context);
 		}else{
 			throw new CoreException("不支持该业务类型的交易");
 		}
+		
+		String comNo=context.getData("comNo").toString();
+		EupsActSysPara eupsActSysParas=new EupsActSysPara();
+		eupsActSysParas.setComNo(comNo);
+		String comNoAcps=eupsActSysParaRepository.find(eupsActSysParas).get(0).getSplNo();
+		String fileName="BATC"+comNoAcps+"0.txt";
+		context.setData(ParamKeys.FLE_NME, fileName);
 		log.info("====================End   BatchFileChangeAction");
 	}
 }
