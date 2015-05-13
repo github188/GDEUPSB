583141f75fac27b62ee93427f74f9c5d4831ff83
Li_Yawei <Li_Yawei@2ac714ae-9a16-11e4-ad83-2f06aba1930a>
2015-05-13 11:55:00 +0000

    git-svn-id: svn://182.53.32.18/bbip/project/GDEUPSB/TRUNK/GDEUPSB@50629 2ac714ae-9a16-11e4-ad83-2f06aba1930a

--- src/com/bocom/bbip/gdeupsb/action/common/CommNotifyBatchStatusAction.java
+++ src/com/bocom/bbip/gdeupsb/action/common/CommNotifyBatchStatusAction.java
@@ -12,8 +12,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import com.bocom.bbip.comp.BBIPPublicService;
 import com.bocom.bbip.eups.action.BaseAction;
 import com.bocom.bbip.eups.common.ErrorCodes;
+import com.bocom.bbip.eups.entity.EupsBatchConsoleInfo;
+import com.bocom.bbip.eups.entity.EupsBatchInfoDetail;
 import com.bocom.bbip.eups.repository.EupsBatchConsoleInfoRepository;
+import com.bocom.bbip.eups.repository.EupsBatchInfoDetailRepository;
 import com.bocom.bbip.file.MftpTransfer;
+import com.bocom.bbip.gdeupsb.entity.GDEupsBatchConsoleInfo;
 import com.bocom.bbip.gdeupsb.repository.GDEupsBatchConsoleInfoRepository;
 import com.bocom.bbip.utils.DateUtils;
 import com.bocom.jump.bp.core.Context;
@@ -32,7 +36,32 @@ public class CommNotifyBatchStatusAction extends BaseAction{
 				
 				BBIPPublicService bbipPublicService=get(BBIPPublicService.class);
 				String path="/home/bbipadm/data/mftp/";
-				String fileNme=context.getData("batNo")+".result";
+				String batNo=context.getData("batNo").toString();
+				String fileNme=batNo+".result";
+				EupsBatchConsoleInfo eupsBatchConsoleInfo=eupsBatchConsoleInfoRepository.findOne(batNo);
+				if(eupsBatchConsoleInfo !=null){
+					String rsvFld2=eupsBatchConsoleInfo.getRsvFld1();
+					GDEupsBatchConsoleInfo gdEupsBatchConsoleInfo=gdEupsBatchConsoleInfoRepository.findOne(rsvFld2);
+					if(gdEupsBatchConsoleInfo.getEupsBusTyp().toString().trim().equals("FSAG00")){
+						String returnFileName=gdEupsBatchConsoleInfo.getComNo()+"_"+DateUtils.format(new Date(), DateUtils.STYLE_yyyyMMdd);
+						eupsBatchConsoleInfo.setPayCnt(null);
+						eupsBatchConsoleInfoRepository.update(eupsBatchConsoleInfo);
+						//同步调用
+						String mothed="eups.commNotifyBatchStatus";
+						bbipPublicService.synExecute(mothed, context);
+				        context.setData("ApFmt",  "48211");
+				        context.setData("batNo", null);
+				        context.setData("subDte",  null);
+				        context.setData("comNme", returnFileName );
+				        context.setData("batSts",  null);
+				        context.setData("totCnt",  null);
+				        context.setData("totAmt",  null);
+				        context.setData("sucTotCnt"  ,null);
+				        context.setData("sucTotAmt", null);
+				        log.info("==============End  CommNotifyBatchStatusAction");
+				        return;
+					}
+				}
 				try {			
 					bbipPublicService.getFileFromBBOS(new File(path,fileNme), fileNme, MftpTransfer.FTYPE_NORMAL);	
 				}catch (Exception e) {
@@ -59,12 +88,10 @@ public class CommNotifyBatchStatusAction extends BaseAction{
 					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
-				
-				
 				context.setData("comNo",  comNo);
 				//异步调用 反盘文件
 				String mothed="eups.commNotifyBatchStatus";
-				bbipPublicService.synExecute(mothed, context);
+				bbipPublicService.asynExecute(mothed, context);
 				String returnFileName=comNo+"_"+DateUtils.format(new Date(), DateUtils.STYLE_yyyyMMdd);
 				//返回文件名
 		        context.setData("ApFmt",  "48211");
