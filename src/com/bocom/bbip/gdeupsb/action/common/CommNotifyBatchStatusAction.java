b3d135e3d1abb13616c7b4a17156576b09a1a4c5
Li_Yawei <Li_Yawei@2ac714ae-9a16-11e4-ad83-2f06aba1930a>
2015-05-13 14:59:02 +0000

    git-svn-id: svn://182.53.32.18/bbip/project/GDEUPSB/TRUNK/GDEUPSB@50721 2ac714ae-9a16-11e4-ad83-2f06aba1930a

--- src/com/bocom/bbip/gdeupsb/action/common/CommNotifyBatchStatusAction.java
+++ src/com/bocom/bbip/gdeupsb/action/common/CommNotifyBatchStatusAction.java
@@ -1,20 +1,13 @@
 package com.bocom.bbip.gdeupsb.action.common;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
 import java.util.Date;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.bocom.bbip.comp.BBIPPublicService;
 import com.bocom.bbip.eups.action.BaseAction;
-import com.bocom.bbip.eups.common.ErrorCodes;
 import com.bocom.bbip.eups.entity.EupsBatchConsoleInfo;
 import com.bocom.bbip.eups.repository.EupsBatchConsoleInfoRepository;
-import com.bocom.bbip.file.MftpTransfer;
 import com.bocom.bbip.gdeupsb.entity.GDEupsBatchConsoleInfo;
 import com.bocom.bbip.gdeupsb.repository.GDEupsBatchConsoleInfoRepository;
 import com.bocom.bbip.utils.DateUtils;
@@ -27,81 +20,38 @@ public class CommNotifyBatchStatusAction extends BaseAction{
 		EupsBatchConsoleInfoRepository eupsBatchConsoleInfoRepository;
 		@Autowired
 		GDEupsBatchConsoleInfoRepository gdEupsBatchConsoleInfoRepository;
+		@Autowired
+		BBIPPublicService bbipPublicService;
 		@Override
 		public void execute(Context context) throws CoreException,
 				CoreRuntimeException {
 				log.info("==============Start  CommNotifyBatchStatusAction");
-				
-				BBIPPublicService bbipPublicService=get(BBIPPublicService.class);
-				String path="/home/bbipadm/data/mftp/";
-				String batNo=context.getData("batNo").toString();
-				String fileNme=batNo+".result";
+				String batNo=context.getData("batNo").toString().trim();
+				//获取总行批次信息
 				EupsBatchConsoleInfo eupsBatchConsoleInfo=eupsBatchConsoleInfoRepository.findOne(batNo);
-				if(eupsBatchConsoleInfo !=null){
-					String rsvFld2=eupsBatchConsoleInfo.getRsvFld1();
-					GDEupsBatchConsoleInfo gdEupsBatchConsoleInfo=gdEupsBatchConsoleInfoRepository.findOne(rsvFld2);
-					if(gdEupsBatchConsoleInfo.getEupsBusTyp().toString().trim().equals("FSAG00")){
-						String returnFileName=gdEupsBatchConsoleInfo.getComNo()+"_"+DateUtils.format(new Date(), DateUtils.STYLE_yyyyMMdd);
-						eupsBatchConsoleInfo.setPayCnt(null);
-						eupsBatchConsoleInfoRepository.update(eupsBatchConsoleInfo);
-						log.info("==============update  eupsBatchConsoleInfo's  payCnt   success");
-						//同步调用
-						String mothed="eups.commNotifyBatchStatus";
-						bbipPublicService.synExecute(mothed, context);
-				        context.setData("ApFmt",  "48211");
-				        context.setData("batNo", null);
-				        context.setData("subDte",  null);
-				        context.setData("comNme", returnFileName );
-				        context.setData("batSts",  null);
-				        context.setData("totCnt",  null);
-				        context.setData("totAmt",  null);
-				        context.setData("sucTotCnt"  ,null);
-				        context.setData("sucTotAmt", null);
-				        log.info("==============End  CommNotifyBatchStatusAction");
-				        return;
-					}
-				}
-				try {			
-					bbipPublicService.getFileFromBBOS(new File(path,fileNme), fileNme, MftpTransfer.FTYPE_NORMAL);	
-				}catch (Exception e) {
-					throw new CoreException(ErrorCodes.EUPS_MFTP_FILEDOWN_FAIL);
-				}
-				get(BBIPPublicService.class).asynExecute("gdeupsb.batchFileZHAG", context);
-				//读取第一行 获取单位编号
-				String comNo="";
-				try {
-					FileReader fileReader = new FileReader(path+"//"+fileNme);
-					BufferedReader bufferedReader=new BufferedReader(fileReader);
-					String firstLine=null;
-					int i=0;
-					while((firstLine=bufferedReader.readLine())!=null && i<1){
-						//TODO 确认 | 可行
-						comNo=firstLine.split("|")[0];
-					}
-					bufferedReader.close();
-					fileReader.close();
-				} catch (FileNotFoundException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-				context.setData("comNo",  comNo);
+				String batNos=eupsBatchConsoleInfo.getRsvFld1();
+				eupsBatchConsoleInfo.setPayCnt(null);
+				//更改总行控制 使其可以手动调用反盘文件
+				eupsBatchConsoleInfoRepository.update(eupsBatchConsoleInfo);
+				log.info("==============update  eupsBatchConsoleInfo  set  payCnt = null ; batNo = "+batNo);
+				log.info("============================"+eupsBatchConsoleInfo);
 				//异步调用 反盘文件
 				String mothed="eups.commNotifyBatchStatus";
 				bbipPublicService.asynExecute(mothed, context);
-				String returnFileName=comNo+"_"+DateUtils.format(new Date(), DateUtils.STYLE_yyyyMMdd);
 				//返回文件名
+				GDEupsBatchConsoleInfo gdEupsBatchConsoleInfo=gdEupsBatchConsoleInfoRepository.findOne(batNos);
+				String fileName=gdEupsBatchConsoleInfo.getComNo()+"_"+DateUtils.format(new Date(), DateUtils.STYLE_yyyyMMdd);
+				//返回字段配置
 		        context.setData("ApFmt",  "48211");
-		        context.setData("batNo", null);
-		        context.setData("subDte",  null);
-		        context.setData("comNme", returnFileName );
-		        context.setData("batSts",  null);
-		        context.setData("totCnt",  null);
-		        context.setData("totAmt",  null);
-		        context.setData("sucTotCnt"  ,null);
-		        context.setData("sucTotAmt", null);
+		        context.setData("batNo",  gdEupsBatchConsoleInfo.getBatNo());
+		        context.setData("comNo",  gdEupsBatchConsoleInfo.getComNo());
+		        context.setData("subDte",  gdEupsBatchConsoleInfo.getSubDte());
+		        context.setData("comNme", fileName );
+		        context.setData("batSts",  gdEupsBatchConsoleInfo.getBatSts());
+		        context.setData("totCnt",  gdEupsBatchConsoleInfo.getTotCnt());
+		        context.setData("totAmt",  gdEupsBatchConsoleInfo.getTotAmt());
+		        context.setData("sucTotCnt"  ,gdEupsBatchConsoleInfo.getSucTotCnt());
+		        context.setData("sucTotAmt", gdEupsBatchConsoleInfo.getSucTotAmt());
 		        log.info("==============End  CommNotifyBatchStatusAction");
 		}
 }
