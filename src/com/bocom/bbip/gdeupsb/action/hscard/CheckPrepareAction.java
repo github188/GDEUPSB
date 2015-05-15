9cc9884ae316ca75d49f5255ccd80fb3e3b8a83c
Li_Guilin <Li_Guilin@2ac714ae-9a16-11e4-ad83-2f06aba1930a>
2015-05-15 07:02:56 +0000

    git-svn-id: svn://182.53.32.18/bbip/project/GDEUPSB/TRUNK/GDEUPSB@51078 2ac714ae-9a16-11e4-ad83-2f06aba1930a

--- src/com/bocom/bbip/gdeupsb/action/hscard/CheckPrepareAction.java
+++ src/com/bocom/bbip/gdeupsb/action/hscard/CheckPrepareAction.java
@@ -2,7 +2,9 @@ package com.bocom.bbip.gdeupsb.action.hscard;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 
+import com.bocom.bbip.comp.BBIPPublicService;
 import com.bocom.bbip.eups.action.BaseAction;
 import com.bocom.bbip.eups.common.ParamKeys;
 import com.bocom.jump.bp.core.Context;
@@ -13,9 +15,17 @@ public class CheckPrepareAction extends BaseAction{
 
 	private static final Log log = LogFactory.getLog(CheckPrepareAction.class);
 	
+	@Autowired
+	BBIPPublicService bbipPublicService;
+	
 	public void execute(Context ctx) throws CoreRuntimeException,CoreException{
 		log.info("CheckPrepareAction start.......");
 		ctx.setData(ParamKeys.EUPS_BUSS_TYPE, "HSSC00");
 		ctx.setData("ftpNo", "hsscCheckFile");
+		ctx.setData(ParamKeys.BK, bbipPublicService.getParam("GDEUPSB", "THD_BR")); // 分行号
+		ctx.setData(ParamKeys.BR, bbipPublicService.getParam("GDEUPSB", "HSBR")); // 机构号
+		String teller = bbipPublicService.getETeller(ctx.getData(ParamKeys.BK).toString());
+		
+		ctx.setData(ParamKeys.TELLER, teller);
 	}
 }
