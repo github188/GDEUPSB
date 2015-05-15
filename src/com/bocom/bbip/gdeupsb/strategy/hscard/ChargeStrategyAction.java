ea3384dcbbeb3d8deb789be86292e26965ab3440
Li_Guilin <Li_Guilin@2ac714ae-9a16-11e4-ad83-2f06aba1930a>
2015-05-15 07:10:37 +0000

    git-svn-id: svn://182.53.32.18/bbip/project/GDEUPSB/TRUNK/GDEUPSB@51083 2ac714ae-9a16-11e4-ad83-2f06aba1930a

--- src/com/bocom/bbip/gdeupsb/strategy/hscard/ChargeStrategyAction.java
+++ src/com/bocom/bbip/gdeupsb/strategy/hscard/ChargeStrategyAction.java
@@ -18,60 +18,67 @@ import com.bocom.bbip.eups.entity.EupsTransJournal;
 import com.bocom.bbip.eups.repository.EupsTransJournalRepository;
 import com.bocom.bbip.gdeupsb.common.GDParamKeys;
 import com.bocom.bbip.utils.DateUtils;
+import com.bocom.bbip.utils.StringUtils;
 import com.bocom.jump.bp.core.Context;
 import com.bocom.jump.bp.core.CoreException;
 import com.bocom.jump.bp.core.CoreRuntimeException;
 import com.bocom.jump.bp.core.Executable;
 
-public class ChargeStrategyAction implements Executable{
-	
-	private final static Log logger=LogFactory.getLog(ChargeStrategyAction.class);
-	
+public class ChargeStrategyAction implements Executable {
+
+	private final static Log logger = LogFactory
+			.getLog(ChargeStrategyAction.class);
+
 	@Autowired
 	BBIPPublicService bbipPublicService;
-	
-	
+
 	@Autowired
 	EupsTransJournalRepository eupsTransJournalRepository;
-	
-	
-	public void execute(Context context) throws CoreException,CoreRuntimeException{
-		logger.info("charge strategy start......");
-		
 
-		
-		Date tTxnDte=DateUtils.parse(context.getData("tTxnDte").toString(), DateUtils.STYLE_yyyyMMdd);//第三方交易日期
-		
+	public void execute(Context context) throws CoreException,
+			CoreRuntimeException {
+		logger.info("charge strategy start......");
+
+		Date tTxnDte = DateUtils.parse(context.getData("tTxnDte").toString(),
+				DateUtils.STYLE_yyyyMMdd);// 第三方交易日期
+
 		context.setData(ParamKeys.THD_TXN_DATE, tTxnDte);
-		context.setData(ParamKeys.BK, bbipPublicService.getParam("GDEUPSB", "THD_BR")); // 分行号
-		context.setData(ParamKeys.BR, bbipPublicService.getParam("GDEUPSB", "HSBR")); // 机构号
-		String teller = bbipPublicService.getETeller(context.getData(ParamKeys.BK).toString());
-		
+
+		context.setData(ParamKeys.BK,
+				bbipPublicService.getParam("GDEUPSB", "THD_BR")); // 分行号
+		context.setData(ParamKeys.BR,
+				bbipPublicService.getParam("GDEUPSB", "HSBR")); // 机构号
+		String teller = bbipPublicService.getETeller(context.getData(
+				ParamKeys.BK).toString());
 		context.setData(ParamKeys.TELLER, teller);
-		context.setData(ParamKeys.BUS_TYP, Constants.BUS_TYP_2); //待缴
-//		检查报文是否重复
+
+		logger.info("get eTeller after:" + teller);
+		context.setData(ParamKeys.BUS_TYP, Constants.BUS_TYP_2); // 待缴
+		// 检查报文是否重复
 		EupsTransJournal eups = new EupsTransJournal();
-		eups.setBakFld1((String)context.getData(ParamKeys.BAK_FLD1));//备用字段1，学校代号。
-		eups.setThdTxnDte(tTxnDte); //第三方交易日期
-		eups.setThdSqn((String)context.getData(ParamKeys.THD_SEQUENCE));//此处的第三方流水号即校方流水号，请求方流水号也赋值为校方流水号。
+		eups.setBakFld1((String) context.getData(ParamKeys.BAK_FLD1));// 备用字段1，学校代号。
+		eups.setThdTxnDte(tTxnDte); // 第三方交易日期
+		eups.setThdSqn((String) context.getData(ParamKeys.THD_SEQUENCE));// 此处的第三方流水号即校方流水号，请求方流水号也赋值为校方流水号。
 		List<EupsTransJournal> journal = eupsTransJournalRepository.find(eups);
-		if(!CollectionUtils.isEmpty(journal)){
-			
+		if (!CollectionUtils.isEmpty(journal)) {
+
 			context.setData(ParamKeys.RSP_CDE, ErrorCodes.EUPS_SQN_IS_EXIST);
 			context.setData("responseMessage", "交易流水已存在");
 			throw new CoreException(ErrorCodes.EUPS_SQN_IS_EXIST);
 		}
-		 //金额控制，不能超过400.但标准版交易流水表中有交易金额和请求交易金额两个字段，应该用交易金额。
-			double i=Double.parseDouble(context.getData(ParamKeys.TXN_AMT).toString());
-			double d=i/100;
-			DecimalFormat df=new DecimalFormat("#.00");
-			BigDecimal txnAmt=new BigDecimal(df.format(d));
-			context.setData(ParamKeys.TXN_AMT,txnAmt );
-	      if((txnAmt.compareTo(new BigDecimal(400)))>0){
-	  
-	    	  context.setData(ParamKeys.RSP_CDE, ErrorCodes.EUPS_CHECK_TXN_AMT_FAIL);
-	    	  context.setData(ParamKeys.RSP_MSG, "金额超限");
-	    	  throw new CoreException(ErrorCodes.EUPS_LIM_AMT_FULL);
-	      }
+		// 金额控制，不能超过400.但标准版交易流水表中有交易金额和请求交易金额两个字段，应该用交易金额。
+		double i = Double.parseDouble(context.getData(ParamKeys.TXN_AMT)
+				.toString());
+		double d = i / 100;
+		DecimalFormat df = new DecimalFormat("#.00");
+		BigDecimal txnAmt = new BigDecimal(df.format(d));
+		context.setData(ParamKeys.TXN_AMT, txnAmt);
+		if ((txnAmt.compareTo(new BigDecimal(400))) > 0) {
+
+			context.setData(ParamKeys.RSP_CDE,
+					ErrorCodes.EUPS_CHECK_TXN_AMT_FAIL);
+			context.setData(ParamKeys.RSP_MSG, "金额超限");
+			throw new CoreException(ErrorCodes.EUPS_LIM_AMT_FULL);
+		}
 	}
 }
