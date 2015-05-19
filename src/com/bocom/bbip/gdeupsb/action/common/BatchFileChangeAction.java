d4478459f12fb33c582b82cfe4d364897b70ee59
Li_Yawei <Li_Yawei@2ac714ae-9a16-11e4-ad83-2f06aba1930a>
2015-05-19 13:59:51 +0000

    git-svn-id: svn://182.53.32.18/bbip/project/GDEUPSB/TRUNK/GDEUPSB@51925 2ac714ae-9a16-11e4-ad83-2f06aba1930a

--- src/com/bocom/bbip/gdeupsb/action/common/BatchFileChangeAction.java
+++ src/com/bocom/bbip/gdeupsb/action/common/BatchFileChangeAction.java
@@ -1,5 +1,8 @@
 package com.bocom.bbip.gdeupsb.action.common;
 
+import java.util.List;
+
+import org.python.parser.ast.listcompType;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.bocom.bbip.comp.BBIPPublicService;
@@ -8,6 +11,9 @@ import com.bocom.bbip.eups.action.BaseAction;
 import com.bocom.bbip.eups.common.ParamKeys;
 import com.bocom.bbip.eups.entity.EupsActSysPara;
 import com.bocom.bbip.eups.repository.EupsActSysParaRepository;
+import com.bocom.bbip.gdeupsb.entity.GDEupsBatchConsoleInfo;
+import com.bocom.bbip.gdeupsb.repository.GDEupsBatchConsoleInfoRepository;
+import com.bocom.bbip.utils.CollectionUtils;
 import com.bocom.jump.bp.core.Context;
 import com.bocom.jump.bp.core.CoreException;
 import com.bocom.jump.bp.core.CoreRuntimeException;
@@ -27,6 +33,17 @@ public class BatchFileChangeAction extends BaseAction{
 		if(eupsBusTyp.equals("FSAG00")){
 			log.info("====================Start   BatchFileChangeAction  FSAG00");
 			bbipPublicService.synExecute("eups.fileBatchPayCreateDataProcess",context);
+			GDEupsBatchConsoleInfo gdEupsBatchConsoleInfo=new GDEupsBatchConsoleInfo();
+			gdEupsBatchConsoleInfo.setFleNme((String)context.getData("fleNme"));
+			gdEupsBatchConsoleInfo.setEupsBusTyp("FSAG00");
+			List<GDEupsBatchConsoleInfo> list=get(GDEupsBatchConsoleInfoRepository.class).find(gdEupsBatchConsoleInfo);
+			if(CollectionUtils.isEmpty(list)){
+					throw new CoreException("批次信息错误");
+			}
+			GDEupsBatchConsoleInfo gdEupsBatchConsoleInfos=list.get(0);
+			context.setData("totCnt",gdEupsBatchConsoleInfos.getTotCnt());
+			context.setData("totAmt",gdEupsBatchConsoleInfos.getTotAmt());
+			context.setData("rsvFld7",gdEupsBatchConsoleInfos.getRsvFld7());
 		}else if(eupsBusTyp.equals("ZHAG00")){
 			log.info("====================Start   BatchFileChangeAction  ZHAG00");
 			bbipPublicService.asynExecute("eups.fileBatchPayCreateDataProcess",context);
