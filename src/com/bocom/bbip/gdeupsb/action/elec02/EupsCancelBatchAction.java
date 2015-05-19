e08611ac11c59bb450234a9596fc53f258e01165
Qian_Chen <Qian_Chen@2ac714ae-9a16-11e4-ad83-2f06aba1930a>
2015-05-19 10:40:46 +0000

    git-svn-id: svn://182.53.32.18/bbip/project/GDEUPSB/TRUNK/GDEUPSB@51856 2ac714ae-9a16-11e4-ad83-2f06aba1930a

--- src/com/bocom/bbip/gdeupsb/action/elec02/EupsCancelBatchAction.java
+++ src/com/bocom/bbip/gdeupsb/action/elec02/EupsCancelBatchAction.java
@@ -1,5 +1,8 @@
 package com.bocom.bbip.gdeupsb.action.elec02;
 
+import java.util.List;
+import java.util.Map;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
@@ -11,6 +14,8 @@ import com.bocom.bbip.gdeupsb.common.GDConstants;
 import com.bocom.bbip.gdeupsb.entity.GDEupsBatchConsoleInfo;
 import com.bocom.bbip.gdeupsb.repository.GDEupsBatchConsoleInfoRepository;
 import com.bocom.bbip.utils.Assert;
+import com.bocom.bbip.utils.CollectionUtils;
+import com.bocom.bbip.utils.StringUtils;
 import com.bocom.jump.bp.core.Context;
 import com.bocom.jump.bp.core.CoreException;
 
@@ -22,13 +27,27 @@ public class EupsCancelBatchAction extends BaseAction {
         final String fileName=context.getData("FilNm");
         GDEupsBatchConsoleInfo info = new GDEupsBatchConsoleInfo();
 		info.setFleNme(fileName);
-		GDEupsBatchConsoleInfo ret = get(GDEupsBatchConsoleInfoRepository.class).findConsoleInfo(info);
-		if(null==ret){
+//		GDEupsBatchConsoleInfo ret = get(GDEupsBatchConsoleInfoRepository.class).findConsoleInfo(info);
+//		if(null==ret){
+//			context.setData("QSFlg", "4");
+//			logger.info("批次控制信息不存在");
+//			throw new CoreException(ErrorCodes.EUPS_BAT_CTL_INFO_NOT_EXIST);
+//		}
+		List<Map<String, Object>> ret =get(GDEupsBatchConsoleInfoRepository.class).findConsoleInfo(info);
+		if(CollectionUtils.isEmpty(ret)){
 			context.setData("QSFlg", "4");
 			logger.info("批次控制信息不存在");
 			throw new CoreException(ErrorCodes.EUPS_BAT_CTL_INFO_NOT_EXIST);
 		}
-		context.setData(ParamKeys.BAT_NO, ret.getBatNo());
+		else{
+			Map retMap=ret.get(0);
+			String batNo=(String)retMap.get("BAT_NO");
+			if(StringUtils.isEmpty(batNo)){
+				 batNo=(String)retMap.get("BATNO");
+			}
+			context.setData(ParamKeys.BAT_NO, batNo);
+		}
+		
         /**批次撤销公共process*/
        	((BBIPPublicServiceImpl)get(GDConstants.BBIP_PUBLIC_SERVICE)).
        	synExecute(GDConstants.BATCH_CANCEL_PROCESSID, context);
