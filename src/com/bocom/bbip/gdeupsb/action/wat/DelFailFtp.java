0644ac91413113d91617754126e6704777e842bf
Li_Guilin <Li_Guilin@2ac714ae-9a16-11e4-ad83-2f06aba1930a>
2015-05-25 02:46:51 +0000

    git-svn-id: svn://182.53.32.18/bbip/project/GDEUPSB/TRUNK/GDEUPSB@52555 2ac714ae-9a16-11e4-ad83-2f06aba1930a

--- src/com/bocom/bbip/gdeupsb/action/wat/DelFailFtp.java
+++ src/com/bocom/bbip/gdeupsb/action/wat/DelFailFtp.java
@@ -1,6 +1,5 @@
 package com.bocom.bbip.gdeupsb.action.wat;
 
-import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
@@ -13,18 +12,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import com.bocom.bbip.comp.BBIPPublicService;
 import com.bocom.bbip.eups.action.BaseAction;
+import com.bocom.bbip.eups.action.common.OperateFTPAction;
 import com.bocom.bbip.eups.action.common.OperateFileAction;
 import com.bocom.bbip.eups.common.ParamKeys;
-import com.bocom.bbip.eups.entity.EupsBatchConsoleInfo;
-import com.bocom.bbip.eups.entity.EupsBatchInfoDetail;
 import com.bocom.bbip.eups.entity.EupsThdFtpConfig;
 import com.bocom.bbip.eups.repository.EupsThdFtpConfigRepository;
-import com.bocom.bbip.gdeupsb.action.common.OperateFTPActionExt;
 import com.bocom.bbip.gdeupsb.entity.GdEupsWatAgtInf;
 import com.bocom.bbip.gdeupsb.repository.GdEupsWatAgtInfRepository;
-import com.bocom.bbip.utils.BeanUtils;
 import com.bocom.bbip.utils.DateUtils;
-import com.bocom.bbip.utils.StringUtils;
 import com.bocom.jump.bp.core.Context;
 import com.bocom.jump.bp.core.CoreException;
 import com.bocom.jump.bp.core.CoreRuntimeException;
@@ -43,47 +38,95 @@ public class DelFailFtp extends BaseAction{
 	@Autowired
 	OperateFileAction operateFile;
 	
+	@Autowired
+	OperateFTPAction operateFTPAction;
+	
 	private final static Log log = LogFactory.getLog(DelFailFtp.class);
 	public void execute(Context ctx) throws CoreRuntimeException,CoreException{
 		log.info("DelFailFtp start......");
-		Map<String, Object> resultMap = new HashMap<String, Object>();
-		String sqn = service.getBBIPSequence();
-		String date = DateUtils.format(new Date(), DateUtils.STYLE_yyyyMMdd);
+		String clrDat = ctx.getData("clrDat");
+		
+		if(clrDat==null|"".equals(clrDat)){
+			Map<String, Object> resultMap = new HashMap<String, Object>();
+			String sqn = service.getBBIPSequence();
+			String date = DateUtils.format(new Date(), DateUtils.STYLE_yyyyMMdd);
+			
+			
+			
+			//文件名
+			String fileName=date;
+			//文件内容 
+//			GDEupsEleTmp gdEupsEleTmps =new GDEupsEleTmp();
+//			gdEupsEleTmps.setRsvFld5(batNo);
+//			List<GDEupsEleTmp> detailList=gdEupsEleTmpRepository.find(gdEupsEleTmps);
+//			resultMap.put(ParamKeys.EUPS_FILE_DETAIL, detailList);
+//			GdEupsWatAgtInf gdEupsWatAgtInf = new GdEupsWatAgtInf();
+//			gdEupsWatAgtInf.setAgtSts("F");
+			Map<String, Object> map = new HashMap<String, Object>();
+			map.put("agtSts", "F");
+			map.put("agrExpDte", new Date());
+			map.put("pedAgrSts", "1");
+			List<GdEupsWatAgtInf> detailList = gdEupsWatAgtInfRepository.findDelFail(map);
+			
+			
+			
+			List<Object> list1 = new ArrayList<Object>();
+			
+			resultMap.put(ParamKeys.EUPS_FILE_DETAIL, detailList);
+			
+			EupsThdFtpConfig eupsThdFtpConfig =eupsThdFtpConfigRepository.findOne("watDelAgtFail");
+			
+			// 生成文件
+			operateFile.createCheckFile(eupsThdFtpConfig, "watAgtDelFail", fileName, resultMap);
+			eupsThdFtpConfig.setLocFleNme(fileName);
+			eupsThdFtpConfig.setRmtFleNme(fileName);
+			log.info("start to put......"+eupsThdFtpConfig);
+			
+			
+			
+			operateFTPAction.putCheckFile(eupsThdFtpConfig);
+		}else{
+			Map<String, Object> resultMap = new HashMap<String, Object>();
+			String sqn = service.getBBIPSequence();
+			String date = clrDat;
+			Date selDat = DateUtils.parse(date);
+			
+			
+			//文件名
+			String fileName=date;
+			//文件内容 
+//			GDEupsEleTmp gdEupsEleTmps =new GDEupsEleTmp();
+//			gdEupsEleTmps.setRsvFld5(batNo);
+//			List<GDEupsEleTmp> detailList=gdEupsEleTmpRepository.find(gdEupsEleTmps);
+//			resultMap.put(ParamKeys.EUPS_FILE_DETAIL, detailList);
+//			GdEupsWatAgtInf gdEupsWatAgtInf = new GdEupsWatAgtInf();
+//			gdEupsWatAgtInf.setAgtSts("F");
+			Map<String, Object> map = new HashMap<String, Object>();
+			map.put("agtSts", "F");
+			map.put("agrExpDte", selDat);
+			map.put("pedAgrSts", "1");
+			List<GdEupsWatAgtInf> detailList = gdEupsWatAgtInfRepository.findDelFail(map);
+			
+			
+			
+			List<Object> list1 = new ArrayList<Object>();
+			
+			resultMap.put(ParamKeys.EUPS_FILE_DETAIL, detailList);
+			
+			EupsThdFtpConfig eupsThdFtpConfig =eupsThdFtpConfigRepository.findOne("watDelAgtFail");
+			
+			// 生成文件
+			operateFile.createCheckFile(eupsThdFtpConfig, "watAgtDelFail", fileName, resultMap);
+			eupsThdFtpConfig.setLocFleNme(fileName);
+			eupsThdFtpConfig.setRmtFleNme(fileName);
+			log.info("start to put......"+eupsThdFtpConfig);
+			
+			
+			
+			operateFTPAction.putCheckFile(eupsThdFtpConfig);
+		}
 		
 		
-		
-		//文件名
-		String fileName=date;
-		//文件内容 
-//		GDEupsEleTmp gdEupsEleTmps =new GDEupsEleTmp();
-//		gdEupsEleTmps.setRsvFld5(batNo);
-//		List<GDEupsEleTmp> detailList=gdEupsEleTmpRepository.find(gdEupsEleTmps);
-//		resultMap.put(ParamKeys.EUPS_FILE_DETAIL, detailList);
-//		GdEupsWatAgtInf gdEupsWatAgtInf = new GdEupsWatAgtInf();
-//		gdEupsWatAgtInf.setAgtSts("F");
-		Map<String, Object> map = new HashMap<String, Object>();
-		map.put("agtSts", "F");
-		map.put("agrExpDte", new Date());
-		map.put("pedAgrSts", "1");
-		List<GdEupsWatAgtInf> detailList = gdEupsWatAgtInfRepository.findDelFail(map);
-		
-		
-		
-		List<Object> list1 = new ArrayList<Object>();
-		
-		resultMap.put(ParamKeys.EUPS_FILE_DETAIL, detailList);
-		
-		EupsThdFtpConfig eupsThdFtpConfig =eupsThdFtpConfigRepository.findOne("watDelAgtFail");
-		
-		// 生成文件
-		operateFile.createCheckFile(eupsThdFtpConfig, "watAgtDelFail", fileName, resultMap);
-		eupsThdFtpConfig.setLocFleNme(fileName);
-		eupsThdFtpConfig.setRmtFleNme(fileName);
-		log.info("start to put......");
-		
-		//TODO:路径配的是绝对路径，ftp方式为被动
-		OperateFTPActionExt operateFTP = new OperateFTPActionExt();
-		operateFTP.putCheckFile(eupsThdFtpConfig);
 		
 	}
 
