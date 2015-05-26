09d5d462270e8a23c25a49117f468f972e6d70d8
Wang_Mingqin <Wang_Mingqin@2ac714ae-9a16-11e4-ad83-2f06aba1930a>
2015-05-26 09:36:50 +0000

    git-svn-id: svn://182.53.32.18/bbip/project/GDEUPSB/TRUNK/GDEUPSB@52704 2ac714ae-9a16-11e4-ad83-2f06aba1930a

---
+++ src/com/bocom/bbip/gdeupsb/action/gzag/batch/ChangeFileFmtAction.java
@@ -0,0 +1,19 @@
+package com.bocom.bbip.gdeupsb.action.gzag.batch;
+
+import com.bocom.bbip.eups.action.BaseAction;
+import com.bocom.jump.bp.core.Context;
+import com.bocom.jump.bp.core.CoreException;
+import com.bocom.jump.bp.core.CoreRuntimeException;
+/**
+ * 广州文本格式转换
+ * @author WMQ
+ *
+ */
+public class ChangeFileFmtAction extends BaseAction {
+	@Override
+	public void execute(Context context) throws CoreException,
+			CoreRuntimeException {
+		
+		
+	}
+}
