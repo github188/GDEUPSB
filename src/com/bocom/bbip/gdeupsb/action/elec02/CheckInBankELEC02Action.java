0daa6dd8f0d66b0c4028a518dc52b2c88684dfce
Qian_Chen <Qian_Chen@2ac714ae-9a16-11e4-ad83-2f06aba1930a>
2015-06-01 08:04:21 +0000

    git-svn-id: svn://182.53.32.18/bbip/project/GDEUPSB/TRUNK/GDEUPSB@53057 2ac714ae-9a16-11e4-ad83-2f06aba1930a

--- src/com/bocom/bbip/gdeupsb/action/elec02/CheckInBankELEC02Action.java
+++ src/com/bocom/bbip/gdeupsb/action/elec02/CheckInBankELEC02Action.java
@@ -1,4 +1,3 @@
-
 package com.bocom.bbip.gdeupsb.action.elec02;
 
 import java.math.BigDecimal;
@@ -32,31 +31,32 @@ import com.bocom.jump.bp.core.CoreException;
 
 /**
  * @author wuyh
- *
+ * 
  */
 public class CheckInBankELEC02Action extends BaseAction {
 	private static final Log logger = LogFactory.getLog(CheckInBankELEC02Action.class);
 
 	public void process(Context context) throws CoreException {
-            logger.info("对账开始");
-            List<Map<String,String>> ret=getData( context);
-            /**得到总金额*/
-            BigDecimal decimal= getTotAmt(context);
-            /**生成发送对账文件到第三方*/
-            sendCheckFile( context, decimal, ret);
-            /**通知第三方*/
-            context.setData("AppTradeCode", "50");
-            context.setData("WorkDate", DateUtils.format(new Date(), "yyyyMMdd"));
-            context.setData("SendTime", DateUtils.format(new Date(),DateUtils.STYLE_yyyyMMddHHmmss));
-            context.setData("chn", "00");
-            Map<String,Object>map= get(CallThdService.class).callTHD(context);
+		logger.info("对账开始");
+		List<Map<String, String>> ret = getData(context);
+		/** 得到总金额 */
+		BigDecimal decimal = getTotAmt(context);
+		/** 生成发送对账文件到第三方 */
+		sendCheckFile(context, decimal, ret);
+		/** 通知第三方 */
+		context.setData("AppTradeCode", "50");
+		context.setData("WorkDate", DateUtils.format(new Date(), "yyyyMMdd"));
+		context.setData("SendTime", DateUtils.format(new Date(), DateUtils.STYLE_yyyyMMddHHmmss));
+		context.setData("chn", "00");
+		Map<String, Object> map = get(CallThdService.class).callTHD(context);
 	}
-	private void sendCheckFile(Context context,BigDecimal decimal,
-	    List<Map<String,String>> ret)throws CoreException {
+
+	private void sendCheckFile(Context context, BigDecimal decimal,
+			List<Map<String, String>> ret) throws CoreException {
 		String fileDate = DateUtils.format(new Date(), "yyyyMMdd");
-		Map<String,String>header=new HashMap<String, String>();
-		Map<String,Object>retMap=new HashMap<String, Object>();
-		EupsThdFtpConfig config=get(EupsThdFtpConfigRepository.class).findOne((String)context.getData(ParamKeys.FTP_ID));
+		Map<String, String> header = new HashMap<String, String>();
+		Map<String, Object> retMap = new HashMap<String, Object>();
+		EupsThdFtpConfig config = get(EupsThdFtpConfigRepository.class).findOne((String) context.getData(ParamKeys.FTP_ID));
 		Assert.isFalse(null == config, ErrorCodes.EUPS_THD_FTP_CONFIG_NOTEXIST);
 		header.put("totAmt", String.valueOf(decimal));
 		header.put("totNum", String.valueOf(ret.size()));
@@ -66,11 +66,12 @@ public class CheckInBankELEC02Action extends BaseAction {
 		retMap.put("detail", ret);
 		String FileName = "301190003dz" + fileDate + ".txt"; // 301190003dz20080101.txt
 		context.setData("FileName", FileName);
-		((OperateFileAction)get("opeFile")).
-		createCheckFile(config, "ELEC02Check", FileName, retMap);
-		((OperateFTPAction)get("opeFTP")).putCheckFile(config);
+		((OperateFileAction) get("opeFile")).
+				createCheckFile(config, "ELEC02Check", FileName, retMap);
+		((OperateFTPAction) get("opeFTP")).putCheckFile(config);
 	}
-	private List<Map<String,String>>getData(Context context)throws CoreException {
+
+	private List<Map<String, String>> getData(Context context) throws CoreException {
 		final String comNo = (String) context.getData(ParamKeys.COMPANY_NO);
 		final String txndte = (String) context.getData("WDO");
 		List<String> condList = new ArrayList<String>();
@@ -88,16 +89,16 @@ public class CheckInBankELEC02Action extends BaseAction {
 		subMap.put("conditions", condList);
 
 		dynSqlMap.put("subMap", subMap);
-        List<Object> list = get(EupsTransJournalRepository.class).getCheckDynamicSql(dynSqlMap);
+		List<Object> list = get(EupsTransJournalRepository.class).getCheckDynamicSql(dynSqlMap);
 		Assert.isNotEmpty(list, ErrorCodes.EUPS_QUERY_NO_DATA);
 		List<Map<String, String>> jnlList = new ArrayList<Map<String, String>>();
 		for (Object obj : list) {
 			EupsTransJournal map = (EupsTransJournal) obj;
-			Date date=map.getTxnDte();
-			String txnDte=DateUtils.format(date, "yyyyMMdd");
-			Map mm=BeanUtils.toFlatMap(map);
+			Date date = map.getTxnDte();
+			String txnDte = DateUtils.format(date, "yyyyMMdd");
+			Map mm = BeanUtils.toFlatMap(map);
 			/***/
-			List<Map> tempmap = JsonUtils.objectFromJson( map.getRsvFld3(),List.class);
+			List<Map> tempmap = JsonUtils.objectFromJson(map.getRsvFld3(), List.class);
 			for (Map tmp : tempmap) {
 				mm.putAll(tmp);
 				mm.put("txnDte", txndte);
@@ -105,30 +106,31 @@ public class CheckInBankELEC02Action extends BaseAction {
 			}
 
 		}
-		
-		
+
 		return jnlList;
 	}
 
 	private BigDecimal getTotAmt(Context context) throws CoreException {
 		final String comNo = (String) context.getData(ParamKeys.COMPANY_NO);
 		final String txndte = (String) context.getData("WDO");
-		EupsTransJournal temp=new EupsTransJournal();
+		EupsTransJournal temp = new EupsTransJournal();
 		temp.setTxnSts("S");
 		temp.setTxnDte(DateUtils.parse(txndte));
 		temp.setComNo(comNo);
 		temp.setSvrNme("eups.payFeeOnline");
-		/**单笔缴费*/
+		/** 单笔缴费 */
 		List<EupsTransJournal> list = get(EupsTransJournalRepository.class).findBySvrNme(temp);
 		temp.setSvrNme("eups.payUnilateralToBank");
-		/**单笔扣费*/
+		/** 单笔扣费 */
 		List<EupsTransJournal> lst = get(EupsTransJournalRepository.class).findBySvrNme(temp);
-		
-		BigDecimal decinal=list.get(0).getTotalAmt();
-		BigDecimal decinl=lst.get(0).getTotalAmt();
-		
-		//不能使用double!..............
-		BigDecimal ret=new BigDecimal(decinal.doubleValue()+decinl.doubleValue());
+
+		BigDecimal decinal = list.get(0).getTotalAmt();
+		BigDecimal decinl = lst.get(0).getTotalAmt();
+
+		// BigDecimal ret=new
+		// BigDecimal(decinal.doubleValue()+decinl.doubleValue());
+		BigDecimal ret = decinal.add(decinl); // modify by qc 0601
+
 		return ret;
 	}
 }
