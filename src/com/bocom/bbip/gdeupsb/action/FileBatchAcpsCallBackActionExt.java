c1f155bdf9309d9f208654ff5ec367fa3e6cf93d
Qian_Chen <Qian_Chen@2ac714ae-9a16-11e4-ad83-2f06aba1930a>
2015-06-01 02:00:43 +0000

    git-svn-id: svn://182.53.32.18/bbip/project/GDEUPSB/TRUNK/GDEUPSB@53017 2ac714ae-9a16-11e4-ad83-2f06aba1930a

--- src/com/bocom/bbip/gdeupsb/action/FileBatchAcpsCallBackActionExt.java
+++ src/com/bocom/bbip/gdeupsb/action/FileBatchAcpsCallBackActionExt.java
@@ -70,6 +70,8 @@ public class FileBatchAcpsCallBackActionExt extends BaseAction
     Integer sucTotCnt = (Integer)context.getData("sucTotCnt");
     BigDecimal sucTotAmt = (BigDecimal)context.getData("sucTotAmt");
 
+    
+    //TODO:不能使用double!...............
     Integer falTotCnt = Integer.valueOf(totCnt.intValue() - sucTotCnt.intValue());
     double falTotAmtdouble = totAmt.doubleValue() - sucTotAmt.doubleValue();
     BigDecimal falTotAmt = new BigDecimal(falTotAmtdouble);
